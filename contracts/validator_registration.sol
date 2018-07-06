ca5cdaad18ca19289eb2b9d259a0535cb344c18e
Mikerah <mikerahqc@protonmail.com>
2018-07-06 18:04:46 -0400

    Added skeleton code to the validator registration smart contract. Also added more base files for the general directory structure

---
+++ contracts/validator_registration.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.4.23;
+
+contract ValidatorRegistration {
+
+}
--- truffle.js
+++ truffle.js
@@ -13,6 +13,24 @@
  */
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  development: {
+            //host: "localhost",
+            port: 8545,
+            host: "127.0.0.1",
+            // port: 7545,
+            network_id: "0",
+            gas: 4600000,
+            gasLimit: 10000000
+  },
+  geth_testnet: {
+            host: "127.0.0.1",
+            port: 8545,
+            // host: "127.0.0.1",
+            // port: 7545,
+            network_id: "*",
+            //from: "0x43EC6d0942f7fAeF069F7F63D0384a27f529B062",
+            gas: 580000,
+            gasLimit: 10000000
+   },
+
 };
