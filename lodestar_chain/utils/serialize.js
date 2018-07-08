a51686a80db1b802a451481a38f3d0f7a08b011b
Mikerah <mikerahqc@protonmail.com>
2018-07-07 22:27:52 -0400

    Implemented necessary functions for serializing objects in serialize.js

---
+++ lodestar_chain/utils/serialize.js
@@ -0,0 +1,21 @@
+var exports = module.exports = {};
+
+const function serialize(obj) {
+    return JSON.stringify(obj);
+}
+
+const function deserialize(jsonObj) {
+
+}
+
+const function eq(obj1, obj2) {
+
+}
+
+const function deepcopy(obj) {
+    return JSON.parse(JSON.stringify(obj));
+}
+
+const toObj(toObjectify) {
+
+}
