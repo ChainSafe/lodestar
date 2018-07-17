f69f163d31f59f230533bdf79d48ae3c0e70f3de
greg <gregorymarkou@gmail.com>
2018-07-17 10:46:40 -0400

    refactor exports in bitfield

--- lodestar_chain/utils/bitfield.js
+++ lodestar_chain/utils/bitfield.js
@@ -1,36 +1,27 @@
-var exports = module.exports = {};
-
 // Takes a bitfield(str) and index(int) and returns a bool
-const hasVoted(bitfield, index) => {
+const hasVoted = (bitfield, index) => {
     return bitfield[Math.floor(index / 8)] & (128 >> (index % 8));
 }
 
-exports.hasVoted = hasVoted;
-
-const setVoted(bitfield, index) => {
-    var byteIndex = Math.floor(index / 8);
-    var bitIndex = index % 8;
-    var newByteValue = bitfield[byteIndex] | (128 >> bitIndex);
+const setVoted = (bitfield, index) => {
+    const byteIndex = Math.floor(index / 8);
+    const bitIndex = index % 8;
+    const newByteValue = bitfield[byteIndex] | (128 >> bitIndex);
     return bitfield[:byteIndex] + newByteValue + bitfield[byteIndex + 1:];
 }
 
-exports.setVoted = setVoted;
-
-const getBitfieldLength(bitCount) => {
+const getBitfieldLength = (bitCount) => {
     return Math.floor((bitCount + 7) / 8);
 }
 
-exports.getBitfieldLength = getBitfieldLength;
 
-const getEmptyBitfield(bitCount) => {
+const getEmptyBitfield = (bitCount) => {
     return 'x00'.repeat(getBitfieldLength(bitCount));
 }
 
-exports.getEmptyBitfield = getEmptyBitfield;
-
-const getVoteCount(bitfield) => {
-    var votes = 0;
-    for(var i=0; i < bitfield.length * 8; i++) {
+const getVoteCount = (bitfield) => {
+    let votes = 0;
+    for(const i=0; i < bitfield.length * 8; i++) {
       if(hasVoted(bitfield, i)) {
         votes += 1;
       }
@@ -38,4 +29,10 @@ const getVoteCount(bitfield) => {
     return votes
 }
 
-exports.getVoteCount = getVoteCount;
+module.exports = {
+  hasVoted,
+  setVoted,
+  getBitfieldLength,
+  getEmptyBitfield,
+  getVoteCount
+};
