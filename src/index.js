3020bc37346f1e7381de0ce95596816be7055382
Mikerah <mikerahqc@protonmail.com>
2018-09-16 23:26:11 -0400

    Started implementation of serialize fn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 var exports = module.exports = {};
 
-function serialize(val, type) {
+const assert = require('assert');
 
+function serialize(value, type) {
+    if (type === null && val.hasOwnProperty('fields')) {
+        type = typeof(value);
+    }
+
+    if (['hash32', 'address'].includes(type)) {
+        if(type === 'address') {
+            assert(value.length === 20);
+        } else {
+            assert(value.length === 32);
+        }
+        return value;
+    }
+    else if (type instanceof String && type[:3] === 'int') {
+        length = Number(type[:3]);
+        assert(length % 8 === 0);
+        return value
+    }
 }
 
 function _deserialize(data, start, type) {
@@ -21,5 +39,11 @@ function deepcopy(x) {
 }
 
 function toObj(x) {
-    
+
 }
+
+exports.serialize = serialize;
+exports.deserialize = deserialize;
+exports.eq = eq;
+exports.deepcopy = deepcopy;
+exports.toObj = toObj;
