d3e42a36a73ffc50a8eb97490d68b8e6fb849908
Mikerah <mikerahqc@protonmail.com>
2018-09-16 21:52:42 -0400

    Added skeleton functions to index.js

---
+++ src/index.js
@@ -0,0 +1,25 @@
+var exports = module.exports = {};
+
+function serialize(val, type) {
+
+}
+
+function _deserialize(data, start, type) {
+
+}
+
+function deserialize(data, type) {
+    return _deserialize(data, 0, type)[0]
+}
+
+function eq(x,y) {
+
+}
+
+function deepcopy(x) {
+
+}
+
+function toObj(x) {
+    
+}
