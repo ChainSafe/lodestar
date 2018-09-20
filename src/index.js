7140a3be3faf61e1efb1dfbf15d576ff559e07b8
Mikerah <mikerahqc@protonmail.com>
2018-09-20 09:22:49 -0400

    Mostly completed serialize function in ssz

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ var exports = module.exports = {};
 
 const assert = require('assert');
 
+function _toBytes(n, length, endianess) {
+    var buffer = new ArrayBuffer(length);
+    var dataView = new DataView(buffer);
+    if(endianess === 'big') {
+        dataView.setInt32(0, n, true);
+    } else {
+        dataView.setInt32(0, n, false);
+    }
+
+    var byteArray = Array.from(buffer);
+    return byteArray;
+
+}
+
 function serialize(value, type) {
     if (type === null && val.hasOwnProperty('fields')) {
         type = typeof(value);
@@ -18,8 +32,20 @@ function serialize(value, type) {
     else if (type instanceof String && type[:3] === 'int') {
         length = Number(type[:3]);
         assert(length % 8 === 0);
-        return value
+        return _toBytes(value, Math.floor(length / 8), 'big');
     }
+    else if (type === 'bytes') {
+        return _toBytes(value.length, 4, 'big') + type;
+    }
+    else if (type instanceof Array) {
+        assert(type.length === 1);
+        for(x in value) {
+            sub = [serialize(x, type[0])].join('');
+            return _toBytes(sub.length, 4, 'big') + sub
+        }
+    }
+
+    throw new Error('Cannot serialize: value -> ' + value + ' type -> ' + type);
 }
 
 function _deserialize(data, start, type) {
