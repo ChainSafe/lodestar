d9e1a9cd38bb979d7d35d9d85d520e9b97d03aa7
Mikerah <mikerahqc@protonmail.com>
2018-10-25 12:10:14 -0400

    Updated most of the helper functions.

--- lodestar_chain/state/helpers.js
+++ lodestar_chain/state/helpers.js
@@ -9,14 +9,12 @@ const Block = require('./block.js');
 const ValidatorRecord = require('./validatorRecord.js');
 const fs = require('fs');
 const constants = JSON.parse(fs.readFileSync('constants.json', 'utf8'));
+const ValidatorStatusCodes = JSON.parse(fs.readFileSync('validatorStatusCodes.json', 'utf8'));
 
-function getActiveValidatorIndices(validators, dynasty) {
-    var indexSet = [];
-    for(var i=0; i < validators.length; i++) {
-        if((validators[i].start_dynasty <= dynasty) && (dynasty < validators[i].end_dynasty)) {
-            indexSet.push(i);
-        }
-    }
+function getActiveValidatorIndices(validators) {
+    var indexSet = validators.filter(
+        validator => validator.status === ValidatorStatusCodes['ACTIVE']
+    )
     return indexSet;
 }
 
@@ -32,149 +30,149 @@ function byteArrayToInt(array) {
     return val;
 }
 
-function shuffle(list, seed) {
-    assert(list.length <= 16777216);
+function shuffle(values, seed) {
 
-    var tmp = list.slice();
+    var valuesCount = values.length;
+
+    // Entropy is consumed from the seed in 3-byte (24-bit) chunks.
+    var randBytes = 3;
+    // The highest possible result of the RNG
+    var randMax = 2 **(randBytes * 8) - 1;
+
+    // The range of the RNG places an upper-bound on the size of the list that
+    // may be shuffled. It is a logic error to supply an oversized list.
+    assert(valuesCount < randMax, "The number of values is more than the highest possible result of the RNG");
+
+    var output = values.slice();
     var source = seed;
     var i = 0;
 
-    while(i < list.length) {
+    while(i < valuesCount - 1) {
+        // Re-hash the 'source' to obtain a new pattern of bytes
         source = blake(source);
-        for(var pos=0; i < 30; i += 3) {
-            var m = byteArrayToInt(source[pos: pos+3], "big");
-            var remaining = list.length - i;
-            if(remaining === 0) {
+        // Iterate through the 'source' bytes in 3-byte chunks
+        for(var pos=0; i < (32 - (32 % randBytes)); i += randBytes) {
+            // Determine the number of indices remaining in 'values' and exit
+            // once the last index is reached.
+            var remaining = valuesCount - i;
+            if(remaining === 1) {
                 break;
             }
-            var rand_max = 16777216 - (16777216 % remaining);
-            if(m < rand_max) {
-                var replacement_pos = (m % remaining) + i;
-                tmp[i] = tmp[replacement_pos];
-                tmp[replacement_pos] = tmp[i];
+
+            // Read 3-bytes of 'source' as a 32-bit signed int.
+            // This is temporary until we can figure out how to read a 24-bit signed int.
+            var sampleFromSource = Buffer.from(source[pos: pos+3]).readInt32BE(0);
+
+            // Sample values greater than or equal to 'sampleMax' will cause
+            // modulo bias when mapped into the 'remaining' range
+            var sampleMax = randMax - (randMax % remaining);
+
+            // Perform a swap if the consumed entropy will not cause modulo bias
+            if(sampleFromSource < sampleMax) {
+                // Select a replacement index for the current index
+                var replacementPosition = (sampleFromSource % remaining) + i;
+                // Swap the current index with the replacement index
+                output[i] = output[replacementPosition];
+                output[replacementPosition] = output[i];
                 i += 1;
+            } else {
+                // The sample cause modulo bia. A new sample should be read
+
             }
         }
+
     }
 
-    return tmp;
+    return output;
 }
 
-function split(list, N) {
-    if(N < 2){
-        return [list];
-    }
-
-    var len = list.length;
-    var out = [];
-    var i = 0;
-    var size;
-
-    if(len % n == 0) {
-        size = MAth.floor(len / n);
-        while(i < len) {
-            out.push(list.slice(i, i += size));
-        }
-    } else {
-        while(i < len) {
-            size = Math.ceil((len -i) / n--);
-            out.push(a.slice(i, i += size));
-        }
-    }
-
-    return out;
-}
-
-function getNewShuffling(seed, validators, dynasty, crosslinkStartShard) {
-    var activeValidatorSet = getActiveValidatorIndices(validators, dynasty);
-    if(activeValidatorSet.length >= constants['CYCLE_LENGTH'] * constants['MIN_COMMITTEE_SIZE']) {
-        var committeePerSlot = Math.floor(activeValidatorSet.length / constants['CYCLE_LENGTH'] / ((constants['MIN_COMMITTEE_SIZE'] * 2) + 1));
-        var slotsPerCommittee = 1;
-    } else {
-        var committeesPerSlot = 1;
-        var slotsPerCommittee = 1;
-        while(activeValidatorSet.length * slotsPerCommittee < constants['CYCLE_LENGTH'] * constants['MIN_COMMITTEE_SIZE'] && slotsPerCommittee < constants['CYCLE_LENGTH']) {
-            slotsPerCommittee *= 2;
-        }
-    }
-
+function split(list, splitCount) {
+    var listLength = list.length;
     var tmp = [];
-
-    var shuffledActiveValidatorSet = shuffle(activeValidatorSet, seed);
-    var splitActiveValidatorSet = split(shuffledActiveValidatorSet, constants['CYCLE_LENGTH']);
-
-    var iterator = splitActiveValidatorSet.entries();
-    for(let item in iterator){
-        var i = item[0];
-        var slotIndices = item[1];
-
-        var shardIndices = split(slotIndices, committeesPerSlot);
-        var shardIdStart = crosslinkingStartShard + i * Math.floor(committeesPerSlot / slotsPerCommittee);
-
-        var iterator1 = shardIndices.entries();
-        for(let item1 in iterator1) {
-            var j = item1[0];
-            var indices = item1[1];
-
-            var obj = {
-                "shard_id" : (shardIdStart + j) % constants["SHARD_COUNT"],
-                "committee": indices
-            };
-
-            tmp.push(new ShardAndCommittee(obj));
-
-        }
+    for(var i = 0; i < splitCount; i++) {
+        tmp.push(list.slice(
+                Math.floor((listLength * i) / splitCount),
+                Math.floor((listLength * (i+1)) / splitCount)
+            )
+            );
     }
 
     return tmp;
 
 }
 
-function getIndicesForSlot(crystallizedState, slot) {
-    var ifhStart = crystallizedState.last_state_recalc - constants["CYCLE_LENGTH"];
-    assert(ifhStart <= slot && slot < ifhStart + constants["CYCLE_LENGTH"] * 2);
-    return crystallizedState.indices_for_slots[slot - ifh_start];
-}
-
-function getBlockHash(activeState, curblock, slot) {
-    var sback = curblock.slot_number - constants["CYCLE_LENGTH"] * 2;
-    assert(sback <= slot && slot < sback + constants["CYCLE_LENGTH"] * 2);
-    return activeState.recent_block_hashes[slot - sback];
-}
-
-function getNewRecentBlockHashes(oldBlockHashes, parentSlot, currentSlot, parentHash) {
-    var d = currentSlot - parentSlot;
-    return oldBlockHashes.slice(d).concat([parentHash].fill(Math.min(d, oldBlockHashes.length)));
-}
-
-function getSignedParentHashes(activeState, block, attestation) {
-    var cycleLength = constants["CYCLE_LENGTH"];
-
-    var parentHashes = [];
-
-    for(var i=0; i < (cycleLength - attestation.oblique_parent_hashes.length); i++) {
-        parentHashes.push(getBlockHash(activeState, block, attestation.slot - cycleLength + i));
+function clamp(minval, maxval, x) {
+    if (x <= minval) {
+        return minval;
+    } else if (x >= maxval) {
+        return maxval;
+    } else {
+        return x;
     }
 
-    parentHashes = parentHashes.concat(attestation.oblique_parent_hashes);
-    return parentHashes;
 }
 
-function getAttestationIndices(crystallizedState, attestation) {
-    var shardId = attestation.shard_id;
+function getNewShuffling(seed, validators, crosslinkingStartShard) {
+    var activeValidators = getActiveValidators(validators);
+    var activeValidatorsSize = len(activeValidators);
 
-    var filteredIndicesForSlot = getIndicesForSlot(
-        crystallizedState, attestation.slot
-    ).filter(function(x) {
-        return x.shard_id === shardId
-    });
+    var committeesPerSlot = clamp(
+        1,
+        Math.floor(constants["SHARD_COUNT"] / constants["CYCLE_LENGTH"]),
+        Math.floor(activeValidatorsSize / constants["CYCLE_LENGTH"] / (constants["MIN_COMMITTEE_SIZE"] * 2)) + 1
+    );
 
-    var attestationIndices = [];
-    if(filteredIndicesForSlot.length > 0) {
-        attestationIndices = filteredIndicesForSlot[0].committee;
+    var output = [];
+
+    // Shuffle with seed
+    var shuffledActiveValidatorIndices = shuffle(activeValidators, seed);
+
+    // Split the shuffled list into cycle_length pieces
+    var validatorsPerSlot = split(shuffledActiveValidatorIndices, constants["CYCLE_LENGTH"]);
+
+    // i[0] = slot
+    // i[1] = slot_indices
+    for(let i of validatorsPerSlot.entries()) {
+        // Split the shuffled list into committees_per_slot pieces
+        var shardIndices = split(i[1], committeesPerSlot);
+
+        var shardIdStart = crosslinkingStartShard + i[0] * committeesPerSlot;
+
+        var shardsAndCommitteesForSlot = [];
+
+        // j[0] = shard_position
+        // j[1] = indices
+        for(let j of shardIndices.entries()) {
+            var shardAndCommitteeObj = new ShardAndCommittee(
+                (shardIdStart + shardPosition) % constants["SHARD_COUNT"],
+                j[1]
+            );
+            shardsAndCommitteesForSlot.push(shardAndCommitteeObj);
+        }
+
+        output.push(shardsAndCommitteesForSlot);
     }
+    return output;
+}
 
-    return attestationIndices;
+function getShardsAndCommitteesForSlot(crystallizedState, slot) {
+    var earliestSlotInArray = crystallizedState.last_start_recalculation - constants["CYCLE_LENGTH"];
+    if(earliestSlotInArray <= slot && slot < earliestSlotInArray + constants["CYCLE_LENGTH"] * 2) {
+        throw ;
+    }
+    return crystallizedState.shard_and_committee_for_slots[slot - earliestSlotInArray];
+}
+
+function getBlockHash(activeState, currentBlock, slot) {
+    var earliestSlotInArray = currentBlock.slot - activeState.recent_block_hashes.length;
+    if(earliestSlotInArray <= slot && slot < currentBlock.slot) {
+        throw ;
+    }
+    return activeState.recent_block_hashes[slot - earliestSlotInArray];
+}
+
+function addValidatorSetChangeRecord(crystallizedState, index, pubkey, flag) {
+
 }
 
 module.exports = {
@@ -182,9 +180,7 @@ module.exports = {
     shuffle,
     split,
     getNewShuffling,
-    getIndicesForSlot,
+    getShardAndCommitteesForSlot,
     getBlockHash,
-    getNewRecentBlockHashes,
-    getSignedParentHashes,
-    getAttestationIndices
+    addValidatorSetChangeRecord
 }
