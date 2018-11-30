0411fb935b4a3cea36eb13177f635e2c0b273f4a
Gregory Markou <gregorymarkou@gmail.com>
2018-11-30 16:12:11 -0500

    Closes #34, Closes #30, Closes #29, Closes #24 - Add alias for unsupported math types

--- beaconChain/typescript/helpers/stateTransition.ts
+++ beaconChain/typescript/helpers/stateTransition.ts
@@ -1,65 +1,72 @@
-// // Helper functions related to state transition functions
-// // TODO convert Number (in return value for getActiveValidatorIndeces) to and number
-//
-// import {BeaconState, ShardAndCommittee, ValidatorRecord} from "../numbererfaces/state";
-// import { ValidatorStatusCodes } from "../constants/enums";
-// import constants from "../constants/constants"
-// import {AttestationSignedData, BeaconBlock} from "../numbererfaces/blocks";
-//
-// /**
-//  * The following is a function that gets active validator indices from the validator list.
-//  * @param {ValidatorRecord[]} validators
-//  * @returns {Number[]}
-//  */
-// // TODO Need to setup the enums to perform a proper comparison
-// function getActiveValidatorIndices(validators: ValidatorRecord[]): Number[] {
-//   return validators.filter((validator: ValidatorRecord, index: number) => {
-//     // Check if validator status is set ACTIVE
-//     if (validator.status === ValidatorStatusCodes.ACTIVE) return index;
-//   })
-// }
-//
-// /**
-//  * The following is a function that shuffles any list; we primarily use it for the validator list.
-//  * @param {any[]} values
-//  * @param {Hash32} seed
-//  * @returns Returns the shuffled values with seed as entropy.
-//  */
-// // TODO finish this
-// function shuffle(values: any[], seed: Hash32): any[] {
-//   const valuesCount: number = values.length;
-//   // Entropy is consumed from the seed in 3-byte (24 bit) chunks.
-//   const randBytes = 3;
-//   // Highest possible result of the RNG
-//   const randMax = 2 ** (randBytes * 8) - 1;
-//
-//   // The range of the RNG places an upper-bound on the size of the list that may be shuffled.
-//   // It is a logic error to supply an oversized list.
-//   if (valuesCount < randMax) throw new Error("Oversized list supplied to shuffle()!");
-//
-//   // Make a copy of the values
-//   let output = values.slice();
-//   const source = seed; // REALLY??
-//   let index = 0; // REALLY??
-//   while (index < valuesCount -1) {
-//     // Re-hash the `source` to obtain a new pattern of bytes.
-//     // TODO figure out what this hash function is in python -> JS
-//     let source = hash(source)
-//
-//     // Iterate through the `source` bytes in 3-byte chunks.
-//
-//   }
-//
-//   return []
-// }
-//
-// /**
-//  * Splits a list numbero split_count pieces.
-//  * @param {any[]} values
-//  * @param {Number} splitCount
-//  * @returns {any[]}
-//  */
-export function split(values: any[], splitCount: number): any[] {
+// Helper functions related to state transition functions
+// TODO convert int (in return value for getActiveValidatorIndeces) to and int
+
+import {BeaconState, ShardAndCommittee, ValidatorRecord} from "../interfaces/state";
+import { ValidatorStatusCodes } from "../constants/enums";
+import constants from "../constants/constants"
+import {AttestationSignedData, BeaconBlock} from "../interfaces/blocks";
+
+type int = number;
+type bytes = number;
+type uint24 = number;
+type hash32 = number;
+
+/**
+ * The following is a function that gets active validator indices from the validator list.
+ * @param {ValidatorRecord[]} validators
+ * @returns {int[]}
+ */
+// TODO .map may cause mutation issues, probably want to look into fixing this later on...
+function getActiveValidatorIndices(validators: ValidatorRecord[]): int[] {
+  const result = validators.map((validator: ValidatorRecord, index: int) => {
+    if (validator.status === ValidatorStatusCodes.ACTIVE) {
+      return index;
+    }
+  });
+  return result;
+}
+
+/**
+ * The following is a function that shuffles any list; we primarily use it for the validator list.
+ * @param {any[]} values
+ * @param {hash32} seed
+ * @returns Returns the shuffled values with seed as entropy.
+ */
+// TODO finish this
+function shuffle(values: any[], seed: hash32): any[] {
+  const valuesCount: int = values.length;
+  // Entropy is consumed from the seed in 3-byte (24 bit) chunks.
+  const randBytes = 3;
+  // Highest possible result of the RNG
+  const randMax = 2 ** (randBytes * 8) - 1;
+
+  // The range of the RNG places an upper-bound on the size of the list that may be shuffled.
+  // It is a logic error to supply an oversized list.
+  if (valuesCount < randMax) throw new Error("Oversized list supplied to shuffle()!");
+
+  // Make a copy of the values
+  let output = values.slice();
+  const source = seed; // REALLY??
+  let index = 0; // REALLY??
+  while (index < valuesCount -1) {
+    // Re-hash the `source` to obtain a new pattern of bytes.
+    // TODO figure out what this hash function is in python -> JS
+    // let source = hash(source)
+
+    // Iterate through the `source` bytes in 3-byte chunks.
+
+  }
+
+  return []
+}
+
+/**
+ * Splits a list into split_count pieces.
+ * @param {any[]} values
+ * @param {int} splitCount
+ * @returns {any[]}
+ */
+export function split(values: any[], splitCount: int): any[] {
   // Returns the split ``seq`` in ``split_count`` pieces in protocol.
   const listLength = values.length;
   let array: any[] = [];
@@ -73,12 +80,12 @@ export function split(values: any[], splitCount: number): any[] {
 
 /**
  * Helper function for readability.
- * @param {number} minval
- * @param {number} maxval
- * @param {number} x
- * @returns {Number}
+ * @param {int} minval
+ * @param {int} maxval
+ * @param {int} x
+ * @returns {int}
  */
-export function clamp(minval: number, maxval: number, x: number): number {
+export function clamp(minval: int, maxval: int, x: int): int {
   if (x <= minval) {
     return minval;
   }
@@ -87,100 +94,102 @@ export function clamp(minval: number, maxval: number, x: number): number {
   }
   return x;
 }
-//
-// /**
-//  * Determines the shards and committee for a given beacon block.
-//  * Should not change unless the validator set changes.
-//  * @param {BeaconState} state
-//  * @param {number} slot
-//  * @returns {ShardAndCommittee[] | Error}
-//  */
-// function getShardsAndCommitteesForSlot(state: BeaconState, slot: number): ShardAndCommittee[] {
-//   const earliestSlotInArray: number = state.lastStateRecalculationSlot - constants.CYCLE_LENGTH;
-//   // TODO Figure out why this is an error
-//   if (earliestSlotInArray <= slot < earliestSlotInArray + constants.CYCLE_LENGTH * 2) throw new Error();
-//   return state.shardAndCommitteeForSlots[slot - earliestSlotInArray];
-// }
-//
-// /**
-//  * Retrieves hash for a given beacon block.
-//  * It should always return the block hash in the beacon chain slot for `slot`.
-//  * @param {BeaconState} state
-//  * @param {BeaconBlock} currentBlock
-//  * @param {number} slot
-//  * @returns {Hash32}
-//  */
-// function getBlockHash(state: BeaconState, currentBlock: BeaconBlock, slot: number): Hash32 {
-//   const earliestSlotInArray = currentBlock.slot - state.recentBlockHashes.length;
-//   // TODO Figure out why this is an error
-//   if (earliestSlotInArray <= slot < currentBlock.slot) throw new Error();
-//   return state.recentBlockHashes[slot - earliestSlotInArray];
-// }
-//
-// /**
-//  * Determines the proposer of a beacon block.
-//  * @param {BeaconState} state
-//  * @param {number} slot
-//  * @returns {Number}
-//  */
-// function getBeaconProposerIndex(state: BeaconState, slot: number): number {
-//   const firstCommittee = getShardsAndCommitteesForSlot(state, slot)[0].committee;
-//   return firstCommittee[slot % firstCommittee.length];
-// }
-//
-// // TODO finish
-// function getAttestationParticipants(state: BeaconState, attestationData: AttestationSignedData, participationBitfield: bytes): number[] {
-//   const sncsForSlot: ShardAndCommittee[] = getShardsAndCommitteesForSlot(state, attestationData.slot);
-//   const snc: ShardAndCommittee = sncsForSlot.filter((x: ShardAndCommittee) => {
-//     if (x.shard === attestationData.shard) return x;
-//   })[0];
-//
-//   // TODO Figure out why this is an error
-//   // TODO implement error based on python pseudo code
-//   // TODO what is ceil_div8()
-//   // assert len(participation_bitfield) == ceil_div8(len(snc.committee))
-//
-//   const participants: number[] = snc.committee.filter((validator: 'unumber24', index: number) => {
-//     const bit = (participationBitfield[Math.floor(index / 8)] >> (7 - (index % 8))) % 2;
-//     if (bit === 1) {
-//       return index;
-//     }
-//   });
-//   return participants;
-// }
-//
-// /**
-//  * Determine the balance of a validator.
-//  * Used for determining punishments and calculating stake.
-//  * @param {ValidatorRecord} validator
-//  * @returns {Number}
-//  */
-// // TODO Math.min requires number, validator.record is a unumber64
-// function balanceAtStake(validator: ValidatorRecord): number {
-//   return Math.min(validator.balance, constants.DEPOSIT_SIZE);
-// }
-//
-// // TODO figure out what bytes1() does in python
-// function getNewValidatorSetDeltaHashChain(currentValidator: hash32, index: number, pubkey: number, flag: number): hash32 {
+
+/**
+ * Determines the shards and committee for a given beacon block.
+ * Should not change unless the validator set changes.
+ * @param {BeaconState} state
+ * @param {int} slot
+ * @returns {ShardAndCommittee[] | Error}
+ */
+function getShardsAndCommitteesForSlot(state: BeaconState, slot: int): ShardAndCommittee[] {
+  const earliestSlotInArray: int = state.lastStateRecalculationSlot - constants.CYCLE_LENGTH;
+  // TODO Figure out why this is an error
+  // TODO fix error with `<`
+  // if (earliestSlotInArray <= slot < earliestSlotInArray + constants.CYCLE_LENGTH * 2) throw new Error();
+  return state.shardAndCommitteeForSlots[slot - earliestSlotInArray];
+}
+
+/**
+ * Retrieves hash for a given beacon block.
+ * It should always return the block hash in the beacon chain slot for `slot`.
+ * @param {BeaconState} state
+ * @param {BeaconBlock} currentBlock
+ * @param {int} slot
+ * @returns {hash32}
+ */
+function getBlockHash(state: BeaconState, currentBlock: BeaconBlock, slot: int): hash32 {
+  const earliestSlotInArray = currentBlock.slot - state.recentBlockHashes.length;
+  // TODO Figure out why this is an error
+  // TODO fix error with `<`
+// if (earliestSlotInArray <= slot < currentBlock.slot) throw new Error();
+  return state.recentBlockHashes[slot - earliestSlotInArray];
+}
+
+/**
+ * Determines the proposer of a beacon block.
+ * @param {BeaconState} state
+ * @param {int} slot
+ * @returns {int}
+ */
+function getBeaconProposerIndex(state: BeaconState, slot: int): int {
+  const firstCommittee = getShardsAndCommitteesForSlot(state, slot)[0].committee;
+  return firstCommittee[slot % firstCommittee.length];
+}
+
+// TODO finish
+function getAttestationParticipants(state: BeaconState, attestationData: AttestationSignedData, participationBitfield: bytes): int[] {
+  const sncsForSlot: ShardAndCommittee[] = getShardsAndCommitteesForSlot(state, attestationData.slot);
+  const snc: ShardAndCommittee = sncsForSlot.filter((x: ShardAndCommittee) => {
+    if (x.shard === attestationData.shard) return x;
+  })[0];
+
+  // TODO Figure out why this is an error
+  // TODO implement error based on python pseudo code
+  // TODO what is ceil_div8()
+  // assert len(participation_bitfield) == ceil_div8(len(snc.committee))
+
+  const participants: int[] = snc.committee.filter((validator: uint24, index: int) => {
+    const bit = (participationBitfield[Math.floor(index / 8)] >> (7 - (index % 8))) % 2;
+    if (bit === 1) {
+      return index;
+    }
+  });
+  return participants;
+}
+
+/**
+ * Determine the balance of a validator.
+ * Used for determining punishments and calculating stake.
+ * @param {ValidatorRecord} validator
+ * @returns {int}
+ */
+// TODO Math.min requires int, validator.record is a uint64
+function balanceAtStake(validator: ValidatorRecord): int {
+  return Math.min(validator.balance, constants.DEPOSIT_SIZE);
+}
+
+// TODO figure out what bytes1() does in python
+// function getNewValidatorSetDeltaHashChain(currentValidator: hash32, index: int, pubkey: int, flag: int): hash32 {
 //   return newValidatorSetDeltaHashChain = hash(
 //     currentValidator + bytes1(flag) + bytes3(index) + bytes32(pubkey)
 //   )
 // }
-//
-// /**
-//  * Calculate the largest numbereger k such that k**2 <= n.
-//  * Used in reward/penalty calculations
-//  * @param {number} n
-//  * @returns {Number}
-//  */
-// // TODO Can use built in JS function if available
-// function numberSqrt(n: number): number {
-//   let x = n;
-//   let y = Math.floor((x + 1) / 2);
-//   while (y < x) {
-//     x = y;
-//     y = Math.floor(x + Math.floor(n / x) / 2)
-//   }
-//   return x;
-// }
-//
+
+/**
+ * Calculate the largest integer k such that k**2 <= n.
+ * Used in reward/penalty calculations
+ * @param {int} n
+ * @returns {int}
+ */
+// TODO Can use built in JS function if available
+function intSqrt(n: int): int {
+  let x = n;
+  let y = Math.floor((x + 1) / 2);
+  while (y < x) {
+    x = y;
+    y = Math.floor(x + Math.floor(n / x) / 2)
+  }
+  return x;
+}
+
--- beaconChain/typescript/interfaces/state.ts
+++ beaconChain/typescript/interfaces/state.ts
@@ -4,97 +4,102 @@
 
 import { AttestationRecord } from "./blocks";
 
+type uint24 = number;
+type uint64 = number;
+type uint384 = number;
+type hash32 = number;
+
 export interface BeaconState {
   // Slot of last validator set change
-  validatorSetChangeSlot: 'uint64',
+  validatorSetChangeSlot: uint64,
   // List of validators
   validators: ValidatorRecord[],
   // Most recent crosslink for each shard
   crosslinks: CrosslinkRecord[],
   // Last cycle-boundary state recalculation
-  lastStateRecalculationSlot: 'uint64',
+  lastStateRecalculationSlot: uint64,
   // Last finalized slot
-  lastFinalizedSlot: 'uint64',
+  lastFinalizedSlot: uint64,
   // Last justified slot
-  lastJustifiedSlot: 'uint64',
+  lastJustifiedSlot: uint64,
   // Number of consecutive justified slots
-  justifiedStreak: 'uint64',
+  justifiedStreak: uint64,
   // Committee members and their assigned shard, per slot
   shardAndCommitteeForSlots: [[ShardAndCommittee]],
   // Persistent shard committees
-  persistentCommittees: [['uint24']],
+  persistentCommittees: [[uint24]],
   persistentCommitteeReassignments: ShardReassignmentRecord[],
   // Randao seed used for next shuffling
-  nextShufflingSeed: 'hash32',
+  nextShufflingSeed: hash32,
   // Total deposits penalized in the given withdrawal period
-  depositsPenalizedInPeriod: 'uint64'[],
+  depositsPenalizedInPeriod: uint64[],
   // Hash chain of validator set changes (for light clients to easily track deltas)
-  validatorSetDeltaHashChain: 'hash32'
+  validatorSetDeltaHashChain: hash32
   // Current sequence number for withdrawals
-  currentExitSeq: 'uint64',
+  currentExitSeq: uint64,
   // Genesis time
-  genesisTime: 'uint64',
+  genesisTime: uint64,
   // PoW receipt root
-  processedPowReceiptRoot: 'hash32',
+  processedPowReceiptRoot: hash32,
   candidatePowReceiptRoots: CandidatePoWReceiptRootRecord[],
   // Parameters relevant to hard forks / versioning.
   // Should be updated only by hard forks.
-  preForkVersion: 'uint64',
-  postForkVersion: 'uint64',
-  forkSlotNumber: 'uint64',
+  preForkVersion: uint64,
+  postForkVersion: uint64,
+  forkSlotNumber: uint64,
   // Attestations not yet processed
   pendingAttestations: AttestationRecord[],
   // recent beacon block hashes needed to process attestations, older to newer
-  recentBlockHashes: 'hash32'[],
+  recentBlockHashes: hash32[],
   // RANDAO state
-  randaoMix: 'hash32'
+  randaoMix: hash32
 }
 
 export interface  ValidatorRecord {
   // BLS public key
-  pubkey: 'uint384',
+  pubkey: uint384,
   // Withdrawal credentials
-  withdrawalCredentials: 'hash32',
+  withdrawalCredentials: hash32,
   // RANDAO commitment
-  randaoCommitment: 'hash32',
+  randaoCommitment: hash32,
   // Slot the proposer has skipped (ie. layers of RANDAO expected)
-  randaoSkips: 'uint64',
+  randaoSkips: uint64,
   // Balance in Gwei
-  balance: 'uint64',
+  balance: uint64,
   // Status code
-  status: 'uint64',
+  status: uint64,
   // Slot when validator last changed status (or 0)
-  lastStatusChangeSlot: 'uint64'
+  lastStatusChangeSlot: uint64
   // Sequence number when validator exited (or 0)
-  exit_seq: 'uint64'
+  exit_seq: uint64
 }
 
 export interface CrosslinkRecord {
   // Slot number
-  slot: 'uint64',
+  slot: uint64,
   // Shard chain block hash
-  shardBlockHash: 'hash32'
+  shardBlockHash: hash32
 }
 
 export interface ShardAndCommittee {
   // Shard number
-  shard: 'uint64',
+  shard: uint64,
   // Validator indices
-  committee: 'uint24'[]
+  committee: uint24[]
 }
 
 export interface ShardReassignmentRecord {
   // Which validator to reassign
-  validatorIndex: 'uint24',
+  validatorIndex: uint24,
   // To which shard
-  shard: 'uint64',
+  shard: uint64,
   // When
-  slot: 'uint64'
+  slot: uint64
 }
 
 export interface CandidatePoWReceiptRootRecord {
   // Candidate PoW receipt root
-  candidatePowReceiptRoot: 'hash32',
+  candidatePowReceiptRoot: hash32,
   // Vote count
-  votes: 'uint64'
+  votes: uint64
 }
