8e92654cf3f41400b633b7fbf686d5e7f7a4d830
chainsafe <aidan@chainsafe.io>
2019-01-07 16:00:14 -0500

    updated license

--- test/test_bls.js
+++ test/test_bls.js
@@ -1,5 +1,5 @@
-//BLS JS
-//Copyright (C) 2018 ChainSafe Systems
+// BLS JS
+// Copyright (C) 2018 ChainSafe Systems
 
  // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
