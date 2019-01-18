03cb3f2bdd77b905edf6af6cbf9b84b05ad58d3c
Gregory Markou <gregorymarkou@gmail.com>
2019-01-18 11:03:02 -0500

    setup base beaconnode class

--- beaconChain/node/index.ts
+++ beaconChain/node/index.ts
@@ -1,6 +1,18 @@
-class BeaconNode {
-  opts: object;
-  constructor(opts: object) {
-    this.opts = opts || {};
-  }
+/**
+ * All properties are optional such we can declare defaults.
+ */
+interface BeaconNodeCtx {
+  chain?: string;
 }
+
+class BeaconNode {
+  opts: BeaconNodeCtx;
+  constructor(opts: BeaconNodeCtx) {
+    this.opts = opts;
+
+    this.opts.chain = opts.chain ? opts.chain : "mainnet";
+  }
+
+}
+
+export default BeaconNode;
