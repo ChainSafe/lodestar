7341e447d5595fcab6e667adcb8d8084805e5ea2
Gregory Markou <gregorymarkou@gmail.com>
2019-01-19 14:19:46 -0500

    alter the constructor

--- beaconChain/node/index.ts
+++ beaconChain/node/index.ts
@@ -1,18 +1,37 @@
 /**
  * All properties are optional such we can declare defaults.
  */
+import {BeaconState} from "../interfaces/state";
+import {DEPOSIT_CONTRACT_ADDRESS} from "../constants/constants";
+
 interface BeaconNodeCtx {
   chain?: string;
+  state?: BeaconState;
+  powChainAddress?: string;
 }
 
 class BeaconNode {
-  opts: BeaconNodeCtx;
-  constructor(opts: BeaconNodeCtx) {
-    this.opts = opts;
+  chain?: string;
+  state?: BeaconState;
+  powChainAddress?: string;
 
-    this.opts.chain = opts.chain ? opts.chain : "mainnet";
+  constructor(opts: BeaconNodeCtx) {
+    this.chain = opts.chain ? opts.chain : "mainnet";
+    // Stubbed waititng for #73
+    // this.state = opts.state ? opts.state : getInitialState(); // real
+    this.state = opts.state ? opts.state : null; // not real
+    this.powChainAddress = opts.powChainAddress ? opts.powChainAddress : DEPOSIT_CONTRACT_ADDRESS;
   }
 
+  // Register all services
+  public start = () => {
+
+  }
+
+  // Stop all services
+  public stop = () => {
+
+  }
 }
 
 export default BeaconNode;
