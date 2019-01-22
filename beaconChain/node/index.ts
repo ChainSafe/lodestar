889212df2a38ffce1e0ced2c5b9bee29a6233dfb
Gregory Markou <gregorymarkou@gmail.com>
2019-01-22 17:42:24 -0500

    remove optional params

--- beaconChain/node/index.ts
+++ beaconChain/node/index.ts
@@ -1,22 +1,20 @@
-import {DEPOSIT_CONTRACT_ADDRESS} from "../constants/constants";
-import {BeaconState} from "../interfaces/state";
+import { DEPOSIT_CONTRACT_ADDRESS } from "../constants/constants";
+import { BeaconState } from "../interfaces/state";
 
 interface BeaconNodeCtx {
-  chain?: string;
-  state?: BeaconState;
-  powChainAddress?: string;
+  chain: string;
+  state: BeaconState;
+  powChainAddress: string;
 }
 
 class BeaconNode {
-  public chain?: string;
-  public state?: BeaconState;
-  public powChainAddress?: string;
+  public chain: string;
+  public state: BeaconState;
+  public powChainAddress: string;
 
   constructor(opts: BeaconNodeCtx) {
     this.chain = opts.chain ? opts.chain : "mainnet";
-    // Stubbed waititng for #73
-    // this.state = opts.state ? opts.state : getInitialState(); // real
-    this.state = opts.state ? opts.state : null; // not real
+    this.state = opts.state ? opts.state : getInitialState(); // real
     this.powChainAddress = opts.powChainAddress ? opts.powChainAddress : DEPOSIT_CONTRACT_ADDRESS;
   }
 }
