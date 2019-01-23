e076e20c24af4adbd879a26840156150f10bf4c7
Gregory Markou <gregorymarkou@gmail.com>
2019-01-23 13:58:47 -0500

    fix linter

--- beaconChain/pow/fake.ts
+++ beaconChain/pow/fake.ts
@@ -1,4 +1,4 @@
-import {Deposit} from "../interfaces/blocks";
+import {Deposit, DepositData, DepositInput} from "../interfaces/blocks";
 
 type int = number;
 type hash32 = Uint8Array;
@@ -11,30 +11,34 @@ interface DummyChainStart {
 
 export function getInitialDeposits(): DummyChainStart {
   return {
+    depositRoot: new Uint8Array(32),
     deposits: generateFakeDeposits(),
     genesisTime: Date.now() / 1000 | 0,
-    depositRoot: new Uint8Array(32)
-  }
+  };
 }
 
 function generateFakeDeposits(): Deposit[] {
-  let deposits: Deposit[] = [];
+  const deposits: Deposit[] = [];
 
   for (let i: number = 0; i < 10; i++) {
+    const depositInput: DepositInput = {
+      custodyCommitment: new Uint8Array(32),
+      proofOfPossession: [486468, 486484],
+      pubkey: 0xe4b32544f1d3fa0a071e3629a1a22e76dc216312,
+      randaoCommitment: new Uint8Array(32),
+      withdrawalCredentials: new Uint8Array(32),
+    };
+
+    const depositData: DepositData = {
+      amount: 32000000000,
+      depositInput,
+      timestamp: Date.now() / 1000 | 0,
+    };
+
     const deposit: Deposit = {
       branch: [new Uint8Array(32)],
+      depositData,
       index: i,
-      depositData: {
-        amount: 32000000000,
-        timestamp: Date.now() / 1000 | 0,
-        depositInput: {
-          pubkey: 0xe4b32544f1d3fa0a071e3629a1a22e76dc216312,
-          withdrawalCredentials: new Uint8Array(32),
-          randaoCommitment: new Uint8Array(32),
-          custodyCommitment: new Uint8Array(32),
-          proofOfPossession: [486468,486484]
-        }
-      }
     };
     deposits.push(deposit);
   }
