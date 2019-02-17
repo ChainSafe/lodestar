cf37139e335492938d552d3aec96efbed852de07
David Ansermino <david.ansermino@mail.utoronto.ca>
2019-02-16 19:35:47 -0500

    Implements getBitfieldBit and tests

--- beaconChain/tests/helpers/stateTransitionHelpers.test.ts
+++ beaconChain/tests/helpers/stateTransitionHelpers.test.ts
@@ -3,8 +3,21 @@ import { assert } from "chai";
 
 import { SLOTS_PER_EPOCH, TARGET_COMMITTEE_SIZE } from "../../constants";
 import {
-  clamp, getActiveValidatorIndices, getCurrentEpoch, getDomain, getEpochCommitteeCount, getEpochStartSlot, getForkVersion,
-  intSqrt, isActiveValidator, isDoubleVote, isPowerOfTwo, isSurroundVote, slotToEpoch, split,
+    clamp,
+    getActiveValidatorIndices,
+    getBitfieldBit,
+    getCurrentEpoch,
+    getDomain,
+    getEpochCommitteeCount,
+    getEpochStartSlot,
+    getForkVersion,
+    intSqrt,
+    isActiveValidator,
+    isDoubleVote,
+    isPowerOfTwo,
+    isSurroundVote,
+    slotToEpoch,
+    split,
 } from "../../helpers/stateTransitionHelpers";
 import {Epoch, Fork, Slot, uint64, Validator} from "../../types";
 import {generateAttestationData} from "../utils/attestation";
@@ -454,3 +467,22 @@ describe("getDomain", () => {
     assert(result.eq(expected));
   });
 });
+
+describe("getBitfieldBit", () => {
+  it("should return 1 for the 4th (index 3) bit of [0x8]", () => {
+    const result = getBitfieldBit(Buffer.from([0x8]), 3);
+    assert(result === 1, `returned ${result} not 1`);
+  });
+  it("should return 0 for the 3rd (index 2) bit of [0x8]", () => {
+    const result = getBitfieldBit(Buffer.from([0x8]), 2);
+    assert(result === 0, `returned ${result} not 0`);
+  });
+  it("should return 1 for the 18th (index 17) bit of [0x8, 0x4, 0x2, 0x1]", () => {
+    const result = getBitfieldBit(Buffer.from([0x8, 0x4, 0x2, 0x1]), 17);
+    assert(result === 1, `returned ${result} not 1`);
+  });
+  it("should return 1 for the 19th (index 18) bit of [0x8, 0x4, 0x2, 0x1]", () => {
+    const result = getBitfieldBit(Buffer.from([0x8, 0x4, 0x2, 0x1]), 18);
+    assert(result === 0, `returned ${result} not 0`);
+  });
+})
