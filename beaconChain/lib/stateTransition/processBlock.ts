58ff667604fe689067a3be0243da82d5610fb752
Cayman <caymannava@gmail.com>
2019-02-19 18:37:37 -0600

    Use assertion-style checks

--- beaconChain/lib/stateTransition/processBlock.ts
+++ beaconChain/lib/stateTransition/processBlock.ts
@@ -1,6 +1,7 @@
-import {serialize, treeHash} from "@chainsafesystems/ssz";
-import xor from "buffer-xor";
+import assert from "assert";
 import BN from "bn.js";
+import xor from "buffer-xor";
+import {serialize, treeHash} from "@chainsafesystems/ssz";
 
 import {
   AttestationData,
@@ -84,9 +85,8 @@ export function processBlock(state: BeaconState, block: BeaconBlock, verifySigna
   const currentEpoch = getCurrentEpoch(state);
 
   // Slot
-  if (!block.slot.eq(state.slot)) {
-    throw new Error("block root must equal state root");
-  }
+  assert(block.slot.eq(state.slot),
+    "block root must equal state root");
 
   // Proposer signature
   block.signature = EMPTY_SIGNATURE;
@@ -106,9 +106,7 @@ export function processBlock(state: BeaconState, block: BeaconBlock, verifySigna
     getDomain(state.fork, currentEpoch, Domain.PROPOSAL),
   );
 
-  if (!blockSignatureVerified) {
-    throw new Error();
-  }
+  assert(blockSignatureVerified);
 
   // RANDAO
   const proposer = state.validatorRegistry[getBeaconProposerIndex(state, state.slot)];
@@ -118,9 +116,7 @@ export function processBlock(state: BeaconState, block: BeaconBlock, verifySigna
     block.randaoReveal,
     getDomain(state.fork, currentEpoch, Domain.PROPOSAL),
   );
-  if (!randaoRevealVerified) {
-    throw new Error();
-  }
+  assert(randaoRevealVerified);
   state.latestRandaoMixes[currentEpoch.modn(LATEST_RANDAO_MIXES_LENGTH)] =
     xor(getRandaoMix(state, currentEpoch), hash(block.randaoReveal));
 
@@ -141,120 +137,78 @@ export function processBlock(state: BeaconState, block: BeaconBlock, verifySigna
   // Transactions
 
   // Proposer slashings
-  if (!(block.body.proposerSlashings.length <= MAX_PROPOSER_SLASHINGS)) {
-    throw new Error();
-  }
+  assert(block.body.proposerSlashings.length <= MAX_PROPOSER_SLASHINGS);
   for (const proposerSlashing of block.body.proposerSlashings) {
     const proposer: Validator =
       state.validatorRegistry[proposerSlashing.proposerIndex.toNumber()];
 
-    if (!proposerSlashing.proposalData1.slot.eq(proposerSlashing.proposalData2.slot)) {
-      throw new Error();
-    }
-    if (!proposerSlashing.proposalData1.shard.eq(proposerSlashing.proposalData2.shard)) {
-      throw new Error();
-    }
-    if (!proposerSlashing.proposalData1.blockRoot.equals(proposerSlashing.proposalData2.blockRoot)) {
-      throw new Error();
-    }
-    if (!(proposer.slashedEpoch.gt(currentEpoch))) {
-      throw new Error();
-    }
+    assert(proposerSlashing.proposalData1.slot.eq(proposerSlashing.proposalData2.slot));
+    assert(proposerSlashing.proposalData1.shard.eq(proposerSlashing.proposalData2.shard));
+    assert(proposerSlashing.proposalData1.blockRoot.equals(proposerSlashing.proposalData2.blockRoot));
+    assert(proposer.slashedEpoch.gt(currentEpoch));
     const proposalData1Verified = blsVerify(
       proposer.pubkey,
       treeHash(proposerSlashing.proposalData1),
       proposerSlashing.proposalSignature1,
       getDomain(state.fork, slotToEpoch(proposerSlashing.proposalData1.slot), Domain.PROPOSAL)
     );
-    if (!proposalData1Verified) {
-      throw new Error();
-    }
+    assert(proposalData1Verified);
     const proposalData2Verified = blsVerify(
       proposer.pubkey,
       treeHash(proposerSlashing.proposalData2),
       proposerSlashing.proposalSignature2,
       getDomain(state.fork, slotToEpoch(proposerSlashing.proposalData2.slot), Domain.PROPOSAL)
     );
-    if (!proposalData2Verified) {
-      throw new Error();
-    }
+    assert(proposalData2Verified);
     slashValidator(state, proposerSlashing.proposerIndex);
   }
 
   // Attester slashings
-  if (!(block.body.attesterSlashings.length <= MAX_ATTESTER_SLASHINGS)) {
-    throw new Error();
-  }
+  assert(block.body.attesterSlashings.length <= MAX_ATTESTER_SLASHINGS);
   for (const attesterSlashing of block.body.attesterSlashings) {
     const slashableAttestation1 = attesterSlashing.slashableAttestation1;
     const slashableAttestation2 = attesterSlashing.slashableAttestation2;
 
-    if (serialize(slashableAttestation1.data, AttestationData).eq(serialize(slashableAttestation2.data, AttestationData))) {
-      throw new Error();
-    }
-    if (isDoubleVote(slashableAttestation1.data, slashableAttestation2.data) ||
-        isSurroundVote(slashableAttestation1.data, slashableAttestation2.data)) {
-      throw new Error();
-    }
-    if (!verifySlashableAttestation(state, slashableAttestation1)) {
-      throw new Error();
-    }
-    if (!verifySlashableAttestation(state, slashableAttestation2)) {
-      throw new Error();
-    }
+    assert(!serialize(slashableAttestation1.data, AttestationData).eq(serialize(slashableAttestation2.data, AttestationData)));
+    assert(isDoubleVote(slashableAttestation1.data, slashableAttestation2.data) ||
+      isSurroundVote(slashableAttestation1.data, slashableAttestation2.data));
+    assert(verifySlashableAttestation(state, slashableAttestation1));
+    assert(verifySlashableAttestation(state, slashableAttestation2));
     const slashableIndices = slashableAttestation1.validatorIndices
       .filter((validatorIndex1) => (
         slashableAttestation2.validatorIndices.find((validatorIndex2) => validatorIndex1.eq(validatorIndex2)) &&
         state.validatorRegistry[validatorIndex1.toNumber()].slashedEpoch.gt(currentEpoch)
       ));
-    if (!(slashableIndices.length >= 1)) {
-      throw new Error();
-    }
+    assert(slashableIndices.length >= 1);
     slashableIndices.forEach((index) => slashValidator(state, index));
   }
 
   // Attestations
-  if (!(block.body.attestations.length <= MAX_ATTESTATIONS)) {
-    throw new Error();
-  }
+  assert(block.body.attestations.length <= MAX_ATTESTATIONS);
   for (const attestation of block.body.attestations) {
-    if (!(attestation.data.slot.lte(state.slot.subn(MIN_ATTESTATION_INCLUSION_DELAY))) ||
-        !(state.slot.subn(MIN_ATTESTATION_INCLUSION_DELAY).lt(attestation.data.slot.addn(SLOTS_PER_EPOCH)))) {
-      throw new Error();
-    }
+    assert(attestation.data.slot.lte(state.slot.subn(MIN_ATTESTATION_INCLUSION_DELAY)) &&
+      state.slot.subn(MIN_ATTESTATION_INCLUSION_DELAY).lt(attestation.data.slot.addn(SLOTS_PER_EPOCH)));
     const justifiedEpoch = slotToEpoch(attestation.data.slot.addn(1)).gte(currentEpoch) ?
       state.justifiedEpoch : state.previousJustifiedEpoch;
-    if (!attestation.data.justifiedEpoch.eq(justifiedEpoch)) {
-      throw new Error();
-    }
-    if (!attestation.data.justifiedBlockRoot.equals(getBlockRoot(state, getEpochStartSlot(attestation.data.justifiedEpoch)))) {
-      throw new Error();
-    }
-    if(!serialize(state.latestCrosslinks[attestation.data.shard.toNumber()], Crosslink).eq(serialize(attestation.data.latestCrosslink), Crosslink) &&
-      !serialize(state.latestCrosslinks[attestation.data.shard.toNumber()], Crosslink).eq(
+    assert(attestation.data.justifiedEpoch.eq(justifiedEpoch));
+    assert(attestation.data.justifiedBlockRoot.equals(getBlockRoot(state, getEpochStartSlot(attestation.data.justifiedEpoch))));
+    assert(serialize(state.latestCrosslinks[attestation.data.shard.toNumber()], Crosslink).eq(serialize(attestation.data.latestCrosslink), Crosslink) ||
+      serialize(state.latestCrosslinks[attestation.data.shard.toNumber()], Crosslink).eq(
         serialize({
           epoch: slotToEpoch(attestation.data.slot),
           shardBlockRoot: attestation.data.shardBlockRoot
-        } as Crosslink, Crosslink))) {
-      throw new Error();
-    }
+        } as Crosslink, Crosslink)));
 
     // Remove this condition in Phase 1
-    if (!(attestation.custodyBitfield.equals(Buffer.alloc(attestation.custodyBitfield.length)))) {
-      throw new Error();
-    }
-    if ((attestation.aggregationBitfield.equals(Buffer.alloc(attestation.aggregationBitfield.length)))) {
-      throw new Error();
-    }
+    assert((attestation.custodyBitfield.equals(Buffer.alloc(attestation.custodyBitfield.length))));
+    assert(attestation.aggregationBitfield.equals(Buffer.alloc(attestation.aggregationBitfield.length)));
 
     const crosslinkCommittee = getCrosslinkCommitteesAtSlot(state, attestation.data.slot)
       .filter(({shard}) => shard.eq(attestation.data.shard))
       .map(({validatorIndices}) => validatorIndices)[0]
     for (let i = 0; i < crosslinkCommittee.length; i++) {
       if (getBitfieldBit(attestation.aggregationBitfield, i) === 0b0) {
-        if (getBitfieldBit(attestation.custodyBitfield, i) !== 0b0) {
-          throw new Error();
-        }
+        assert(getBitfieldBit(attestation.custodyBitfield, i) === 0b0);
       }
     }
     const participants = getAttestationParticipants(state, attestation.data, attestation.aggregationBitfield);
@@ -273,13 +227,9 @@ export function processBlock(state: BeaconState, block: BeaconBlock, verifySigna
       attestation.aggregateSignature,
       getDomain(state.fork, slotToEpoch(attestation.data.slot), Domain.ATTESTATION)
     );
-    if (!custodyBitsVerified) {
-      throw new Error();
-    }
+    assert(custodyBitsVerified);
     // Remove the following conditional in Phase 1
-    if (attestation.data.shardBlockRoot.equals(ZERO_HASH)) {
-      throw new Error();
-    }
+    assert(attestation.data.shardBlockRoot.equals(ZERO_HASH));
     state.latestAttestations.push({
       data: attestation.data,
       aggregationBitfield: attestation.aggregationBitfield,
@@ -289,18 +239,12 @@ export function processBlock(state: BeaconState, block: BeaconBlock, verifySigna
   }
 
   // Deposits
-  if (!(block.body.deposits.length <= MAX_DEPOSITS)) {
-    throw new Error();
-  }
+  assert(block.body.deposits.length <= MAX_DEPOSITS);
   // TODO: add logic to ensure that deposits from 1.0 chain are processed in order
   for (const deposit of block.body.deposits) {
     const serializedDepositData = serialize(deposit.depositData, DepositData);
-    if (!deposit.index.eq(state.depositIndex)) {
-      throw new Error();
-    }
-    if (!verifyMerkleBranch(hash(serializedDepositData), deposit.branch, DEPOSIT_CONTRACT_TREE_DEPTH, deposit.index.toNumber(), state.latestEth1Data.depositRoot)) {
-      throw new Error();
-    }
+    assert(deposit.index.eq(state.depositIndex));
+    assert(verifyMerkleBranch(hash(serializedDepositData), deposit.branch, DEPOSIT_CONTRACT_TREE_DEPTH, deposit.index.toNumber(), state.latestEth1Data.depositRoot));
     processDeposit(
       state,
       deposit.depositData.depositInput.pubkey,
@@ -312,17 +256,11 @@ export function processBlock(state: BeaconState, block: BeaconBlock, verifySigna
   }
 
   // Voluntary Exits
-  if (!(block.body.voluntaryExits.length <= MAX_VOLUNTARY_EXITS)) {
-    throw new Error();
-  }
+  assert(block.body.voluntaryExits.length <= MAX_VOLUNTARY_EXITS);
   for (const exit of block.body.voluntaryExits) {
     const validator = state.validatorRegistry[exit.validatorIndex.toNumber()];
-    if (!(validator.exitEpoch.gt(getEntryExitEffectEpoch(currentEpoch)))) {
-      throw new Error();
-    }
-    if (!currentEpoch.gte(exit.epoch)) {
-      throw new Error();
-    }
+    assert(validator.exitEpoch.gt(getEntryExitEffectEpoch(currentEpoch)));
+    assert(currentEpoch.gte(exit.epoch));
     const exitMessage = treeHash({
       epoch: exit.epoch,
       validatorIndex: exit.validatorIndex,
@@ -334,37 +272,21 @@ export function processBlock(state: BeaconState, block: BeaconBlock, verifySigna
       exit.signature,
       getDomain(state.fork, exit.epoch, Domain.EXIT)
     );
-    if (!exitMessageVerified) {
-      throw new Error();
-    }
+    assert(exitMessageVerified);
     initiateValidatorExit(state, exit.validatorIndex);
   }
 
   // Transfers
   // Note: Transfers are a temporary functionality for phases 0 and 1, to be removed in phase 2.
-  if (!(block.body.transfers.length <= MAX_TRANSFERS)) {
-    throw new Error();
-  }
+  assert(block.body.transfers.length <= MAX_TRANSFERS);
   for (const transfer of block.body.transfers) {
-    if (!(state.validatorBalances[transfer.from.toNumber()].gte(transfer.amount))) {
-      throw new Error();
-    }
-    if (!(state.validatorBalances[transfer.from.toNumber()].gte(transfer.fee))) {
-      throw new Error();
-    }
-    if (!state.validatorBalances[transfer.from.toNumber()].eq(transfer.amount.add(transfer.fee)) &&
-        !state.validatorBalances[transfer.from.toNumber()].gte(transfer.amount.add(transfer.fee).addn(MIN_DEPOSIT_AMOUNT))) {
-      throw new Error();
-    }
-    if (!state.slot.eq(transfer.slot)) {
-      throw new Error();
-    }
-    if (!currentEpoch.gte(state.validatorRegistry[transfer.from.toNumber()].withdrawalEpoch)) {
-      throw new Error();
-    }
-    if (!state.validatorRegistry[transfer.from.toNumber()].withdrawalCredentials.equals(Buffer.concat([BLS_WITHDRAWAL_PREFIX_BYTE, hash(transfer.pubkey).slice(1)]))) {
-      throw new Error();
-    }
+    assert(state.validatorBalances[transfer.from.toNumber()].gte(transfer.amount));
+    assert(state.validatorBalances[transfer.from.toNumber()].gte(transfer.fee));
+    assert(state.validatorBalances[transfer.from.toNumber()].eq(transfer.amount.add(transfer.fee)) ||
+      state.validatorBalances[transfer.from.toNumber()].gte(transfer.amount.add(transfer.fee).addn(MIN_DEPOSIT_AMOUNT)));
+    assert(state.slot.eq(transfer.slot));
+    assert(currentEpoch.gte(state.validatorRegistry[transfer.from.toNumber()].withdrawalEpoch));
+    assert(state.validatorRegistry[transfer.from.toNumber()].withdrawalCredentials.equals(Buffer.concat([BLS_WITHDRAWAL_PREFIX_BYTE, hash(transfer.pubkey).slice(1)])));
     const transferMessage = treeHash({
       from: transfer.from,
       to: transfer.to,
@@ -379,9 +301,7 @@ export function processBlock(state: BeaconState, block: BeaconBlock, verifySigna
       transfer.signature,
       getDomain(state.fork, slotToEpoch(transfer.slot), Domain.TRANSFER)
     );
-    if (!transferMessageVerified) {
-      throw new Error();
-    }
+    assert(transferMessageVerified);
     state.validatorBalances[transfer.from.toNumber()] = state.validatorBalances[transfer.from.toNumber()].sub(transfer.amount.add(transfer.fee));
     state.validatorBalances[transfer.to.toNumber()] = state.validatorBalances[transfer.to.toNumber()].add(transfer.amount);
     state.validatorBalances[getBeaconProposerIndex(state, state.slot)] = state.validatorBalances[getBeaconProposerIndex(state, state.slot)].add(transfer.fee)
