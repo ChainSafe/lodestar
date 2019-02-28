f5016af3e46029dee312fd6acb66ca8eeb6353e7
Gregory Markou <gregorymarkou@gmail.com>
2019-02-28 11:46:24 -0500

    removed comments

--- beaconChain/src/stateTransition/epoch/crosslinks.ts
+++ beaconChain/src/stateTransition/epoch/crosslinks.ts
@@ -9,15 +9,9 @@ export function processCrosslinks(
   state: BeaconState,
   previousEpoch: Epoch,
   nextEpoch: Epoch): void {
-  // For every slot in range(get_epoch_start_slot(previous_epoch), get_epoch_start_slot(next_epoch)),
-  // let crosslink_committees_at_slot = get_crosslink_committees_at_slot(state, slot).
-  // For every (crosslink_committee, shard) in crosslink_committees_at_slot, compute:
 
-  // Set state.latest_crosslinks[shard] = Crosslink(epoch=slot_to_epoch(slot), shard_block_root=winning_root(crosslink_committee))
-  // if 3 * total_attesting_balance(crosslink_committee) >= 2 * get_total_balance(crosslink_committee).
   const start: number = getEpochStartSlot(previousEpoch).toNumber();
   const end: number = getEpochStartSlot(nextEpoch).toNumber();
-
   for (let slot: number = start; slot < end; slot++) {
     getCrosslinkCommitteesAtSlot(state, new BN(slot)).map((value: CrosslinkCommittee) => {
       const { shard, validatorIndices } = value;
