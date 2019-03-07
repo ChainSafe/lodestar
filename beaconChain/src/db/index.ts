d895ff948345adaf8ba1527a5e105bd34f6e4f4e
Cayman <caymannava@gmail.com>
2019-03-07 09:56:49 -0600

    Add more stub code to db

--- beaconChain/src/db/index.ts
+++ beaconChain/src/db/index.ts
@@ -1,5 +1,28 @@
-export class DB {
-  public constructor(opts) {}
+import {EventEmitter} from "events";
+
+import {
+  BeaconState,
+} from "../types";
+
+export class DB extends EventEmitter {
+  public constructor(opts) {
+    super();
+  }
   public async start() {}
   public async stop() {}
+
+  /**
+   * Fetch the canonical beacon chain's state
+   * @returns {BeaconState}
+   */
+  public get state(): BeaconState {
+    return {} as BeaconState;
+  }
+
+  /**
+   * Set the canonical beacon chain's state
+   * @param {BeaconState} state
+   */
+  public set state(state: BeaconState) {
+  }
 }
