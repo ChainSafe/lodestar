02ee1afa5f23eb9c40b06e649d61984e07b33794
Mikerah <mikerahqc@protonmail.com>
2019-04-08 07:30:52 -0400

    Basic stubs and structure for p2p server

--- src/p2p/index.ts
+++ src/p2p/index.ts
@@ -1,4 +1,14 @@
 import {EventEmitter} from "events";
+import {LodestarNode} from "node";
+
+const defaultOpts = {
+  // logger
+  maxPeers: 25,
+  refreshInterval: 15000,
+  multiaddrs: ['/ip4/127.0.0.1/tcp/9000'],
+  key: null,
+  bootnodes: []
+};
 
 /**
  * The P2PNetwork service manages p2p connection/subscription objects
@@ -6,8 +16,51 @@ import {EventEmitter} from "events";
 export class P2PNetwork extends EventEmitter {
   public constructor(opts) {
     super();
-    // build gossipsub object
+    options = { ...defaultOpts, ..opt};
+
+    this.maxPeers = options.maxPeers;
+    this.refreshInterval = options.refreshInterval;
+    this.started = false;
+    this.multiaddrs = opts.multiaddrs;
+    this.key = opts.key;
+    this.bootnodes = opts.bootnodes;
+    this.node = null;
+    this.peers = new Map();
   }
-  public async start() {}
-  public async stop() {}
+
+  get isRunning () {
+    return this.started;
+  }
+
+  public async start() {
+    if (this.started) {
+      throw new Error("P2P Network already started");
+    }
+
+    if (!this.node) {
+      this.node new LodestarNode({
+        peerInfo: await this.createPeerInfo(),
+	bootnodes: this.bootnodes
+      });
+
+      this.node.on('peer:discovery', async (peerInfo) => {
+      
+      });
+
+      this.node.on('peer:connect', async (peerInfo) => {
+      
+      });
+
+    }
+
+    this.started = true;
+  }
+  public async stop() {
+    this.started = false;
+    // TODO: Add logger
+  }
+
+  public async createPeerInfo() {
+  
+  }
 }
