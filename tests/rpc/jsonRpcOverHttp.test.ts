b89f9fa157e362e069505a2c63289c255497170f
Marin Petrunić <marin.petrunic@gmail.com>
2019-04-08 13:16:43 +0200

    add tests for http transport

---
+++ tests/rpc/jsonRpcOverHttp.test.ts
@@ -0,0 +1,79 @@
+import { assert } from "chai";
+import * as request from "supertest";
+import {MockAPI, JSONRPC} from "../../src/rpc";
+import HttpServer from "../../src/rpc/transport/http";
+import {generateRPCCall} from "../utils/rpcCall";
+
+describe("Json RPC over http", () => {
+  let rpc;
+  let server;
+
+  before(async () => {
+    const rpcServer = new HttpServer({port: 32421});
+    server = rpcServer.server;
+    rpc = new JSONRPC({}, {transport: rpcServer, api: new MockAPI()})
+    await rpc.start();
+  });
+
+  after(async () => {
+    await rpc.stop();
+  });
+
+  it("should get the chain head",  (done) => {
+    request.default(server)
+        .post('/')
+        .send(generateRPCCall('BeaconChain.getChainHead', []))
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+            if (err) return done(err);
+            if (res.body.error) {
+                return done(res.body.error)
+            }
+            done();
+        });
+  })
+
+  it("should fail for unknown methods",  (done) => {
+      request.default(server)
+          .post('/')
+          .send(generateRPCCall('BeaconChain.notExistingMethod', []))
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function(err, res) {
+              if (err) return done(err);
+              if(res.body.error) {
+                  return done()
+              }
+              assert.fail('Should not be successfull');
+              done();
+          });
+  })
+
+    it("should fail for methods other than POST",  (done) => {
+        request.default(server)
+            .get('/')
+            .set('Accept', 'application/json')
+            .expect(400)
+            .end((err) => {
+                done(err);
+            });
+    })
+
+    it("should fail to start on existing port",   (done) => {
+        const rpc = new JSONRPC({}, {transport: new HttpServer({port: 32421}), api: new MockAPI()});
+        rpc.start()
+            .then(async () => {
+                await rpc.stop();
+                done(new Error('Should not be able to start!'));
+            })
+            .catch((e) => {
+                done();
+            })
+    })
+
+
+
+});
