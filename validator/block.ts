9950dcb28c5d238d15f69f71f41fbb0dcfe1acfe
Gregory Markou <gregorymarkou@gmail.com>
2019-04-10 10:16:15 +1000

    slightly cleaned up block proposing

--- validator/block.ts
+++ validator/block.ts
@@ -1,9 +1,9 @@
-import {hashTreeRoot} from "@chainsafe/ssz";
+import ssz from "@chainsafe/ssz";
 import {ValidatorIndex, BeaconBlock, BeaconState, bytes48, Epoch} from "../src/types";
 import RPCProvider from "./stubs";
 import {blsSign} from "../src/stubs/bls";
 import {getDomain, slotToEpoch} from "../src/chain/helpers/stateTransitionHelpers";
-import {DOMAIN_RANDAO} from "./constants";
+import {DOMAIN_BEACON_BLOCK, DOMAIN_RANDAO} from "./constants";
 import {getEmptyBlock} from "../src/chain/helpers/genesis";
 
 export default class BlockProcessingService {
@@ -41,7 +41,6 @@ export default class BlockProcessingService {
     let block: BeaconBlock = getEmptyBlock();
     block = await this.assembleHeader(block);
     block = await this.assembleBody(block);
-    block = await this.assembleAttestations(block);
   }
 
   private async assembleHeader(block: BeaconBlock): Promise<BeaconBlock> {
@@ -53,23 +52,24 @@ export default class BlockProcessingService {
     // containing a stub for the state_root. It is useful to be able to run a state transition function that does not
     // validate signatures or state root for this purpose.
     block.slot = slot;
-    block.parentRoot = hashTreeRoot(prevBlock, BeaconBlock);
-    block.stateRoot = hashTreeRoot(curState, BeaconState);
+    block.parentRoot = ssz.hashTreeRoot(prevBlock, BeaconBlock);
+    block.stateRoot = ssz.hashTreeRoot(curState, BeaconState);
     block.randaoReveal = blsSign(
       this.privateKey,
-      hashTreeRoot(epoch, Epoch),
+      ssz.hashTreeRoot(epoch, Epoch),
       getDomain(curState.fork, epoch, DOMAIN_RANDAO)
     );
     // TODO Eth1Data
-    // TODO Signature
+    block.signature = blsSign(
+      this.privateKey,
+      ssz.signedRoot(block, BeaconBlock),
+      getDomain(curState.fork, epoch, DOMAIN_BEACON_BLOCK)
+    );
+
     return block;
   }
 
   private async assembleBody(block: BeaconBlock): Promise<BeaconBlock> {
-    return block;
-  }
-
-  private async assembleAttestations(block: BeaconBlock): Promise<BeaconBlock> {
     return block;
   }
 
--- validator/constants.ts
+++ validator/constants.ts
@@ -1,3 +1,4 @@
 export const KEYSTORE_DIR = "./keystores";
 
+export const DOMAIN_BEACON_BLOCK = 0;
 export const DOMAIN_RANDAO = 1;
