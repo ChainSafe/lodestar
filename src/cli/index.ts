803786f7e6b5fa5bbf365117bc1ce065099b756d
Marin Petrunić <marin.petrunic@gmail.com>
2019-04-10 22:59:28 +0200

    validator deposit cli on eth1

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,13 +1,17 @@
 // NOTE :: All commands are stubbed as examples
 // Useful repo https://github.com/tsantef/commander-starter
 import program from "commander";
-import {PrivateEth1Network} from "../eth1/dev/network";
+import {PrivateEth1Network} from "../eth1/dev/";
+import {ethers, Wallet} from "ethers";
+import {Eth1Wallet} from "../eth1";
+import defaults from "../eth1/defaults";
+import logger from "../logger/winston";
 
 program
   .version('0.0.1');
 
 program
-  .command('dev')
+  .command('eth1:dev')
   .description('Start private eth1 chain with deposit contract and 10 accounts with balance')
   .option("-p, --port [port]", 'Port on which private network node should start', 8545)
   .option("-h, --host [host]", 'Host on which node will be', '127.0.0.1')
@@ -21,12 +25,34 @@ program
 program
   .command('deposit')
   .description('Start private network with deposit contract and 10 accounts with balance')
-  .option("-k, --privateKey [privateKey]", 'Private key of account that will deposit 32 ETH')
-  .option("-p, --seed [seed]", 'If seed is submitted, first 10 accounts will deposit 32 ETH')
-  .option("-h, --host [host]", 'Host on which eth node is running', '127.0.0.1')
-  .option("-p, --port [host]", 'Port on which eth node is running', 8545)
-  .action(({privateKey, seed, port, host}) => {
-
+  .option("-k, --privateKey [privateKey]", 'Private key of account that will make deposit')
+  .option("-m, --mnemonic [mnemonic]", 'If mnemonic is submitted, first 10 accounts will make deposit')
+  .option("-n, --node [node]", 'Url of eth1 node', 'http://127.0.0.1:8545')
+  .option("-v, --value [value]", 'Amount of ether to deposit', "32")
+  .option("-c, --contract [contract]", 'Address of deposit contract', defaults.depositContract.address)
+  .action(async ({privateKey, mnemonic, node, value, contract}) => {
+      const provider = new ethers.providers.JsonRpcProvider(node);
+      const wallets = [];
+      if(mnemonic) {
+        for (let i=0; i<10; i++) {
+          const wallet = Wallet.fromMnemonic(mnemonic, `m/44'/60'/0'/0/${i}`);
+          wallet.connect(provider);
+          wallets.push(wallet);
+        }
+      } else if (privateKey) {
+        wallets.push(new Wallet(privateKey, provider))
+      } else {
+        return logger.error('You have to submit either privateKey or mnemonic. Check --help');
+      }
+      wallets.forEach(async wallet => {
+        try {
+          const hash = await (new Eth1Wallet(wallet.privateKey, provider))
+            .createValidatorDeposit(contract, ethers.utils.parseEther(value));
+          logger.info(`Successfully deposited ${value} ETH from ${wallet.address} to deposit contract. Tx hash: ${hash}`);
+        } catch (e) {
+          logger.error(`Failed to make deposit for account ${wallet.address}. Reason: ${e.message}`);
+        }
+      });
   });
 
 program.on('command:*', function () {
--- src/eth1/wallet.ts
+++ src/eth1/wallet.ts
@@ -1,6 +1,8 @@
 import defaults from "./defaults";
 import {ContractTransaction, ethers, Wallet} from "ethers";
 import {Provider} from "ethers/providers";
+import {number64} from "../types";
+import {BigNumber} from "ethers/utils";
 
 export class Eth1Wallet {
 
@@ -16,13 +18,13 @@ export class Eth1Wallet {
   /**
    * Will deposit 32 ETH to eth2.0 deposit contract.
    * @param address address of deposit contract
+   * @param amount amount to wei to deposit on contract
    */
-  public async createValidatorDeposit(address: string): Promise<string> {
-    const contract = new ethers.Contract(address, defaults.depositContract.abi, this.wallet.provider);
-    contract.connect(this.wallet);
+  public async createValidatorDeposit(address: string, amount: BigNumber): Promise<string> {
+    const contract = new ethers.Contract(address, defaults.depositContract.abi, this.wallet);
     //TODO: Implement real deposit arguments according to spec : https://github.com/ethereum/eth2.0-specs/blob/dev/specs/core/0_beacon-chain.md#deposit-arguments
     const depositData = Buffer.alloc(512);
-    const tx: ContractTransaction = await contract.deposit(depositData, {value: ethers.utils.parseEther('32.0')});
+    const tx: ContractTransaction = await contract.deposit(depositData, {value: amount});
     await tx.wait();
     return tx.hash;
   }
