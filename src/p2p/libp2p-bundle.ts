0c2847c2ff0f3ef2fa1447efc5f42e62c412ffca
Mikerah <mikerahqc@protonmail.com>
2019-04-12 22:15:23 -0400

    Added browser and KadDHT

--- src/p2p/libp2p-bundle.ts
+++ src/p2p/libp2p-bundle.ts
@@ -3,7 +3,7 @@ import {TCP} from "libp2p-tcp";
 import {Mplex} from "libp2p-mplex";
 import {Bootstrap} from "libp2p-bootstrap";
 import {WStar} from "libp2p-webrtc-star";
-import {waterfall} from "async/waterfall";
+import {KadDHT} from "libp2p-kad-dht";
 import {PeerInfo} from "peer-info";
 import {defaultsDeep} from "@nodeutils/defaults-deep";
 import {promisify} from "promisify-es6";
@@ -16,7 +16,8 @@ export class LodestarNode extends libp2p {
       modules: {
         transport: [TCP, wrtcStar],
 	streamMuxer: [Mplex],
-	peerDiscovery: [Bootstrap, wrtcStar.discovery]
+	peerDiscovery: [Bootstrap, wrtcStar.discovery],
+	dht: KadDHT
       },
       config: {
         peerDiscovery: {
@@ -25,7 +26,11 @@ export class LodestarNode extends libp2p {
             enabled: true,
             list: _options.bootstrap || []
           }
-        }
+	},
+        dht: {
+	  enabled: true,
+	  kBucketSize: 20
+	}
       }
     };
 
@@ -38,6 +43,7 @@ export class LodestarNode extends libp2p {
     const peerInfo = await promisify(PeerInfo.create(callback));
     // Opens a tcp socket at port 9000. Will change
     peerInfo.multiaddrs.add('ip4/0.0.0.0/tcp/9000');
+    peerInfo.multiaddrs.add('ip4/0.0.0.0/ws');
     node = new LodestarNode({
       peerInfo
     });
