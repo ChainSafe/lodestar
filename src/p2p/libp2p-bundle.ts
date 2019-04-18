db8ae9250043fadc1057dc769ef8bae4b8d78fbf
Mikerah <mikerahqc@protonmail.com>
2019-04-18 09:03:20 -0400

    Simplified libp2p-bundle.ts

--- src/p2p/libp2p-bundle.ts
+++ src/p2p/libp2p-bundle.ts
@@ -1,23 +1,23 @@
-import {libp2p} from "libp2p";
-import {TCP} from "libp2p-tcp";
-import {Mplex} from "libp2p-mplex";
-import {Bootstrap} from "libp2p-bootstrap";
-import {WStar} from "libp2p-webrtc-star";
-import {KadDHT} from "libp2p-kad-dht";
-import {PeerInfo} from "peer-info";
-import {defaultsDeep} from "@nodeutils/defaults-deep";
-import {promisify} from "promisify-es6";
+'use strict';
 
-export class LodestarNode extends libp2p {
-  constructor(_options: object) {
+import Libp2p from "libp2p";
+import TCP from "libp2p-tcp";
+import Mplex from "libp2p-mplex";
+import Bootstrap from "libp2p-bootstrap";
+import WStar from "libp2p-webrtc-star";
+import KadDHT from "libp2p-kad-dht";
+import defaultsDeep from "@nodeutils/defaults-deep";
+
+class LodestarNode extends Libp2p {
+  public constructor(_options: Libp2p.OptionsConfig) {
     const wrtcStar = new WStar({ id: _options.peerInfo.id });
 
     const defaults = {
       modules: {
         transport: [TCP, wrtcStar],
-	streamMuxer: [Mplex],
-	peerDiscovery: [Bootstrap, wrtcStar.discovery],
-	dht: KadDHT
+        streamMuxer: [Mplex],
+        peerDiscovery: [Bootstrap, wrtcStar.discovery],
+        dht: KadDHT
       },
       config: {
         peerDiscovery: {
@@ -26,36 +26,19 @@ export class LodestarNode extends libp2p {
             enabled: true,
             list: _options.bootstrap || []
           }
-	},
+        },
         dht: {
 	  enabled: true,
 	  kBucketSize: 20
-	}
+        },
+        EXPERIMENTAL: {
+          pubsub: true
+        }
       }
     };
 
     super(defaultsDeep(_options, defaults));
   }
-
-  static async createNode (callback) {
-    var node: LodestarNode;
-
-    const peerInfo = await promisify(PeerInfo.create(callback));
-    // Opens a tcp socket at port 9000. Will change
-    peerInfo.multiaddrs.add('ip4/0.0.0.0/tcp/9000');
-    peerInfo.multiaddrs.add('ip4/0.0.0.0/ws');
-    node = new LodestarNode({
-      peerInfo
-    });
-    node.start(callback);
-
-    node.pubsub = new FloodSub(node);
-    node.pubsub.start((err) => {
-      if (err) {
-        throw new Error('PubSub failed to start.');
-      }
-    });
-  }
-
 }
 
+export { LodestarNode };
