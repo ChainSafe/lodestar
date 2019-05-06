808861a7c05f1d69f2b0ab165ac21c2ad752c87b
Marin Petrunić <marin.petrunic@gmail.com>
2019-05-06 12:02:15 +0200

    some code refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,57 @@
-import {BLSDomain, BLSPrivKey, BLSPubkey, BLSSignature, bytes32} from "./types";
+import {BLSDomain, BLSSecretKey, BLSPubkey, BLSSignature, bytes32} from "./types";
 import {Keypair} from "./keypair";
 import {PrivateKey} from "./privateKey";
 import {G2point} from "./helpers/g2point";
-import {PublicKey} from "./publicKey";
 import {G1point} from "./helpers/g1point";
 
-export default class BLS {
+/**
+ * Generates public key from given secret.
+ * @param {BLSSecretKey} secretKey
+ */
+function generatePublicKey(secretKey: BLSSecretKey): BLSPubkey {
+  const keypair = new Keypair(PrivateKey.fromBytes(secretKey));
+  return keypair.publicKey.toBytesCompressed();
+}
 
-  public static generatePublicKey(privateKey: BLSPrivKey): BLSPubkey {
-    const keypair = new Keypair(PrivateKey.fromBytes(privateKey));
-    return keypair.publicKey.toBytesCompressed();
-  }
+/**
+ * Signs given message using secret key.
+ * @param secretKey
+ * @param messageHash
+ * @param domain
+ */
+function sign(secretKey: BLSSecretKey, messageHash: bytes32, domain: BLSDomain): BLSSignature {
+  const privateKey = PrivateKey.fromBytes(secretKey);
+  const hash = G2point.hashToG2(messageHash, domain);
+  return privateKey.sign(hash).toBytesCompressed();
+}
 
-  public static sign(secretKey: BLSPrivKey, messageHash: bytes32, domain: BLSDomain): BLSSignature {
-    const privateKey = PrivateKey.fromBytes(secretKey);
-    const hash = G2point.hashToG2(messageHash, domain);
-    return privateKey.sign(hash).toBytesCompressed();
-  }
+/**
+ * Compines all given signature into one.
+ * @param signatures
+ */
+function aggregateSignatures(signatures: BLSSignature[]) : BLSSignature {
+  return signatures.map((signature) => {
+    return G2point.fromCompressedBytes(signature)
+  }).reduce((previousValue, currentValue) => {
+    return previousValue.add(currentValue);
+  }).toBytesCompressed();
+}
 
-  public static aggregateSignatures(signatures: BLSSignature[]) : BLSSignature {
-    return signatures.map((signature) => {
-      return G2point.fromCompressedBytes(signature)
-    }).reduce((previousValue, currentValue) => {
-      return previousValue.add(currentValue);
-    }).toBytesCompressed();
-  }
+/**
+ * Combines all given public keys into single one
+ * @param publicKeys
+ */
+function aggregatePubkeys(publicKeys: BLSPubkey[]) : BLSPubkey {
+  return publicKeys.map((publicKey) => {
+    return G1point.fromBytesCompressed(publicKey)
+  }).reduce((previousValue, currentValue) => {
+    return previousValue.add(currentValue);
+  }).toBytesCompressed();
+}
 
-  public static aggregatePubkeys(publicKeys: BLSPubkey[]) : BLSPubkey {
-    return publicKeys.map((publicKey) => {
-      return G1point.fromBytesCompressed(publicKey)
-    }).reduce((previousValue, currentValue) => {
-      return previousValue.add(currentValue);
-    }).toBytesCompressed();
-  }
+export default {
+  generatePublicKey,
+  sign,
+  aggregateSignatures,
+  aggregatePubkeys
 }
