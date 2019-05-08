4fbde99aa2843e0c602dd9f773d9402954a3b4a7
Marin Petrunić <marin.petrunic@gmail.com>
2019-05-08 14:37:09 +0200

    add get index method to validator api

--- src/rpc/api/validator/interface.ts
+++ src/rpc/api/validator/interface.ts
@@ -4,7 +4,7 @@
 import {
   Attestation,
   AttestationData,
-  BeaconBlock,
+  BeaconBlock, BLSPubkey,
   bytes,
   bytes32,
   bytes48,
@@ -23,37 +23,54 @@ import {CommitteeAssignment} from "../../../validator/types";
 export interface IValidatorApi extends IApi {
 
   /**
-   * Requests the BeaconNode to provide a set of “duties”, which are actions that should be performed by ValidatorClients. This API call should be polled at every slot, to ensure that any chain reorganisations are catered for, and to ensure that the currently connected BeaconNode is properly synchronised.
-   * @param {bytes48[]} validatorPubkey
-   * @returns {Promise<{currentVersion: bytes4; validatorDuty: ValidatorDuty}>} A list of unique validator public keys, where each item is a 0x encoded hex string.
+   * Obtains validator index attached to his public key
+   * @param validatorPublicKey
    */
-  getDuties(validatorPubkey: bytes48): Promise<{currentVersion: Fork; validatorDuty: ValidatorDuty}>;
+  getIndex(validatorPublicKey: BLSPubkey): Promise<ValidatorIndex>;
+
+  /**
+   * Requests the BeaconNode to provide a set of “duties”,
+   * which are actions that should be performed by ValidatorClients.
+   * This API call should be polled at every slot,
+   * to ensure that any chain reorganisations are catered for,
+   * and to ensure that the currently connected BeaconNode is properly synchronised.
+   * @param validatorIndex
+   * @returns {Promise<{currentVersion: bytes4; validatorDuty: ValidatorDuty}>}
+   * A list of unique validator public keys, where each item is a 0x encoded hex string.
+   */
+  getDuties(
+    validatorIndex: ValidatorIndex
+  ): Promise<{currentVersion: Fork; validatorDuty: ValidatorDuty}>;
 
   /**
    * Requests to check if a validator should propose for a given slot.
-   * @param {bytes48} validatorPubkey
+   * @param index
    * @param {Slot} slot
    * @returns {Promise<{slot: Slot, proposer: boolean}}
    */
   isProposer(index: ValidatorIndex, slot: Slot): Promise<boolean>;
 
   /**
-   * Requests a validators committeeAssignment, can be used for past, current and one epoch in the future
+   * Requests a validators committeeAssignment,
+   * can be used for past, current and one epoch in the future
    * @param {ValidatorIndex} index
    * @param {Epoch} epoch
    */
   getCommitteeAssignment(index: ValidatorIndex, epoch: Epoch): Promise<CommitteeAssignment>;
 
   /**
-   * Requests a BeaconNode to produce a valid block, which can then be signed by a ValidatorClient.
+   * Requests a BeaconNode to produce a valid block,
+   * which can then be signed by a ValidatorClient.
    * @param {Slot} slot
    * @param {bytes} randaoReveal
-   * @returns {Promise<BeaconBlock>} A proposed BeaconBlock object, but with the signature field left blank.
+   * @returns {Promise<BeaconBlock>} A proposed BeaconBlock object,
+   * but with the signature field left blank.
    */
   produceBlock(slot: Slot, randaoReveal: bytes): Promise<BeaconBlock>;
 
   /**
-   * Requests that the BeaconNode produce an IndexedAttestation, with a blank signature field, which the ValidatorClient will then sign.
+   * Requests that the BeaconNode produce an IndexedAttestation,
+   * with a blank signature field, which the ValidatorClient will then sign.
    * @param {Slot} slot
    * @param {Shard} shard
    * @returns {Promise<Attestation>}
@@ -61,14 +78,16 @@ export interface IValidatorApi extends IApi {
   produceAttestation(slot: Slot, shard: Shard): Promise<AttestationData>;
 
   /**
-   * Instructs the BeaconNode to publish a newly signed beacon block to the beacon network, to be included in the beacon chain.
+   * Instructs the BeaconNode to publish a newly signed beacon block
+   * to the beacon network, to be included in the beacon chain.
    * @param {BeaconBlock} beaconBlock
    * @returns {Promise<void>}
    */
   publishBlock(beaconBlock: BeaconBlock): Promise<void>;
 
   /**
-   * Instructs the BeaconNode to publish a newly signed IndexedAttestation object, to be incorporated into the beacon chain.
+   * Instructs the BeaconNode to publish a newly signed IndexedAttestation object,
+   * to be incorporated into the beacon chain.
    * @param {Attestation} attestation
    * @returns {Promise<void>}
    */
--- src/rpc/api/validator/validator.ts
+++ src/rpc/api/validator/validator.ts
@@ -2,7 +2,7 @@ import {
   Attestation,
   AttestationData,
   BeaconBlock,
-  BeaconState,
+  BeaconState, BLSPubkey,
   bytes,
   bytes48,
   Epoch,
@@ -33,7 +33,7 @@ export class ValidatorApi implements IValidatorApi {
     this.opPool = opPool;
   }
 
-  public async getDuties(validatorPubkey: bytes48): Promise<{currentVersion: Fork; validatorDuty: ValidatorDuty}> {
+  public async getDuties(validatorIndex: ValidatorIndex): Promise<{currentVersion: Fork; validatorDuty: ValidatorDuty}> {
     // eslint-disable-next-line @typescript-eslint/no-object-literal-type-assertion
     return {} as {currentVersion: Fork; validatorDuty: ValidatorDuty};
   }
@@ -64,5 +64,15 @@ export class ValidatorApi implements IValidatorApi {
 
   public async publishAttestation(attestation: Attestation): Promise<void> {
     await this.opPool.receiveAttestation(attestation);
+  }
+
+  public async getIndex(validatorPublicKey: BLSPubkey): Promise<ValidatorIndex> {
+    const state = await this.db.getState();
+    state.validatorRegistry.forEach((validator, index) => {
+      if(validator.pubkey === validatorPublicKey) {
+        return index;
+      }
+    });
+    return null;
   }
 }
