5bb28c3c1f4027523ea2480ac5099974bf19d3c5
Austin Abell <austinabell8@gmail.com>
2019-05-13 14:24:47 -0400

    Implemented wallet CLI command to save privateKey and publicKey objects from bls

---
+++ src/cli/commands/wallet.ts
@@ -0,0 +1,53 @@
+/**
+ * @module cli/commands
+ */
+
+import {CliCommand} from "./interface";
+import {CommanderStatic} from "commander";
+import logger from "../../logger";
+import fs from "fs";
+import {CliError} from "../error";
+import bls from '@chainsafe/bls-js';
+
+interface IWalletCommandOptions {
+  outputFile: string;
+}
+
+export class CreateWalletCommand implements CliCommand {
+  public register(commander: CommanderStatic): void {
+    commander
+      .command("wallet")
+      .description("Generate wallet with bls-js private and public key")
+      .option("-o, --outputFile [output_file]", "Path to output file destination", "keys/bls.json")
+      .action(async (options) => {
+        // library is not awaiting this method so don't allow error propagation 
+        // (unhandled promise rejections)
+        try {
+          await this.action(options);
+        } catch (e) {
+          logger.error(e.message);
+        }
+      });
+  }
+
+  public async action(options: IWalletCommandOptions): Promise<void> {
+    if (fs.existsSync(options.outputFile)) {
+      throw new CliError(`${options.outputFile} already exists`);
+    }
+
+    const keyPair = bls.generateKeyPair();
+
+    const object = {
+      privateKey: keyPair.privateKey,
+      publicKey: keyPair.publicKey
+    }
+
+    try {
+      fs.writeFileSync(options.outputFile, JSON.stringify(object, null, 2));
+    } catch (err) {
+      throw new CliError(`Failed to write to ${options.outputFile}: ${err}`);
+    }
+
+    logger.info(`Successfully wrote keys to: ${options.outputFile}`);
+  }
+}
