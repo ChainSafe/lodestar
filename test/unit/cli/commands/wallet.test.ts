f2269bd7ccdbfe295b93f0a7fe221e8028b206fa
Austin Abell <austinabell8@gmail.com>
2019-05-21 12:18:45 -0400

    Added tests for Keystore functionality

--- test/unit/cli/commands/wallet.test.ts
+++ test/unit/cli/commands/wallet.test.ts
@@ -3,33 +3,29 @@ import logger from "../../../../src/logger/winston";
 import {CliError} from "../../../../src/cli/error";
 
 import program from "commander";
-import { CreateWalletCommand } from '../../../../src/cli/commands/wallet';
+import {CreateWalletCommand} from '../../../../src/cli/commands/wallet';
 
 describe('[CLI] wallet', () => {
 
-  before(async () => {
+  before(() => {
     logger.silent(true);
   });
 
-  after(async () => {
+  after(() => {
     logger.silent(false);
   });
 
   it('Should be able to register', async () => {
     const command = new CreateWalletCommand();
     const commandCount = program.commands.length;
-    await expect(
-      command.register(program)
-    ).to.not.throw;
+    await expect(command.register(program)).to.not.throw;
     expect(program.commands.length).to.be.equal(commandCount + 1);
   });
 
   it('Should throw error if output file exists', async () => {
     const command = new CreateWalletCommand();
     await expect(
-      command.action({
-        outputFile: "src",
-      })
+      command.action({outputFile: "src"})
     ).to.be.rejectedWith(CliError, 'src already exists');
   });
 });
