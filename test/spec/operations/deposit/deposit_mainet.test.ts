2a1e5197562ce91e192bb562621205413445080b
Marin Petrunić <marin.petrunic@gmail.com>
2019-06-12 12:21:45 +0200

    fix deposit spec test comaprison

--- test/spec/operations/deposit/deposit_mainet.test.ts
+++ test/spec/operations/deposit/deposit_mainet.test.ts
@@ -7,6 +7,9 @@ import {restore, rewire} from "@chainsafe/bls-js";
 import sinon from "sinon";
 import {depositsFromYaml} from "../../../utils/deposit";
 import {processDeposit} from "../../../../src/chain/stateTransition/block/deposits";
+import {hashTreeRoot} from "@chainsafe/ssz";
+import {Validator} from "../../../../src/types";
+
 
 describeSpecTest(
   join(__dirname, "../../test-cases/tests/operations/deposit/deposit_mainnet.yaml"),
@@ -29,6 +32,12 @@ describeSpecTest(
   },
   () => false,
   (_1, _2, expected, actual) => {
+    if(expected && actual) {
+      expected.balances = expected.balances.map(b => b.toString());
+      actual.balances = actual.balances.map(b => b.toString());
+      expected.validatorRegistry = expected.validatorRegistry.map(b => hashTreeRoot(b, Validator));
+      actual.validatorRegistry = actual.validatorRegistry.map(b => hashTreeRoot(b, Validator));
+    }
     expect(expected).to.be.deep.equal(actual);
     restore();
   }
