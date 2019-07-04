9cf4f3f5d054026f34d519a026d10ccffd3ffb02
sadiq1971 <sadiqurr8@gmail.com>
2019-07-04 14:46:04 +0600

    removed unused imports

--- test/unit/chain/stateTransition/epoch/finalUpdates.test.ts
+++ test/unit/chain/stateTransition/epoch/finalUpdates.test.ts
@@ -1,17 +1,12 @@
-// @ts-ignore
 import sinon from "sinon";
 import {expect} from "chai";
-
 import * as utils from "../../../../../src/chain/stateTransition/util";
 import {generateState} from "../../../../utils/state";
 import {generateValidator} from "../../../../utils/validator";
 import * as hashTreeRoot from "@chainsafe/ssz";
 import {processFinalUpdates} from "../../../../../src/chain/stateTransition/epoch/finalUpdates";
-// @ts-ignore
 import BN from "bn.js";
 import {SLOTS_PER_ETH1_VOTING_PERIOD} from "../../../../../src/constants";
-import {number64} from "../../../../../src/types";
-
 
 describe('process epoch - final updates', function () {
 
@@ -46,6 +41,7 @@ describe('process epoch - final updates', function () {
     getActiveValidatorIndicesStub.returns([1,2,3,4]);
     getRandaoMixStub.returns(0);
     hashTreeRootStub.returns(Buffer.from("1010"));
+
     try {
       processFinalUpdates(state);
       expect(getCurrentEpochStub.calledOnceWith(state)).to.be.true;
