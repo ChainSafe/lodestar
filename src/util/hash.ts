bade37edb5a2af442a969aea122057d7a2259263
Cayman <caymannava@gmail.com>
2019-07-16 19:58:14 -0500

    Add optimized merkleize zero padding

--- src/util/hash.ts
+++ src/util/hash.ts
@@ -8,6 +8,12 @@ import { size } from "../size";
 import { _serialize } from "../serialize";
 import { hash } from "../hash";
 
+// create array of "zero hashes", successively hashed zero chunks
+const zeroHashes = [Buffer.alloc(BYTES_PER_CHUNK)];
+for (let i = 0; i < 52; i++) {
+  zeroHashes.push(hash(zeroHashes[i], zeroHashes[i]));
+}
+
 /** @ignore */
 export function pack (input: SerializableValue[], type: FullSSZType): Buffer[] {
   if (input.length === 0) {
@@ -20,10 +26,15 @@ export function pack (input: SerializableValue[], type: FullSSZType): Buffer[] {
   for (const v of input) {
     index = _serialize(v, type, packedBuf, index);
   }
-  const chunkLength = Math.max(Math.ceil(packedLength / BYTES_PER_CHUNK), 1);
+  return chunkify(packedBuf);
+}
+
+/** @ignore */
+export function chunkify (input: Buffer): Buffer[] {
+  const chunkLength = Math.max(Math.ceil(input.length / BYTES_PER_CHUNK), 1);
   // Chop buffer into chunks
   const chunks = Array.from({ length: chunkLength },
-    (_, i) => packedBuf.slice(i * BYTES_PER_CHUNK, i * BYTES_PER_CHUNK + BYTES_PER_CHUNK));
+    (_, i) => input.slice(i * BYTES_PER_CHUNK, i * BYTES_PER_CHUNK + BYTES_PER_CHUNK));
   const lastChunk = chunks[chunkLength - 1];
   if (lastChunk.length < BYTES_PER_CHUNK) {
     chunks[chunkLength - 1] = Buffer.concat([lastChunk, Buffer.alloc(BYTES_PER_CHUNK - lastChunk.length)]);
@@ -35,7 +46,8 @@ export function pack (input: SerializableValue[], type: FullSSZType): Buffer[] {
 function bitLength (n: number): number {
   let length = 0;
   while (n !== 0) {
-    n = n >> 1;
+    // rshift only works to 32 bits, so we int div by 2 instead
+    n = Math.floor(n / 2);
     length++;
   }
   return length;
@@ -48,15 +60,21 @@ function nextPowerOf2 (n: number): number {
 
 /** @ignore */
 export function merkleize(chunks: Buffer[], padFor: number = 0): Buffer {
-  const lengthToPad = nextPowerOf2(padFor || chunks.length) - chunks.length;
-  if (lengthToPad) {
-    // Add zeroed chunks as leaf nodes to create full binary tree
-    const emptyChunk = Buffer.alloc(BYTES_PER_CHUNK);
-    chunks = chunks.concat(
-      Array.from({ length: lengthToPad }, () => emptyChunk)
-    );
-  }
-  while (chunks.length > 1) {
+  let lengthToPad = nextPowerOf2(padFor || chunks.length) - chunks.length;
+  // Instead of pushing on all padding zero chunks at the leaf level
+  // we push on zero hash chunks at the highest possible level to avoid over-hashing
+  for (let layer = 0; chunks.length + lengthToPad > 0; layer++) {
+    const addZeroHash = lengthToPad % 2 == 1;
+    lengthToPad = Math.floor(lengthToPad / 2);
+
+    // if the lengthToAdd is odd, then the chunks.length is also odd
+    // we need to push on the zero-hash of that level to merkleize that level
+    if (addZeroHash) {
+      chunks.push(zeroHashes[layer]);
+    }
+    if (chunks.length == 1) {
+      break
+    }
     for (let i = 0; i < chunks.length; i += 2) {
       chunks[i / 2] = hash(chunks[i], chunks[i + 1]);
     }
