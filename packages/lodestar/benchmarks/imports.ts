5b8c2f00bcac2bbfb16624148411780c75120e4c
Gregory Markou <gregorymarkou@gmail.com>
2019-07-24 01:31:58 -0400

    fix import issues

--- packages/lodestar/benchmarks/imports.ts
+++ packages/lodestar/benchmarks/imports.ts
@@ -1,1 +1,1 @@
-export * from "./example";
+export * from "./examples";
--- packages/lodestar/benchmarks/index.ts
+++ packages/lodestar/benchmarks/index.ts
@@ -1,8 +1,7 @@
 import {createReportFile} from "./utils"
 
 // Import benchmarks
-// import * as benchmarks from "./imports"; // This doesn't seem to work
-import * as benchmarks from "./example/example";
+import * as benchmarks from "./imports";
 
 // Create file
 const file = createReportFile();
