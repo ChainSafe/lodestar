33e28221b9dcd74a80b5d9c56a68b09a21f0eaec
Marin Petrunić <marin.petrunic@gmail.com>
2019-07-25 13:03:55 +0200

    merkle tree repository

---
+++ packages/lodestar/src/db/api/beacon/repositories/merkleTree.ts
@@ -0,0 +1,21 @@
+import {FullDatabaseRepository} from "../repository";
+import {MerkleTree} from "../../../../types";
+import {IBeaconConfig} from "../../../../config";
+import {IDatabaseController} from "../../../controller";
+import {Bucket} from "../../../schema";
+import {IProgressiveMerkleTree, ProgressiveMerkleTree} from "../../../../util/merkleTree";
+
+export class MerkleTreeRepository extends FullDatabaseRepository<MerkleTree> {
+
+  public constructor(
+    config: IBeaconConfig,
+    db: IDatabaseController) {
+    super(config, db, Bucket.merkleTree, config.types.MerkleTree);
+  }
+
+  public async getProgressiveMerkleTree(index: number): Promise<IProgressiveMerkleTree> {
+    const tree = await this.get(index);
+    return new ProgressiveMerkleTree(tree.depth, tree.tree);
+  }
+
+}
