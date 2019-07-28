402c43624299169d683e3a724f618fbcc849df57
Marin Petrunić <marin.petrunic@gmail.com>
2019-07-28 21:09:45 +0200

    add minimal spec tests

---
+++ packages/lodestar/test/spec/shufling/shuffling_minimal.test.ts
@@ -0,0 +1,39 @@
+import {join} from "path";
+import {describeSpecTest} from "@chainsafe/eth2.0-spec-test-util";
+import {expect} from "chai";
+
+import {config} from "../../../src/config/presets/minimal";
+import {computeShuffledIndex} from "../../../src/chain/stateTransition/util";
+import {bytes32} from "../../../src/types";
+
+describeSpecTest(
+  join(__dirname, "../test-cases/tests/shuffling/core/shuffling_minimal.yaml"),
+  (seed: bytes32, count: number) => {
+    const output = [];
+    for(let i = 0; i < count; i++) {
+      output[i] = computeShuffledIndex(
+        config,
+        i,
+        count,
+        seed
+      );
+    }
+    return output;
+  },
+  (input) => {
+    return [Buffer.from(input.seed.slice(2), 'hex'), input.count.toNumber()];
+  },
+  (expected) => {
+    return expected.shuffled.map((value) => value.toNumber());
+  },
+  result => result,
+  (_) => {
+    return false;
+  },
+  () => false,
+  (_1, _2, expected, actual) => {
+    expect(expected).to.be.deep.equal(actual);
+  },
+  0
+);
+
