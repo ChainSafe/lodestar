2c85f2262766cecd0d43d0b18595f4e58deca371
Marin Petrunić <marin.petrunic@gmail.com>
2019-08-12 12:41:55 +0200

    initial api structure

---
+++ packages/lodestar/src/api/interface.ts
@@ -0,0 +1,25 @@
+/**
+ * @module api
+ */
+import {IBeaconConfig} from "@chainsafe/eth2.0-config";
+import {IBeaconChain} from "../chain";
+import {IBeaconDb} from "../db/api";
+import {IEth1Notifier} from "../eth1";
+import {IApiOptions} from "./options";
+
+export interface IApiModules {
+    config: IBeaconConfig;
+    chain: IBeaconChain;
+    db: IBeaconDb;
+    eth1: IEth1Notifier;
+}
+
+export interface IApi {
+
+    /**
+     * Name space for API commands
+     */
+    namespace: string;
+    
+    new(opts: Partial<IApiOptions>, modules: IApiModules): IApi;
+}
