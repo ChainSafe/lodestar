f9458e779852b21b9733c6682a3820f46e7498d0
Marin Petrunić <marin.petrunic@gmail.com>
2019-08-31 20:46:17 +0200

    update rest api version string

--- packages/lodestar/src/api/rest/routes/beacon/version.ts
+++ packages/lodestar/src/api/rest/routes/beacon/version.ts
@@ -4,6 +4,6 @@ import {IApiModules} from "../../../interface";
 
 export const registerVersionEndpoint = (fastify: IFastifyServer, modules: IApiModules): void => {
   fastify.get<fastify.DefaultQuery, {}, unknown>('/version', {}, (request, reply) => {
-    reply.code(200).type('application/json').send("Lodestar x.x.x");
+    reply.code(200).type('application/json').send(`lodestar-${process.env.npm_package_version}`);
   });
 };
