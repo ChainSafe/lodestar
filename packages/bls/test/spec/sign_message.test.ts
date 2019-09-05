294412f8f685f80da8f9f8a26a1fe948898205b8
Marin Petrunić <marin.petrunic@gmail.com>
2019-09-05 15:06:46 +0200

    upgrade bls tests to 0.8.3

--- packages/bls/test/spec/sign_message.test.ts
+++ packages/bls/test/spec/sign_message.test.ts
@@ -1,20 +1,34 @@
-import {join} from "path";
-import {describeSpecTest} from "@chainsafe/eth2.0-spec-test-util";
+import path from "path";
 import bls from "../../src";
 import {padLeft} from "../../src/helpers/utils";
+import {describeDirectorySpecTest, InputType} from "@chainsafe/eth2.0-spec-test-util/lib/single";
 
-describeSpecTest(
-  join(__dirname, "../../../spec-test-cases/tests/bls/sign_msg/sign_msg.yaml"),
-  bls.sign,
-  ({input}) => {
-    const domain = padLeft(Buffer.from(input.domain.replace('0x', ''), 'hex'), 8);
-    return [
-      Buffer.from(input.privkey.replace('0x', ''), 'hex'),
-      Buffer.from(input.message.replace('0x', ''), 'hex'),
-      domain
-    ];
-  },
-  ({output}) => output,
-  (result) => `0x${result.toString('hex')}`,
-  () => false,
+interface SignMessageTestCase {
+    data: {
+        input: {
+            privkey: string,
+            message: string,
+            domain: string
+        };
+        output: string;
+    };
+}
+
+describeDirectorySpecTest<SignMessageTestCase, string>(
+    "priv_to_pub",
+    path.join(__dirname, "../../../spec-test-cases/tests/general/phase0/bls/sign_msg/small"),
+    (testCase => {
+        const signature =  bls.sign(
+            Buffer.from(testCase.data.input.privkey.replace("0x", ""), "hex"),
+            Buffer.from(testCase.data.input.message.replace("0x", ""), "hex"),
+            padLeft(Buffer.from(testCase.data.input.domain.replace('0x', ''), 'hex'), 8)
+        );
+        return `0x${signature.toString("hex")}`;
+    }),
+    {
+        inputTypes: {
+            data: InputType.YAML,
+        },
+        getExpected: (testCase => testCase.data.output)
+    }
 );
