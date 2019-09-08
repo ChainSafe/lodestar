ce3dd7d13c31e14093c1d1739b4629858300623a
Gregory Markou <gregorymarkou@gmail.com>
2019-09-08 14:00:51 -0400

    remove constructor

--- packages/lodestar/src/opPool/modules/attestation.ts
+++ packages/lodestar/src/opPool/modules/attestation.ts
@@ -1,25 +1,18 @@
 import {Attestation, Slot, BeaconState} from "@chainsafe/eth2.0-types";
 
 import {OperationsModule} from "./abstract";
-import { AttestationRepository } from "../../db/api/beacon/repositories";
-import { IBeaconConfig } from "@chainsafe/eth2.0-config";
-import { getAttestationDataSlot } from "../../chain/stateTransition/util";
+import {AttestationRepository} from "../../db/api/beacon/repositories";
+import {IBeaconConfig} from "@chainsafe/eth2.0-config";
+import {getAttestationDataSlot} from "../../chain/stateTransition/util";
 
 export class AttestationOperations extends OperationsModule<Attestation> {
 
-    public readonly db: AttestationRepository;
-
-    public constructor(db: AttestationRepository) {
-        super(db)
-        this.db = db;
-    }
-
-    public async getValid(state: BeaconState, config: IBeaconConfig): Promise<Attestation[]> {
-        const attestations: Attestation[] = await this.getAll();
-        return attestations.filter((a: Attestation) => {
-            const attestationSlot: Slot = getAttestationDataSlot(config, state, a.data);
-            return (attestationSlot + config.params.MIN_ATTESTATION_INCLUSION_DELAY <= state.slot && 
-                state.slot <= attestationSlot + config.params.SLOTS_PER_EPOCH)
-        })
-    }
+  public async getValid(state: BeaconState, config: IBeaconConfig): Promise<Attestation[]> {
+    const attestations: Attestation[] = await this.getAll();
+    return attestations.filter((a: Attestation) => {
+      const attestationSlot: Slot = getAttestationDataSlot(config, state, a.data);
+      return (attestationSlot + config.params.MIN_ATTESTATION_INCLUSION_DELAY <= state.slot &&
+          state.slot <= attestationSlot + config.params.SLOTS_PER_EPOCH);
+    });
+  }
 }
