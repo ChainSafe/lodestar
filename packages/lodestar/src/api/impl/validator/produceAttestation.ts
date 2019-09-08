99671aef247ab34da88457007a1b3706b8c33bae
Marin Petrunić <marin.petrunic@gmail.com>
2019-09-08 13:52:49 -0400

    process block and attestations in queue

--- packages/lodestar/src/api/impl/validator/produceAttestation.ts
+++ packages/lodestar/src/api/impl/validator/produceAttestation.ts
@@ -4,10 +4,6 @@ import {IBeaconDb} from "../../../db/api";
 import {IBeaconChain} from "../../../chain";
 import {IBeaconConfig} from "@chainsafe/eth2.0-config";
 
-export function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 export async function produceAttestation(
   {config, db, chain}: {config: IBeaconConfig; db: IBeaconDb; chain: IBeaconChain},
   validatorPubKey: BLSPubkey,
@@ -15,7 +11,6 @@ export async function produceAttestation(
   slot: Slot
 ): Promise<Attestation|null> {
   try {
-    await sleep(3000);
     const [headState, headBlock, validatorIndex] = await Promise.all([
       chain.latestState,
       db.block.get(chain.forkChoice.head()),
