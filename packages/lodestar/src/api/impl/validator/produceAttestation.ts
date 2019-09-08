a8b1354d232ee41a0b40857006bb8097f1930762
Marin Petrunić <marin.petrunic@gmail.com>
2019-09-08 13:56:43 -0400

    clone state when producing attestation

--- packages/lodestar/src/api/impl/validator/produceAttestation.ts
+++ packages/lodestar/src/api/impl/validator/produceAttestation.ts
@@ -1,8 +1,9 @@
 import {assembleAttestation} from "../../../chain/factory/attestation";
-import {Attestation, BLSPubkey, Shard, Slot} from "@chainsafe/eth2.0-types";
+import {Attestation, BeaconState, BLSPubkey, Shard, Slot} from "@chainsafe/eth2.0-types";
 import {IBeaconDb} from "../../../db/api";
 import {IBeaconChain} from "../../../chain";
 import {IBeaconConfig} from "@chainsafe/eth2.0-config";
+import {clone} from "@chainsafe/ssz";
 
 export async function produceAttestation(
   {config, db, chain}: {config: IBeaconConfig; db: IBeaconDb; chain: IBeaconChain},
@@ -12,11 +13,11 @@ export async function produceAttestation(
 ): Promise<Attestation|null> {
   try {
     const [headState, headBlock, validatorIndex] = await Promise.all([
-      chain.latestState,
+      clone(chain.latestState, config.types.BeaconState),
       db.block.get(chain.forkChoice.head()),
       db.getValidatorIndex(validatorPubKey)
     ]);
-    return await assembleAttestation({config, db}, {...headState}, headBlock, validatorIndex, shard, slot);
+    return await assembleAttestation({config, db}, headState as unknown as BeaconState, headBlock, validatorIndex, shard, slot);
   } catch (e) {
     throw e;
   }
