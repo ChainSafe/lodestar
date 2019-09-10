1c738900e58238d36b7bb78cf34b1962978f8905
Gregory Markou <gregorymarkou@gmail.com>
2019-09-10 14:43:35 -0400

    add start script and comment out custom winston

--- packages/lodestar/src/logger/winston.ts
+++ packages/lodestar/src/logger/winston.ts
@@ -30,22 +30,22 @@ export class WinstonLogger implements ILogger {
               format: 'YYYY-MM-DD HH:mm:ss'
             }),
             format.printf((info) => {
-              const screenSize = process.stdout.columns;
-              const paddingBeforeInfo = 21;
+              // const screenSize = process.stdout.columns;
+              // const paddingBeforeInfo = 21;
               const paddingBetweenInfo = 30;
-              const paddingPreMsg = paddingBeforeInfo + paddingBetweenInfo + info.level.length - 19;
+              // const paddingPreMsg = paddingBeforeInfo + paddingBetweenInfo + info.level.length - 19;
 
               const infoString = (info.module || info.namespace || "");
               const infoPad = paddingBetweenInfo - infoString.length;
 
-              if (info.message.length + paddingPreMsg > screenSize) {
-                const max = screenSize - paddingPreMsg;
-                const p2 = info.message.length - max;
-                const msgs = [info.message.substring(0, max), info.message.substring(max)]; 
-                return (`${info.timestamp}  [${infoString.toUpperCase()}] ${info.level.padStart(infoPad)}: ${msgs[0]}\n${msgs[1].padStart(paddingPreMsg + 2)}`)
-              } else {
+              // if (info.message.length + paddingPreMsg > screenSize) {
+              //   const max = screenSize - paddingPreMsg;
+              //   const p2 = info.message.length - max;
+              //   const msgs = [info.message.substring(0, max), info.message.substring(max)]; 
+              //   return (`${info.timestamp}  [${infoString.toUpperCase()}] ${info.level.padStart(infoPad)}: ${msgs[0]}\n${msgs[1].padStart(paddingPreMsg + 2)}`)
+              // } else {
                 return `${info.timestamp}  [${infoString.toUpperCase()}] ${info.level.padStart(infoPad)}: ${info.message}`
-              }
+              // }
             })
           ),
           handleExceptions: true
---
+++ packages/lodestar/start-multi.sh
@@ -0,0 +1,51 @@
+nodes=$1
+TOTAL_VALIDATORS=$2
+range=$3
+WWW=$4
+BASE_URL=""
+
+# Split the configuration into an array
+VALIDATOR_ARRAY=(${range//,/ })
+VALIDATOR_ARRAY_LENGTH=${#VALIDATOR_ARRAY[@]}
+
+# Check for LAN
+if [ $WWW -eq "true" ]; then
+    echo "Using BASE_URL 0.0.0.0"
+    BASE_URL="0.0.0.0"
+else
+    echo "Using BASE_URL 127.0.0.1"
+    BASE_URL="127.0.0.1"
+fi
+
+# Sum the validator configuration
+v_count=0
+for i in "${VALIDATOR_ARRAY[@]}"; do 
+    v_count=$[$v_count+$i]
+done
+
+# Check that the sum of validator configuration is equal to total number of validators
+if [ $TOTAL_VALIDATORS -ne $v_count ]; then
+    echo "Total validator count not equal to set! Total:" $TOTAL_VALIDATORS "Set:" $v_count
+    exit
+fi
+
+# Ensure that the length of the validator configuration 
+if [ $nodes -ne $VALIDATOR_ARRAY_LENGTH ]; then
+    echo "Invalid validator set suppplied! Nodes:" $nodes "Validator Set:" $VALIDATOR_ARRAY_LENGTH 
+    exit
+fi
+
+# Set gensis start time
+START_TIME=$(date +%s)
+BASE_PORT=3060
+
+# The new-session needs to be created externally
+tmux new-session -d -s interop "./bin/lodestar interop -p minimal --db l0 -q $START_TIME,$TOTAL_VALIDATORS --multiaddrs /ip4/127.0.0.1/tcp/${BASE_PORT}0 -v ${VALIDATOR_ARRAY[0]} -r";
+for (( i=1; i<$nodes; i++ )) do
+    tmux split-window -v -t interop "./bin/lodestar interop -p minimal --db l$i -q $START_TIME,$TOTAL_VALIDATORS --multiaddrs /ip4/127.0.0.1/tcp/$BASE_PORT$i -v ${VALIDATOR_ARRAY[$i-1]},${VALIDATOR_ARRAY[$i]} -r"
+done
+
+# Change the layout
+tmux select-layout -t interop tiled
+
+tmux attach -t interop 
