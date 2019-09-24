4d8de68cf1ef9f4461979027e2a47681eefeea76
Marin Petrunić <marin.petrunic@gmail.com>
2019-09-24 22:29:07 +0200

    address PR comments

--- packages/eth2.0-utils/src/yaml/index.ts
+++ packages/eth2.0-utils/src/yaml/index.ts
@@ -1,14 +1,22 @@
-import {load} from "js-yaml";
+import {load, dump} from "js-yaml";
 import {readFileSync} from "fs";
 import {schema} from "./schema";
 // eslint-disable-next-line import/default
 import camelcaseKeys from "camelcase-keys";
 
 export function loadYamlFile(path: string): object {
+  return loadYaml(readFileSync(path, "utf8"));
+}
+
+export function loadYaml(yaml: string): object {
   return camelcaseKeys(
     load(
-      readFileSync(path, "utf8"),
+      yaml,
       {schema}
     ), {deep: true}
   );
+}
+
+export function dumpYaml(yaml: any): string {
+  return dump(yaml, {schema});
 }
