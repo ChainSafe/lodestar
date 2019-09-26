e623c16bd50880a6c3c3785689a36a44b2268aad
Marin Petrunić <marin.petrunic@gmail.com>
2019-09-26 12:06:31 +0200

    fix e2e tests

--- packages/lodestar/test/e2e/opPool/modules/deposit.test.ts
+++ packages/lodestar/test/e2e/opPool/modules/deposit.test.ts
@@ -1,7 +1,7 @@
 import {describe, it, beforeEach, afterEach} from "mocha";
 import {OpPool} from "../../../../src/opPool";
 import sinon from "sinon";
-import {EthersEth1Notifier} from "../../../../src/eth1";
+import {EthersEth1Notifier, IEth1Notifier} from "../../../../src/eth1";
 import {BeaconDb, IBeaconDb} from "../../../../src/db/api";
 import {LevelDbController} from "../../../../src/db/controller";
 import {config} from "@chainsafe/eth2.0-config/src/presets/minimal";
@@ -17,65 +17,65 @@ import {generateDeposit} from "../../../utils/deposit";
 
 describe("opPool - deposits", function () {
 
-    this.timeout(10000);
+  this.timeout(10000);
 
-    const sandbox = sinon.createSandbox();
-    const dbLocation = "./.__testdb";
-    const eth1 = sandbox.createStubInstance(EthersEth1Notifier);
+  const sandbox = sinon.createSandbox();
+  const dbLocation = "./.__testdb";
+  const eth1 = sandbox.createStubInstance(EthersEth1Notifier) as IEth1Notifier;
 
-    let opPool: OpPool, db: BeaconDb;
+  let opPool: OpPool, db: BeaconDb;
 
-    beforeEach(async function () {
-        const testDb = level(
-            dbLocation, {
-                keyEncoding: 'binary',
-                valueEncoding: 'binary',
-            });
-        db = new BeaconDb({
-            config,
-            controller: new LevelDbController(
-                {db: testDb, name: "test"},
-                {logger: sandbox.createStubInstance(WinstonLogger)}
-                )
-        });
-        opPool = new OpPool({}, {eth1, db});
-        await db.start();
+  beforeEach(async function () {
+    const testDb = level(
+      dbLocation, {
+        keyEncoding: "binary",
+        valueEncoding: "binary",
+      });
+    db = new BeaconDb({
+      config,
+      controller: new LevelDbController(
+        {db: testDb, name: "test"},
+        {logger: sandbox.createStubInstance(WinstonLogger)}
+      )
     });
+    opPool = new OpPool({}, {eth1, db, config});
+    await db.start();
+  });
 
-    afterEach(async function () {
-        await db.stop();
-        await promisify(leveldown.destroy)(dbLocation);
-    });
+  afterEach(async function () {
+    await db.stop();
+    await promisify(leveldown.destroy)(dbLocation);
+  });
 
-    it('should fetch deposit from range - upper and lower limit',async function () {
-        await Promise.all([
-            db.deposit.set(0, generateDeposit()),
-            db.deposit.set(2, generateDeposit()),
-            db.deposit.set(4, generateDeposit()),
-            db.deposit.set(6, generateDeposit())
-        ]);
-        const result = await opPool.deposits.getAllBetween(1, 7);
-        expect(result.length).to.be.equal(3);
-    });
+  it("should fetch deposit from range - upper and lower limit",async function () {
+    await Promise.all([
+      db.deposit.set(0, generateDeposit()),
+      db.deposit.set(2, generateDeposit()),
+      db.deposit.set(4, generateDeposit()),
+      db.deposit.set(6, generateDeposit())
+    ]);
+    const result = await opPool.deposits.getAllBetween(1, 7);
+    expect(result.length).to.be.equal(3);
+  });
 
-    it('should fetch deposit from range - upper limit only',async function () {
-        await Promise.all([
-            db.deposit.set(2, generateDeposit()),
-            db.deposit.set(4, generateDeposit()),
-            db.deposit.set(6, generateDeposit())
-        ]);
-        const result = await opPool.deposits.getAllBetween(null, 4);
-        expect(result.length).to.be.equal(1);
-    });
+  it("should fetch deposit from range - upper limit only",async function () {
+    await Promise.all([
+      db.deposit.set(2, generateDeposit()),
+      db.deposit.set(4, generateDeposit()),
+      db.deposit.set(6, generateDeposit())
+    ]);
+    const result = await opPool.deposits.getAllBetween(null, 4);
+    expect(result.length).to.be.equal(1);
+  });
 
-    it('should fetch deposit from range - lower limit only',async function () {
-        await Promise.all([
-            db.deposit.set(2, generateDeposit()),
-            db.deposit.set(4, generateDeposit()),
-            db.deposit.set(6, generateDeposit())
-        ]);
-        const result = await opPool.deposits.getAllBetween(0, null);
-        expect(result.length).to.be.equal(3);
-    });
+  it("should fetch deposit from range - lower limit only",async function () {
+    await Promise.all([
+      db.deposit.set(2, generateDeposit()),
+      db.deposit.set(4, generateDeposit()),
+      db.deposit.set(6, generateDeposit())
+    ]);
+    const result = await opPool.deposits.getAllBetween(0, null);
+    expect(result.length).to.be.equal(3);
+  });
 
 });
