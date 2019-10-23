bd93edd972c75c945e36dc0451f899fa0302572d
Marin Petrunić <marin.petrunic@gmail.com>
2019-10-23 15:00:23 +0200

    fix unit tests

--- packages/lodestar/test/unit/opPool/modules/attestation.test.ts
+++ packages/lodestar/test/unit/opPool/modules/attestation.test.ts
@@ -8,19 +8,22 @@ import {config} from "@chainsafe/eth2.0-config/lib/presets/minimal";
 import * as attestationUtils from "../../../../src/chain/stateTransition/util";
 import * as blockOperations from "../../../../src/chain/stateTransition/block/operations";
 import {generateState} from "../../../utils/state";
+import {AttestationDataRepository} from "../../../../src/db/api/beacon/repositories/attestationsData";
 
 describe("opPool - deposits", function () {
 
   const sandbox = sinon.createSandbox();
 
-  let dbStub: any, service: any, aggregateAttestationStub: any, processAttestationStub: any;
+  let dbStub: any, service: any, canAggregateStub: any, aggregateAttestationStub: any, processAttestationStub: any;
 
   beforeEach(function () {
     dbStub = {
-      attestation: sandbox.createStubInstance(AttestationRepository)
+      attestation: sandbox.createStubInstance(AttestationRepository),
+      attestationData: sandbox.createStubInstance(AttestationDataRepository),
     };
-    service = new AttestationOperations(dbStub.attestation, {config});
+    service = new AttestationOperations(dbStub.attestation, dbStub.attestationData, {config});
     aggregateAttestationStub = sandbox.stub(attestationUtils, "aggregateAttestation");
+    canAggregateStub = sandbox.stub(attestationUtils, "canBeAggregated");
     processAttestationStub = sandbox.stub(blockOperations, "processAttestation");
   });
 
@@ -28,13 +31,27 @@ describe("opPool - deposits", function () {
     sandbox.restore();
   });
 
+  it("should receive and store (no previous attestations", async function () {
+    const data = generateEmptyAttestation();
+    dbStub.attestationData.get.resolves([]);
+    dbStub.attestation.set.resolves();
+    await service.receive(data);
+    expect(dbStub.attestation.get.called).to.be.false;
+    expect(dbStub.attestationData.get.calledOnce).to.be.true;
+    expect(canAggregateStub.called).to.be.false;
+    expect(dbStub.attestation.setUnderRoot.calledOnce).to.be.true;
+  });
+
   it("should receive without aggregation", async function () {
     const data = generateEmptyAttestation();
 
     dbStub.attestation.get.resolves(null);
+    dbStub.attestationData.get.resolves([Buffer.alloc(32)]);
+    canAggregateStub.returns(false);
     dbStub.attestation.set.resolves();
     await service.receive(data);
     expect(dbStub.attestation.get.calledOnce).to.be.true;
+    expect(dbStub.attestationData.get.calledOnce).to.be.true;
     expect(aggregateAttestationStub.called).to.be.false;
     expect(dbStub.attestation.setUnderRoot.calledOnce).to.be.true;
   });
@@ -43,11 +60,16 @@ describe("opPool - deposits", function () {
     const data = generateEmptyAttestation();
 
     dbStub.attestation.get.resolves(data);
-    dbStub.attestation.set.resolves();
+    dbStub.attestationData.get.resolves([Buffer.alloc(32)]);
+    aggregateAttestationStub.resolves(data);
+    canAggregateStub.returns(true);
     await service.receive(data);
     expect(dbStub.attestation.get.calledOnce).to.be.true;
-    expect(aggregateAttestationStub.calledOnce).to.be.true;
-    expect(dbStub.attestation.setUnderRoot.calledOnce).to.be.true;
+    expect(dbStub.attestationData.get.calledOnce).to.be.true;
+    expect(aggregateAttestationStub.called).to.be.true;
+    //new and aggregated
+    expect(dbStub.attestation.setUnderRoot.calledTwice).to.be.true;
+    expect(dbStub.attestation.delete.calledOnce).to.be.true;
   });
 
   it("should get valid attestations", async function () {
--- packages/lodestar/test/unit/sync/regular.test.ts
+++ packages/lodestar/test/unit/sync/regular.test.ts
@@ -1,7 +1,7 @@
 import sinon from "sinon";
 import {expect} from "chai";
 import {config} from "@chainsafe/eth2.0-config/lib/presets/mainnet";
-
+import {afterEach, beforeEach, describe, it} from "mocha";
 import {BeaconChain} from "../../../src/chain";
 import {Libp2pNetwork} from "../../../src/network";
 import {AttestationOperations, OpPool} from "../../../src/opPool";
@@ -11,11 +11,12 @@ import {generateState} from "../../utils/state";
 import {generateEmptyBlock} from "../../utils/block";
 import {generateEmptyAttestation} from "../../utils/attestation";
 import {AttestationRepository, BlockRepository, StateRepository} from "../../../src/db/api/beacon/repositories";
+import {AttestationDataRepository} from "../../../src/db/api/beacon/repositories/attestationsData";
 
 describe("syncing", function () {
-  let sandbox = sinon.createSandbox();
+  const sandbox = sinon.createSandbox();
   let regularSync: RegularSync;
-  let chainStub, networkStub, dbStub, opPoolStub, logger;
+  let chainStub: any, networkStub: any, dbStub: any, opPoolStub: any, logger: any;
 
   beforeEach(() => {
     chainStub = sandbox.createStubInstance(BeaconChain);
@@ -23,6 +24,7 @@ describe("syncing", function () {
     dbStub = {
       block: sandbox.createStubInstance(BlockRepository),
       attestation: sandbox.createStubInstance(AttestationRepository),
+      attestationData: sandbox.createStubInstance(AttestationDataRepository),
       state: sandbox.createStubInstance(StateRepository),
     };
     opPoolStub = sandbox.createStubInstance(OpPool);
@@ -44,8 +46,8 @@ describe("syncing", function () {
   });
 
 
-  it('should able to receive block', async function () {
-    let block = generateEmptyBlock();
+  it("should able to receive block", async function () {
+    const block = generateEmptyBlock();
     dbStub.block.has.resolves(false);
     chainStub.receiveBlock.resolves(0);
     try {
@@ -57,8 +59,8 @@ describe("syncing", function () {
 
   });
 
-  it('should skip attestation - already exists', async function () {
-    let attestation = generateEmptyAttestation();
+  it("should skip attestation - already exists", async function () {
+    const attestation = generateEmptyAttestation();
     dbStub.attestation.has.resolves(true);
     try {
       await regularSync.receiveAttestation(attestation);
@@ -67,9 +69,9 @@ describe("syncing", function () {
     }
 
   });
-  it('should skip attestation - too old', async function () {
-    let attestation = generateEmptyAttestation();
-    let state = generateState();
+  it("should skip attestation - too old", async function () {
+    const attestation = generateEmptyAttestation();
+    const state = generateState();
     state.finalizedCheckpoint.epoch = 2;
     attestation.data.target.epoch = 1;
     dbStub.attestation.has.resolves(false);
@@ -81,14 +83,14 @@ describe("syncing", function () {
     }
 
   });
-  it('should receive attestation', async function () {
-    let attestation = generateEmptyAttestation();
-    let state = generateState();
+  it("should receive attestation", async function () {
+    const attestation = generateEmptyAttestation();
+    const state = generateState();
     state.finalizedCheckpoint.epoch = 1;
     attestation.data.target.epoch = 2;
     dbStub.attestation.has.resolves(false);
     dbStub.state.getLatest.resolves(state);
-    opPoolStub.attestations = new AttestationOperations(dbStub.attestation, {config});
+    opPoolStub.attestations = new AttestationOperations(dbStub.attestation, dbStub.attestationData, {config});
     dbStub.attestation.setUnderRoot.resolves(0);
     chainStub.receiveAttestation.resolves(0);
     try {
