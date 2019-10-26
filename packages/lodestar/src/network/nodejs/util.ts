5c36810e32b8f7dfffdce7e94d8f08384d23e16b
Tuyen <tuyen@wetrust.io>
2019-10-26 13:48:26 +0700

    Address comments

--- packages/lodestar/src/network/nodejs/util.ts
+++ packages/lodestar/src/network/nodejs/util.ts
@@ -6,6 +6,11 @@ import fs from "fs";
 import PeerId from "peer-id";
 // @ts-ignore
 import promisify from "promisify-es6";
+import LibP2p from "libp2p";
+import {NodejsNode} from ".";
+import {initializePeerInfo} from "../util";
+import {INetworkOptions} from "../options";
+import defaults from "../defaults";
 
 /**
  * Save a peer id to disk
@@ -21,4 +26,17 @@ export async function savePeerId(path: string, peerId: PeerId): Promise<void> {
 export async function loadPeerId(path: string): Promise<PeerId> {
   const data = await promisify(fs.readFile)(path);
   return await promisify(PeerId.createFromJSON)(JSON.parse(data));
+}
+
+/**
+ * 
+ * @param peerId Create an instance of NodejsNode asynchronously
+ */
+export async function createLibP2p(peerIdOrPromise: PeerId | Promise<PeerId>, 
+  network: Partial<INetworkOptions> = {}): Promise<LibP2p> {
+  const peerId = await Promise.resolve(peerIdOrPromise);
+  const multiaddrs = network.multiaddrs || defaults.multiaddrs;
+  const bootnodes = network.bootnodes || defaults.bootnodes;
+  const peerInfo = await initializePeerInfo(peerId, multiaddrs);
+  return new NodejsNode({peerInfo, bootnodes: bootnodes});
 }
