1a0927da0e062d34cda350b361de9ca478f40ded
Tuyen <tuyen@wetrust.io>
2019-11-09 17:42:28 +0700

    Add new spec test of 0.8.4

---
+++ packages/lodestar/test/spec/epoch_processing/rewardsAndPenalties/rewards_and_penalties_minimal.test.ts
@@ -0,0 +1,35 @@
+import {join} from "path";
+import {expect} from "chai";
+import {equals} from "@chainsafe/ssz";
+
+import {config} from "@chainsafe/eth2.0-config/lib/presets/minimal";
+import {processRewardsAndPenalties} from "@chainsafe/eth2.0-state-transition";
+import {BeaconState} from "@chainsafe/eth2.0-types";
+import {describeDirectorySpecTest, InputType} from "@chainsafe/eth2.0-spec-test-util/lib/single";
+import {StateTestCase} from "../../../utils/specTestTypes/stateTestCase";
+import {SPEC_TEST_LOCATION} from "../../../utils/specTestCases";
+
+describeDirectorySpecTest<StateTestCase, BeaconState>(
+  "rewards and penalties minimal",
+  join(SPEC_TEST_LOCATION, "tests/minimal/phase0/epoch_processing/rewards_and_penalties/pyspec_tests"),
+  (testcase) => {
+    const state = testcase.pre;
+    processRewardsAndPenalties(config, state)
+    return state;
+  },
+  {
+    inputTypes: {
+      pre: InputType.SSZ,
+      post: InputType.SSZ,
+    },
+    sszTypes: {
+      pre: config.types.BeaconState,
+      post: config.types.BeaconState,
+    },
+    getExpected: (testCase => testCase.post),
+    expectFunc: (testCase, expected, actual) => {
+      expect(equals(actual, expected, config.types.BeaconState)).to.be.true;
+    }
+  }
+);
+
