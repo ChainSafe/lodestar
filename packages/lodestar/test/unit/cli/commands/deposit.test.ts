9de013077a25a0d77eab0b09c765eed1505ceb0e
Tuyen <tuyen@wetrust.io>
2019-11-11 09:39:15 +0700

    Revert deposit.test.ts after rebase

--- packages/lodestar/test/unit/cli/commands/deposit.test.ts
+++ packages/lodestar/test/unit/cli/commands/deposit.test.ts
@@ -41,8 +41,7 @@ describe('[CLI] deposit', function() {
     expect(program.commands.length).to.be.equal(commandCount + 1);
   });
 
-  // TODO tuyennhv: this happens with other branches
-  it.skip('Should throw error if unable to connect to eth1 network', async () => {
+  it('Should throw error if unable to connect to eth1 network', async () => {
     const command = new DepositCommand();
     await expect(
       command.action(
