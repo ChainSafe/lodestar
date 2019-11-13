efb6bf0af68960e75f612151dbf4a86eced7b041
Marin Petrunić <marin.petrunic@gmail.com>
2019-11-13 19:53:04 +0100

    fix req resp tests

--- packages/lodestar/src/network/gossip/constants.ts
+++ packages/lodestar/src/network/gossip/constants.ts
@@ -2,7 +2,7 @@
  * @module network/gossip
  */
 
-import {CommitteeIndex, number64} from "@chainsafe/eth2.0-types";
+import {CommitteeIndex} from "@chainsafe/eth2.0-types";
 import {ATTESTATION_SUBNET_COUNT} from "../../constants";
 
 export enum GossipEvent {
