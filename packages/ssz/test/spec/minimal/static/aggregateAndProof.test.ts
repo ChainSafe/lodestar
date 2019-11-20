d8433c49d9ae8a846ed2133157781f4becf1166e
Marin Petrunić <marin.petrunic@gmail.com>
2019-11-20 12:54:26 +0100

    add aggregate and proof spec tests

---
+++ packages/ssz/test/spec/minimal/static/aggregateAndProof.test.ts
@@ -0,0 +1,56 @@
+import {describeDirectorySpecTest, InputType} from "@chainsafe/eth2.0-spec-test-util/lib/single";
+import {AggregateAndProof, Hash} from "@chainsafe/eth2.0-types";
+import {join} from "path";
+import {config} from "@chainsafe/eth2.0-config/lib/presets/minimal";
+import {expect} from "chai";
+import {IBaseSSZStaticTestCase} from "../../type";
+import {hashTreeRoot, serialize, signingRoot} from "../../../../src";
+import {TEST_CASE_LOCATION} from "../../../util/testCases";
+
+interface IResult {
+  root: Hash;
+  serialized: Buffer;
+}
+
+["ssz_lengthy", "ssz_max", "ssz_nil", "ssz_one", "ssz_random", "ssz_random_chaos", "ssz_zero"].forEach((caseName) => {
+
+  describeDirectorySpecTest<IBaseSSZStaticTestCase<AggregateAndProof>, IResult>(
+    `attestation ${caseName} minimal`,
+    join(
+      __dirname,
+      `${TEST_CASE_LOCATION}/tests/minimal/phase0/ssz_static/AggregateAndProof/${caseName}`
+    ),
+    (testcase: IBaseSSZStaticTestCase<AggregateAndProof>) => {
+      const serialized = serialize(testcase.serialized, config.types.AggregateAndProof);
+      const root = hashTreeRoot(testcase.serialized, config.types.AggregateAndProof);
+      return {
+        serialized,
+        root
+      };
+    },
+    {
+      // @ts-ignore
+      inputTypes: {
+        roots: InputType.YAML,
+        serialized: InputType.SSZ
+      },
+      // @ts-ignore
+      sszTypes: {
+        serialized: config.types.AggregateAndProof,
+      },
+      getExpected: (testCase => {
+        return {
+          root: Buffer.from(testCase.roots.root.replace("0x", ""), "hex"),
+          // @ts-ignore
+          serialized: testCase.serialized_raw
+        };
+      }),
+      expectFunc: (testCase, expected, actual) => {
+        expect(expected.serialized.equals(actual.serialized)).to.be.true;
+        expect(expected.root.equals(actual.root)).to.be.true;
+      },
+      unsafeInput: true,
+    }
+  );
+
+});
