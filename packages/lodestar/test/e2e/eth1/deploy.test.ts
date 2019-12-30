491b825a307986ed976ee541f3ce601e0e741711
Tuyen <tuyen@wetrust.io>
2019-12-30 17:13:27 +0700

    Try adding timeout for deposit test as it always pass in local

--- packages/lodestar/test/e2e/eth1/deploy.test.ts
+++ packages/lodestar/test/e2e/eth1/deploy.test.ts
@@ -67,7 +67,7 @@ describe("Eth1Notifier - using deployed contract", () => {
 
 
     await wallet.createValidatorDeposit(depositContractAddress, ethers.utils.parseEther("32.0"));
-    await sleep(300);
+    await sleep(600);
     assert(cb.calledOnce, "deposit event did not fire");
   });
 
