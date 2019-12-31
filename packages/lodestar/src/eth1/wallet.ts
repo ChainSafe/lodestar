c6f8c42f61862981ac09753efa1af2013d9d3091
Tuyen <tuyen@wetrust.io>
2019-12-31 17:40:36 +0700

    Add logs to wallet.ts

--- packages/lodestar/src/eth1/wallet.ts
+++ packages/lodestar/src/eth1/wallet.ts
@@ -49,13 +49,22 @@ export class Eth1Wallet {
   public async createValidatorDeposit(address: string, value: BigNumber): Promise<string> {
     const amount = BigInt(value.toString()) / 1000000000n;
 
+    console.log("createValidatorDeposit 000");
     const contract = new ethers.Contract(address, this.contractAbi, this.wallet);
+    console.log("createValidatorDeposit 111");
+
     const privateKey = PrivateKey.random();
+    console.log("createValidatorDeposit 222");
+
     const pubkey = privateKey.toPublicKey().toBytesCompressed();
+    console.log("createValidatorDeposit 333");
+
     const withdrawalCredentials = Buffer.concat([
       this.config.params.BLS_WITHDRAWAL_PREFIX_BYTE,
       hash(pubkey).slice(1),
     ]);
+
+    console.log("createValidatorDeposit 444");
 
     // Create deposit data
     const depositData: DepositData = {
@@ -65,11 +74,16 @@ export class Eth1Wallet {
       signature: Buffer.alloc(96)
     };
 
+    console.log("createValidatorDeposit 555");
+
+
     depositData.signature = bls.sign(
       privateKey.toBytes(),
       signingRoot(this.config.types.DepositData, depositData),
       Buffer.from([0, 0, 0, DomainType.DEPOSIT])
     );
+    console.log("createValidatorDeposit 666");
+
     // Send TX
     try {
       const tx: ContractTransaction = await contract.deposit(
@@ -78,7 +92,11 @@ export class Eth1Wallet {
         depositData.signature,
         hashTreeRoot(this.config.types.DepositData, depositData),
         {value});
+      console.log("createValidatorDeposit 777");
+
       await tx.wait();
+      console.log("createValidatorDeposit 888");
+
       return tx.hash || "";
     } catch(e) {
       this.logger.error(e.message);
