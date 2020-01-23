c553cb57b5359c4a0ffc8f0c6bcba740b9042008
Cayman <caymannava@gmail.com>
2020-01-23 10:22:14 -0600

    Add new ssz static tests, update ssz static tests

--- packages/spec-test-runner/test/spec/ssz/util/testCases.ts
+++ packages/spec-test-runner/test/spec/ssz/util/testCases.ts
@@ -1,30 +1,36 @@
 import {describeDirectorySpecTest, InputType, safeType} from "@chainsafe/eth2.0-spec-test-util";
-import {bytes32, IBeaconSSZTypes} from "@chainsafe/eth2.0-types";
+import {Bytes32, IBeaconSSZTypes} from "@chainsafe/eth2.0-types";
 import {join} from "path";
 import {config} from "@chainsafe/eth2.0-config/lib/presets/minimal";
 import {expect} from "chai";
-import {hashTreeRoot, serialize, parseType} from "@chainsafe/ssz";
 
 import {IBaseSSZStaticTestCase} from "../type";
 
 export const TEST_CASE_LOCATION = "../../../../../../node_modules/@chainsafe/eth2-spec-tests";
 
 interface IResult {
-  root: bytes32;
-  serialized: Buffer;
+  root: Bytes32;
+  serialized: Uint8Array;
 }
 
 export function testStatic(type: keyof IBeaconSSZTypes): void {
-  const Type = safeType(parseType(config.types[type]));
-
-  ["ssz_lengthy", "ssz_max", "ssz_nil", "ssz_one", "ssz_random", "ssz_random_chaos", "ssz_zero"].forEach((caseName) => {
+  const Type = safeType(config.types[type]);
+  [
+    "ssz_lengthy",
+    "ssz_max",
+    "ssz_one",
+    "ssz_nil",
+    "ssz_random",
+    "ssz_random_chaos",
+    "ssz_zero",
+  ].forEach((caseName) => {
 
     describeDirectorySpecTest<IBaseSSZStaticTestCase<any>, IResult>(
       `SSZ - ${type} ${caseName} minimal`,
       join(__dirname, `${TEST_CASE_LOCATION}/tests/minimal/phase0/ssz_static/${type}/${caseName}`),
       (testcase) => {
-        const serialized = serialize(Type, testcase.serialized);
-        const root = hashTreeRoot(Type, testcase.serialized);
+        const serialized = Type.serialize(testcase.serialized);
+        const root = Type.hashTreeRoot(testcase.serialized);
         return {
           serialized,
           root
@@ -48,9 +54,18 @@ export function testStatic(type: keyof IBeaconSSZTypes): void {
           };
         }),
         expectFunc: (testCase, expected, actual) => {
-          expect(expected.serialized.equals(actual.serialized)).to.be.true;
-          expect(expected.root.equals(actual.root)).to.be.true;
+          if (true && (!expected.serialized.equals(actual.serialized) || !expected.root.equals(actual.root))) {
+            console.log("testCase", testCase)
+            console.log("serialize expected", expected.serialized.toString('hex'))
+            console.log("serialize actual  ", Buffer.from(actual.serialized).toString('hex'))
+            console.log("hashTreeRoot expected", expected.root.toString('hex'))
+            console.log("hashTreeRoot actual  ", Buffer.from(actual.root).toString('hex'))
+          }
+          expect(expected.serialized.equals(actual.serialized), "incorrect serialize").to.be.true;
+          expect(expected.root.equals(actual.root), "incorrect hashTreeRoot").to.be.true;
         },
+        //shouldSkip: (a, b, i) => i !== 0,
+        //shouldSkip: (a, b, i) => b !== 'case_10',
       }
     );
   });
