104a31b1ac044657c7687854aa2a7b9ffa1197b2
Cayman <caymannava@gmail.com>
2020-01-24 21:54:12 -0600

    Add a few comments

--- packages/ssz/src/backings/tree/abstract.ts
+++ packages/ssz/src/backings/tree/abstract.ts
@@ -3,37 +3,99 @@ import {Node, TreeBacking, Gindex, LeafNode, countToDepth, toGindexBitstring} fr
 import {CompositeType} from "../../types";
 import {isBackedValue, BackingType} from "..";
 
+/**
+ * The ITreeBackedValue interface represents the public API that attach to tree-backed Proxy objects
+ *
+ * This is an alternative way of calling methods of the attached TreeHandler
+ */
 export interface ITreeBackedValue<T extends object> {
   type(): CompositeType<T>;
 
-  clone(): TreeBackedValue<T>;
+  /**
+   * Equality
+   *
+   * If both values are tree-backed, use equality by merkle root, else use structural equality
+   */
   equals(other: T): boolean;
+  /**
+   * Clone / Copy
+   */
+  clone(): TreeBackedValue<T>;
 
+  /**
+   * Serialized byte length
+   */
   size(): number;
-  serialize(): Uint8Array;
+  /**
+   * Low-level serialization
+   *
+   * Serializes to a pre-allocated Uint8Array
+   */
   toBytes(output: Uint8Array, offset: number): number;
+  /**
+   * Serialization
+   */
+  serialize(): Uint8Array;
 
+  /**
+   * Merkleization
+   */
   hashTreeRoot(): Uint8Array;
 
-  backingHandler(): TreeHandler<T>;
+  // Backing
+
+  /**
+   * The merkle tree backing
+   */
   backing(): TreeBacking;
+  /**
+   * The attached TreeHandler
+   */
+  backingHandler(): TreeHandler<T>;
+  /**
+   * The BackingType associated with the merkle tree backing
+   */
   backingType(): BackingType;
 }
 
+/**
+ * Since tree-backed values return tree-backed-values from non-basic-type property getters,
+ * we need this type to recursively wrap subobjects (non-basic values) as tree-backed values.
+ */
 export type TreeBackedValueify<T> = {
   [P in keyof T]: T[P] extends object ? TreeBackedValue<T[P]> : T[P];
 };
 
+/**
+ * A tree-backed value has the ITreeBackedValue public API as well as tree-backed getters/setters
+ */
 export type TreeBackedValue<T extends object> = ITreeBackedValue<T> & TreeBackedValueify<T> & T;
 
+/**
+ * Every property of a 'basic' tree-backed value is of a basic type, ie not a tree-backed value
+ */
 export type PropOfBasicTreeBackedValue<T extends object, V extends keyof T> = T[V];
 
+/**
+ * Every property of a 'composite' tree-backed value is of a composite type, ie a tree-backed value
+ */
 export type PropOfCompositeTreeBackedValue<T extends object, V extends keyof T> =
   T[V] extends object ? TreeBackedValue<T[V]> : never;
 
 export type PropOfTreeBackedValue<T extends object, V extends keyof T> =
   PropOfBasicTreeBackedValue<T, V> | PropOfCompositeTreeBackedValue<T, V>;
 
+/**
+ * A TreeHandler instance handles tree-backed-specific logic.
+ * It is a property of its associated CompositeType, and vice-versa.
+ * It is also attached to each TreeBackedValue as its ES6 Proxy handler
+ *
+ * These methods can be used in both contexts (when part of ITreeBackedValue):
+ * eg:
+ *   Type.tree.hashTreeRoot(treeBacking)
+ *   and
+ *   treeBackedValue.hashTreeRoot()
+ */
 export class TreeHandler<T extends object> implements ProxyHandler<T> {
   _type: CompositeType<T>;
   constructor(type: CompositeType<T>) {
@@ -42,33 +104,56 @@ export class TreeHandler<T extends object> implements ProxyHandler<T> {
   type(): CompositeType<T> {
     return this._type;
   }
+  /**
+   * The merkle tree backing
+   */
   backing(target: TreeBacking): TreeBacking {
     return target;
   }
+  /**
+   * The attached TreeHandler
+   */
   backingHandler(): this {
     return this;
   }
+  /**
+   * The BackingType associated with the merkle tree backing
+   */
   backingType(): BackingType {
     return BackingType.tree;
   }
+  /**
+   * Default merkle tree
+   */
   defaultNode(): Node {
     throw new Error("Not implemented");
   }
+  /**
+   * Default constructor
+   */
   defaultValue(): TreeBackedValue<T> {
     return this.createBackedValue(new TreeBacking(this.defaultNode()));
   }
   createValue(value: T): TreeBackedValue<T> {
     throw new Error("Not implemented");
   }
+  /**
+   * Return an ES6 Proxy-wrapped tree backing
+   */
   createBackedValue(target: TreeBacking): TreeBackedValue<T> {
     return new Proxy(target, this) as TreeBackedValue<T>;
   }
+  /**
+   * Clone / copy
+   */
   clone(target: TreeBacking): TreeBackedValue<T> {
     return this.createBackedValue(new TreeBacking(target.node));
   }
-  size(target: TreeBacking): number {
-    return this._type.structural.size(this.createBackedValue(target));
-  }
+  /**
+   * Equality
+   *
+   * If both values are tree-backed, use equality by merkle root, else use structural equality
+   */
   equals(target: TreeBacking, other: TreeBackedValue<T>): boolean {
     if (isBackedValue(other) && other.backingType() === this.backingType()) {
       const aRoot = this.hashTreeRoot(target);
@@ -77,18 +162,50 @@ export class TreeHandler<T extends object> implements ProxyHandler<T> {
     }
     return this._type.structural.equals(this.createBackedValue(target), other);
   }
+
+  // Serialization
+
+  /**
+   * Serialized byte length
+   */
+  size(target: TreeBacking): number {
+    return this._type.structural.size(this.createBackedValue(target));
+  }
+  /**
+   * Low-level deserialization
+   */
+  fromBytes(data: Uint8Array, start: number, end: number): TreeBackedValue<T> {
+    throw new Error("Not implemented");
+  }
+  /**
+   * Deserialization
+   */
   deserialize(data: Uint8Array): TreeBackedValue<T> {
     throw new Error("Not implemented");
   }
+  /**
+   * Low-level serialization
+   *
+   * Serializes to a pre-allocated Uint8Array
+   */
+  toBytes(target: TreeBacking, output: Uint8Array, offset: number): number {
+    return this._type.structural.toBytes(this.createBackedValue(target), output, offset);
+  }
+  /**
+   * Serialization
+   */
   serialize(target: TreeBacking): Uint8Array {
     const output = new Uint8Array(this.size(target));
     this.toBytes(target, output, 0);
     return output;
   }
-  toBytes(target: TreeBacking, output: Uint8Array, offset: number): number {
-    return this._type.structural.toBytes(this.createBackedValue(target), output, offset);
-  }
   _depth: number;
+
+  // Merkleization
+
+  /**
+   * The depth of the merkle tree
+   */
   depth(): number {
     if (!this._depth) {
       this._depth = countToDepth(BigInt(this._type.chunkCount()));
@@ -107,15 +224,28 @@ export class TreeHandler<T extends object> implements ProxyHandler<T> {
   setBackingAtChunk(target: TreeBacking, index: number, value: TreeBacking): void {
     target.set(this.gindexOfChunk(target, index), value.node);
   }
+  /**
+   * Merkleization
+   */
   hashTreeRoot(target: TreeBacking): Uint8Array {
     return target.node.merkleRoot;
   }
+
+  /**
+   * Return a ITreeBackedValue method, to be called using the ITreeBackedValue interface
+   */
   getMethod<V extends keyof ITreeBackedValue<T>>(target: TreeBacking, methodName: V): ITreeBackedValue<T>[V] {
     return (this as any)[methodName].bind(this, target);
   }
+  /**
+   * Return a property of T, either a subtree TreeBackedValue or a primitive, of a basic type
+   */
   getProperty(target: TreeBacking, property: keyof T): PropOfTreeBackedValue<T, keyof T> {
     throw new Error("Not implemented");
   }
+  /**
+   * ES6 Proxy trap to get a ITreeBackedValue method or property of T
+   */
   get(target: any, property: PropertyKey): PropOfTreeBackedValue<T, keyof T> | ITreeBackedValue<T>[keyof ITreeBackedValue<T>] {
     if (property in this) {
       return this.getMethod(target, property as keyof ITreeBackedValue<T>);
@@ -123,6 +253,9 @@ export class TreeHandler<T extends object> implements ProxyHandler<T> {
       return this.getProperty(target, property as keyof T);
     }
   }
+  /**
+   * ES6 Proxy trap to set a property of T
+   */
   set(target: any, property: PropertyKey, value: any): boolean {
     throw new Error("Not implemented");
   }
--- packages/ssz/src/types/abstract.ts
+++ packages/ssz/src/types/abstract.ts
@@ -1,55 +1,156 @@
+/**
+ * SSZ is a type system that defines:
+ * - efficient serialization / deserialization
+ * - stable merkleization
+ * - default constructor
+ *
+ * Along with these standardized operations, we provide:
+ * - equality
+ * - valid value assertion
+ * - copy / clone
+ * - constructor from partially formed value
+ * - serialized byte length (for serialization)
+ * - chunk count (for merkleization)
+ *
+ * This library operates on values of several kinds of 'backings', or underlying representations of data.
+ * Each backing has runtime tradeoffs for the above operations that arise from the nature of the underlying
+ * representation. Effort has been made to minimize the differences between backings for the core API, which
+ * includes the above operations, property getter/setters, and iteration (value iteration for vectors/lists
+ * and enumerable key iteration for containers).
+ *
+ * We support the following backings, which correspond to the core operations of serialization and merkleization:
+ *
+ * - Structural - This backing has a native javascript type representation.
+ *     Containers are constructed as js Objects, vectors and lists as Arrays (or TypedArrays)
+ *     Within operations, property access is performed using js getter notation, with gets
+ *     corresponding to the structure of the value's type. Because structural non-constructor operations do not
+ *     assume the underlying representation of values, all backings can be operated on in this context.
+ *
+ * - Tree - This backing has an immutable merkle tree representation.
+ *     The data is always represented as a tree, and within operations, the tree
+ *     structure is harnessed as much as possible. Property getters return subtrees except for basic types,
+ *     when the native value corresponding th that type is returned.
+ *     Values backed by a tree are wrapped in an ES6 Proxy object to provide a convenient, 'structural' interface
+ *     for property getters/setters.
+ *
+ * - Serialized - This backing has a byte array representation.
+ *     The data is always represented as a Uint8Array, and within operations,
+ *     the serialized structure is harnessed as much as possible.
+ *     Property getters return sub-arrays except for basic types, when the native value
+ *     corresponding to that type is returned.
+ *     Values backed by an array are wrapped in an ES6 Proxy object to provide a convenient, 'structural' interface
+ *     for property getters/setters.
+ */
 import {
   BackedValue, isBackedValue,
   StructuralHandler, TreeHandler,
 } from "../backings";
 
+/**
+ * A Type is either a BasicType or a CompositeType.
+*/
 export type Type<T> = BasicType<T> | (T extends object ? CompositeType<T>: never);
 
+/**
+ * A BasicType is a terminal type, which has no flexibility in its representation.
+ *
+ * It is serialized as, at maximum, 32 bytes and merkleized as, at maximum, a single chunk
+ */
 export class BasicType<T> {
-  isVariableSize(): boolean {
-    return false;
-  }
   isBasic(): this is BasicType<T> {
     return true;
   }
-  size(): number {
+
+  /**
+   * Valid value assertion
+   */
+  assertValidValue(value: any): asserts value is T {
     throw new Error("Not implemented");
   }
-  assertValidValue(value: any): void {
+
+  /**
+   * Default constructor
+   */
+  defaultValue(): T {
     throw new Error("Not implemented");
   }
+
+  /**
+   * Clone / copy
+   */
   clone(value: T): T {
     return value;
   }
+
+  /**
+   * Constructor for partially formed input
+   *
+   * For basic types, there is no partially formed input
+   */
+  createValue(value: any): T {
+    this.assertValidValue(value);
+    return value;
+  }
+
+  /**
+   * Equality
+   */
   equals(value1: T, value2: T): boolean {
     this.assertValidValue(value1);
     this.assertValidValue(value2);
     return value1 === value2;
   }
-  toBytes(value: T, output: Uint8Array, offset: number): number {
+
+  // Serialization / Deserialization
+
+  /**
+   * Check if type has a variable number of elements (or subelements)
+   *
+   * For basic types, this is always false
+   */
+  isVariableSize(): boolean {
+    return false;
+  }
+  /**
+   * Serialized byte length
+   */
+  size(): number {
     throw new Error("Not implemented");
   }
+
+  /**
+   * Low-level deserialization
+   */
   fromBytes(data: Uint8Array, offset: number): T {
     throw new Error("Not implemented");
   }
+  /**
+   * Deserialization
+   */
   deserialize(data: Uint8Array): T {
     return this.fromBytes(data, 0);
   }
-  defaultValue(): T {
+
+  /**
+   * Low-level serialization
+   *
+   * Serializes to a pre-allocated Uint8Array
+   */
+  toBytes(value: T, output: Uint8Array, offset: number): number {
     throw new Error("Not implemented");
   }
-  createValue(value: any): T {
-    this.assertValidValue(value);
-    return value;
-  }
+  /**
+   * Serialization
+   */
   serialize(value: T): Uint8Array {
     const output = new Uint8Array(this.size());
     this.toBytes(value, output, 0);
     return output;
   }
-  toBytes(value: T, output: Uint8Array, offset: number): number {
-    throw new Error("Not implemented");
-  }
+
+  /**
+   * Merkleization
+   */
   hashTreeRoot(value: T): Uint8Array {
     const output = new Uint8Array(32);
     this.toBytes(value, output, 0);
@@ -57,29 +158,28 @@ export class BasicType<T> {
   }
 }
 
+/**
+ * A CompositeType is a type containing other types, and is flexible in its representation.
+ *
+ */
 export class CompositeType<T extends object> {
   structural: StructuralHandler<T>;
   tree: TreeHandler<T>;
 
-  indexElementType(index: number): Type<any> {
-    throw new Error("Not implemented");
-  }
-  isVariableSize(): boolean {
-    throw new Error("Not implemented");
-  }
   isBasic(): this is BasicType<T> {
     return false;
   }
-  size(value: BackedValue<T> | T): number {
-    if (isBackedValue(value)) {
-      return value.size();
-    } else {
-      return this.structural.size(value);
-    }
-  }
-  assertValidValue(value: any): void {
+
+  /**
+   * Valid value assertion
+   */
+  assertValidValue(value: any): asserts value is T {
     this.structural.assertValidValue(value);
   }
+
+  /**
+   * Equality
+   */
   equals(value1: BackedValue<T> | T, value2: BackedValue<T> | T): boolean {
     if (isBackedValue(value1) && isBackedValue(value2)) {
       return value1.equals(value2);
@@ -87,9 +187,17 @@ export class CompositeType<T extends object> {
       return this.structural.equals(value1, value2);
     }
   }
+
+  /**
+   * Default constructor
+   */
   defaultValue(): T {
     return this.structural.defaultValue();
   }
+
+  /**
+   * Clone / copy
+   */
   clone(value: BackedValue<T> | T): BackedValue<T> | T {
     if (isBackedValue(value)) {
       return value.clone() as BackedValue<T>;
@@ -97,20 +205,51 @@ export class CompositeType<T extends object> {
       return this.structural.clone(value);
     }
   }
+
+  /**
+   * Constructor for partially formed input
+   */
   createValue(value: any): T {
     throw new Error("Not implemented");
   }
-  // serialize related
+
+  // Serialization / Deserialization
+
+  /**
+   * Check if type has a variable number of elements (or subelements)
+   */
+  isVariableSize(): boolean {
+    throw new Error("Not implemented");
+  }
+  /**
+   * Serialized byte length
+   */
+  size(value: BackedValue<T> | T): number {
+    if (isBackedValue(value)) {
+      return value.size();
+    } else {
+      return this.structural.size(value);
+    }
+  }
+
+  /**
+   * Low-level deserialization
+   */
+  fromBytes(data: Uint8Array, start: number, end: number): T {
+    throw new Error("Not implemented");
+  }
+  /**
+   * Deserialization
+   */
   deserialize(data: Uint8Array): T {
     return this.structural.deserialize(data);
   }
-  serialize(value: BackedValue<T> | T): Uint8Array {
-    if (isBackedValue(value)) {
-      return value.serialize();
-    } else {
-      return this.structural.serialize(value);
-    }
-  }
+
+  /**
+   * Low-level serialization
+   *
+   * Serializes to a pre-allocated Uint8Array
+   */
   toBytes(value: BackedValue<T> | T, output: Uint8Array, offset: number): number {
     if (isBackedValue(value)) {
       return value.toBytes(output, offset);
@@ -118,32 +257,27 @@ export class CompositeType<T extends object> {
       return this.structural.toBytes(value, output, offset);
     }
   }
-  // hash tree root related
+  /**
+   * Serialization
+   */
+  serialize(value: BackedValue<T> | T): Uint8Array {
+    if (isBackedValue(value)) {
+      return value.serialize();
+    } else {
+      return this.structural.serialize(value);
+    }
+  }
+
+  // Merkleization
+
+  /**
+   * Return the number of leaf chunks to be merkleized
+   */
   chunkCount(): number {
     throw new Error("Not implemented");
   }
-  /*
-  nonzeroChunkCount(value: BackedValue<T> | T): number {
-    if (isBackedValue(value)) {
-      return value.nonzeroChunkCount();
-    } else {
-      return this.structural.nonzeroChunkCount(value);
-    }
-  }
-  chunk(value: BackedValue<T> | T, index: number): Uint8Array {
-    if (isBackedValue(value)) {
-      return value.chunk(index);
-    } else {
-      return this.structural.chunk(value, index);
-    }
-  }
-  chunks(value: BackedValue<T> | T): Iterable<Uint8Array> {
-    if (isBackedValue(value)) {
-      return value.chunks();
-    } else {
-      return this.structural.chunks(value);
-    }
-  }
+  /**
+   * Merkleization
    */
   hashTreeRoot(value: BackedValue<T> | T): Uint8Array {
     if (isBackedValue(value)) {
