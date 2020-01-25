b9c0ff488aca96f9fb585983de07ee3ed395e646
Cayman <caymannava@gmail.com>
2020-01-25 11:58:36 -0600

    Add comment

--- packages/ssz/src/backings/backedValue.ts
+++ packages/ssz/src/backings/backedValue.ts
@@ -6,6 +6,13 @@ export enum BackingType {
   serialized = "serialized",
 }
 
+/**
+ * A BackedValue is a value that is backed by a non-structural type
+ *
+ * It is implemented as an ES6 Proxy object that provides
+ * - convenient access to the structural properties corresponding to its type
+ * - additional methods for backing-specific implementations of ssz operations
+ */
 export type BackedValue<T extends object> = TreeBackedValue<T>;
 
 export function isBackedValue<T extends object>(value: any): value is BackedValue<T> {
