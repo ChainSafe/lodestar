27542ad2af403a85be9a580bb76da4b64ef20dbe
Cayman <caymannava@gmail.com>
2020-01-29 17:39:17 -0600

    Add byteVector-specific deserialization

--- packages/ssz/src/backings/structural/byteVector.ts
+++ packages/ssz/src/backings/structural/byteVector.ts
@@ -11,6 +11,15 @@ export class ByteVectorStructuralHandler extends BasicVectorStructuralHandler<Ve
   defaultValue(): Vector<number> {
     return new Uint8Array(this._type.length);
   }
+  fromBytes(data: Uint8Array, start: number, end: number): Vector<number> {
+    const length = end - start;
+    if (length !== this._type.length) {
+      throw new Error(`Invalid deserialized vector length: expected ${this._type.length}, actual: ${length}`);
+    }
+    const value = new Uint8Array(length);
+    value.set(data.slice(start, end));
+    return value;
+  }
   toBytes(value: Vector<number>, output: Uint8Array, offset: number): number {
     output.set(value, offset);
     return offset + this._type.length;
