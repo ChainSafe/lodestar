a9705bac68d7cf0d36aecd425942825b6ecaf230
Marin Petrunić <marin.petrunic@gmail.com>
2020-02-05 20:35:31 +0100

    add task for joining random interop subnets

--- packages/lodestar/src/tasks/index.ts
+++ packages/lodestar/src/tasks/index.ts
@@ -8,12 +8,17 @@ import {IBeaconDb} from "../db/api";
 import {IBeaconChain} from "../chain";
 import {Checkpoint} from "@chainsafe/eth2.0-types";
 import {ArchiveBlocksTask} from "./tasks/archiveBlocks";
-import {ILogger} from  "@chainsafe/eth2.0-utils/lib/logger";
+import {ILogger} from "@chainsafe/eth2.0-utils/lib/logger";
+import {Sync} from "../sync";
+import {InteropSubnetsJoiningTask} from "./tasks/interopSubnetsJoiningTask";
+import {INetwork} from "../network";
 
 export interface ITasksModules {
   db: IBeaconDb;
   logger: ILogger;
   chain: IBeaconChain;
+  sync: Sync;
+  network: INetwork;
 }
 
 /**
@@ -23,11 +28,10 @@ export interface ITasksModules {
 export class TasksService implements IService {
 
   private readonly config: IBeaconConfig;
-
   private readonly db: IBeaconDb;
-
   private readonly chain: IBeaconChain;
-
+  private readonly sync: Sync;
+  private readonly network: INetwork;
   private readonly logger: ILogger;
 
   public constructor(config: IBeaconConfig, modules: ITasksModules) {
@@ -35,18 +39,26 @@ export class TasksService implements IService {
     this.db = modules.db;
     this.chain = modules.chain;
     this.logger = modules.logger;
+    this.sync = modules.sync;
+    this.network = modules.network;
   }
 
   public async start(): Promise<void> {
     this.chain.on("finalizedCheckpoint", this.handleFinalizedCheckpointChores);
+    this.sync.on("regularSyncStarted", this.handleRegularSyncStartedTasks);
   }
 
   public async stop(): Promise<void> {
     this.chain.removeListener("finalizedCheckpoint", this.handleFinalizedCheckpointChores);
+    this.sync.removeListener("regularSyncStarted", this.handleRegularSyncStartedTasks);
   }
 
   private handleFinalizedCheckpointChores = async (finalizedCheckpoint: Checkpoint): Promise<void> => {
     new ArchiveBlocksTask(this.config, {db: this.db, logger: this.logger}, finalizedCheckpoint).run();
+  };
+  
+  private handleRegularSyncStartedTasks = async (): Promise<void> => {
+    new InteropSubnetsJoiningTask(this.config, {network: this.network}); 
   };
 
 }
