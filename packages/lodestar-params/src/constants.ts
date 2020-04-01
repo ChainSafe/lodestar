5b8f38728ccbd904cc3ae7698e0f982f961a78bb
Tuyen Nguyen <vutuyen2636@gmail.com>
2020-04-02 04:53:06 +0700

    Fix unit tests

--- packages/lodestar-params/src/constants.ts
+++ packages/lodestar-params/src/constants.ts
@@ -1,3 +1,2 @@
-export const GENESIS_FORK_VERSION = Buffer.alloc(4);
 export const GENESIS_START_SHARD = 0;
 export const BASE_REWARDS_PER_EPOCH = 4;
--- packages/lodestar-params/src/presets/minimal.ts
+++ packages/lodestar-params/src/presets/minimal.ts
@@ -5,8 +5,4 @@ import {convertTypes, schema} from "../utils";
 import {typeMap} from "../types";
 import {load} from "js-yaml";
 
-
-// TODO: fix this
-const params2 = convertTypes(load(minimalYaml, {schema}), typeMap);
-params2.GENESIS_FORK_VERSION[3] = 1;
-export const params = params2;
+export const params = convertTypes(load(minimalYaml, {schema}), typeMap);
