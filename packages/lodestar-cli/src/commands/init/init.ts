f4fff31bcc8a41f55c8caaa223b6c55485486440
Cayman <caymannava@gmail.com>
2020-04-22 16:38:35 -0500

    Clarify config is beacon-specific

--- packages/lodestar-cli/src/commands/init/init.ts
+++ packages/lodestar-cli/src/commands/init/init.ts
@@ -1,7 +1,7 @@
 import * as path from "path";
 
 import {mkdir} from "../../lodestar/util";
-import {initConfig} from "../../config";
+import {initBeaconConfig} from "../../config";
 import {initPeerId, initEnr, readPeerId} from "../../network";
 
 /**
@@ -12,8 +12,8 @@ export async function init(lodestarRoot: string): Promise<void> {
   await mkdir(lodestarRoot);
   // initialize network directory
   await mkdir(path.join(lodestarRoot, "network"));
-  // initialize main configuration file
-  await initConfig(path.join(lodestarRoot, "lodestar.config"));
+  // initialize beacon configuration file
+  await initBeaconConfig(path.join(lodestarRoot, "beacon.config"));
   // initialize peer id
   await initPeerId(path.join(lodestarRoot, "network", "peer-id"));
   const peerId = await readPeerId(path.join(lodestarRoot, "network", "peer-id"));
--- packages/lodestar-cli/src/config/config.ts
+++ packages/lodestar-cli/src/config/config.ts
@@ -6,18 +6,18 @@ import {BeaconNodeOptions} from "../lodestar/node/options";
 import {generateTomlConfig} from "../lodestar/util/toml";
 import {validateConfig} from "../lodestar/util/config";
 
-export function createConfig(): IBeaconNodeOptions {
+export function createBeaconConfig(): IBeaconNodeOptions {
   return {...defaults};
 }
 
-export async function writeConfig(filename: string, config: IBeaconNodeOptions): Promise<void> {
+export async function writeBeaconConfig(filename: string, config: IBeaconNodeOptions): Promise<void> {
   await writeFile(filename, generateTomlConfig(config, BeaconNodeOptions) as Json);
 }
 
-export async function readConfig(filename: string): Promise<IBeaconNodeOptions> {
+export async function readBeaconConfig(filename: string): Promise<IBeaconNodeOptions> {
   return validateConfig<IBeaconNodeOptions>(await readFile(filename), BeaconNodeOptions) as IBeaconNodeOptions;
 }
 
-export async function initConfig(filename: string): Promise<void> {
-  await writeConfig(filename, createConfig());
+export async function initBeaconConfig(filename: string): Promise<void> {
+  await writeBeaconConfig(filename, createBeaconConfig());
 }
