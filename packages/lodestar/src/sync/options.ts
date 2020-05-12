a19e0d38fadb0a1832379c4bd10fb7331bf08d65
Marin Petrunić <marin.petrunic@gmail.com>
2020-05-12 20:29:57 +0200

    add option for min peers, new dafault is 3

--- packages/lodestar/src/sync/options.ts
+++ packages/lodestar/src/sync/options.ts
@@ -1,11 +1,18 @@
 export interface ISyncOptions {
   blockPerChunk: number;
+  /**
+   * max slots to import before waiting for
+   * chain to process them
+   */
+  maxSlotImport: number;
   minPeers: number;
 }
 
 
 const config: ISyncOptions = {
   minPeers: 3,
+  //2 epochs
+  maxSlotImport: 64,
   blockPerChunk: 20
 };
 
