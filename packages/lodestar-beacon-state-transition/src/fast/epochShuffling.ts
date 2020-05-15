4d68bad56ebe5d94fbcb09601a90be7c8f7aed5c
Cayman <caymannava@gmail.com>
2020-05-15 16:20:20 -0500

    Add IEpochShuffling and computeEpochShuffling

---
+++ packages/lodestar-beacon-state-transition/src/fast/epochShuffling.ts
@@ -0,0 +1,97 @@
+import {Epoch, ValidatorIndex, BeaconState} from "@chainsafe/lodestar-types";
+import {IBeaconConfig} from "@chainsafe/lodestar-config";
+import {getSeed} from "../util";
+import { DomainType } from "../constants";
+import { intDiv } from "@chainsafe/lodestar-utils";
+
+export interface IEpochShuffling {
+  /**
+   * Epoch being shuffled
+   */
+  epoch: Epoch;
+
+  /**
+   * Non-shuffled active validator indices
+   */
+  activeIndices: ValidatorIndex[];
+
+  /**
+   * The active validator indices, shuffled into their committee
+   */
+  shuffling: ValidatorIndex[];
+
+  /**
+   * List of list of committees Committees
+   *
+   * Committees by index, by slot
+   *
+   * Note: With a high amount of shards, or low amount of validators,
+   * some shards may not have a committee this epoch
+   */
+  committees: ValidatorIndex[][][];
+}
+
+function shuffleList(activeIndices: ValidatorIndex[], seed: Uint8Array): ValidatorIndex[] {
+  return activeIndices;
+}
+
+export function computeCommitteeCount(config: IBeaconConfig, activeValidatorCount: number): number {
+  const validatorsPerSlot = intDiv(activeValidatorCount, config.params.SLOTS_PER_EPOCH);
+  let committeesPerSlot = intDiv(validatorsPerSlot, config.params.TARGET_COMMITTEE_SIZE);
+  if (config.params.MAX_COMMITTEES_PER_SLOT < committeesPerSlot) {
+    committeesPerSlot = config.params.MAX_COMMITTEES_PER_SLOT;
+  }
+  if (committeesPerSlot === 0) {
+    committeesPerSlot = 1;
+  }
+  return committeesPerSlot;
+}
+
+export function computeEpochShuffling(
+  config: IBeaconConfig,
+  state: BeaconState,
+  indicesBounded: [ValidatorIndex, Epoch, Epoch][],
+  epoch: Epoch
+): IEpochShuffling {
+  const seed = getSeed(config, state, epoch, DomainType.BEACON_ATTESTER);
+
+  const activeIndices: ValidatorIndex[] = [];
+  indicesBounded.forEach(([index, activationEpoch, exitEpoch]) => {
+    if (activationEpoch <= epoch && epoch < exitEpoch) {
+      activeIndices.push(index);
+    }
+  });
+
+  // copy
+  const shuffling = activeIndices.slice();
+  shuffleList(shuffling, seed);
+
+  const activeValidatorCount = activeIndices.length;
+  const committeesPerSlot = computeCommitteeCount(config, activeValidatorCount);
+
+  const committeeCount = committeesPerSlot * config.params.SLOTS_PER_EPOCH;
+
+  const sliceCommittee = (slot: number, committeeIndex: number): ValidatorIndex[] => {
+    const index = (slot * committeesPerSlot) + committeeIndex;
+    const startOffset = intDiv(activeValidatorCount * index, committeeCount);
+    const endOffset = intDiv(activeValidatorCount * (index + 1), committeeCount);
+    if (!(startOffset < endOffset)) {
+      throw new Error("Invalid offsets");
+    }
+    return shuffling.slice(startOffset, endOffset);
+  };
+
+  const committees = Array.from({length: config.params.SLOTS_PER_EPOCH}, (_, slot) => {
+    return Array.from({length: committeesPerSlot}, (_, committeeIndex) => {
+      return sliceCommittee(slot, committeeIndex);
+    });
+  });
+
+  return {
+    epoch,
+    activeIndices,
+    shuffling,
+    committees,
+  };
+}
+
