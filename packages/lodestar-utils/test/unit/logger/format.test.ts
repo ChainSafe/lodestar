f0a127162f7ada120e0ad3547b60ac5f44e02b03
Marin Petrunić <marin.petrunic@gmail.com>
2020-05-20 15:53:07 +0200

    add new winston format

---
+++ packages/lodestar-utils/test/unit/logger/format.test.ts
@@ -0,0 +1,23 @@
+import {serializeContext} from "../../../src/logger";
+import {expect} from "chai";
+import {config} from "@chainsafe/lodestar-config/lib/presets/minimal";
+
+describe("log format", function () {
+
+  describe("serialize context", function () {
+    it("should serialize context", function () {
+      const serialized = serializeContext({
+        array: config.types.Uint64.hashTreeRoot(10n),
+        buffer: Buffer.alloc(2, 0),
+        string: "test",
+        bigint: 10n
+      });
+      expect(serialized)
+        .to.equal(
+          "array=0x0a00000000000000000000000000000000000000000000000000000000000000,"
+          +" buffer=0x0000, string=test, bigint=10"
+        );
+    });
+  });
+
+});
