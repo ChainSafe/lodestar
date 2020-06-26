10b66189532fcfa1f173fadc3f41409699da27e4
Cayman <caymannava@gmail.com>
2020-06-26 11:46:44 -0500

    Expose metrics options to cli

--- packages/lodestar-cli/src/cmds/beacon/cmds/run/options/metrics.ts
+++ packages/lodestar-cli/src/cmds/beacon/cmds/run/options/metrics.ts
@@ -9,6 +9,33 @@ export const metricsEnabled: Options = {
   group: "metrics",
 };
 
+export const metricsTimeout: Options = {
+  alias: [
+    "metrics.timeout",
+  ],
+  type: "number",
+  default: 5000,
+  group: "metrics",
+};
+
+export const metricsPushGateway: Options = {
+  alias: [
+    "metrics.pushGateway",
+  ],
+  type: "boolean",
+  default: false,
+  group: "metrics",
+};
+
+export const metricsGatewayUrl: Options = {
+  alias: [
+    "metrics.gatewayUrl",
+  ],
+  type: "string",
+  default: "",
+  group: "metrics",
+};
+
 export const metricsServerPort: Options = {
   alias: [
     "metrics.serverPort",
