69f4864a9d17264fab5962b45cd60de312ec9bb7
Marin Petrunić <marin.petrunic@gmail.com>
2020-06-30 13:40:06 +0200

    store root to slot refs in block archive

--- packages/lodestar/src/db/api/beacon/repositories/blockArchive.ts
+++ packages/lodestar/src/db/api/beacon/repositories/blockArchive.ts
@@ -1,10 +1,11 @@
-import {SignedBeaconBlock, Slot} from "@chainsafe/lodestar-types";
+import {Root, SignedBeaconBlock, Slot} from "@chainsafe/lodestar-types";
 import {IBeaconConfig} from "@chainsafe/lodestar-config";
-import {bytesToInt} from "@chainsafe/lodestar-utils";
+import {bytesToInt, intToBytes} from "@chainsafe/lodestar-utils";
 
 import {IDatabaseController, IFilterOptions} from "../../../controller";
-import {Bucket} from "../../schema";
+import {Bucket, encodeKey} from "../../schema";
 import {Repository} from "./abstract";
+import {ArrayLike} from "@chainsafe/ssz";
 
 export interface IBlockFilterOptions extends IFilterOptions<Slot> {
   step?: number;
@@ -20,6 +21,42 @@ export class BlockArchiveRepository extends Repository<Slot, SignedBeaconBlock> 
     db: IDatabaseController<Buffer, Buffer>,
   ) {
     super(config, db, Bucket.blockArchive, config.types.SignedBeaconBlock);
+  }
+
+  public async add(value: SignedBeaconBlock): Promise<void> {
+    await Promise.all([
+      super.add(value),
+      this.storeRootRef(value)
+    ]);
+  }
+
+  public async getByRoot(root: Root): Promise<SignedBeaconBlock|null> {
+    const slot = await this.getSlotByRoot(root);
+    if(slot && slot !== 0) {
+      return this.get(slot);
+    }
+    return null;
+  }
+
+  public async getSlotByRoot(root: Root): Promise<Slot> {
+    const value = await this.db.get(
+      encodeKey(
+        Bucket.blockArchiveRootRef,
+        root.valueOf() as Uint8Array
+      )
+    );
+    if(value) {
+      return bytesToInt(value, "be");
+    }
+    return null;
+  }
+
+
+  public async batchAdd(values: ArrayLike<SignedBeaconBlock>): Promise<void> {
+    await Promise.all([
+      super.batchAdd(values),
+      ...Array.from(values).map((block) => this.storeRootRef(block))
+    ]);
   }
 
   public decodeKey(data: Buffer): number {
@@ -52,5 +89,15 @@ export class BlockArchiveRepository extends Repository<Slot, SignedBeaconBlock> 
         }
       }
     })();
+  }
+
+  private async storeRootRef(block: SignedBeaconBlock): Promise<void> {
+    return this.db.put(
+      encodeKey(
+        Bucket.blockArchiveRootRef,
+        this.config.types.BeaconBlock.hashTreeRoot(block.message)
+      ),
+      intToBytes(block.message.slot, 64, "be")
+    );
   }
 }
