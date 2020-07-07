2c14e60629481ee90c6feba1809bde606a5f6625
Patrick Michot <k0m0d0tr0n@gmail.com>
2020-07-07 16:05:04 -0500

    genesisStateFile check

--- packages/lodestar-cli/src/cmds/beacon/cmds/init/index.ts
+++ packages/lodestar-cli/src/cmds/beacon/cmds/init/index.ts
@@ -14,9 +14,14 @@ export function builder(yargs: Argv<IBeaconArgs>): Argv<IBeaconArgs> {
   yargs.options(beaconInitOptions);
   const args = parseArgs(yargs) as IBeaconInitArgs;
 
-  if (args.templateConfigFile) {
-    require("fs").readFileSync(args.templateConfigFile, "utf8");
-    yargs.config(readBeaconConfig(args.templateConfigFile));
+  if (args.chain && args.chain.genesisStateFile) {
+    require("fs").readFileSync(args.chain.genesisStateFile, "utf8");
+  }
+
+  const { templateConfigFile } = args;
+  if (templateConfigFile) {
+    require("fs").readFileSync(templateConfigFile, "utf8");
+    yargs.config(readBeaconConfig(templateConfigFile));
   }
   return yargs as unknown as Argv<IBeaconArgs>;
 }
