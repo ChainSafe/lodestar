4b5edb3955198fe8f4e868d8a5b19a4033d39ae4
3xtr4t3rr3str14l <32028745+3xtr4t3rr3str14l@users.noreply.github.com>
2020-07-08 13:48:22 -0500

    Update index.ts

--- packages/lodestar-cli/src/cmds/beacon/cmds/init/index.ts
+++ packages/lodestar-cli/src/cmds/beacon/cmds/init/index.ts
@@ -13,7 +13,6 @@ export const description = "Initialize lodestar beacon node";
 export function builder(yargs: Argv<IBeaconArgs>): Argv<IBeaconArgs> {
   yargs.options(beaconInitOptions);
   const args = parseArgs(yargs) as IBeaconInitArgs;
-
   if (args.templateConfigFile) {
     yargs.config(readBeaconConfig(args.templateConfigFile));
   }
