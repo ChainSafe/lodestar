1a93df2d8d80fd473717bca191dc0114b5ca66a8
dapplion <guillem.lion.benosa@gmail.com>
2020-07-26 19:47:31 +0200

    Fix global option preset default value

--- packages/lodestar-cli/src/options/globalOptions.ts
+++ packages/lodestar-cli/src/options/globalOptions.ts
@@ -7,16 +7,16 @@ export interface IGlobalArgs {
 
 export const globalOptions = {
   rootDir: {
-    default: "./.lodestar",
     description: "Lodestar root directory",
     normalize: true,
+    default: "./.lodestar",
     type: "string"
   } as Options,
 
   preset: {
     description: "Specifies the default eth2 spec type",
-    type: "string",
     choices: ["mainnet", "minimal"],
-    defaultDescription: "mainnet"
+    default: "mainnet",
+    type: "string"
   } as Options
 };
