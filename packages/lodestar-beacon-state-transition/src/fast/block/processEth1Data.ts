02df2900028f929dae0dcae1a7a7e806ae769dbe
Cayman <caymannava@gmail.com>
2020-07-27 12:00:09 -0500

    Fix linter error

--- packages/lodestar-beacon-state-transition/src/fast/block/processEth1Data.ts
+++ packages/lodestar-beacon-state-transition/src/fast/block/processEth1Data.ts
@@ -22,8 +22,8 @@ export function processEth1Data(
   if (Eth1Data.equals(state.eth1Data, newEth1Data)) {
     return; // Nothing to do if the state already has this as eth1data (happens a lot after majority vote is in)
   }
-  // TODO fast read-only iteration
-  const sameVotesCount = readOnlyMap(state.eth1DataVotes, (v) => v).filter((e) => Eth1Data.equals(e, newEth1Data)).length;
+  const sameVotesCount = readOnlyMap(state.eth1DataVotes, (v) => v)
+    .filter((e) => Eth1Data.equals(e, newEth1Data)).length;
   if (sameVotesCount * 2 > SLOTS_PER_ETH1_VOTING_PERIOD) {
     state.eth1Data = newEth1Data;
   }
