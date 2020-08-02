b27211625672e00561495121df4c33ee3fcd1209
dapplion <guillem.lion.benosa@gmail.com>
2020-08-02 21:29:17 +0200

    tsc friendly way of accessing Map value

--- packages/lodestar/src/chain/blocks/pool.ts
+++ packages/lodestar/src/chain/blocks/pool.ts
@@ -27,8 +27,9 @@ export class BlockPool {
 
   public addPendingBlock(job: IBlockProcessJob): void {
     const key = this.getKey(job.signedBlock);
-    if(this.pool.has(key)) {
-      this.pool.get(key).push(job);
+    const jobPool = this.pool.get(key);
+    if(jobPool) {
+      jobPool.push(job);
     } else {
       this.pool.set(key, [job]);
       //this prevents backward syncing
