e9b5acde07747623ee7b4cfaddaa43c2cc2d1c1e
Gregory Markou <16929357+GregTheGreek@users.noreply.github.com>
2020-08-02 12:17:16 -0400

    Update packages/lodestar-cli/src/util/ethers.ts
    Co-authored-by: Cayman <caymannava@gmail.com>

--- packages/lodestar-cli/src/util/ethers.ts
+++ packages/lodestar-cli/src/util/ethers.ts
@@ -23,8 +23,7 @@ export async function getEthersSigner({
     const eth1Provider = rpcUrl
       ? new ethers.providers.JsonRpcProvider(rpcUrl)
       : ethers.getDefaultProvider();
-    const providerWallet = wallet.connect(eth1Provider);
-    return providerWallet;
+    return wallet.connect(eth1Provider);
   }
 
   if (rpcUrl) {
