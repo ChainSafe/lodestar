53da70f9ef2e291375603892028737870a54ca37
dapplion <guillem.lion.benosa@gmail.com>
2020-08-03 09:52:47 +0200

    Use name suggestion pendingBlockPool

--- packages/lodestar/src/chain/blocks/pool.ts
+++ packages/lodestar/src/chain/blocks/pool.ts
@@ -27,9 +27,9 @@ export class BlockPool {
 
   public addPendingBlock(job: IBlockProcessJob): void {
     const key = this.getKey(job.signedBlock);
-    const jobPool = this.pool.get(key);
-    if(jobPool) {
-      jobPool.push(job);
+    const pendingBlockPool = this.pool.get(key);
+    if(pendingBlockPool) {
+      pendingBlockPool.push(job);
     } else {
       this.pool.set(key, [job]);
       //this prevents backward syncing
