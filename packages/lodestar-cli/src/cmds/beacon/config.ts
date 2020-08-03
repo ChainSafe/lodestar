c0cff556c0ba3ad77e0ba6b55f6671183d8049ff
Marin Petrunić <marin.petrunic@gmail.com>
2020-08-03 15:16:37 +0200

    fix beacon option array merging

--- packages/lodestar-cli/src/cmds/beacon/config.ts
+++ packages/lodestar-cli/src/cmds/beacon/config.ts
@@ -43,7 +43,7 @@ export function readBeaconConfig(filename: string): Partial<IBeaconNodeOptions> 
 
 /**
  * Reads config files and merges their options with default options and user options
- * @param options 
+ * @param options
  */
 export function mergeConfigOptions(options: IBeaconOptions): IBeaconOptions {
   const optionsFromFile = deepmerge(
@@ -54,12 +54,33 @@ export function mergeConfigOptions(options: IBeaconOptions): IBeaconOptions {
   return deepmerge(
     deepmerge(
       defaultOptions as Partial<IBeaconOptions>,
-      optionsFromFile
+      optionsFromFile,
+      {
+        arrayMerge
+      }
     ),
-    options
+    options,
+    {
+      arrayMerge
+    }
   );
 }
 
 export async function initBeaconConfig(filename: string, args: Partial<IBeaconNodeOptions>): Promise<void> {
   await writeBeaconConfig(filename, createBeaconConfig(args));
+}
+
+/**
+ * If override array option (source) is defined and has items
+ * replace target (original option).
+ * Example: network.multiaddrs has default ['/ip4/127.0.0.1/tcp/30606'] and we don't wanna append to that with cli flag
+ * as it could result in port taken
+ * @param target
+ * @param source
+ */
+function arrayMerge(target: unknown[], source: unknown[]): unknown[] {
+  if(source.length === 0) {
+    return target;
+  }
+  return source;
 }
