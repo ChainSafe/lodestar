80079d3b51d6598044d0087ec21d59c59da4c6bc
3xtr4t3rr3str14l <k0m0d0tr0n@gmail.com>
2020-08-06 14:57:03 -0500

    ganache-core cleanup

--- packages/lodestar-cli/test/setup.ts
+++ packages/lodestar-cli/test/setup.ts
@@ -3,19 +3,17 @@ import {before, after} from "mocha";
 import rimraf from "rimraf";
 import {rootDir} from "./constants";
 
-const ganache = require("ganache-core");
+import  ganache from "ganache-core";
 
 const server = ganache.server();
 const provider = server.provider;
 
 before(async () => {
   await initBLS();
-  server.listen(8545, function(err: any, blockchain: any) { 
-    console.log('error: ', err);
-   });
+  server.listen(8545);
 });
 
 after(async () => {
-  server.close((a: any) => console.log('closing server....', a))
+  server.close(() => console.log('closing server....'))
   await new Promise(resolve => rimraf(rootDir, resolve));
 });
