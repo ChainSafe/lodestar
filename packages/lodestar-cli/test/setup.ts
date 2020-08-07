59a47e44ef60bfb158b1ee46fa09d1f74bc7542d
3xtr4t3rr3str14l <k0m0d0tr0n@gmail.com>
2020-08-07 13:30:22 -0500

    deleting .tmp in before()

--- packages/lodestar-cli/test/setup.ts
+++ packages/lodestar-cli/test/setup.ts
@@ -5,6 +5,7 @@ import {rootDir, passphraseFile} from "./constants";
 import bre from "@nomiclabs/buidler";
 
 before(async () => {
+  await new Promise(resolve => rimraf(rootDir, resolve));
   await initBLS();
   bre.run("node");
 });
