7f1cb7467222a1b4622e23dbdcb90d75bea9ff30
dapplion <guillem.lion.benosa@gmail.com>
2020-08-09 20:26:41 +0200

    Revert changes in beacon/paths logic

--- packages/lodestar-cli/src/cmds/beacon/paths.ts
+++ packages/lodestar-cli/src/cmds/beacon/paths.ts
@@ -26,15 +26,21 @@ export type IBeaconPaths = {
  */
 // Using Pick<IGlobalArgs, "rootDir"> make changes in IGlobalArgs throw a type error here
 export function getBeaconPaths(options: Partial<IBeaconPaths> & Pick<IGlobalArgs, "rootDir">): IBeaconPaths {
-  const globalPaths = getGlobalPaths(options);
-  const beaconDir = globalPaths.rootDir;
+  options = {
+    ...options,
+    ...getGlobalPaths(options),
+  };
+  const rootDir = options.rootDir;
+  const beaconDir = rootDir;
   const dbDir = joinIfRelative(beaconDir, options.dbDir || "chain-db");
   const peerStoreDir = joinIfRelative(beaconDir, options.dbDir || "peerstore");
   const configFile = joinIfRelative(beaconDir, options.configFile || "beacon.config.json");
   const peerIdFile = joinIfRelative(beaconDir, options.peerIdFile || "peer-id.json");
   const enrFile = joinIfRelative(beaconDir, options.enrFile || "enr.json");
   const logFile = options.logFile && joinIfRelative(beaconDir, options.logFile);
+
   return {
+    ...options,
     beaconDir,
     dbDir,
     configFile,
@@ -42,7 +48,7 @@ export function getBeaconPaths(options: Partial<IBeaconPaths> & Pick<IGlobalArgs
     peerIdFile,
     enrFile,
     logFile
-  };
+  } as IBeaconPaths;
 }
 
 /**
