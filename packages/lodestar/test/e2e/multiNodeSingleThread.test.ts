ba882a986ffb91e6ec6e370384307f3d0cea0b61
Cayman <caymannava@gmail.com>
2020-08-14 11:46:49 -0500

    Update packages/lodestar/test/e2e/multiNodeSingleThread.test.ts
    Co-authored-by: Marin Petrunić <mpetrunic@users.noreply.github.com>

--- packages/lodestar/test/e2e/multiNodeSingleThread.test.ts
+++ packages/lodestar/test/e2e/multiNodeSingleThread.test.ts
@@ -7,7 +7,7 @@ import {getDevValidator} from "../utils/node/validator";
 import {Validator} from "@chainsafe/lodestar-validator/lib";
 import {BeaconNode} from "../../src/node";
 
-describe("Run multi node single thread interop validators (no eth1) until checkpoint", function () {
+describe.skip("Run multi node single thread interop validators (no eth1) until checkpoint", function () {
   const checkpointEvent = "justifiedCheckpoint";
   const validatorsPerNode = 8;
   const beaconParams: Partial<IBeaconParams> = {
@@ -89,4 +89,3 @@ describe("Run multi node single thread interop validators (no eth1) until checkp
     onDoneHandlers = [];
   });
 });
-
