ae781ce2f20983541e9da867fcbf4f7ac5de9a8f
dapplion <guillem.lion.benosa@gmail.com>
2020-08-15 10:37:37 +0200

    Rename typedocs generation script and dir as typedocs

--- scripts/prepare-docs.sh
+++ scripts/prepare-docs.sh
@@ -7,7 +7,7 @@ rm -rf $DOCS_DIR/packages
 mkdir -p $DOCS_DIR/packages
 for PACKAGE_DIR in packages/* ; do
     echo "Copying $PACKAGE_DIR docs"
-    cp -r $PACKAGE_DIR/docs $DOCS_DIR/$PACKAGE_DIR
+    cp -r $PACKAGE_DIR/typedocs $DOCS_DIR/$PACKAGE_DIR
 done
 
 # Copy contributing doc
