51d5ac7e7ccad93efe2a7250f3753680b40064b4
dapplion <guillem.lion.benosa@gmail.com>
2020-09-19 17:43:40 +0300

    Fix depositContract test name

--- packages/lodestar/test/unit/eth1/utils/depositContract.test.ts
+++ packages/lodestar/test/unit/eth1/utils/depositContract.test.ts
@@ -2,7 +2,7 @@ import {expect} from "chai";
 import {goerliMedallaLogs, goerliMedallaDepositEvents, getMedallaConfig} from "../../../utils/medalla";
 import {parseDepositLog} from "../../../../src/eth1/utils/depositContract";
 
-describe("eth1 / util / eth1Vote", function () {
+describe("eth1 / util / depositContract", function () {
   it("Should parse a raw deposit log", () => {
     const config = getMedallaConfig();
     const depositEvents = goerliMedallaLogs.map((log) => parseDepositLog(config, log));
