0c8e26967b16be3b0a54ad2f4bff562b7452c22a
Marin Petrunić <marin.petrunic@gmail.com>
2020-09-25 15:24:53 +0200

    update events test

--- packages/lodestar/test/unit/api/impl/events/events.test.ts
+++ packages/lodestar/test/unit/api/impl/events/events.test.ts
@@ -1,10 +1,9 @@
-import {EventsApi} from "../../../../../src/api/impl/events";
+import {BeaconEventType, EventsApi} from "../../../../../src/api/impl/events";
 import {config} from "@chainsafe/lodestar-config/lib/presets/minimal";
 import sinon, {SinonStubbedInstance} from "sinon";
 import {BeaconChain, ChainEventEmitter, IBeaconChain} from "../../../../../src/chain";
 import {generateBlockSummary, generateSignedBlock} from "../../../../utils/block";
 import {expect} from "chai";
-import {BeaconEventType} from "../../../../../src/api/impl/events";
 import {generateAttestation, generateEmptySignedVoluntaryExit} from "../../../../utils/attestation";
 import {generateState} from "../../../../utils/state";
 
@@ -19,9 +18,22 @@ describe("Events api impl", function () {
       chainStub.emitter = chainEventEmmitter;
     });
 
+    it("should ignore not sent topics", async function () {
+      const api = new EventsApi({}, {config, chain: chainStub});
+      const stream = api.getEventStream([BeaconEventType.HEAD]);
+      const headSummary = generateBlockSummary();
+      chainEventEmmitter.emit("forkChoice:reorg", headSummary, headSummary, 2);
+      chainEventEmmitter.emit("forkChoice:head", headSummary);
+      const event = await stream[Symbol.asyncIterator]().next();
+      expect(event?.value).to.not.be.null;
+      expect(event.value.type).to.equal(BeaconEventType.HEAD);
+      expect(event.value.message).to.not.be.null;
+      stream.stop();
+    });
+
     it("should process head event", async function () {
       const api = new EventsApi({}, {config, chain: chainStub});
-      const stream = api.getEventStream();
+      const stream = api.getEventStream([BeaconEventType.HEAD]);
       const headSummary = generateBlockSummary();
       chainEventEmmitter.emit("forkChoice:head", headSummary);
       const event = await stream[Symbol.asyncIterator]().next();
@@ -33,7 +45,7 @@ describe("Events api impl", function () {
 
     it("should process block event", async function () {
       const api = new EventsApi({}, {config, chain: chainStub});
-      const stream = api.getEventStream();
+      const stream = api.getEventStream([BeaconEventType.BLOCK]);
       const block = generateSignedBlock();
       chainEventEmmitter.emit("block", block);
       const event = await stream[Symbol.asyncIterator]().next();
@@ -45,7 +57,7 @@ describe("Events api impl", function () {
 
     it("should process attestation event", async function () {
       const api = new EventsApi({}, {config, chain: chainStub});
-      const stream = api.getEventStream();
+      const stream = api.getEventStream([BeaconEventType.ATTESTATION]);
       const attestation = generateAttestation();
       chainEventEmmitter.emit("attestation", attestation);
       const event = await stream[Symbol.asyncIterator]().next();
@@ -57,7 +69,7 @@ describe("Events api impl", function () {
 
     it("should process voluntary exit event", async function () {
       const api = new EventsApi({}, {config, chain: chainStub});
-      const stream = api.getEventStream();
+      const stream = api.getEventStream([BeaconEventType.VOLUNTARY_EXIT]);
       const exit = generateEmptySignedVoluntaryExit();
       chainEventEmmitter.emit("voluntaryExit", exit);
       const event = await stream[Symbol.asyncIterator]().next();
@@ -69,9 +81,9 @@ describe("Events api impl", function () {
 
     it("should process finalized checkpoint event", async function () {
       const api = new EventsApi({}, {config, chain: chainStub});
-      const stream = api.getEventStream();
+      const stream = api.getEventStream([BeaconEventType.FINALIZED_CHECKPOINT]);
       const checkpoint = generateState().finalizedCheckpoint;
-      chainEventEmmitter.emit("forkChoice:finalized", checkpoint);
+      chainEventEmmitter.emit("finalized", checkpoint);
       const event = await stream[Symbol.asyncIterator]().next();
       expect(event?.value).to.not.be.null;
       expect(event.value.type).to.equal(BeaconEventType.FINALIZED_CHECKPOINT);
@@ -81,7 +93,7 @@ describe("Events api impl", function () {
 
     it("should process chain reorg event", async function () {
       const api = new EventsApi({}, {config, chain: chainStub});
-      const stream = api.getEventStream();
+      const stream = api.getEventStream([BeaconEventType.CHAIN_REORG]);
       const oldHead = generateBlockSummary({slot: 4});
       const newHead = generateBlockSummary({slot: 3});
       chainEventEmmitter.emit("forkChoice:reorg", oldHead, newHead, 3);
