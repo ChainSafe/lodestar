da5d31cf675ae29118a25fca6f3d20e39dcff43e
Cayman <caymannava@gmail.com>
2020-09-30 17:20:27 -0500

    Fix linter error

--- packages/lodestar/src/api/impl/events/types.ts
+++ packages/lodestar/src/api/impl/events/types.ts
@@ -1,55 +1,55 @@
 import {
-    Attestation,
-    BlockEventPayload,
-    ChainHead,
-    ChainReorg,
-    FinalizedCheckpoint,
-    SignedVoluntaryExit,
+  Attestation,
+  BlockEventPayload,
+  ChainHead,
+  ChainReorg,
+  FinalizedCheckpoint,
+  SignedVoluntaryExit,
 } from "@chainsafe/lodestar-types";
 
 export enum BeaconEventType {
-    HEAD = "head",
-    BLOCK = "block",
-    ATTESTATION = "attestation",
-    VOLUNTARY_EXIT = "voluntary_exit",
-    CHAIN_REORG = "chain_reorg",
-    FINALIZED_CHECKPOINT = "finalized_checkpoint",
+  HEAD = "head",
+  BLOCK = "block",
+  ATTESTATION = "attestation",
+  VOLUNTARY_EXIT = "voluntary_exit",
+  CHAIN_REORG = "chain_reorg",
+  FINALIZED_CHECKPOINT = "finalized_checkpoint",
 }
 
 export type BeaconHeadEvent = {
-    type: typeof BeaconEventType.HEAD;
-    message: ChainHead;
+  type: typeof BeaconEventType.HEAD;
+  message: ChainHead;
 };
 
 export type BeaconBlockEvent = {
-    type: typeof BeaconEventType.BLOCK;
-    message: BlockEventPayload;
+  type: typeof BeaconEventType.BLOCK;
+  message: BlockEventPayload;
 };
 
 export type BeaconAttestationEvent = {
-    type: typeof BeaconEventType.ATTESTATION;
-    message: Attestation;
+  type: typeof BeaconEventType.ATTESTATION;
+  message: Attestation;
 };
 
 export type VoluntaryExitEvent = {
-    type: typeof BeaconEventType.VOLUNTARY_EXIT;
-    message: SignedVoluntaryExit;
+  type: typeof BeaconEventType.VOLUNTARY_EXIT;
+  message: SignedVoluntaryExit;
 };
 
 export type FinalizedCheckpointEvent = {
-    type: typeof BeaconEventType.FINALIZED_CHECKPOINT;
-    message: FinalizedCheckpoint;
+  type: typeof BeaconEventType.FINALIZED_CHECKPOINT;
+  message: FinalizedCheckpoint;
 };
 
 export type BeaconChainReorgEvent = {
-    type: typeof BeaconEventType.CHAIN_REORG;
-    message: ChainReorg;
+  type: typeof BeaconEventType.CHAIN_REORG;
+  message: ChainReorg;
 };
 
 export type BeaconEvent =
-    | BeaconHeadEvent
-    | BeaconBlockEvent
-    | BeaconAttestationEvent
-    | VoluntaryExitEvent
-    | BeaconChainReorgEvent
-    | FinalizedCheckpointEvent;
+  | BeaconHeadEvent
+  | BeaconBlockEvent
+  | BeaconAttestationEvent
+  | VoluntaryExitEvent
+  | BeaconChainReorgEvent
+  | FinalizedCheckpointEvent;
