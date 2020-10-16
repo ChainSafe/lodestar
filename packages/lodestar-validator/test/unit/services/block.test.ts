25a765b5c5fd42b701272ad4eadaa4044e25f411
dapplion <guillem.lion.benosa@gmail.com>
2020-10-16 12:12:41 +0200

    Update tests

--- packages/lodestar-validator/test/unit/services/block.test.ts
+++ packages/lodestar-validator/test/unit/services/block.test.ts
@@ -2,23 +2,24 @@ import sinon from "sinon";
 import {expect} from "chai";
 import {config} from "@chainsafe/lodestar-config/lib/presets/mainnet";
 import {Keypair} from "@chainsafe/bls";
-import {ApiClientOverInstance} from "../../../src/api";
 import {generateEmptySignedBlock, generateEmptyBlock} from "@chainsafe/lodestar/test/utils/block";
+import {ZERO_HASH} from "@chainsafe/lodestar-beacon-state-transition";
 import BlockProposingService from "../../../src/services/block";
 import {generateFork} from "../../utils/fork";
-import {MockValidatorDB} from "../../utils/mocks/MockValidatorDB";
-import {ZERO_HASH} from "@chainsafe/lodestar-beacon-state-transition";
+import {SinonStubbedBeaconApi} from "../../utils/apiStub";
 import {silentLogger} from "../../utils/logger";
+import {SlashingProtection, InvalidBlockError, InvalidBlockErrorCode} from "../../../src";
 
 describe("block proposing service", function () {
   const sandbox = sinon.createSandbox();
 
-  let rpcClientStub: any, dbStub: any;
+  let rpcClientStub: SinonStubbedBeaconApi;
+  let slashingProtectionStub: sinon.SinonStubbedInstance<SlashingProtection>;
   const logger = silentLogger;
 
   beforeEach(() => {
-    rpcClientStub = sandbox.createStubInstance(ApiClientOverInstance);
-    dbStub = sandbox.createStubInstance(MockValidatorDB);
+    rpcClientStub = new SinonStubbedBeaconApi(sandbox);
+    slashingProtectionStub = sandbox.createStubInstance(SlashingProtection);
   });
 
   afterEach(() => {
@@ -28,21 +29,38 @@ describe("block proposing service", function () {
   it("should not produce block in same slot", async function () {
     const lastBlock = generateEmptySignedBlock();
     lastBlock.message.slot = 1;
-    dbStub.getBlock.resolves(lastBlock);
-    const service = new BlockProposingService(config, [Keypair.generate()], rpcClientStub, dbStub, logger);
+
+    // Simulate a double vote detection
+    slashingProtectionStub.checkAndInsertBlockProposal.rejects(
+      new InvalidBlockError({code: InvalidBlockErrorCode.DOUBLE_BLOCK_PROPOSAL} as any)
+    );
+
+    const service = new BlockProposingService(
+      config,
+      [Keypair.generate()],
+      rpcClientStub,
+      slashingProtectionStub,
+      logger
+    );
     const result = await service.createAndPublishBlock(0, 1, generateFork(), ZERO_HASH);
     expect(result).to.be.null;
   });
 
   it("should produce correct block - last signed is null", async function () {
     const slot = 2;
-    rpcClientStub.validator = {
-      produceBlock: sandbox.stub(),
-      publishBlock: sandbox.stub(),
-    };
-    rpcClientStub.validator.produceBlock.withArgs(slot, sinon.match.any).resolves(generateEmptyBlock());
-    dbStub.getBlock.resolves(null);
-    const service = new BlockProposingService(config, [Keypair.generate()], rpcClientStub, dbStub, logger);
+    rpcClientStub.validator.produceBlock = sandbox.stub();
+    rpcClientStub.validator.publishBlock = sandbox.stub();
+    rpcClientStub.validator.produceBlock.resolves(generateEmptyBlock());
+
+    slashingProtectionStub.checkAndInsertBlockProposal.resolves();
+
+    const service = new BlockProposingService(
+      config,
+      [Keypair.generate()],
+      rpcClientStub,
+      slashingProtectionStub,
+      logger
+    );
     const result = await service.createAndPublishBlock(0, slot, generateFork(), ZERO_HASH);
     expect(result).to.not.be.null;
     expect(rpcClientStub.validator.publishBlock.calledOnce).to.be.true;
@@ -50,15 +68,19 @@ describe("block proposing service", function () {
 
   it("should produce correct block - last signed in previous epoch", async function () {
     const slot = config.params.SLOTS_PER_EPOCH;
-    rpcClientStub.validator = {
-      produceBlock: sandbox.stub(),
-      publishBlock: sandbox.stub(),
-    };
-    rpcClientStub.validator.produceBlock
-      .withArgs(slot, sinon.match.any, sinon.match.any)
-      .resolves(generateEmptyBlock());
-    dbStub.getBlock.resolves(generateEmptySignedBlock());
-    const service = new BlockProposingService(config, [Keypair.generate()], rpcClientStub, dbStub, logger);
+    rpcClientStub.validator.produceBlock = sandbox.stub();
+    rpcClientStub.validator.publishBlock = sandbox.stub();
+    rpcClientStub.validator.produceBlock.resolves(generateEmptyBlock());
+
+    slashingProtectionStub.checkAndInsertBlockProposal.resolves();
+
+    const service = new BlockProposingService(
+      config,
+      [Keypair.generate()],
+      rpcClientStub,
+      slashingProtectionStub,
+      logger
+    );
     const result = await service.createAndPublishBlock(0, slot, generateFork(), ZERO_HASH);
     expect(result).to.not.be.null;
     expect(rpcClientStub.validator.publishBlock.calledOnce).to.be.true;
