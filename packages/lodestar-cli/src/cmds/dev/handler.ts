824bbe974949328ebf3c424cc79a92d0c048451e
dapplion <guillem.lion.benosa@gmail.com>
2020-10-18 16:54:47 +0200

    Fix rootDir path issue in dev command

--- packages/lodestar-cli/src/cmds/dev/handler.ts
+++ packages/lodestar-cli/src/cmds/dev/handler.ts
@@ -1,20 +1,22 @@
-import fs, {mkdirSync} from "fs";
+import fs from "fs";
+import path from "path";
 import {promisify} from "util";
 import {initBLS} from "@chainsafe/bls";
 import {BeaconNode} from "@chainsafe/lodestar/lib/node";
 import {createNodeJsLibp2p} from "@chainsafe/lodestar/lib/network/nodejs";
 import {WinstonLogger} from "@chainsafe/lodestar-utils";
-import {createEnr, createPeerId} from "../../config";
-import {IGlobalArgs} from "../../options";
-import rimraf from "rimraf";
-import {join} from "path";
-import {IDevArgs} from "./options";
-import {getInteropValidator} from "../validator/utils/interop/validator";
 import {Validator} from "@chainsafe/lodestar-validator/lib";
 import {initDevChain, storeSSZState} from "@chainsafe/lodestar/lib/node/utils/state";
+import {createEnr, createPeerId} from "../../config";
+import {IGlobalArgs} from "../../options";
+import {mkdir} from "../../util";
+import rimraf from "rimraf";
+import {IDevArgs} from "./options";
+import {getInteropValidator} from "../validator/utils/interop/validator";
 import {getValidatorApiClient} from "./utils/validator";
 import {onGracefulShutdown} from "../../util/process";
 import {initializeOptionsAndConfig} from "../init/handler";
+import {defaultRootDir} from "../../paths/global";
 
 /**
  * Run a beacon node
@@ -29,45 +31,45 @@ export async function devHandler(args: IDevArgs & IGlobalArgs): Promise<void> {
   const enr = createEnr(peerId);
   beaconNodeOptions.set({network: {discv5: {enr}}});
 
-  const chainDir = join(args.rootDir, "dev", "beacon");
-  const validatorsDir = join(args.rootDir, "dev", "validators");
-  mkdirSync(chainDir, {recursive: true});
-  mkdirSync(validatorsDir, {recursive: true});
+  // Custom paths different than regular beacon, validator paths
+  const rootDir = path.join(args.rootDir || defaultRootDir, "dev");
+  const chainDir = path.join(rootDir, "beacon");
+  const validatorsDir = path.join(rootDir, "validators");
+  const dbPath = path.join(chainDir, "db-" + peerId.toB58String());
+  const genesisStateFilePath = path.join(rootDir, "genesis.ssz");
+
+  mkdir(chainDir);
+  mkdir(validatorsDir);
 
   // TODO: Rename db.name to db.path or db.location
-  const dbPath = join(chainDir, "db-" + peerId.toB58String());
   beaconNodeOptions.set({db: {name: dbPath}});
   beaconNodeOptions.set({eth1: {enabled: false}});
 
   // BeaconNode setup
   const options = beaconNodeOptions.getWithDefaults();
+  console.log(options.network);
   const libp2p = await createNodeJsLibp2p(peerId, options.network);
   const logger = new WinstonLogger();
   const node = new BeaconNode(options, {config, libp2p, logger});
 
   if (args.genesisValidators) {
     const state = await initDevChain(node, args.genesisValidators);
-    storeSSZState(node.config, state, join(args.rootDir, "dev", "genesis.ssz"));
+    storeSSZState(node.config, state, genesisStateFilePath);
   } else if (args.genesisStateFile) {
     await node.chain.initializeBeaconChain(
-      config.types.BeaconState.tree.deserialize(await fs.promises.readFile(join(args.rootDir, args.genesisStateFile)))
+      config.types.BeaconState.tree.deserialize(await fs.promises.readFile(args.genesisStateFile))
     );
   }
 
   let validators: Validator[] = [];
 
   onGracefulShutdown(async () => {
-    await Promise.all([
-      Promise.all(validators.map((v) => v.stop())),
-      node.stop(),
-      async () => {
-        if (args.reset) {
-          logger.info("Cleaning db directories");
-          await promisify(rimraf)(chainDir);
-          await promisify(rimraf)(validatorsDir);
-        }
-      },
-    ]);
+    await Promise.all([Promise.all(validators.map((v) => v.stop())), node.stop()]);
+    if (args.reset) {
+      logger.info("Cleaning db directories");
+      await promisify(rimraf)(chainDir);
+      await promisify(rimraf)(validatorsDir);
+    }
   }, logger.info.bind(logger));
 
   await node.start();
@@ -76,15 +78,7 @@ export async function devHandler(args: IDevArgs & IGlobalArgs): Promise<void> {
     const range = args.startValidators.split(":").map((s) => parseInt(s));
     const api = getValidatorApiClient(args.server, logger, node);
     validators = Array.from({length: range[1] + range[0]}, (v, i) => i + range[0]).map((index) => {
-      return getInteropValidator(
-        node.config,
-        validatorsDir,
-        {
-          api,
-          logger,
-        },
-        index
-      );
+      return getInteropValidator(node.config, validatorsDir, {api, logger}, index);
     });
     validators.forEach((v) => v.start());
   }
--- packages/lodestar-cli/src/paths/global.ts
+++ packages/lodestar-cli/src/paths/global.ts
@@ -1,6 +1,8 @@
 import {IGlobalArgs} from "../options";
 import {joinIfRelative} from "../util";
 
+export const defaultRootDir = "./.lodestar";
+
 export interface IGlobalPaths {
   rootDir: string;
   paramsFile: string;
@@ -17,7 +19,7 @@ export interface IGlobalPaths {
 
 export function getGlobalPaths(args: Partial<IGlobalArgs>): IGlobalPaths {
   // Set rootDir to testnet name iff rootDir is not set explicitly
-  const rootDir = args.rootDir || (args.testnet ? `.${args.testnet}` : "./.lodestar");
+  const rootDir = args.rootDir || (args.testnet ? `.${args.testnet}` : defaultRootDir);
   const paramsFile = joinIfRelative(rootDir, args.paramsFile || "config.yaml");
   return {
     rootDir,
