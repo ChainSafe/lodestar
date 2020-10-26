6d766fce48dc744da03a6dc7cf64bc065224411c
3xtr4t3rr3str14l <k0m0d0tr0n@protonmail.com>
2020-10-26 15:21:18 -0500

    FileENR constructor/initFromENR rework

--- packages/lodestar-cli/src/network/fileEnr.ts
+++ packages/lodestar-cli/src/network/fileEnr.ts
@@ -6,28 +6,37 @@ import PeerId from "peer-id";
  * `FileENR` is an `ENR` that saves the ENR contents to a file on every modification
  */
 export class FileENR extends ENR {
-  private filename: string;
-  private localPeerId: PeerId;
+  private filename!: string;
+  private localPeerId!: PeerId;
 
-  constructor(filename: string, peerId: PeerId) {
-    super();
+  constructor(
+    filename: string,
+    peerId: PeerId,
+    kvs: Record<string, Uint8Array> | undefined,
+    seq: bigint,
+    signature: Buffer | null
+  ) {
+    super(kvs, seq, signature);
+    Object.setPrototypeOf(this, FileENR.prototype);
     this.filename = filename;
     this.localPeerId = peerId;
+    return this;
   }
 
   static initFromFile(filename: string, peerId: PeerId): FileENR {
     const enr = FileENR.decodeTxt(readFileSync(filename, "utf8")) as FileENR;
     return this.initFromENR(filename, peerId, enr);
   }
-
   static initFromENR(filename: string, peerId: PeerId, enr: FileENR): FileENR {
-    Object.setPrototypeOf(enr, FileENR.prototype);
-    enr.filename = filename;
-    enr.localPeerId = peerId;
-    return enr;
+    const kvs = Array.from(enr.entries()).reduce((obj: Record<ENRKey, ENRValue>, kv) => {
+      obj[kv[0]] = kv[1];
+      return obj;
+    }, {});
+    return new FileENR(filename, peerId, kvs, enr.seq, enr.signature);
   }
 
   saveToFile(): void {
+    if (!this.localPeerId) return;
     const keypair = createKeypairFromPeerId(this.localPeerId);
     writeFileSync(this.filename, this.encodeTxt(keypair.privateKey));
   }
