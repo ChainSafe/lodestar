677c114005e575eafe1bf3fe4bd359eb15253b07
Marin Petrunić <marin.petrunic@gmail.com>
2020-11-19 14:17:28 +0100

    remove dummy contructors

--- packages/lodestar-validator/src/api/impl/rest/beacon/blocks.ts
+++ packages/lodestar-validator/src/api/impl/rest/beacon/blocks.ts
@@ -1,15 +1,8 @@
-import {IBeaconConfig} from "@chainsafe/lodestar-config";
-import {ILogger} from "@chainsafe/lodestar-utils";
-import {HttpClient} from "../../../../util/httpClient";
-import {IBeaconBlocksApi} from "../../../interface/beacon";
 import {SignedBeaconBlock} from "@chainsafe/lodestar-types";
+import {IBeaconBlocksApi} from "../../../interface/beacon";
 import {RestApi} from "./abstract";
 
 export class RestBeaconBlocksApi extends RestApi implements IBeaconBlocksApi {
-  public constructor(config: IBeaconConfig, client: HttpClient, logger: ILogger) {
-    super(config, client, logger);
-  }
-
   public async publishBlock(block: SignedBeaconBlock): Promise<void> {
     return this.client.post("/blocks", this.config.types.SignedBeaconBlock.toJson(block, {case: "snake"}));
   }
--- packages/lodestar-validator/src/api/impl/rest/beacon/pool.ts
+++ packages/lodestar-validator/src/api/impl/rest/beacon/pool.ts
@@ -1,15 +1,8 @@
-import {IBeaconConfig} from "@chainsafe/lodestar-config";
 import {Attestation} from "@chainsafe/lodestar-types";
-import {ILogger} from "@chainsafe/lodestar-utils";
-import {HttpClient} from "../../../../util/httpClient";
 import {IBeaconPoolApi} from "../../../interface/beacon";
 import {RestApi} from "./abstract";
 
 export class RestBeaconPoolApi extends RestApi implements IBeaconPoolApi {
-  public constructor(config: IBeaconConfig, client: HttpClient, logger: ILogger) {
-    super(config, client, logger);
-  }
-
   public async submitAttestation(attestation: Attestation): Promise<void> {
     return this.client.post("/pool/attestations", this.config.types.Attestation.toJson(attestation, {case: "snake"}));
   }
