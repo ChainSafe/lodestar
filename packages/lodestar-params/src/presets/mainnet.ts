4729c611739d7e103215464cbe5ef0c1e1113b43
dapplion <guillem.lion.benosa@gmail.com>
2020-11-21 15:36:57 +0000

    Reference with v1.0.0 tag

--- packages/lodestar-params/src/presets/mainnet.ts
+++ packages/lodestar-params/src/presets/mainnet.ts
@@ -2,5 +2,6 @@ import {mainnetYaml} from "./mainnetYaml";
 import {createIBeaconParams, mapValuesNumToString} from "../utils";
 import {IBeaconParams} from "../interface";
 
+export const commit = "v1.0.0";
 export const yaml = mapValuesNumToString(mainnetYaml);
 export const params = createIBeaconParams(yaml) as IBeaconParams;
--- packages/lodestar-params/src/presets/minimal.ts
+++ packages/lodestar-params/src/presets/minimal.ts
@@ -2,5 +2,6 @@ import {minimalYaml} from "./minimalYaml";
 import {createIBeaconParams, mapValuesNumToString} from "../utils";
 import {IBeaconParams} from "../interface";
 
+export const commit = "v1.0.0";
 export const yaml = mapValuesNumToString(minimalYaml);
 export const params = createIBeaconParams(yaml) as IBeaconParams;
