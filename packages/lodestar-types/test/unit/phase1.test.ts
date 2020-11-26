d70684e409774c625f2835c56253214a1ef27d05
Marin Petrunić <marin.petrunic@gmail.com>
2020-11-26 18:37:36 +0100

    add phase 1 types test

---
+++ packages/lodestar-types/test/unit/phase1.test.ts
@@ -0,0 +1,10 @@
+import {types as mainnetTypes} from "../../src/ssz/presets/mainnet";
+import {types as minimalTypes} from "../../src/ssz/presets/minimal";
+import {expect} from "chai";
+
+describe("phase1 types", function () {
+  it("should generate minimal and mainnet", function () {
+    expect(Object.keys(mainnetTypes.phase1).length).to.be.greaterThan(0);
+    expect(Object.keys(minimalTypes.phase1).length).to.be.greaterThan(0);
+  });
+});
