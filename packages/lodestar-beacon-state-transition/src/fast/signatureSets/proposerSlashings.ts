6433cdac600e2856afa230c0b1239d7c4e660e37
dapplion <guillem.lion.benosa@gmail.com>
2020-12-04 13:31:28 +0000

    Use native .flat() method

--- packages/lodestar-beacon-state-transition/src/fast/signatureSets/proposerSlashings.ts
+++ packages/lodestar-beacon-state-transition/src/fast/signatureSets/proposerSlashings.ts
@@ -3,16 +3,13 @@ import {BeaconState, SignedBeaconBlock} from "@chainsafe/lodestar-types";
 import {ISignatureSet} from "./types";
 import {EpochContext} from "../index";
 import {getProposerSlashingSignatureSets} from "../block/processProposerSlashing";
-import {flatten} from "./util";
 
 export function getProposerSlashingsSignatureSets(
   epochCtx: EpochContext,
   state: BeaconState,
   signedBlock: SignedBeaconBlock
 ): ISignatureSet[] {
-  return flatten(
-    readOnlyMap(signedBlock.message.body.proposerSlashings, (proposerSlashing) =>
-      getProposerSlashingSignatureSets(epochCtx, state, proposerSlashing)
-    )
-  );
+  return readOnlyMap(signedBlock.message.body.proposerSlashings, (proposerSlashing) =>
+    getProposerSlashingSignatureSets(epochCtx, state, proposerSlashing)
+  ).flat(1);
 }
--- packages/lodestar-beacon-state-transition/src/fast/signatureSets/util.ts
+++
@@ -1,9 +0,0 @@
-export function flatten<T>(arrNested: T[][]): T[] {
-  const flat: T[] = [];
-  for (const arr of arrNested) {
-    for (const el of arr) {
-      flat.push(el);
-    }
-  }
-  return flat;
-}
