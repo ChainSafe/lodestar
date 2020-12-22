a8e2b507f75a9dfd83e4e13fd08dd036291728f8
dapplion <guillem.lion.benosa@gmail.com>
2020-12-22 12:53:46 +0100

    it-pipe type first argument being array

--- packages/lodestar/types/it-pipe/index.d.ts
+++ packages/lodestar/types/it-pipe/index.d.ts
@@ -1,84 +1,92 @@
 // Overwrite types until this issue is fixed https://github.com/alanshaw/it-pipe/issues/4
 // Original types contain
 // ```
-// export function pipe (first: any, ...rest: any[]): any
+// export function pipe (f1: any, ...rest: any[]): any
 // ```
 // Which act as a @ts-ignore compromising type safety
 
 declare module "it-pipe" {
-  export function pipe<A, B>(first: A | (() => A), second: (source: A) => B): B;
+  export function pipe<A, B>(f1: A | (() => A), f2: (source: A) => B): B;
 
-  export function pipe<A, B, C>(first: A | (() => A), second: (source: A) => B, third: (source: B) => C): C;
+  export function pipe<A, B, C>(f1: A | (() => A), f2: (source: A) => B, f3: (source: B) => C): C;
 
   export function pipe<A, B, C, D>(
-    first: A | (() => A),
-    second: (source: A) => B,
-    third: (source: B) => C,
-    fourth: (source: C) => D
+    f1: A | (() => A),
+    f2: (source: A) => B,
+    f3: (source: B) => C,
+    f4: (source: C) => D
   ): D;
 
   export function pipe<A, B, C, D, E>(
-    first: A | (() => A),
-    second: (source: A) => B,
-    third: (source: B) => C,
-    fourth: (source: C) => D,
-    fifth: (source: D) => E
+    f1: A | (() => A),
+    f2: (source: A) => B,
+    f3: (source: B) => C,
+    f4: (source: C) => D,
+    f5: (source: D) => E
   ): E;
 
   export function pipe<A, B, C, D, E, F>(
-    first: A | (() => A),
-    second: (source: A) => B,
-    third: (source: B) => C,
-    fourth: (source: C) => D,
-    fifth: (source: D) => E,
-    sixth: (source: E) => F
+    f1: A | (() => A),
+    f2: (source: A) => B,
+    f3: (source: B) => C,
+    f4: (source: C) => D,
+    f5: (source: D) => E,
+    f6: (source: E) => F
   ): F;
 
   export function pipe<A, B, C, D, E, F, G>(
-    first: A | (() => A),
-    second: (source: A) => B,
-    third: (source: B) => C,
-    forth: (source: C) => D,
-    fifth: (source: D) => E,
-    sixth: (source: E) => F,
-    seventh: (source: F) => G
+    f1: A | (() => A),
+    f2: (source: A) => B,
+    f3: (source: B) => C,
+    f4: (source: C) => D,
+    f5: (source: D) => E,
+    f6: (source: E) => F,
+    f7: (source: F) => G
   ): G;
 
   export function pipe<A, B, C, D, E, F, G, H>(
-    first: A | (() => A),
-    second: (source: A) => B,
-    third: (source: B) => C,
-    forth: (source: C) => D,
-    fifth: (source: D) => E,
-    sixth: (source: E) => F,
-    seventh: (source: F) => G,
-    eighth: (source: G) => H
+    f1: A | (() => A),
+    f2: (source: A) => B,
+    f3: (source: B) => C,
+    f4: (source: C) => D,
+    f5: (source: D) => E,
+    f6: (source: E) => F,
+    f7: (source: F) => G,
+    f8: (source: G) => H
   ): H;
 
   export function pipe<A, B, C, D, E, F, G, H, I>(
-    first: A | (() => A),
-    second: (source: A) => B,
-    third: (source: B) => C,
-    forth: (source: C) => D,
-    fifth: (source: D) => E,
-    sixth: (source: E) => F,
-    seventh: (source: F) => G,
-    eighth: (source: G) => H,
-    ninth: (source: H) => I
+    f1: A | (() => A),
+    f2: (source: A) => B,
+    f3: (source: B) => C,
+    f4: (source: C) => D,
+    f5: (source: D) => E,
+    f6: (source: E) => F,
+    f7: (source: F) => G,
+    f8: (source: G) => H,
+    f9: (source: H) => I
   ): I;
 
-  export function pipe<A, B, C, D, E, F, G, H, I, J>(
-    first: A | (() => A),
-    second: (source: A) => B,
-    third: (source: B) => C,
-    forth: (source: C) => D,
-    fifth: (source: D) => E,
-    sixth: (source: E) => F,
-    seventh: (source: F) => G,
-    eighth: (source: G) => H,
-    ninth: (source: H) => I,
-    tenth: (source: I) => J
-  ): I;
+  // First argument is array
+
+  export function pipe<A, B>(f1: A[], f2: (source: AsyncIterable<A>) => B): B;
+
+  export function pipe<A, B, C>(f1: A[], f2: (source: AsyncIterable<A>) => B, f3: (source: B) => C): C;
+
+  export function pipe<A, B, C, D>(
+    f1: A[],
+    f2: (source: AsyncIterable<A>) => B,
+    f3: (source: B) => C,
+    f4: (source: C) => D
+  ): D;
+
+  export function pipe<A, B, C, D, E>(
+    f1: A[],
+    f2: (source: AsyncIterable<A>) => B,
+    f3: (source: B) => C,
+    f4: (source: C) => D,
+    f5: (source: D) => E
+  ): E;
 
   export default pipe;
 }
