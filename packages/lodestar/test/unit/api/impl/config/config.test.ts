c50ba1f4c56c5e8ce26cd6ee8a26bf84af4c77d0
3xtr4t3rr3str14l <k0m0d0tr0n@protonmail.com>
2021-02-10 14:28:22 -0600

    test

---
+++ packages/lodestar/test/unit/api/impl/config/config.test.ts
@@ -0,0 +1,18 @@
+// import {INodeApi} from "../../../../api";
+
+// describe("config api implementation", function () {
+//   let api: INodeApi;
+
+//   beforeEach(function () {
+//     // api = new ConfigApi({}, {network: networkStub, sync: syncStub});
+//   });
+//   describe("getForkSchedule", function () {
+//     it("should get known scheduled forks", async function () {
+//     });
+//   });
+
+//   describe("getDepositContract", function () {
+//     it("should get the deposit contract from config", async function () {
+//     });
+//   });
+// });
