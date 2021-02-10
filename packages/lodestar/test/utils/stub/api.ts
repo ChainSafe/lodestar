3661ff516c45e7a28d2d38ef9ed20320ec638dc2
3xtr4t3rr3str14l <k0m0d0tr0n@protonmail.com>
2021-02-10 17:47:34 -0600

    add config to StubbdApi

--- packages/lodestar/test/utils/stub/api.ts
+++ packages/lodestar/test/utils/stub/api.ts
@@ -7,6 +7,7 @@ import {StubbedBeaconApi} from "./beaconApi";
 import {EventsApi} from "../../../src/api/impl/events";
 import {DebugApi} from "../../../src/api/impl/debug";
 import {DebugBeaconApi} from "../../../src/api/impl/debug/beacon";
+import {ConfigApi} from "../../../src/api/impl/config";
 
 export class StubbedApi implements SinonStubbedInstance<IApi> {
   beacon: StubbedBeaconApi;
@@ -14,6 +15,7 @@ export class StubbedApi implements SinonStubbedInstance<IApi> {
   validator: SinonStubbedInstance<ValidatorApi>;
   events: SinonStubbedInstance<EventsApi>;
   debug: SinonStubbedInstance<DebugApi>;
+  config: SinonStubbedInstance<ConfigApi>;
 
   constructor(sandbox: SinonSandbox = sinon) {
     this.beacon = new StubbedBeaconApi(sandbox);
@@ -22,5 +24,6 @@ export class StubbedApi implements SinonStubbedInstance<IApi> {
     this.events = sandbox.createStubInstance(EventsApi);
     const debugBeacon = sandbox.createStubInstance(DebugBeaconApi);
     this.debug = {beacon: debugBeacon} as SinonStubbedInstance<DebugApi>;
+    this.config = sandbox.createStubInstance(ConfigApi);
   }
 }
