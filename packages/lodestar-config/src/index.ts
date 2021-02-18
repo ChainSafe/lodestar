3ca2b7150370e3847987db9373f5364b0c0fa9af
Cayman <caymannava@gmail.com>
2021-02-18 17:51:52 -0600

    Update config for namespaced types

--- packages/lodestar-config/src/index.ts
+++ packages/lodestar-config/src/index.ts
@@ -1,13 +1,18 @@
 import {IBeaconParams} from "@chainsafe/lodestar-params";
-import {createIBeaconSSZTypes} from "@chainsafe/lodestar-types";
+import {createIBeaconSSZTypes, IPhase0SSZTypes, Slot} from "@chainsafe/lodestar-types";
 
 import {IBeaconConfig} from "./interface";
 
 export * from "./interface";
 
 export function createIBeaconConfig(params: IBeaconParams): IBeaconConfig {
+  const types = createIBeaconSSZTypes(params);
   return {
     params,
-    types: createIBeaconSSZTypes(params),
+    types,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    getTypes(slot: Slot): IPhase0SSZTypes {
+      return types.phase0;
+    },
   };
 }
