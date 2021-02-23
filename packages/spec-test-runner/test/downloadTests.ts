6eee8df998bbcc7817a8feb85169b88e1075bfd9
Marin Petrunić <marin.petrunic@gmail.com>
2021-02-23 13:10:11 +0100

    update to new hf1 spec tests

--- packages/spec-test-runner/test/downloadTests.ts
+++ packages/spec-test-runner/test/downloadTests.ts
@@ -1,10 +1,11 @@
 import {downloadTests} from "@chainsafe/lodestar-spec-test-util";
-import {SPEC_TEST_LOCATION, SPEC_TEST_VERSION} from "./utils/specTestCases";
+import {SPEC_TEST_LOCATION, SPEC_TEST_VERSION, SPEC_TEST_REPO_URL} from "./utils/specTestCases";
 
 /* eslint-disable no-console */
 
 downloadTests(
   {
+    specTestsRepoUrl: SPEC_TEST_REPO_URL,
     specVersion: SPEC_TEST_VERSION,
     outputDir: SPEC_TEST_LOCATION,
   },
