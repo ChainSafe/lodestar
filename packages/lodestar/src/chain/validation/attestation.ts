81425c8bd282231ba04c120f4b1541a7d0d4fbcb
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2021-03-25 15:27:37 +0100

    Fix validateAggregateAttestation types + error (#2262)
    * Type validateAggregateAttestation let declarations
    
    * Add sub error to MISSING_ATTESTATION_PRESTATE
    
    * Type validateGossipAttestation let

--- packages/lodestar/src/chain/validation/attestation.ts
+++ packages/lodestar/src/chain/validation/attestation.ts
@@ -1,7 +1,7 @@
 import {IBeaconConfig} from "@chainsafe/lodestar-config";
 import {IBeaconDb} from "../../db/api";
 import {IAttestationJob, IBeaconChain} from "..";
-import {computeEpochAtSlot} from "@chainsafe/lodestar-beacon-state-transition";
+import {CachedBeaconState, computeEpochAtSlot} from "@chainsafe/lodestar-beacon-state-transition";
 import {phase0} from "@chainsafe/lodestar-beacon-state-transition";
 import {AttestationError, AttestationErrorCode} from "../errors";
 import {ATTESTATION_PROPAGATION_SLOT_RANGE} from "../../constants";
@@ -73,12 +73,13 @@ export async function validateGossipAttestation(
     });
   }
 
-  let attestationPreState;
+  let attestationPreState: CachedBeaconState<phase0.BeaconState>;
   try {
     attestationPreState = await chain.regen.getCheckpointState(attestation.data.target);
   } catch (e) {
     throw new AttestationError({
       code: AttestationErrorCode.MISSING_ATTESTATION_PRESTATE,
+      error: e as Error,
       job: attestationJob,
     });
   }
