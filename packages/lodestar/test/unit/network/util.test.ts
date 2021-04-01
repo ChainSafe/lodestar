25e2ff2fc2b20833c805a37855e07b5b30fc6995
3xtr4t3rr3str14l <k0m0d0tr0n@protonmail.com>
2021-04-01 18:07:25 -0500

    getAgentVersionFromPeerStore benchmark test

--- packages/lodestar/test/unit/network/util.test.ts
+++ packages/lodestar/test/unit/network/util.test.ts
@@ -1,7 +1,17 @@
 import Multiaddr from "multiaddr";
 import {expect} from "chai";
-import {createRpcProtocol, isLocalMultiAddr, parseProtocolId} from "../../../src/network";
+import {
+  createNodeJsLibp2p,
+  createRpcProtocol,
+  getAgentVersionFromPeerStore,
+  isLocalMultiAddr,
+  parseProtocolId,
+} from "../../../src/network";
 import {Method, ReqRespEncoding} from "../../../src/constants";
+import {createEnr, createPeerId} from "@chainsafe/lodestar-cli/src/config";
+import {defaultNetworkOptions} from "../../../src/network/options";
+import {fromHexString} from "@chainsafe/ssz";
+import {createNode} from "../../utils/network";
 
 describe("Test isLocalMultiAddr", () => {
   it("should return true for 127.0.0.1", () => {
@@ -39,4 +49,42 @@ describe("ReqResp protocolID parse / render", () => {
       expect(parseProtocolId(protocolId)).to.deep.equal({method, encoding, version});
     });
   }
+});
+
+describe("getAgentVersionFromPeerStore benchmark", () => {
+  it("should not take longer than 10ms to fetch a peer's AgentVersion", async function () {
+    this.timeout(0);
+    const peerId = await createPeerId();
+    const libp2p = await createNodeJsLibp2p(
+      peerId,
+      {
+        discv5: {
+          enabled: false,
+          enr: createEnr(peerId),
+          bindAddr: "/ip4/127.0.0.1/udp/0",
+          bootEnrs: [],
+        },
+        localMultiaddrs: ["/ip4/127.0.0.1/tcp/0"],
+        targetPeers: defaultNetworkOptions.targetPeers,
+        maxPeers: defaultNetworkOptions.maxPeers,
+      },
+      {disablePeerDiscovery: true}
+    );
+
+    const testAgentVersion = fromHexString("0x1234");
+    const numPeers = 2000;
+    for (let i = 0; i < numPeers; i++) {
+      const node = await createNode("/ip4/127.0.0.1/tcp/0");
+      libp2p.peerStore.addressBook.add(node.peerId, node.multiaddrs);
+      libp2p.peerStore.metadataBook._setValue(node.peerId, "AgentVersion", testAgentVersion);
+
+      // start the benchmark
+      const start = Date.now();
+      const version = getAgentVersionFromPeerStore(node.peerId, libp2p.peerStore);
+      const timeDiff = Date.now() - start;
+      // TODO: not sure what value to use here.  longest timeDiff i've seen so far is 7ms
+      expect(timeDiff).to.be.lt(10);
+      expect(version).to.be.equal(testAgentVersion.toString());
+    }
+  });
 });
