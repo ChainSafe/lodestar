b538e9bcd76a69f62e3e4264458126dd6ad0bd27
dapplion <35266934+dapplion@users.noreply.github.com>
2021-04-09 14:44:05 +0200

    Attempt more optimizations

--- packages/beacon-state-transition/test/perf/shuffle/numberMath.perf.ts
+++ packages/beacon-state-transition/test/perf/shuffle/numberMath.perf.ts
@@ -2,21 +2,61 @@ import {BenchmarkRunner} from "../runner";
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 (async function () {
-  const runner = new BenchmarkRunner("shuffle list", {
+  const runner = new BenchmarkRunner("shuffle number math ops", {
     runs: 512,
     maxMs: 10 * 1000,
   });
 
-  const j = 12354233;
-  const forRuns = 1e5;
+  const forRuns = 100e5;
+  const j = forRuns / 2;
 
-  const arrSize = 2e5;
+  const arrSize = forRuns;
   const input: number[] = [];
   const inputUint32Array = new Uint32Array(arrSize);
   for (let i = 0; i < arrSize; i++) {
     input[i] = i;
     inputUint32Array[i] = i;
   }
+
+  await runner.run({
+    id: "if(i == 1)",
+    run: () => {
+      for (let i = 0; i < forRuns; i++) {
+        if (i === 1) {
+          //
+        }
+      }
+    },
+  });
+
+  await runner.run({
+    id: "if(i)",
+    run: () => {
+      for (let i = 0; i < forRuns; i++) {
+        if (i) {
+          //
+        }
+      }
+    },
+  });
+
+  await runner.run({
+    id: "i == j",
+    run: () => {
+      for (let i = 0; i < forRuns; i++) {
+        i == j;
+      }
+    },
+  });
+
+  await runner.run({
+    id: "i === j",
+    run: () => {
+      for (let i = 0; i < forRuns; i++) {
+        i === j;
+      }
+    },
+  });
 
   await runner.run({
     id: "bit opts",
