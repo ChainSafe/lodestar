b2cac1dcf709ee44fb9f5c11145a543aa5fcd9ed
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2021-04-19 17:38:08 +0200

    Don't log ErrorAborted in rest api errorHandler (#2391)

--- packages/lodestar/src/api/rest/errorHandler.ts
+++ packages/lodestar/src/api/rest/errorHandler.ts
@@ -1,5 +1,6 @@
 import {FastifyError, FastifyReply, FastifyRequest} from "fastify";
 import {ServerResponse} from "http";
+import {ErrorAborted} from "@chainsafe/lodestar-utils";
 import {ApiError} from "../impl/errors";
 
 export function errorHandler(e: Error, req: FastifyRequest, resp: FastifyReply<ServerResponse>): void {
@@ -8,7 +9,13 @@ export function errorHandler(e: Error, req: FastifyRequest, resp: FastifyReply<S
     resp.status(400).send((e as FastifyError).validation);
     return;
   }
+
+  // Don't log ErrorAborted errors, they happen on node shutdown and are not usefull
+  if (!(e instanceof ErrorAborted)) {
+    const config = (resp.context.config || {}) as {url: string};
+    req.log.error(`Request ${req.id} ${config.url} failed with unexpected error: `, e.stack || e.message);
+  }
+
   const statusCode = e instanceof ApiError ? (e as ApiError).statusCode : 500;
-  req.log.error(`Request ${req.id} failed with unexpected error: `, e.message, e.stack);
   resp.status(statusCode).send(e);
 }
