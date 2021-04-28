e7044355f54371e42cbf203a8485dc05da549806
dapplion <35266934+dapplion@users.noreply.github.com>
2021-04-28 14:32:37 +0200

    Strip 0x from pre-computed ForkDigest hex

--- packages/lodestar/src/util/forkDigestContext.ts
+++ packages/lodestar/src/util/forkDigestContext.ts
@@ -2,7 +2,7 @@ import {computeForkDigest} from "@chainsafe/lodestar-beacon-state-transition";
 import {ForkName, IBeaconConfig} from "@chainsafe/lodestar-config";
 import {ForkDigest, Root} from "@chainsafe/lodestar-types";
 import {mapValues} from "@chainsafe/lodestar-utils";
-import {toHexString} from "@chainsafe/ssz";
+import {ByteVector, toHexString} from "@chainsafe/ssz";
 
 type ForkDigestHex = string;
 
@@ -26,16 +26,23 @@ export class ForkDigestContext implements IForkDigestContext {
     this.forkNameByForkDigest = new Map<string, ForkName>();
     for (const fork of Object.values(config.forks)) {
       const forkDigest = this.forkDigestByForkName[fork.name];
-      this.forkNameByForkDigest.set(toHexString(forkDigest), fork.name);
+      this.forkNameByForkDigest.set(toHexStringNoPrefix(forkDigest), fork.name);
     }
   }
 
   forkDigest2ForkName(forkDigest: ForkDigest | ForkDigestHex): ForkName | undefined {
-    const hex = typeof forkDigest === "string" ? forkDigest : toHexString(forkDigest);
-    return this.forkNameByForkDigest.get(hex);
+    return this.forkNameByForkDigest.get(toHexStringNoPrefix(forkDigest));
   }
 
   forkName2ForkDigest(forkName: ForkName): ForkDigest {
     return this.forkDigestByForkName[forkName];
   }
+}
+
+function toHexStringNoPrefix(hex: string | ByteVector): string {
+  return strip0xPrefix(typeof hex === "string" ? hex : toHexString(hex));
+}
+
+function strip0xPrefix(hex: string): string {
+  return hex.startsWith("0x") ? hex.slice(2) : hex;
 }
--- packages/lodestar/test/unit/network/gossip/topic.test.ts
+++ packages/lodestar/test/unit/network/gossip/topic.test.ts
@@ -1,5 +1,5 @@
 import {expect} from "chai";
-import sinon, {SinonStubbedInstance} from "sinon";
+import {config} from "@chainsafe/lodestar-config/mainnet";
 import {ForkName} from "@chainsafe/lodestar-config";
 import {
   getGossipTopic,
@@ -11,42 +11,40 @@ import {
 import {ForkDigestContext} from "../../../../src/util/forkDigestContext";
 
 describe("GossipTopic", function () {
-  let forkDigestContext: SinonStubbedInstance<ForkDigestContext>;
+  const genesisValidatorsRoot = config.types.Root.defaultValue();
+  const forkDigestContext = new ForkDigestContext(config, genesisValidatorsRoot);
 
-  beforeEach(() => {
-    forkDigestContext = sinon.createStubInstance(ForkDigestContext);
-  });
-
-  it("should round trip encode/decode gossip topics", async () => {
-    const topics: GossipTopic[] = [
-      {type: GossipType.beacon_block, fork: ForkName.phase0, encoding: DEFAULT_ENCODING},
-      {type: GossipType.beacon_aggregate_and_proof, fork: ForkName.phase0, encoding: DEFAULT_ENCODING},
-      {type: GossipType.beacon_attestation, fork: ForkName.phase0, subnet: 5, encoding: DEFAULT_ENCODING},
-      {type: GossipType.voluntary_exit, fork: ForkName.phase0, encoding: DEFAULT_ENCODING},
-      {type: GossipType.proposer_slashing, fork: ForkName.phase0, encoding: DEFAULT_ENCODING},
-      {type: GossipType.attester_slashing, fork: ForkName.phase0, encoding: DEFAULT_ENCODING},
-    ];
-    for (const topic of topics) {
+  const topics: GossipTopic[] = [
+    {type: GossipType.beacon_block, fork: ForkName.phase0, encoding: DEFAULT_ENCODING},
+    {type: GossipType.beacon_aggregate_and_proof, fork: ForkName.phase0, encoding: DEFAULT_ENCODING},
+    {type: GossipType.beacon_attestation, fork: ForkName.phase0, subnet: 5, encoding: DEFAULT_ENCODING},
+    {type: GossipType.voluntary_exit, fork: ForkName.phase0, encoding: DEFAULT_ENCODING},
+    {type: GossipType.proposer_slashing, fork: ForkName.phase0, encoding: DEFAULT_ENCODING},
+    {type: GossipType.attester_slashing, fork: ForkName.phase0, encoding: DEFAULT_ENCODING},
+  ];
+  for (const topic of topics) {
+    it(`should round trip encode/decode gossip topic ${topic.type} ${topic.fork} ${topic.encoding}`, async () => {
       const topicString = getGossipTopicString(forkDigestContext, topic);
       const outputTopic = getGossipTopic(forkDigestContext, topicString);
       expect(outputTopic).to.deep.equal(topic);
-    }
-  });
-  it("should fail to decode invalid gossip topic strings", async () => {
-    const topicStrings: string[] = [
-      // completely invalid
-      "/different/protocol/entirely",
-      // invalid fork digest
-      "/eth2/ffffffff/beacon_attestation_5/ssz_snappy",
-      // invalid gossip type
-      "/eth2/18ae4ccb/beacon_attestation_foo/ssz_snappy",
-      // invalid gossip type
-      "/eth2/18ae4ccb/something_different/ssz_snappy",
-      // invalid encoding
-      "/eth2/18ae4ccb/beacon_attestation_5/ssz_supersnappy",
-    ];
-    for (const topicString of topicStrings) {
+    });
+  }
+
+  const topicStrings: string[] = [
+    // completely invalid
+    "/different/protocol/entirely",
+    // invalid fork digest
+    "/eth2/ffffffff/beacon_attestation_5/ssz_snappy",
+    // invalid gossip type
+    "/eth2/18ae4ccb/beacon_attestation_foo/ssz_snappy",
+    // invalid gossip type
+    "/eth2/18ae4ccb/something_different/ssz_snappy",
+    // invalid encoding
+    "/eth2/18ae4ccb/beacon_attestation_5/ssz_supersnappy",
+  ];
+  for (const topicString of topicStrings) {
+    it(`should fail to decode invalid gossip topic string ${topicString}`, async () => {
       expect(() => getGossipTopic(forkDigestContext, topicString), topicString).to.throw();
-    }
-  });
+    });
+  }
 });
