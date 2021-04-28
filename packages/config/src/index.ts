82acd0b348141ba44ae22807a73392a83de6484e
dapplion <35266934+dapplion@users.noreply.github.com>
2021-04-28 21:58:48 +0200

    Sort config.forks by ascending order

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -9,18 +9,22 @@ export function createIBeaconConfig(params: IBeaconParams): IBeaconConfig {
 
   const phase0 = {name: ForkName.phase0, epoch: GENESIS_EPOCH, version: params.GENESIS_FORK_VERSION};
   const altair = {name: ForkName.altair, epoch: params.ALTAIR_FORK_EPOCH, version: params.ALTAIR_FORK_VERSION};
+  // Prevents allocating an array on every getForkInfo() call
+  const forksDescendingEpochOrder = [altair, phase0];
 
   return {
     params,
     types,
 
-    forks: {altair, phase0},
+    /** Forks in order order of occurence, `phase0` first */
+    // Note: Downstream code relies on proper ordering.
+    forks: {phase0, altair},
 
     // Fork convenience methods
     getForkInfo(slot: Slot) {
       const epoch = Math.floor(slot / this.params.SLOTS_PER_EPOCH);
       // NOTE: forks must be sorted by descending epoch, latest fork first
-      for (const fork of [altair, phase0]) {
+      for (const fork of forksDescendingEpochOrder) {
         if (epoch >= fork.epoch) return fork;
       }
       return phase0;
---
+++ packages/config/test/unit/index.test.ts
@@ -0,0 +1,17 @@
+import {expect} from "chai";
+import {config} from "../../src/presets/mainnet";
+
+describe("forks", () => {
+  it("Forks should be in ascending order", () => {
+    const forks = Object.values(config.forks);
+    for (let i = 0; i < forks.length - 1; i++) {
+      const fork1 = forks[i];
+      const fork2 = forks[i + 1];
+
+      expect(fork1.epoch < fork2.epoch).to.be.equal(
+        true,
+        `Forks are not sorted ${fork1.name} ${fork1.epoch} -> ${fork2.name} ${fork2.epoch}`
+      );
+    }
+  });
+});
