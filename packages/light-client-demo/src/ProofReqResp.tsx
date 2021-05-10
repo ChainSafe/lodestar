77994bb7c5a711d05a75b3528df9c19e0dfb6628
dapplion <35266934+dapplion@users.noreply.github.com>
2021-05-10 19:09:14 +0200

    Don't crash proof UI

--- packages/light-client-demo/src/ProofReqResp.tsx
+++ packages/light-client-demo/src/ProofReqResp.tsx
@@ -23,21 +23,20 @@ function renderProof(proof: TreeOffsetProof): string {
 }
 
 export function ProofReqResp({client}: {client: Lightclient}): JSX.Element {
-  const [reqStatusProof, setReqStatusProof] = useState<ReqStatus<TreeOffsetProof>>({});
+  const [reqStatusProof, setReqStatusProof] = useState<ReqStatus<{proof: TreeOffsetProof; stateStr: string}>>({});
   const [pathsStr, setPaths] = useState(JSON.stringify([["slot"], ["validators", 0, "exitEpoch"]], null, 2));
-  const [state, setState] = useState<altair.BeaconState>();
-
-  const paths: Path[] = JSON.parse(pathsStr);
-  const stateStr = renderState(paths, state ?? null);
 
   async function fetchProof(): Promise<void> {
     try {
       setReqStatusProof({loading: true});
-      const proof = await client.getStateProof(paths);
-      if (proof.leaves.length > 0) {
-        setState(client.config.types.altair.BeaconState.createTreeBackedFromProofUnsafe(proof));
+      const pathsQueried = JSON.parse(pathsStr);
+      const proof = await client.getStateProof(pathsQueried);
+      if (proof.leaves.length <= 0) {
+        throw Error("Empty proof");
       }
-      setReqStatusProof({result: proof});
+      const state = client.config.types.altair.BeaconState.createTreeBackedFromProofUnsafe(proof);
+      const stateStr = renderState(pathsQueried, state ?? null);
+      setReqStatusProof({result: {proof, stateStr}});
     } catch (e) {
       setReqStatusProof({error: e});
     }
@@ -59,25 +58,26 @@ export function ProofReqResp({client}: {client: Lightclient}): JSX.Element {
               />
             </div>
           </div>
+
+          {reqStatusProof.loading && <p>Fetching proof...</p>}
+          {reqStatusProof.error && <ErrorView error={reqStatusProof.error} />}
+
+          <div className="field">
+            <div className="control">
+              <button className="button is-primary" onClick={fetchProof} disabled={reqStatusProof.loading}>
+                Submit
+              </button>
+            </div>
+          </div>
         </div>
+
         <div className="column section" style={{whiteSpace: "pre"}}>
           <div className="subtitle">State</div>
-          <div>{stateStr}</div>
+          {reqStatusProof.result && <div>{reqStatusProof.result.stateStr}</div>}
 
           <div className="subtitle">Proof</div>
-          {reqStatusProof.result && <div>{renderProof(reqStatusProof.result)}</div>}
+          {reqStatusProof.result && <div>{renderProof(reqStatusProof.result.proof)}</div>}
           <br />
-        </div>
-      </div>
-
-      {reqStatusProof.loading && <p>Fetching proof...</p>}
-      {reqStatusProof.error && <ErrorView error={reqStatusProof.error} />}
-
-      <div className="field">
-        <div className="control">
-          <button className="button is-primary" onClick={fetchProof} disabled={reqStatusProof.loading}>
-            Submit
-          </button>
         </div>
       </div>
     </div>
