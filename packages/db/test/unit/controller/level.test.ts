8dd1121c91d13df23449db981eff2cbf82dfded9
dapplion <35266934+dapplion@users.noreply.github.com>
2021-05-15 12:11:15 +0200

    Fix type issue with leveldown

--- packages/db/test/unit/controller/level.test.ts
+++ packages/db/test/unit/controller/level.test.ts
@@ -15,8 +15,7 @@ describe("LevelDB controller", () => {
   after(async () => {
     await db.stop();
     await new Promise<void>((resolve, reject) => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-      leveldown.destroy(dbLocation, (err: Error) => {
+      leveldown.destroy(dbLocation, (err) => {
         if (err) reject(err);
         else resolve();
       });
