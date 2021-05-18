9f181d497433556fb10b56ee53489ba3480f7c8d
Tuyen Nguyen <vutuyen2636@gmail.com>
2021-05-18 15:44:43 +0700

    Impl translate_participation when upgrading beacon state from phase0 to altair

--- packages/beacon-state-transition/src/altair/upgradeState.ts
+++ packages/beacon-state-transition/src/altair/upgradeState.ts
@@ -1,18 +1,23 @@
 import {altair, ParticipationFlags, phase0, Uint8} from "@chainsafe/lodestar-types";
 import {CachedBeaconState, createCachedBeaconState} from "../allForks/util";
-import {getCurrentEpoch, newZeroedArray} from "../util";
+import {getBlockRoot, getBlockRootAtSlot, getCurrentEpoch, newZeroedArray} from "../util";
 import {List, TreeBacked} from "@chainsafe/ssz";
 import {getSyncCommittee} from "./state_accessor";
 import {IBeaconConfig} from "@chainsafe/lodestar-config";
+import {IParticipationStatus} from "../allForks/util/cachedEpochParticipation";
 
 /**
  * Upgrade a state from phase0 to altair.
- * @param state
  */
 export function upgradeState(state: CachedBeaconState<phase0.BeaconState>): CachedBeaconState<altair.BeaconState> {
   const {config} = state;
-  const postState = upgradeTreeBackedState(config, config.types.phase0.BeaconState.createTreeBacked(state.tree));
-  return createCachedBeaconState(config, postState);
+  const postTreeBackedState = upgradeTreeBackedState(
+    config,
+    config.types.phase0.BeaconState.createTreeBacked(state.tree)
+  );
+  const postState = createCachedBeaconState(config, postTreeBackedState);
+  translateParticipation(postState, Array.from(state.previousEpochAttestations));
+  return postState;
 }
 
 function upgradeTreeBackedState(
@@ -27,7 +32,6 @@ function upgradeTreeBackedState(
     currentVersion: config.params.ALTAIR_FORK_VERSION,
     epoch,
   };
-  // TODO: translate_participation in https://github.com/ethereum/eth2.0-specs/blob/dev/specs/altair/fork.md
   postState.previousEpochParticipation = newZeroedArray(validatorCount) as List<ParticipationFlags>;
   postState.currentEpochParticipation = newZeroedArray(validatorCount) as List<ParticipationFlags>;
   postState.inactivityScores = newZeroedArray(validatorCount) as List<Uint8>;
@@ -35,4 +39,46 @@ function upgradeTreeBackedState(
   postState.currentSyncCommittee = syncCommittee;
   postState.nextSyncCommittee = syncCommittee;
   return postState;
+}
+
+/**
+ * Translate_participation in https://github.com/ethereum/eth2.0-specs/blob/dev/specs/altair/fork.md
+ */
+function translateParticipation(
+  state: CachedBeaconState<altair.BeaconState>,
+  pendingAttesations: phase0.PendingAttestation[]
+): void {
+  const {config} = state;
+  const epochParticipation = state.previousEpochParticipation;
+  const currentEpoch = state.currentShuffling.epoch;
+  for (const attestation of pendingAttesations) {
+    const data = attestation.data;
+    let justifiedCheckpoint;
+    if (data.target.epoch === currentEpoch) {
+      justifiedCheckpoint = state.currentJustifiedCheckpoint;
+    } else {
+      justifiedCheckpoint = state.previousJustifiedCheckpoint;
+    }
+    const isMatchingSource = config.types.phase0.Checkpoint.equals(data.source, justifiedCheckpoint);
+    if (!isMatchingSource) {
+      throw new Error(
+        "Attestation source does not equal justified checkpoint: " +
+          `source=${config.types.phase0.Checkpoint.toJson(data.source)} ` +
+          `justifiedCheckpoint=${config.types.phase0.Checkpoint.toJson(justifiedCheckpoint)}`
+      );
+    }
+    const isMatchingTarget = config.types.Root.equals(data.target.root, getBlockRoot(config, state, data.target.epoch));
+    const isMatchingHead =
+      isMatchingTarget && config.types.Root.equals(data.beaconBlockRoot, getBlockRootAtSlot(config, state, data.slot));
+    const attestingIndices = state.getAttestingIndices(data, attestation.aggregationBits);
+    for (const index of attestingIndices) {
+      const status = epochParticipation.getStatus(index) as IParticipationStatus;
+      const newStatus = {
+        timelyHead: status.timelyHead || isMatchingHead,
+        timelySource: true,
+        timelyTarget: status.timelyTarget || isMatchingTarget,
+      };
+      epochParticipation.setStatus(index, newStatus);
+    }
+  }
 }
