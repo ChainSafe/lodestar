7e945496b2368d026da69d16b7535c679f115667
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2021-05-22 19:37:29 +0200

    Render pre-genesis Eph timestamps better (#2549)

--- packages/utils/test/unit/logger/util.test.ts
+++ packages/utils/test/unit/logger/util.test.ts
@@ -2,13 +2,21 @@ import {expect} from "chai";
 import {formatEpochSlotTime} from "../../../src/logger/util";
 
 describe("logger / util / formatEpochSlotTime", () => {
-  it("Should format epoch slot time", () => {
-    const now = 1619171569709;
-    const genesisTime = (now - 1235423) / 1000;
-    const secondsPerSlot = 12;
-    const slotsPerEpoch = 32;
-    const expectLog = "Eph 3/6 11.423";
+  const nowSec = 1619171569;
+  const secondsPerSlot = 12;
+  const slotsPerEpoch = 32;
 
-    expect(formatEpochSlotTime({genesisTime, secondsPerSlot, slotsPerEpoch}, now)).to.equal(expectLog);
-  });
+  const testCases: {epoch: number; slot: number; sec: number}[] = [
+    {epoch: 3, slot: 6, sec: 11.423},
+    {epoch: -1, slot: 31, sec: 11.423},
+    {epoch: 0, slot: 0, sec: 0.001},
+  ];
+
+  for (const {epoch, slot, sec} of testCases) {
+    const expectLog = `Eph ${epoch}/${slot} ${sec}`; // "Eph 3/6 11.423";
+    it(expectLog, () => {
+      const genesisTime = nowSec - epoch * slotsPerEpoch * secondsPerSlot - slot * secondsPerSlot - sec;
+      expect(formatEpochSlotTime({genesisTime, secondsPerSlot, slotsPerEpoch}, nowSec * 1000)).to.equal(expectLog);
+    });
+  }
 });
