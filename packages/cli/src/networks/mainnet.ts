aed3229dc2a2e3df4029a04dbb65d4d902d911ca
dapplion <35266934+dapplion@users.noreply.github.com>
2021-05-28 11:34:06 +0200

    Let minimal FORK_VERSION take prevalence in CLI

--- packages/cli/src/networks/mainnet.ts
+++ packages/cli/src/networks/mainnet.ts
@@ -11,7 +11,8 @@ export const beaconParams: IBeaconParamsUnparsed = {
 
   MIN_GENESIS_TIME: 1606824000, // Tuesday, December 1, 2020 12:00:00 PM UTC
   GENESIS_DELAY: 604800,
-  GENESIS_FORK_VERSION: "0x00000000",
+  // MUST NOT use `GENESIS_FORK_VERSION` here so for `minimal` networks the preset value of 0x00000001 take prevalence
+  // GENESIS_FORK_VERSION: "0x00000000",
 };
 
 export const depositContractDeployBlock = 11052984;
