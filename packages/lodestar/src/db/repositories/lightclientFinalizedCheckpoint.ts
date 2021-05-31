441abc8b4613729e0c9144bc4e2f8217f41995db
dapplion <35266934+dapplion@users.noreply.github.com>
2021-05-31 10:52:15 +0200

    Update LightclientFinalizedCheckpoint ssz type

--- packages/lodestar/src/db/repositories/lightclientFinalizedCheckpoint.ts
+++ packages/lodestar/src/db/repositories/lightclientFinalizedCheckpoint.ts
@@ -3,10 +3,18 @@ import {IBeaconConfig} from "@chainsafe/lodestar-config";
 import {bytesToInt} from "@chainsafe/lodestar-utils";
 import {IDatabaseController, Bucket, Repository} from "@chainsafe/lodestar-db";
 import {FinalizedCheckpointData} from "@chainsafe/lodestar-light-client/lib/server/LightClientUpdater";
+import {ContainerType} from "@chainsafe/ssz";
 
 export class LightclientFinalizedCheckpoint extends Repository<SyncPeriod, FinalizedCheckpointData> {
   constructor(config: IBeaconConfig, db: IDatabaseController<Buffer, Buffer>) {
-    const type = config.types.altair.LightClientUpdate;
+    // Pick<LightClientUpdate, "header" | "nextSyncCommittee" | "nextSyncCommitteeBranch">
+    const type = new ContainerType<FinalizedCheckpointData>({
+      fields: {
+        header: config.types.altair.LightClientUpdate.getPropertyType("header"),
+        nextSyncCommittee: config.types.altair.LightClientUpdate.getPropertyType("nextSyncCommittee"),
+        nextSyncCommitteeBranch: config.types.altair.LightClientUpdate.getPropertyType("nextSyncCommitteeBranch"),
+      },
+    });
     super(config, db, Bucket.altair_lightclientFinalizedCheckpoint, type);
   }
 
