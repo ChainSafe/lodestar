d45b5d10689ef875aebf0169cfff17d3a222444e
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2021-06-01 18:59:19 +0200

    Add fieldName on createIBeaconParams error (#2604)

--- packages/params/src/utils.ts
+++ packages/params/src/utils.ts
@@ -8,7 +8,12 @@ export function createIBeaconParams(input: Record<string, unknown>): Partial<IBe
   const params: Partial<IBeaconParams> = {};
   for (const [fieldName, fieldType] of Object.entries(BeaconParams.fields)) {
     if (input[fieldName] != null) {
-      (params as Record<string, unknown>)[fieldName] = fieldType.fromJson(input[fieldName] as Json) as unknown;
+      try {
+        (params as Record<string, unknown>)[fieldName] = fieldType.fromJson(input[fieldName] as Json) as unknown;
+      } catch (e) {
+        (e as Error).message = `Error parsing '${fieldName}': ${(e as Error).message}`;
+        throw e;
+      }
     }
   }
   return params;
