54ec19ebb9e75506206a6cfa8a2a01e435a4211e
dapplion <35266934+dapplion@users.noreply.github.com>
2021-06-03 22:33:09 +0200

    Impove processAttestation for altair

--- packages/beacon-state-transition/src/altair/misc.ts
+++ packages/beacon-state-transition/src/altair/misc.ts
@@ -1,4 +1,6 @@
-import {ParticipationFlags} from "@chainsafe/lodestar-types";
+import {IBeaconConfig} from "@chainsafe/lodestar-config";
+import {Gwei, ParticipationFlags} from "@chainsafe/lodestar-types";
+import {bigIntSqrt} from "@chainsafe/lodestar-utils";
 import {TIMELY_HEAD_WEIGHT, TIMELY_SOURCE_WEIGHT, TIMELY_TARGET_WEIGHT} from "./constants";
 
 export const PARTICIPATION_FLAG_WEIGHTS = [TIMELY_SOURCE_WEIGHT, TIMELY_TARGET_WEIGHT, TIMELY_HEAD_WEIGHT];
@@ -10,4 +12,11 @@ export function addFlag(flags: ParticipationFlags, flagIndex: number): Participa
 export function hasFlag(flags: ParticipationFlags, flagIndex: number): boolean {
   const flag = 2 ** flagIndex;
   return (flags & flag) == flag;
+}
+
+export function computeBaseRewardPerIncrement(config: IBeaconConfig, totalActiveStake: Gwei): bigint {
+  return (
+    (config.params.EFFECTIVE_BALANCE_INCREMENT * BigInt(config.params.BASE_REWARD_FACTOR)) /
+    bigIntSqrt(totalActiveStake)
+  );
 }
