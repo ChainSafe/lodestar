12cf1123ad0e00a90683f450ed2593acf6305c79
Tuyen Nguyen <vutuyen2636@gmail.com>
2021-06-09 10:53:15 +0700

    Fix getParticipantIndices() for struct

--- packages/beacon-state-transition/src/util/syncCommittee.ts
+++ packages/beacon-state-transition/src/util/syncCommittee.ts
@@ -4,9 +4,12 @@ import {
   SYNC_COMMITTEE_SUBNET_COUNT,
   TARGET_AGGREGATORS_PER_SYNC_SUBCOMMITTEE,
 } from "@chainsafe/lodestar-params";
+import {altair} from "@chainsafe/lodestar-types";
+import {ssz} from "@chainsafe/lodestar-types";
 import {BLSSignature, phase0} from "@chainsafe/lodestar-types";
 import {bytesToInt, intDiv} from "@chainsafe/lodestar-utils";
-import {hash} from "@chainsafe/ssz";
+import {BitList, hash, isTreeBacked, TreeBacked} from "@chainsafe/ssz";
+import {zipIndexesInBitList} from "./aggregationBits";
 
 /**
  * TODO
@@ -24,4 +27,25 @@ export function isSyncCommitteeAggregator(selectionProof: BLSSignature): boolean
     intDiv(intDiv(SYNC_COMMITTEE_SIZE, SYNC_COMMITTEE_SUBNET_COUNT), TARGET_AGGREGATORS_PER_SYNC_SUBCOMMITTEE)
   );
   return bytesToInt(hash(selectionProof.valueOf() as Uint8Array).slice(0, 8)) % modulo === 0;
+}
+
+export function extractParticipantIndices(
+  committeeIndices: phase0.ValidatorIndex[],
+  syncAggregate: altair.SyncAggregate
+): phase0.ValidatorIndex[] {
+  if (isTreeBacked(syncAggregate)) {
+    return zipIndexesInBitList(
+      committeeIndices,
+      syncAggregate.syncCommitteeBits as TreeBacked<BitList>,
+      ssz.altair.SyncCommitteeBits
+    );
+  } else {
+    const participantIndices = [];
+    for (const [i, index] of committeeIndices.entries()) {
+      if (syncAggregate.syncCommitteeBits[i]) {
+        participantIndices.push(index);
+      }
+    }
+    return participantIndices;
+  }
 }
