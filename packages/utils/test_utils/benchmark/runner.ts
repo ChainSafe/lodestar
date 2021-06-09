caa1d6f61eb1989a9dd9d4b94d504e6c5a530b45
dapplion <35266934+dapplion@users.noreply.github.com>
2021-06-09 17:25:35 +0200

    Upload full benchmark results

--- packages/utils/test_utils/benchmark/runner.ts
+++ packages/utils/test_utils/benchmark/runner.ts
@@ -13,27 +13,24 @@ export type BenchmarkResult = {
   id: string;
   averageNs: number;
   runsDone: number;
+  runsNs: bigint[];
   factor?: number;
 };
 
 export class BenchmarkRunner {
-  opts: BenchmarkOpts;
   results: BenchmarkResult[] = [];
-  constructor(title: string, opts?: BenchmarkOpts) {
+  constructor(private readonly title: string, private readonly opts: BenchmarkOpts = {}) {
     this.opts = opts || {};
     console.log(formatTitle(title));
   }
 
   async run<T1, T2 = T1, R = void>(opts: RunOpts<T1, T2, R>): Promise<number> {
-    const {averageNs, runsDone} = await doRun(opts, this.opts);
-    this.results.push({id: opts.id, averageNs, runsDone});
-    console.log(formatResultRow({id: opts.id, averageNs, runsDone})); // ±1.74%
-
-    return averageNs;
+    const result = await doRun(opts, this.opts);
+    return this.onRun(result);
   }
 
   group(): BenchmarkGroupRunner {
-    return new BenchmarkGroupRunner(this.opts || {}, this.results);
+    return new BenchmarkGroupRunner(this.opts || {}, this.onRun.bind(this));
   }
 
   done(): void {
@@ -45,40 +42,48 @@ export class BenchmarkRunner {
       fs.appendFileSync(absoluteFilePath, formatAsBenchmarkJs(this.results));
     }
   }
+
+  private onRun(result: BenchmarkResult): number {
+    this.results.push(result);
+    console.log(formatResultRow(result)); // ±1.74%
+
+    // Persist full results if requested
+    if (process.env.BENCHMARK_RESULTS_DIR) {
+      const filename = `${this.title.replace(/\s/g, "-")}_${result.id}.csv`;
+      const filepath = path.join(process.env.BENCHMARK_RESULTS_DIR, filename);
+      fs.writeFileSync(filepath, result.runsNs.join("\n"));
+    }
+
+    return result.averageNs;
+  }
 }
 
 class BenchmarkGroupRunner {
   averageNs: number | null = null;
-  opts: BenchmarkOpts;
-  results: BenchmarkResult[];
-  constructor(opts: BenchmarkOpts, results: BenchmarkResult[]) {
-    this.opts = opts || {};
-    this.results = results;
+  constructor(private readonly opts: BenchmarkOpts, private readonly onRun: (result: BenchmarkResult) => number) {
     console.log("---");
   }
 
   async run<T1, T2 = T1, R = void>(opts: RunOpts<T1, T2, R>): Promise<number> {
-    const {averageNs, runsDone} = await doRun(opts, this.opts);
-    this.results.push({id: opts.id, averageNs, runsDone});
+    const result = await doRun(opts, this.opts);
 
-    if (this.averageNs === null) this.averageNs = averageNs;
-    const factor = averageNs / this.averageNs;
+    // Attach factor to result
+    if (this.averageNs === null) this.averageNs = result.averageNs;
+    result.factor = result.averageNs / this.averageNs;
 
-    console.log(formatResultRow({id: opts.id, averageNs, runsDone, factor})); // ±1.74%
-
-    return averageNs;
+    return this.onRun(result);
   }
 }
 
 async function doRun<T1, T2 = T1, R = void>(
   {before, beforeEach, run, check, ...opts}: RunOpts<T1, T2, R>,
   extraOpts: BenchmarkOpts
-): Promise<{averageNs: number; runsDone: number}> {
+): Promise<BenchmarkResult> {
   const runs = opts.runs || extraOpts.runs || 512;
   const maxMs = opts.maxMs || extraOpts.maxMs || 2000;
   const minMs = opts.minMs || extraOpts.minMs || 100;
 
-  const diffsNanoSec: bigint[] = [];
+  const runsNs: bigint[] = [];
 
   const inputAll = before ? await before() : undefined;
 
@@ -93,13 +98,13 @@ async function doRun<T1, T2 = T1, R = void>(
 
     if (check && check(result)) throw Error("Result fails check test");
 
-    diffsNanoSec.push(end - start);
+    runsNs.push(end - start);
   }
 
-  const average = averageBigint(diffsNanoSec);
+  const average = averageBigint(runsNs);
   const averageNs = Number(average);
 
-  return {averageNs, runsDone: i - 1};
+  return {id: opts.id, averageNs, runsDone: i - 1, runsNs};
 }
 
 type PromiseOptional<T> = T | Promise<T>;
