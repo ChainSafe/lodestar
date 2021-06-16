fc230e75e30ec884d3240243b53dfec00c334dfb
3xtr4t3rr3str14l <k0m0d0tr0n@protonmail.com>
2021-06-16 18:08:58 -0500

    get correct validatorCommitteeIndex

--- packages/lodestar/src/chain/factory/duties/index.ts
+++ packages/lodestar/src/chain/factory/duties/index.ts
@@ -1,6 +1,6 @@
 import {routes} from "@chainsafe/lodestar-api";
 import {allForks} from "@chainsafe/lodestar-beacon-state-transition";
-import {BLSPubkey, Epoch, ValidatorIndex} from "@chainsafe/lodestar-types";
+import {BLSPubkey, Epoch, ssz, ValidatorIndex} from "@chainsafe/lodestar-types";
 
 export function assembleAttesterDuties(
   validators: {pubkey: BLSPubkey; index: ValidatorIndex}[],
@@ -11,12 +11,15 @@ export function assembleAttesterDuties(
     .map((validator) => {
       const committeeAssignment = epochCtx.getCommitteeAssignment(epoch, validator.index);
       if (committeeAssignment) {
+        const validatorCommitteeIndex = Object.values(committeeAssignment.validators).findIndex((v) =>
+          ssz.ValidatorIndex.equals(v, validator.index)
+        );
         return {
           pubkey: validator.pubkey,
           validatorIndex: validator.index,
           committeeLength: committeeAssignment.validators.length,
           committeesAtSlot: epochCtx.getCommitteeCountAtSlot(committeeAssignment.slot),
-          validatorCommitteeIndex: validator.index,
+          validatorCommitteeIndex,
           committeeIndex: committeeAssignment.committeeIndex,
           slot: committeeAssignment.slot,
         };
