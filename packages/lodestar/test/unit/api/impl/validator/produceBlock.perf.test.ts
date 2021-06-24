80133d75bd79de9c7fd68eaa4a744adfb221ffc9
3xtr4t3rr3str14l <k0m0d0tr0n@protonmail.com>
2021-06-24 16:25:18 -0500

    assembleBlock stub

--- packages/lodestar/test/unit/api/impl/validator/produceBlock.perf.test.ts
+++ packages/lodestar/test/unit/api/impl/validator/produceBlock.perf.test.ts
@@ -6,16 +6,16 @@ import {ApiModules, getValidatorApi} from "../../../../../src/api";
 import {LocalClock} from "../../../../../src/chain/clock";
 import {Eth1ForBlockProduction} from "../../../../../src/eth1";
 import {IBeaconSync, SyncState} from "../../../../../src/sync";
-import {generateBlockSummary, generateEmptySignedBlock} from "../../../../utils/block";
+import {generateBlockSummary, generateEmptyBlock, generateEmptySignedBlock} from "../../../../utils/block";
 import {testLogger} from "../../../../utils/logger";
 import {ApiImplTestModules, setupApiImplTestServer} from "../index.test";
 import {StateRegenerator} from "../../../../../src/chain/regen";
 import {generateCachedState} from "../../../../utils/state";
 import {generateEmptyAttestation} from "../../../../utils/attestation";
-import {generateDeposit} from "../../../../utils/deposit";
 import {allForks, ssz} from "../../../../../../types/lib";
 import {CachedBeaconState} from "@chainsafe/lodestar-beacon-state-transition";
-import {toHexString} from "@chainsafe/ssz";
+import {SinonStubFn} from "../../../../utils/types";
+import * as blockBodyAssembly from "../../../../../src/chain/factory/block/body";
 
 describe("produceBlock benchmark", () => {
   const logger = testLogger();
@@ -26,19 +26,25 @@ describe("produceBlock benchmark", () => {
   const sandbox = sinon.createSandbox();
   let state: CachedBeaconState<allForks.BeaconState>;
   const currentSlot = 1;
+  let assembleBodyStub: SinonStubFn<typeof blockBodyAssembly["assembleBody"]>;
 
   before(() => {
+    assembleBodyStub = sandbox.stub(blockBodyAssembly, "assembleBody");
     eth1Stub = sinon.createStubInstance(Eth1ForBlockProduction);
     server = setupApiImplTestServer();
     state = generateCachedState({slot: currentSlot});
-    console.log(toHexString(ssz.phase0.BeaconBlockHeader.hashTreeRoot(state.latestBlockHeader)));
-    const summary = generateBlockSummary({slot: currentSlot});
+    const summary = generateBlockSummary({
+      slot: currentSlot,
+      blockRoot: ssz.phase0.BeaconBlockHeader.hashTreeRoot(state.latestBlockHeader),
+    });
     server.forkChoiceStub.getHead.returns(summary);
     server.chainStub.clock = {currentSlot} as LocalClock;
     const regenStub = (server.chainStub.regen = sandbox.createStubInstance(StateRegenerator));
     regenStub.getBlockSlotState.resolves(state);
+    server.dbStub.depositDataRoot.getTreeBacked.resolves(ssz.phase0.DepositDataRootList.defaultTreeBacked());
+    assembleBodyStub.resolves(generateEmptyBlock().body);
     server.dbStub.aggregateAndProof.getBlockAttestations.resolves([generateEmptyAttestation()]);
-    eth1Stub.getEth1DataAndDeposits.resolves({eth1Data: state.eth1Data, deposits: [generateDeposit()]});
+    eth1Stub.getEth1DataAndDeposits.resolves({eth1Data: state.eth1Data, deposits: []});
     modules = {
       chain: server.chainStub,
       config,
@@ -52,11 +58,9 @@ describe("produceBlock benchmark", () => {
   });
 
   it("run the benchmark test", async () => {
-    console.log("test data", state.latestBlockHeader.slot);
     const runner = new BenchmarkRunner("produce block", {
-      maxMs: 60 * 1000,
-      minMs: 15 * 1000,
-      runs: 64,
+      minMs: 1000,
+      runs: 1024,
     });
 
     const api = getValidatorApi(modules);
@@ -64,7 +68,7 @@ describe("produceBlock benchmark", () => {
     await runner.run({
       id: "benchmark produceBlock",
       run: async () => {
-        await api.produceBlock(currentSlot, signedBlock.message.body.randaoReveal, "i wuz here");
+        await api.produceBlock(currentSlot, signedBlock.message.body.randaoReveal, "");
       },
     });
   });
