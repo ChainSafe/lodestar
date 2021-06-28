866da32e87bcba2891edfeca221af2798098d4e8
tuyennhv <vutuyen2636@gmail.com>
2021-06-29 00:19:54 +0700

    Poll node in batch to get validator indices (#2730)
    * Query validator indices in batch
    
    * chore: add log
    
    * Set PUBKEYS_PER_REQUEST to 10
    
    * Loop batches sequentially instead of Promise.all()
    
    * Review PR
    
    Co-authored-by: dapplion <35266934+dapplion@users.noreply.github.com>

---
+++ packages/validator/test/unit/utils/batch.test.ts
@@ -0,0 +1,42 @@
+import {expect} from "chai";
+import {batchItems} from "../../../src/util/batch";
+
+describe("util / batch", function () {
+  const testCases: {items: string[]; expected: string[][]}[] = [
+    {items: [], expected: []},
+    {items: ["1"], expected: [["1"]]},
+    {items: ["1", "2"], expected: [["1", "2"]]},
+    {items: ["1", "2", "3"], expected: [["1", "2"], ["3"]]},
+    {
+      items: ["1", "2", "3", "4"],
+      expected: [
+        ["1", "2"],
+        ["3", "4"],
+      ],
+    },
+    {items: ["1", "2", "3", "4", "5"], expected: [["1", "2"], ["3", "4"], ["5"]]},
+    {
+      items: ["1", "2", "3", "4", "5", "6"],
+      expected: [
+        ["1", "2"],
+        ["3", "4"],
+        ["5", "6"],
+      ],
+    },
+    // Ignore item 7 since it's over the max
+    {
+      items: ["1", "2", "3", "4", "5", "6", "7"],
+      expected: [
+        ["1", "2"],
+        ["3", "4"],
+        ["5", "6"],
+      ],
+    },
+  ];
+
+  for (const {items: pubkeys, expected} of testCases) {
+    it(`Batch ${pubkeys.length} items`, () => {
+      expect(batchItems(pubkeys, {batchSize: 2, maxBatches: 3})).to.deep.equal(expected);
+    });
+  }
+});
