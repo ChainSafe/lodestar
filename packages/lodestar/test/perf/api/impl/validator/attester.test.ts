b58950e6a11d56eff66f301d8d6ddfab9af20e7b
3xtr4t3rr3str14l <k0m0d0tr0n@protonmail.com>
2021-06-29 15:27:46 -0500

    fix lodestar perf tests scheme

--- packages/lodestar/test/perf/api/impl/validator/attester.test.ts
+++ packages/lodestar/test/perf/api/impl/validator/attester.test.ts
@@ -1,5 +1,5 @@
 import {config} from "@chainsafe/lodestar-config/default";
-import {createCachedBeaconState} from "@chainsafe/lodestar-beacon-state-transition";
+import {CachedBeaconState, createCachedBeaconState} from "@chainsafe/lodestar-beacon-state-transition";
 
 import {ForkChoice} from "../../../../../src/chain";
 import {LocalClock} from "../../../../../src/chain/clock";
@@ -10,58 +10,68 @@ import {setupApiImplTestServer} from "../../../../unit/api/impl/index.test";
 import {BLSPubkey, ssz} from "@chainsafe/lodestar-types";
 import {MAX_EFFECTIVE_BALANCE, FAR_FUTURE_EPOCH} from "@chainsafe/lodestar-params";
 import {itBench, setBenchOpts} from "@dapplion/benchmark";
+import {BeaconState} from "../../../../../../beacon-state-transition/src/allForks";
 
-describe("getCommitteeAssignments vs assembleAttesterDuties performance test", async () => {
-  const server = setupApiImplTestServer();
-  const chainStub = server.chainStub;
-  const syncStub = server.syncStub;
-  chainStub.clock = server.sandbox.createStubInstance(LocalClock);
-  chainStub.forkChoice = server.sandbox.createStubInstance(ForkChoice);
-  chainStub.getCanonicalBlockAtSlot.resolves(ssz.phase0.SignedBeaconBlock.defaultValue());
-  const dbStub = server.dbStub;
+describe("getCommitteeAssignments performance test", () => {
+  let state: CachedBeaconState<BeaconState>;
+  let indices: number[];
 
-  const numValidators = 200000;
-  const numAttachedValidators = 200;
+  before(function () {
+    this.timeout(60 * 1000);
+    const server = setupApiImplTestServer();
+    const chainStub = server.chainStub;
+    const syncStub = server.syncStub;
+    chainStub.clock = server.sandbox.createStubInstance(LocalClock);
+    chainStub.forkChoice = server.sandbox.createStubInstance(ForkChoice);
+    chainStub.getCanonicalBlockAtSlot.resolves(ssz.phase0.SignedBeaconBlock.defaultValue());
+    const dbStub = server.dbStub;
 
-  const validators = generateValidators(numValidators, {
-    effectiveBalance: MAX_EFFECTIVE_BALANCE,
-    activationEpoch: 0,
-    exitEpoch: FAR_FUTURE_EPOCH,
-  });
-  syncStub.isSynced.returns(true);
-  server.sandbox.stub(chainStub.clock, "currentEpoch").get(() => 0);
-  server.sandbox.stub(chainStub.clock, "currentSlot").get(() => 0);
-  dbStub.block.get.resolves({message: {stateRoot: Buffer.alloc(32)}} as any);
-  const _state = generateState(
-    {
-      slot: 0,
-      validators,
-      balances: generateInitialMaxBalances(config, numValidators),
-    },
-    config
-  );
-  const state = createCachedBeaconState(config, _state);
-  chainStub.getHeadStateAtCurrentEpoch.resolves(state);
+    const numValidators = 200000;
+    const numAttachedValidators = 200;
 
-  const indices = Array.from({length: numAttachedValidators}, (_, i) => i * 5);
+    const validators = generateValidators(numValidators, {
+      effectiveBalance: MAX_EFFECTIVE_BALANCE,
+      activationEpoch: 0,
+      exitEpoch: FAR_FUTURE_EPOCH,
+    });
+    syncStub.isSynced.returns(true);
+    server.sandbox.stub(chainStub.clock, "currentEpoch").get(() => 0);
+    server.sandbox.stub(chainStub.clock, "currentSlot").get(() => 0);
+    dbStub.block.get.resolves({message: {stateRoot: Buffer.alloc(32)}} as any);
+    const _state = generateState(
+      {
+        slot: 0,
+        validators,
+        balances: generateInitialMaxBalances(config, numValidators),
+      },
+      config
+    );
+    state = createCachedBeaconState(config, _state);
+    chainStub.getHeadStateAtCurrentEpoch.resolves(state);
 
-  setBenchOpts({
-    maxMs: 10 * 1000,
-    minMs: 2 * 1000,
-    runs: 1024,
+    indices = Array.from({length: numAttachedValidators}, (_, i) => i * 5);
+
+    setBenchOpts({
+      maxMs: 10 * 1000,
+      minMs: 2 * 1000,
+      runs: 1024,
+    });
   });
 
   // the new way of getting attester duties
-  itBench("getCommitteeAssignments", () => {
-    const validators = state.validators.persistent;
-    const validatorData: Map<number, BLSPubkey> = new Map<number, BLSPubkey>();
-    for (const index of indices) {
-      const validator = validators.get(index);
-      if (!validator) {
-        throw new Error(`Validator pubkey at index ${index} not in state`);
+  itBench({
+    id: "getCommitteeAssignments",
+    fn: () => {
+      const persistentValidators = state.validators.persistent;
+      const validatorData: Map<number, BLSPubkey> = new Map<number, BLSPubkey>();
+      for (const index of indices) {
+        const validator = persistentValidators.get(index);
+        if (!validator) {
+          throw new Error(`Validator pubkey at index ${index} not in state`);
+        }
+        validatorData.set(index, validator.pubkey);
       }
-      validatorData.set(index, validator.pubkey);
-    }
-    state.epochCtx.getCommitteeAssignments(0, validatorData);
+      state.epochCtx.getCommitteeAssignments(0, validatorData);
+    },
   });
 });
--- packages/lodestar/test/perf/chain/stateCache/stateContextCheckpointsCache.test.ts
+++ packages/lodestar/test/perf/chain/stateCache/stateContextCheckpointsCache.test.ts
@@ -1,23 +1,33 @@
 import {itBench, setBenchOpts} from "@dapplion/benchmark";
 import {config} from "@chainsafe/lodestar-config/default";
-import {ssz} from "@chainsafe/lodestar-types";
+import {allForks, ssz} from "@chainsafe/lodestar-types";
 import {generateCachedState} from "../../../utils/state";
 import {CheckpointStateCache} from "../../../../src/chain/stateCache";
+import {Checkpoint} from "../../../../../types/phase0";
+import {CachedBeaconState} from "@chainsafe/lodestar-beacon-state-transition";
 
 describe("CheckpointStateCache perf tests", function () {
-  setBenchOpts({
-    maxMs: 60 * 1000,
-    minMs: 1 * 1000,
-    runs: 1024,
-  });
+  let state: CachedBeaconState<allForks.BeaconState>;
+  let checkpoint: Checkpoint;
+  let checkpointStateCache: CheckpointStateCache;
 
-  const checkpointStateCache = new CheckpointStateCache(config);
-  const state = generateCachedState();
-  const checkpoint = ssz.phase0.Checkpoint.defaultValue();
+  itBench({
+    id: "CheckpointStateCache - add get delete",
+    beforeEach: () => {
+      setBenchOpts({
+        maxMs: 60 * 1000,
+        minMs: 1 * 1000,
+        runs: 1024,
+      });
 
-  itBench("CheckpointStateCache - add get delete", () => {
-    checkpointStateCache.add(checkpoint, state);
-    checkpointStateCache.get(checkpoint);
-    checkpointStateCache.delete(checkpoint);
+      checkpointStateCache = new CheckpointStateCache(config);
+      state = generateCachedState();
+      checkpoint = ssz.phase0.Checkpoint.defaultValue();
+    },
+    fn: () => {
+      checkpointStateCache.add(checkpoint, state);
+      checkpointStateCache.get(checkpoint);
+      checkpointStateCache.delete(checkpoint);
+    },
   });
 });
