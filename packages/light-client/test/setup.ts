5173df2453acd6f62b1c152980049e6c7fe4ec8a
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2021-06-30 16:06:03 +0200

    Run benchmark script once (#2778)
    * Disable unnecessary benchmarks
    
    * Run all benchmarks at once
    
    * Update perf glob
    
    * Use syncronous bls initialization
    
    * Don't run benchmarks on forks
    
    * Update lodestar benchmark
    
    * Benchmark BLS ops
    
    * Remove test:e2e script in lightclient

--- packages/light-client/test/setup.ts
+++ packages/light-client/test/setup.ts
@@ -1,5 +1,9 @@
 import {init} from "@chainsafe/bls";
 
-before(async function () {
-  await init("blst-native");
+// blst-native initialization is syncronous
+// Initialize bls here instead of in before() so it's available inside describe() blocks
+init("blst-native").catch((e) => {
+  // eslint-disable-next-line no-console
+  console.error(e);
+  process.exit(1);
 });
