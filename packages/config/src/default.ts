ce75d5ac4e57669812bbb552e473c1f9677cb5c6
Tuyen Nguyen <vutuyen2636@gmail.com>
2021-06-30 15:24:20 +0700

    New IBeaconConfig with the cache apis

--- packages/config/src/default.ts
+++ packages/config/src/default.ts
@@ -1,5 +1,6 @@
-import {createIBeaconConfig} from "./beaconConfig";
+import {createIChainForkConfig} from "../src";
 import {defaultChainConfig} from "./chainConfig";
 
 export const chainConfig = defaultChainConfig;
-export const config = createIBeaconConfig(defaultChainConfig);
+// for testing purpose only
+export const config = createIChainForkConfig(defaultChainConfig);
