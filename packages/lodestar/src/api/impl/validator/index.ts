2b3bf2e3585c6c8256f12b633a99974834fe008e
harkamal <gajinder@g11.in>
2021-07-06 16:07:02 +0530

    block production metrics

--- packages/lodestar/src/api/impl/validator/index.ts
+++ packages/lodestar/src/api/impl/validator/index.ts
@@ -136,18 +136,24 @@ export function getValidatorApi({
 
   return {
     async produceBlock(slot, randaoReveal, graffiti = "") {
-      notWhileSyncing();
+      let timer;
+      metrics?.blockProductionRequests.inc();
+      try {
+        notWhileSyncing();
+        await waitForSlot(slot); // Must never request for a future slot > currentSlot
 
-      await waitForSlot(slot); // Must never request for a future slot > currentSlot
-
-      const block = await assembleBlock(
-        {config: config, chain: chain, db: db, eth1: eth1, metrics: metrics},
-        slot,
-        randaoReveal,
-        toGraffitiBuffer(graffiti)
-      );
-
-      return {data: block, version: config.getForkName(block.slot)};
+        timer = metrics?.blockProductionTime.startTimer();
+        const block = await assembleBlock(
+          {config: config, chain: chain, db: db, eth1: eth1, metrics: metrics},
+          slot,
+          randaoReveal,
+          toGraffitiBuffer(graffiti)
+        );
+        metrics?.blockProductionSuccess.inc();
+        return {data: block, version: config.getForkName(block.slot)};
+      } finally {
+        if (timer) timer();
+      }
     },
 
     async produceAttestationData(committeeIndex, slot) {
