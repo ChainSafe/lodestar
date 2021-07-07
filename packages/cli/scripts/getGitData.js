39e810ceb3608feb14c00ffdeb06abd67f87820b
dapplion <35266934+dapplion@users.noreply.github.com>
2021-07-07 12:38:17 +0200

    Fix cli build script

--- packages/cli/scripts/getGitData.js
+++ packages/cli/scripts/getGitData.js
@@ -6,23 +6,12 @@ const shell = (cmd) =>
     .toString()
     .trim();
 
-function getGitData() {
-  try {
-    const branch = shell("git rev-parse --abbrev-ref HEAD");
-    const commit = shell("git rev-parse --verify HEAD");
-    return {branch, commit};
-  } catch (e) {
-    return {};
-  }
-}
-
 // Persist git data and distribute through NPM so CLI consumers can know exactly
 // at what commit was this src build. This is used in the metrics and to log initially
 
 const version = JSON.parse(fs.readFileSync("package.json")).version;
-const gitData = getGitData();
-const branch = gitData?.branch || "-";
-const commit = gitData?.commit || "-";
+const branch = shell("git rev-parse --abbrev-ref HEAD").catch(() => "-");
+const commit = shell("git rev-parse --verify HEAD").catch(() => "-");
 
 const data = {version, branch, commit};
 const versionDataFile = process.argv[2];
