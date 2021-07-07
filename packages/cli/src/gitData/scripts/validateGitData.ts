bf42efdc4e6465727dce30a367f2101b11664c0f
dapplion <35266934+dapplion@users.noreply.github.com>
2021-07-07 14:04:56 +0200

    Log git data file

--- packages/cli/src/gitData/scripts/validateGitData.ts
+++ packages/cli/src/gitData/scripts/validateGitData.ts
@@ -7,6 +7,9 @@ import {gitDataPath, GitDataFile} from "../gitDataPath";
 
 const gitData = JSON.parse(fs.readFileSync(gitDataPath, "utf8")) as GitDataFile;
 
+// eslint-disable-next-line no-console
+console.log({gitDataPath, gitData});
+
 if (gitData.semver) throw Error("No gitData.semver");
 if (gitData.branch) throw Error("No gitData.branch");
 if (gitData.commit) throw Error("No gitData.commit");
