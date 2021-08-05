a6b3e2c01327af6f9d9f49dfe91567f086ecb9bd
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2021-08-05 17:38:20 +0200

    Run only the very expensive functions from epoch process (#2918)

--- packages/beacon-state-transition/test/perf/altair/epoch/epoch.test.ts
+++ packages/beacon-state-transition/test/perf/altair/epoch/epoch.test.ts
@@ -16,6 +16,28 @@ describe("Altair epoch transition steps", () => {
 
   // Note: tests altair only methods. All other are benchmarked in phase/epoch
 
+  // Functions in same order as altair.processEpoch()
+  // Rough summary as of Aug 5th 2021
+  //
+  // epoch process function              | ms / op     | % bar chart
+  // ----------------------------------- | ----------- | --------------
+  // prepareEpochProcessState            | 700.0 ms/op | xxxxxxxxxxxxxx
+  // processJustificationAndFinalization | 0.180 ms/op |
+  // processInactivityUpdates            | 7500. ms/op | xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+  // processRewardsAndPenalties          | 900.0 ms/op | xxxxxxxxxxxxxxxxxx
+  // processRegistryUpdates              | 0.017 ms/op |
+  // processSlashings                    | 0.042 ms/op |
+  // processEth1DataReset                | 0.000 ms/op |
+  // processEffectiveBalanceUpdates      | 57.20 ms/op | x
+  // processSlashingsReset               | 0.000 ms/op |
+  // processRandaoMixesReset             | 0.000 ms/op |
+  // processHistoricalRootsUpdate        | 0.000 ms/op |
+  // processParticipationFlagUpdates     | 300.0 ms/op | xxxxxx
+  // processSyncCommitteeUpdates         | 0.000 ms/op |
+
+  // prepareEpochProcessState - in phase0
+  // processJustificationAndFinalization - in phase0
+
   itBench({
     id: `${idPrefix} - processInactivityUpdates`,
     beforeEach: () => originalState.clone(),
@@ -28,11 +50,21 @@ describe("Altair epoch transition steps", () => {
     fn: (state) => altair.processRewardsAndPenalties(state, epochProcess),
   });
 
-  itBench({
-    id: `${idPrefix} - processSlashings`,
-    beforeEach: () => originalState.clone(),
-    fn: (state) => altair.processSlashings(state, epochProcess),
-  });
+  // processRegistryUpdates - in phase0
+
+  // Very cheap 32.04 us/op and unstable, skip in CI
+  if (!process.env.CI)
+    itBench({
+      id: `${idPrefix} - processSlashings`,
+      beforeEach: () => originalState.clone(),
+      fn: (state) => altair.processSlashings(state, epochProcess),
+    });
+
+  // processEth1DataReset - in phase0
+  // processEffectiveBalanceUpdates - in phase0
+  // processSlashingsReset - in phase0
+  // processRandaoMixesReset - in phase0
+  // processHistoricalRootsUpdate - in phase0
 
   itBench({
     id: `${idPrefix} - processParticipationFlagUpdates`,
@@ -40,12 +72,11 @@ describe("Altair epoch transition steps", () => {
     fn: (state) => altair.processParticipationFlagUpdates(state),
   });
 
-  // very simple and fast function, no need to benchmark
-  if (!process.env.CI) {
+  // Very cheap ??.?? us/op and unstable, skip in CI
+  if (!process.env.CI)
     itBench({
       id: `${idPrefix} - processSyncCommitteeUpdates`,
       beforeEach: () => originalState.clone(),
       fn: (state) => altair.processSyncCommitteeUpdates(state, epochProcess),
     });
-  }
 });
