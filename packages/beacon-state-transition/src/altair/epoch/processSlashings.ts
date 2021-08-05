1035770fd0d08b3cd6b3eb217c78d9483c7dda16
dapplion <35266934+dapplion@users.noreply.github.com>
2021-08-05 15:11:38 +0200

    Revert "Move common processSlashings function to allForks"
    This reverts commit bcdf1d98f457cb9720c08fc95d84b6c787a47f99.

--- packages/beacon-state-transition/src/altair/epoch/processSlashings.ts
+++ packages/beacon-state-transition/src/altair/epoch/processSlashings.ts
@@ -1,7 +1,8 @@
 import {allForks, altair} from "@chainsafe/lodestar-types";
 import {ForkName} from "@chainsafe/lodestar-params";
+
 import {CachedBeaconState, IEpochProcess} from "../../allForks/util";
-import {processSlashingsAllForks} from "../../allForks/epoch/processSlashings";
+import {processSlashingsAllForks} from "../../phase0/epoch/processSlashings";
 
 export function processSlashings(state: CachedBeaconState<altair.BeaconState>, epochProcess: IEpochProcess): void {
   processSlashingsAllForks(ForkName.altair, state as CachedBeaconState<allForks.BeaconState>, epochProcess);
