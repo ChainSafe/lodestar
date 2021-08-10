3e3eb4a48dfe477d7713df0da4dd79809ae5ded8
dapplion <35266934+dapplion@users.noreply.github.com>
2021-08-10 19:32:37 +0200

    Remove space

--- packages/beacon-state-transition/src/util/blockProcess.ts
+++ packages/beacon-state-transition/src/util/blockProcess.ts
@@ -1,6 +1,6 @@
 /**
  * Disposable data computed on-demand to process blocks faster at the cost of more memory.
- * 
+ *
  * Keep in case @tuyen adds block root cache soon
  */
 // eslint-disable-next-line @typescript-eslint/naming-convention
