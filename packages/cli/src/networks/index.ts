0f672b6cae99ff439531d0772f8708899581d374
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2021-08-11 09:17:51 +0200

    Add altair-devnet-3 (#2946)

--- packages/cli/src/networks/index.ts
+++ packages/cli/src/networks/index.ts
@@ -10,10 +10,10 @@ import {IBeaconParamsUnparsed} from "../config/types";
 import * as mainnet from "./mainnet";
 import * as pyrmont from "./pyrmont";
 import * as prater from "./prater";
-import * as altairDevnet2 from "./altair-devnet-2";
+import * as altairDevnet3 from "./altair-devnet-3";
 
-export type NetworkName = "mainnet" | "pyrmont" | "prater" | "dev" | "altair-devnet-2";
-export const networkNames: NetworkName[] = ["mainnet", "pyrmont", "prater", "altair-devnet-2"];
+export type NetworkName = "mainnet" | "pyrmont" | "prater" | "dev" | "altair-devnet-3";
+export const networkNames: NetworkName[] = ["mainnet", "pyrmont", "prater", "altair-devnet-3"];
 /** Networks that infura supports */
 export const infuraNetworks: NetworkName[] = ["mainnet", "pyrmont", "prater"];
 
@@ -33,8 +33,8 @@ function getNetworkData(
       return pyrmont;
     case "prater":
       return prater;
-    case "altair-devnet-2":
-      return altairDevnet2;
+    case "altair-devnet-3":
+      return altairDevnet3;
     default:
       throw Error(`Network not supported: ${network}`);
   }
