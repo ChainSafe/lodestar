79b552c6694f981a8f410b3b1b8d42c27ccfedf7
Cayman <caymannava@gmail.com>
2021-08-17 10:23:41 -0500

    Limit # of gindices allowed in proofs (#2959)
    * Limit # of gindices allowed in proofs
    
    * Add CLI option api.maxGindicesInProof
    
    Co-authored-by: dapplion <35266934+dapplion@users.noreply.github.com>

--- packages/lodestar/src/api/options.ts
+++ packages/lodestar/src/api/options.ts
@@ -1,9 +1,11 @@
 import {restApiOptionsDefault, RestApiOptions} from "./rest";
 
 export interface IApiOptions {
+  maxGindicesInProof?: number;
   rest: RestApiOptions;
 }
 
 export const defaultApiOptions: IApiOptions = {
+  maxGindicesInProof: 512,
   rest: restApiOptionsDefault,
 };
