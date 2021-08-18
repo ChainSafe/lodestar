c7acd0de0b303c72a08c2e269b703c8c38760f02
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2021-08-18 15:21:53 +0200

    Reduce boilerplate in ssz spec tests (#2967)

--- packages/spec-test-runner/test/spec/ssz/static/phase0/phase0.ts
+++ packages/spec-test-runner/test/spec/ssz/static/phase0/phase0.ts
@@ -2,29 +2,32 @@ import {PresetName} from "@chainsafe/lodestar-params";
 import {testStaticPhase0} from "../../util/testCases";
 
 export function runSSZStaticSpecTest(preset: PresetName): void {
-  // phase0
-  testStaticPhase0("AggregateAndProof", preset);
-  testStaticPhase0("AttestationData", preset);
-  testStaticPhase0("Attestation", preset);
-  testStaticPhase0("AttesterSlashing", preset);
-  testStaticPhase0("BeaconBlockBody", preset);
-  testStaticPhase0("BeaconBlockHeader", preset);
-  testStaticPhase0("BeaconBlock", preset);
-  testStaticPhase0("BeaconState", preset);
-  testStaticPhase0("Checkpoint", preset);
-  testStaticPhase0("DepositMessage", preset);
-  testStaticPhase0("DepositData", preset);
-  testStaticPhase0("Deposit", preset);
-  testStaticPhase0("Eth1Data", preset);
-  testStaticPhase0("Fork", preset);
-  testStaticPhase0("HistoricalBatch", preset);
-  testStaticPhase0("IndexedAttestation", preset);
-  testStaticPhase0("PendingAttestation", preset);
-  testStaticPhase0("ProposerSlashing", preset);
-  testStaticPhase0("SignedBeaconBlock", preset);
-  testStaticPhase0("SignedBeaconBlockHeader", preset);
-  testStaticPhase0("SignedVoluntaryExit", preset);
-  testStaticPhase0("SigningData", preset);
-  testStaticPhase0("Validator", preset);
-  testStaticPhase0("VoluntaryExit", preset);
+  for (const typeName of [
+    "AggregateAndProof" as const,
+    "AttestationData" as const,
+    "Attestation" as const,
+    "AttesterSlashing" as const,
+    "BeaconBlockBody" as const,
+    "BeaconBlockHeader" as const,
+    "BeaconBlock" as const,
+    "BeaconState" as const,
+    "Checkpoint" as const,
+    "DepositMessage" as const,
+    "DepositData" as const,
+    "Deposit" as const,
+    "Eth1Data" as const,
+    "Fork" as const,
+    "HistoricalBatch" as const,
+    "IndexedAttestation" as const,
+    "PendingAttestation" as const,
+    "ProposerSlashing" as const,
+    "SignedBeaconBlock" as const,
+    "SignedBeaconBlockHeader" as const,
+    "SignedVoluntaryExit" as const,
+    "SigningData" as const,
+    "Validator" as const,
+    "VoluntaryExit" as const,
+  ]) {
+    testStaticPhase0(typeName, preset);
+  }
 }
