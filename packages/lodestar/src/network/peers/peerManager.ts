2bdb1619ef44f890f42cb75b04b55e53abe897f8
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2021-08-19 11:56:01 +0200

    Increase ping interval

--- packages/lodestar/src/network/peers/peerManager.ts
+++ packages/lodestar/src/network/peers/peerManager.ts
@@ -25,14 +25,14 @@ import {
 /** heartbeat performs regular updates such as updating reputations and performing discovery requests */
 const HEARTBEAT_INTERVAL_MS = 30 * 1000;
 /** The time in seconds between PING events. We do not send a ping if the other peer has PING'd us */
-const PING_INTERVAL_INBOUND_MS = 15 * 1000;
-const PING_INTERVAL_OUTBOUND_MS = 20 * 1000;
+const PING_INTERVAL_INBOUND_MS = 4 * 60 * 1000 - 30 * 1000; // Offset to not ping when outbound reqs
+const PING_INTERVAL_OUTBOUND_MS = 4 * 60 * 1000;
 /** The time in seconds between re-status's peers. */
 const STATUS_INTERVAL_MS = 5 * 60 * 1000;
 /** Expect a STATUS request from on inbound peer for some time. Afterwards the node does a request */
 const STATUS_INBOUND_GRACE_PERIOD = 15 * 1000;
 /** Internal interval to check PING and STATUS timeouts */
-const CHECK_PING_STATUS_INTERVAL = 2 * 1000;
+const CHECK_PING_STATUS_INTERVAL = 10 * 1000;
 
 // TODO:
 // maxPeers and targetPeers should be dynamic on the num of validators connected
