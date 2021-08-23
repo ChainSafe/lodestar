17b6428b4a2ab8ce2bb07815b5a04f8672418001
dapplion <35266934+dapplion@users.noreply.github.com>
2021-08-23 11:32:18 +0200

    Set lodestar-params as const

---
+++ packages/params/test/e2e/setPreset.test.ts
@@ -0,0 +1,31 @@
+import path from "path";
+import util from "util";
+import * as child from "child_process";
+import {expect, use} from "chai";
+import chaiAsPromised from "chai-as-promised";
+
+const scriptNames = {
+  ok: "setPresetOk.ts",
+  error: "setPresetError.ts",
+};
+
+use(chaiAsPromised);
+
+const exec = util.promisify(child.exec);
+
+const tsNodeBinary = path.join(__dirname, "../../../../node_modules/.bin/ts-node");
+
+describe("setPreset", function () {
+  // Allow time for ts-node to compile Typescript source
+  this.timeout(30_000);
+
+  it("Should correctly set preset", async () => {
+    await exec(`${tsNodeBinary} ${path.join(__dirname, scriptNames.ok)}`);
+  });
+
+  it("Should throw trying to set preset in the wrong order", async () => {
+    await expect(exec(`${tsNodeBinary} ${path.join(__dirname, scriptNames.error)}`)).to.be.rejectedWith(
+      "Lodestar preset is already frozen"
+    );
+  });
+});
