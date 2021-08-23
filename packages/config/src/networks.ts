b58c95264315a07bbbb24b6f316a1500336f5871
dapplion <35266934+dapplion@users.noreply.github.com>
2021-08-23 10:25:25 +0200

    Export IChainConfig

--- packages/config/src/networks.ts
+++ packages/config/src/networks.ts
@@ -1,13 +1,13 @@
-import {createIChainForkConfig, IChainForkConfig} from "./beaconConfig";
+import {IChainConfig} from "./chainConfig";
 import {mainnetChainConfig} from "./chainConfig/networks/mainnet";
 import {pyrmontChainConfig} from "./chainConfig/networks/pyrmont";
 import {praterChainConfig} from "./chainConfig/networks/prater";
 
 export {mainnetChainConfig, pyrmontChainConfig, praterChainConfig};
-// for testing purpose only
-export const mainnet = createIChainForkConfig(mainnetChainConfig);
-export const pyrmont = createIChainForkConfig(pyrmontChainConfig);
-export const prater = createIChainForkConfig(praterChainConfig);
 
 export type NetworkName = "mainnet" | "pyrmont" | "prater";
-export const networksChainConfig: Record<NetworkName, IChainForkConfig> = {mainnet, pyrmont, prater};
+export const networksChainConfig: Record<NetworkName, IChainConfig> = {
+  mainnet: mainnetChainConfig,
+  pyrmont: pyrmontChainConfig,
+  prater: praterChainConfig,
+};
