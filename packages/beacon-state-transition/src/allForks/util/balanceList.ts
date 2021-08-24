6d9d209075eba3444bf42e23a92deff2ed41a3c8
Tuyen Nguyen <vutuyen2636@gmail.com>
2021-08-24 17:44:35 +0700

    Use applyDeltaInBatch

--- packages/beacon-state-transition/src/allForks/util/balanceList.ts
+++ packages/beacon-state-transition/src/allForks/util/balanceList.ts
@@ -34,6 +34,10 @@ export class BalanceList implements List<number> {
     this.type.tree_applyDeltaInBatch(this.tree, deltaByIndex);
   }
 
+  applyDeltaInBatch(deltaByIndex: Map<number, number>): void {
+    this.type.tree_applyUint64DeltaInBatch(this.tree, deltaByIndex);
+  }
+
   /** Return the new balances */
   updateAll(deltas: number[]): number[] {
     const [newTree, newBalances] = this.type.tree_newTreeFromDeltas(this.tree, deltas);
