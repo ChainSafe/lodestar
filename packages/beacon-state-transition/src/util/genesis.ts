01b257ccdb6fe9255d474252cdd987b8857c8176
dapplion <35266934+dapplion@users.noreply.github.com>
2021-09-01 15:43:46 +0200

    Apply effectiveBalance updates on test genesis

--- packages/beacon-state-transition/src/util/genesis.ts
+++ packages/beacon-state-transition/src/util/genesis.ts
@@ -160,7 +160,9 @@ export function applyDeposits(
   for (let index = 0; index < validatorLength; index++) {
     const validator = state.validators[index];
     const balance = state.balances[index];
-    validator.effectiveBalance = Math.min(balance - (balance % EFFECTIVE_BALANCE_INCREMENT), MAX_EFFECTIVE_BALANCE);
+    const effectiveBalance = Math.min(balance - (balance % EFFECTIVE_BALANCE_INCREMENT), MAX_EFFECTIVE_BALANCE);
+    validator.effectiveBalance = effectiveBalance;
+    state.effectiveBalances.set(index, effectiveBalance);
 
     if (validator.effectiveBalance === MAX_EFFECTIVE_BALANCE) {
       validator.activationEligibilityEpoch = GENESIS_EPOCH;
