2166b683f8d815a26be2423c1e3cc143edf862b3
dapplion <35266934+dapplion@users.noreply.github.com>
2021-09-06 22:12:25 +0200

    Add merge types

---
+++ packages/types/src/merge/index.ts
@@ -0,0 +1,2 @@
+export * as ts from "./types";
+export * as ssz from "./sszTypes";
--- packages/types/src/types.ts
+++ packages/types/src/types.ts
@@ -1,4 +1,6 @@
 export * from "./primitive/types";
 export {ts as phase0} from "./phase0";
 export {ts as altair} from "./altair";
+export {ts as merge} from "./merge";
+
 export {ts as allForks} from "./allForks";
