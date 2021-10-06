afbc8321994c72833c7d2a375de1a19a3d02895a
harkamal <gajinder@g11.in>
2021-10-06 22:43:52 +0530

    spec casing test

--- packages/api/test/unit/config.test.ts
+++ packages/api/test/unit/config.test.ts
@@ -4,8 +4,15 @@ import {Api, ReqTypes, Spec} from "../../src/routes/config";
 import {getClient} from "../../src/client/config";
 import {getRoutes} from "../../src/server/config";
 import {runGenericServerTest} from "../utils/genericServerTest";
+import {expect} from "chai";
 
 describe("config", () => {
+  it("Spec casing check", function () {
+    const defaultSpec = Spec.defaultValue();
+    const specJson = Spec.toJson(defaultSpec) as Record<string, string>;
+    expect(specJson["SLOTS_PER_EPOCH"] !== undefined).to.be.true;
+  });
+
   runGenericServerTest<Api, ReqTypes>(config, getClient, getRoutes, {
     getDepositContract: {
       args: [],
