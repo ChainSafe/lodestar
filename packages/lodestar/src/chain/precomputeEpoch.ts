0dc4a4e7157f8bd79ed169417bca626edcf9b7b4
Tuyen Nguyen <vutuyen2636@gmail.com>
2021-10-22 10:35:38 +0700

    Schedule to precompute epoch transition based on clock slot

--- packages/lodestar/src/chain/precomputeEpoch.ts
+++ packages/lodestar/src/chain/precomputeEpoch.ts
@@ -1,8 +1,8 @@
 import {AbortSignal} from "@chainsafe/abort-controller";
-import {computeStartSlotAtEpoch} from "@chainsafe/lodestar-beacon-state-transition";
+import {computeEpochAtSlot} from "@chainsafe/lodestar-beacon-state-transition";
 import {IBeaconConfig} from "@chainsafe/lodestar-config";
 import {SLOTS_PER_EPOCH} from "@chainsafe/lodestar-params";
-import {Epoch} from "@chainsafe/lodestar-types";
+import {Slot} from "@chainsafe/lodestar-types";
 import {ILogger, sleep} from "@chainsafe/lodestar-utils";
 import {ChainEvent} from "./emitter";
 import {IBeaconChain} from "./interface";
@@ -21,34 +21,40 @@ export class PrecomputeEpochScheduler {
     private readonly logger: ILogger,
     private readonly signal: AbortSignal
   ) {
-    this.chain.emitter.on(ChainEvent.clockEpoch, this.onEpoch);
+    this.chain.emitter.on(ChainEvent.clockSlot, this.prepareForNextEpoch);
 
     this.signal.addEventListener(
       "abort",
       () => {
-        this.chain.emitter.off(ChainEvent.clockEpoch, this.onEpoch);
+        this.chain.emitter.off(ChainEvent.clockSlot, this.prepareForNextEpoch);
       },
       {once: true}
     );
   }
 
-  private onEpoch = async (epoch: Epoch): Promise<void> => {
+  /**
+   * Use clockSlot instead of clockEpoch to schedule the task at more exact time.
+   */
+  private prepareForNextEpoch = async (clockSlot: Slot): Promise<void> => {
+    // only interested in last slot of epoch
+    if ((clockSlot + 1) % SLOTS_PER_EPOCH !== 0) {
+      return;
+    }
     // Precalculate epoch transition 2/3 of the way through the last slot of the epoch
-    const msSecondsPerSlot = this.config.SECONDS_PER_SLOT * 1000;
-    const msToPrecalculateTime = msSecondsPerSlot * (SLOTS_PER_EPOCH - 1 / 3);
+    const msToPrecalculateTime = (this.config.SECONDS_PER_SLOT * 1000 * 2) / 3;
     await sleep(msToPrecalculateTime, this.signal);
-    const clockSlot = this.chain.clock.currentSlot;
     const {slot: headSlot, blockRoot} = this.chain.forkChoice.getHead();
+    const nextEpoch = computeEpochAtSlot(clockSlot) + 1;
     // node may be syncing or out of synced
     if (headSlot < clockSlot) {
-      this.logger.verbose("No need to precompute epoch transition", {headSlot, clockSlot});
+      this.logger.verbose("No need to precompute epoch transition", {nextEpoch, headSlot, slot: clockSlot});
       return;
     }
     // we want to make sure headSlot === clockSlot to do early epoch transition
-    const nextSlot = computeStartSlotAtEpoch(epoch + 1);
-    this.logger.verbose("Precompute epoch transition", {epoch, headSlot, nextSlot});
+    const nextSlot = clockSlot + 1;
+    this.logger.verbose("Precompute epoch transition", {nextEpoch, headSlot, nextSlot});
     await this.chain.regen.getBlockSlotState(blockRoot, nextSlot, RegenCaller.preComputeEpoch).catch((e) => {
-      this.logger.error("Failed to precompute epoch transition", epoch, e);
+      this.logger.error("Failed to precompute epoch transition", nextEpoch, e);
     });
   };
 }
