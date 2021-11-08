9266bd3bedeb90f43e860e6cc9f4437995f4af22
harkamal <gajinder@g11.in>
2021-11-08 20:49:17 +0530

    spec runner merge sanity and operations fixes

--- packages/beacon-state-transition/src/merge/block/index.ts
+++ packages/beacon-state-transition/src/merge/block/index.ts
@@ -2,11 +2,15 @@ import {allForks, altair, merge} from "@chainsafe/lodestar-types";
 
 import {CachedBeaconState} from "../../allForks/util";
 import {processBlockHeader, processEth1Data, processRandao} from "../../allForks/block";
-import {processOperations} from "../../altair/block/processOperations";
+import {processOperations} from "./processOperations";
 import {processSyncAggregate} from "../../altair/block/processSyncCommittee";
 import {processExecutionPayload} from "./processExecutionPayload";
 import {ExecutionEngine} from "../executionEngine";
 import {isExecutionEnabled} from "../utils";
+import {processAttesterSlashing} from "./processAttesterSlashing";
+import {processProposerSlashing} from "./processProposerSlashing";
+
+export {processOperations, processAttesterSlashing, processProposerSlashing};
 
 export function processBlock(
   state: CachedBeaconState<merge.BeaconState>,
@@ -23,6 +27,6 @@ export function processBlock(
 
   processRandao(state as CachedBeaconState<allForks.BeaconState>, block, verifySignatures);
   processEth1Data(state as CachedBeaconState<allForks.BeaconState>, block.body);
-  processOperations((state as unknown) as CachedBeaconState<altair.BeaconState>, block.body, verifySignatures);
+  processOperations(state, block.body, verifySignatures);
   processSyncAggregate((state as unknown) as CachedBeaconState<altair.BeaconState>, block, verifySignatures);
 }
---
+++ packages/beacon-state-transition/src/merge/epoch/index.ts
@@ -0,0 +1,1 @@
+export {processSlashings} from "./processSlashings";
