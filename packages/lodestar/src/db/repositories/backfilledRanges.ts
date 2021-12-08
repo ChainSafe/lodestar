4a535425e319bf1670b3b295ecd037fd0e151c51
dapplion <35266934+dapplion@users.noreply.github.com>
2021-12-08 16:00:57 +0100

    WIP

--- packages/lodestar/src/db/repositories/backfilledRanges.ts
+++ packages/lodestar/src/db/repositories/backfilledRanges.ts
@@ -3,6 +3,15 @@ import {Slot, ssz} from "@chainsafe/lodestar-types";
 import {IDatabaseController, Bucket, IDbMetrics, Repository} from "@chainsafe/lodestar-db";
 import {bytesToInt} from "@chainsafe/lodestar-utils";
 
+/**
+ * Slot to slot ranges that ensure that block range is fully backfilled
+ *
+ * If node starts backfilling at slots 1000, and backfills to 800, there will be an entry
+ * 1000 -> 800
+ *
+ * When the node is backfilling if it starts at 1200 and backfills to 1000, it will find this sequence and,
+ * jump directly to 800 and delete the key 1000.
+ */
 export class BackfilledRanges extends Repository<Slot, Slot> {
   constructor(config: IChainForkConfig, db: IDatabaseController<Uint8Array, Uint8Array>, metrics?: IDbMetrics) {
     super(config, db, Bucket.backfilled_ranges, ssz.Slot, metrics);
--- packages/lodestar/src/sync/backfill/backfill.ts
+++ packages/lodestar/src/sync/backfill/backfill.ts
@@ -17,6 +17,7 @@ import {shuffleOne} from "../../util/shuffle";
 import {BackfillSyncError, BackfillSyncErrorCode} from "./errors";
 import {verifyBlockProposerSignature, verifyBlockSequence} from "./verify";
 import {SLOTS_PER_EPOCH} from "@chainsafe/lodestar-params";
+import {byteArrayEquals} from "../../util/bytes";
 
 /** Default batch size. Same as range sync (2 epochs) */
 const BATCH_SIZE = 64;
@@ -70,8 +71,17 @@ type BackfillSyncEvents = {
 
 type BackfillSyncEmitter = StrictEventEmitter<EventEmitter, BackfillSyncEvents>;
 
+/**
+ * TODO:
+ *
+ * Gather initial variables first, then start class
+ * - lastBackSyncedSlot: anchor block's slot
+ * - anchorBlock | anchorBlockRoot
+ * - prevFinalizedCheckpointBlock
+ */
 export class BackfillSync extends (EventEmitter as {new (): BackfillSyncEmitter}) {
-  lastBackSyncedSlot: Slot | null = null;
+  /** Lowest slot that we have backfilled to */
+  lastBackSyncedSlot: Slot;
 
   private readonly chain: IBeaconChain;
   private readonly network: INetwork;
@@ -79,9 +89,29 @@ export class BackfillSync extends (EventEmitter as {new (): BackfillSyncEmitter}
   private readonly config: IBeaconConfig;
   private readonly logger: ILogger;
   private readonly metrics: IMetrics | null;
+  /**
+   * Optional because one may start the node without a ws checkpoint and still need to backfill because in a previous
+   * session backfill was not completed. If not provided validation will be skipped
+   */
   private wsCheckpoint: phase0.Checkpoint | null;
+  /**
+   *
+   */
   private checkpointSlot: Slot;
   private opts: BackfillSyncOpts;
+
+  /**
+   * From https://github.com/ethereum/consensus-specs/blob/dev/specs/phase0/weak-subjectivity.md
+   * 1. If db contains a previous finalized checkpoint, that
+   * 2. else, genesis block
+   *
+   * Read latest entry in db.blockArchive, else genesis block. Do as soon as possible before the node archives
+   * a block after the ws.
+   */
+  private prevFinalizedCheckpointBlock: {
+    slot: Slot;
+    root: Root;
+  };
 
   /**
    * At any given point, we should have either the anchorBlock or anchorBlockRoot,
@@ -89,7 +119,9 @@ export class BackfillSync extends (EventEmitter as {new (): BackfillSyncEmitter}
    */
   private anchorBlock: allForks.SignedBeaconBlock | null = null;
   private anchorBlockRoot: Root | null = null;
+  /** Starting point that this specific backfill sync "session" started from */
   private backfillStartFromSlot: Slot | null = null;
+  private hasValidated = false;
 
   private processor = new ItTrigger();
   private peers = new PeerSet();
@@ -164,7 +196,14 @@ export class BackfillSync extends (EventEmitter as {new (): BackfillSyncEmitter}
         break;
       }
 
-      await this.checkUpdateFromBackfillSequences();
+      if (this.lastBackSyncedSlot !== null && this.backfillStartFromSlot !== null) {
+        await this.checkUpdateFromBackfillSequences(this.lastBackSyncedSlot, this.backfillStartFromSlot);
+      }
+
+      if (this.lastBackSyncedSlot <= this.prevFinalizedCheckpointBlock.slot) {
+        await this.checkIfCheckpointSyncedAndValidate();
+      }
+
       await this.checkIfCheckpointSyncedAndValidate();
       if (this.anchorBlock?.message.slot === GENESIS_SLOT) {
         this.logger.verbose("BackfillSync - successfully synced to genesis, exiting");
@@ -202,7 +241,6 @@ export class BackfillSync extends (EventEmitter as {new (): BackfillSyncEmitter}
           this.lastBackSyncedSlot = this.anchorBlock.message.slot;
 
           // Go back to start, do checks
-          this.processor.trigger();
           continue;
         }
 
@@ -249,73 +287,123 @@ export class BackfillSync extends (EventEmitter as {new (): BackfillSyncEmitter}
     this.peers.delete(peerId);
   };
 
+  /**
+   * In case the finalized checkpoint's root is known to the node, resolve its root to get
+   * - anchorBlock.parentRoot
+   * - anchorBlock.slot
+   * - lastBackSyncedSlot = anchorBlock.slot
+   */
   private async initializeSync(): Promise<void> {
     const anchorCp = this.chain.forkChoice.getFinalizedCheckpoint();
+    const anchorBlockFC = this.chain.forkChoice.getBlockHex(anchorCp.rootHex);
+    if (anchorBlockFC) {
+      anchorBlockFC.parentRoot;
+      anchorBlockFC.slot;
+    }
+
     // Look in db, as finalized root might not have moved at all on a quick restart
-    this.anchorBlock = await this.db.blockArchive.getByRoot(anchorCp.root);
-    if (this.anchorBlock) {
+    const anchorBlock = await this.db.blockArchive.getByRoot(anchorCp.root);
+    if (anchorBlock) {
+      // If found anchorBlock resolve it to the block
+      this.anchorBlock = anchorBlock;
       this.lastBackSyncedSlot = this.anchorBlock.message.slot;
     } else {
+      // If not found anchorBlock, set the root and expect reqResp.getBlockByRoot() to find it
       this.anchorBlockRoot = anchorCp.root;
       this.lastBackSyncedSlot = null;
     }
 
-    this.backfillStartFromSlot = anchorCp.epoch * SLOTS_PER_EPOCH;
+    this.backfillStartFromSlot = computeStartSlotAtEpoch(anchorCp.epoch);
     this.logger.info("BackfillSync - initializing from Checkpoint", {
       root: toHexString(anchorCp.root),
       epoch: anchorCp.epoch,
     });
   }
 
+  /**
+   * Ensure that the weak subjectivity checkpoint is the same block tree as the DB if a previous DB is present
+   */
   private async checkIfCheckpointSyncedAndValidate(): Promise<void> {
     if (
+      !this.hasValidated &&
       this.wsCheckpoint !== null &&
       this.lastBackSyncedSlot !== null &&
       this.checkpointSlot >= this.lastBackSyncedSlot
     ) {
-      // Checkpoint root should be in db now!
-      const wsDbCheckpointBlock = await this.db.blockArchive.getByRoot(this.wsCheckpoint.root);
-      if (!wsDbCheckpointBlock || wsDbCheckpointBlock.message.slot !== this.checkpointSlot)
-        // TODO: explode and stop the entire node
-        throw new Error(
-          `InvalidWsCheckpoint checkpointSlot: ${this.checkpointSlot}, ${
-            wsDbCheckpointBlock ? "found at: " + wsDbCheckpointBlock?.message.slot : "not found"
-          }`
-        );
-      this.logger.info("BackfillSync - wsCheckpoint validated!", {
-        root: toHexString(this.wsCheckpoint.root),
-        slot: wsDbCheckpointBlock.message.slot,
-      });
-      // no need to check again
-      this.wsCheckpoint = null;
+      if (this.wsCheckpoint.epoch > this.prevFinalizedCheckpointBlock.epoch) {
+        // From https://github.com/ethereum/consensus-specs/blob/dev/specs/phase0/weak-subjectivity.md
+        // IF epoch_number > store.finalized_checkpoint.epoch, then ASSERT during block sync that block with root
+        // block_root is in the sync path at epoch epoch_number. Emit descriptive critical error if this assert
+        // fails, then exit client process.
+
+        const checkpointBlock = await this.db.blockArchive.get(this.prevFinalizedCheckpointBlock.slot);
+        if (!checkpointBlock) {
+          throw Error("???? TODO");
+        }
+
+        const checkpointBlockRoot = this.config
+          .getForkTypes(checkpointBlock.message.slot)
+          .BeaconBlock.hashTreeRoot(checkpointBlock.message);
+
+        if (byteArrayEquals(checkpointBlockRoot, this.prevFinalizedCheckpointBlock.root)) {
+          // OK
+          this.hasValidated = true;
+        } else {
+          // Critical error
+        }
+      } else {
+        // From https://github.com/ethereum/consensus-specs/blob/dev/specs/phase0/weak-subjectivity.md
+        // IF epoch_number <= store.finalized_checkpoint.epoch, then ASSERT that the block in the canonical chain
+        // at epoch epoch_number has root block_root. Emit descriptive critical error if this assert fails, then
+        // exit client process.
+
+        // Checkpoint root should be in db now!
+        const wsDbCheckpointBlock = await this.db.blockArchive.getByRoot(this.wsCheckpoint.root);
+        if (!wsDbCheckpointBlock || wsDbCheckpointBlock.message.slot !== this.checkpointSlot)
+          // TODO: explode and stop the entire node
+          throw new Error(
+            `InvalidWsCheckpoint checkpointSlot: ${this.checkpointSlot}, ${
+              wsDbCheckpointBlock ? "found at: " + wsDbCheckpointBlock?.message.slot : "not found"
+            }`
+          );
+        this.logger.info("BackfillSync - wsCheckpoint validated!", {
+          root: toHexString(this.wsCheckpoint.root),
+          slot: wsDbCheckpointBlock.message.slot,
+        });
+        this.hasValidated = true;
+        // no need to check again
+        this.wsCheckpoint = null;
+      }
     }
   }
 
-  private async checkUpdateFromBackfillSequences(): Promise<void> {
-    if (this.lastBackSyncedSlot !== null && this.backfillStartFromSlot !== null) {
-      const filteredSeqs = await this.db.backfilledRanges.entries({
-        gte: this.lastBackSyncedSlot,
-        lte: this.backfillStartFromSlot,
-      });
-      const jumpBackTo = filteredSeqs.reduce((acc, entry) => {
-        if (entry.value < acc) acc = entry.value;
-        return acc;
-      }, this.lastBackSyncedSlot);
-      if (jumpBackTo < this.lastBackSyncedSlot) {
+  private async checkUpdateFromBackfillSequences(lastBackSyncedSlot: Slot, backfillStartFromSlot: Slot): Promise<void> {
+    const filteredSeqs = await this.db.backfilledRanges.entries({
+      gte: lastBackSyncedSlot,
+      lte: backfillStartFromSlot,
+    });
+
+    if (filteredSeqs.length > 0) {
+      const jumpBackTo = Math.min(...filteredSeqs.map(({value: justToSlot}) => justToSlot));
+
+      if (jumpBackTo < lastBackSyncedSlot) {
         this.anchorBlock = await this.db.blockArchive.get(jumpBackTo);
         this.anchorBlockRoot = null;
         this.metrics?.backfillSync.totalBlocks.inc(
           {method: BackfillSyncMethod.backfilled_ranges},
-          this.lastBackSyncedSlot - jumpBackTo
+          lastBackSyncedSlot - jumpBackTo
         );
         this.lastBackSyncedSlot = jumpBackTo;
         this.logger.verbose("backfillSync - found previous backfilled sequence in db, jumping back to", {
           slot: jumpBackTo,
         });
       }
-      await this.db.backfilledRanges.put(this.backfillStartFromSlot, this.lastBackSyncedSlot);
+    }
+
+    await this.db.backfilledRanges.put(backfillStartFromSlot, lastBackSyncedSlot);
+    if (filteredSeqs.length > 0) {
       await this.db.backfilledRanges.batchDelete(
-        filteredSeqs.filter((entry) => entry.key !== this.backfillStartFromSlot).map((entry) => entry.key)
+        filteredSeqs.filter((entry) => entry.key !== backfillStartFromSlot).map((entry) => entry.key)
       );
     }
   }
@@ -368,6 +456,9 @@ export class BackfillSync extends (EventEmitter as {new (): BackfillSyncEmitter}
     // If any of the block's proposer signature fail, we can't trust this peer at all
     if (verifiedBlocks.length > 0)
       await verifyBlockProposerSignature(this.chain.bls, this.chain.getHeadState(), verifiedBlocks);
+
+    // TODO: Do WS CHECK
+    // If blocks.slot < this.prevFinalizedCheckpointBlock.slot
 
     await this.db.blockArchive.batchAdd(verifiedBlocks);
     this.metrics?.backfillSync.totalBlocks.inc({method: BackfillSyncMethod.rangesync}, verifiedBlocks.length);
