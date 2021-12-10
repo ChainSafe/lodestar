9ce986e7be68a77b4a88f7b9d251e5312049d459
dapplion <35266934+dapplion@users.noreply.github.com>
2021-12-10 12:35:54 +0100

    Scan all dashboards for exemplar

--- scripts/validate-grafana-dashboard.js
+++ scripts/validate-grafana-dashboard.js
@@ -1,6 +1,17 @@
-let lodestarDash = require("../docker/grafana/provisioning/dashboards/lodestar.json");
-lodestarDash = JSON.stringify(lodestarDash); //get everything in the same line
+const fs = require("fs");
+const path = require("path");
 
-//match something like exemplar : true
-const matches = lodestarDash.match(/(exemplar)(\s)*(["])?(\s)*:(\s)*(["])?(\s)*true/gi);
-if (matches && matches.length > 0) throw new Error("ExemplarQueryNotSupported");
+const dashboardsDir = path.join(__dirname, "../docker/grafana/provisioning/dashboards");
+
+for (const dashboardName of fs.readdirSync(dashboardsDir)) {
+  if (dashboardName.endsWith(".json")) {
+    let lodestarDash = require(path.join(dashboardsDir, dashboardName));
+    lodestarDash = JSON.stringify(lodestarDash); // get everything in the same line
+
+    // match something like exemplar : true
+    const matches = lodestarDash.match(/(exemplar)(\s)*(["])?(\s)*:(\s)*(["])?(\s)*true/gi);
+    if (matches && matches.length > 0) {
+      throw new Error(`ExemplarQueryNotSupported: ${dashboardName}`);
+    }
+  }
+}
