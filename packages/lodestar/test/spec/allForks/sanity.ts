97188a4a2bd4b6f3e6a4fbf07fffe1b76d0311b9
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2021-12-23 13:06:04 +0100

    Move spec tests to main lodestar package (#3544)
    * Move spec tests to main lodestar package
    
    * Use spec test mocha rc
    
    * Fix specTestVersioning import
    
    * Fix test types
    
    * Fix spec tests path
    
    * Fix relative location of spec test coverage

---
+++ packages/lodestar/test/spec/allForks/sanity.ts
@@ -0,0 +1,106 @@
+import {join} from "path";
+import {describeDirectorySpecTest, InputType} from "@chainsafe/lodestar-spec-test-util";
+import {allForks} from "@chainsafe/lodestar-beacon-state-transition";
+import {TreeBacked} from "@chainsafe/ssz";
+import {merge, ssz, Uint64} from "@chainsafe/lodestar-types";
+import {ACTIVE_PRESET, ForkName} from "@chainsafe/lodestar-params";
+import {expectEqualBeaconState, inputTypeSszTreeBacked} from "../util";
+import {SPEC_TEST_LOCATION} from "../specTestVersioning";
+import {IBaseSpecTest} from "../type";
+import {getConfig} from "./util";
+
+export function sanity(fork: ForkName): void {
+  sanitySlot(fork);
+  sanityBlock(fork, `/tests/${ACTIVE_PRESET}/${fork}/sanity/blocks/pyspec_tests`);
+}
+
+export function sanitySlot(fork: ForkName): void {
+  describeDirectorySpecTest<IProcessSlotsTestCase, allForks.BeaconState>(
+    `${ACTIVE_PRESET}/${fork}/sanity/slots`,
+    join(SPEC_TEST_LOCATION, `/tests/${ACTIVE_PRESET}/${fork}/sanity/slots/pyspec_tests`),
+    (testcase) => {
+      const stateTB = (testcase.pre as TreeBacked<allForks.BeaconState>).clone();
+      const state = allForks.createCachedBeaconState(getConfig(fork), stateTB);
+      const postState = allForks.processSlots(state, state.slot + Number(testcase.slots));
+      return postState.type.createTreeBacked(postState.tree);
+    },
+    {
+      inputTypes: {...inputTypeSszTreeBacked, slots: InputType.YAML},
+      sszTypes: {
+        pre: ssz[fork].BeaconState,
+        post: ssz[fork].BeaconState,
+      },
+      shouldError: (testCase) => !testCase.post,
+      timeout: 10000,
+      getExpected: (testCase) => testCase.post,
+      expectFunc: (testCase, expected, actual) => {
+        expectEqualBeaconState(fork, expected, actual);
+      },
+    }
+  );
+}
+
+export function sanityBlock(fork: ForkName, testPath: string): void {
+  describeDirectorySpecTest<IBlockSanityTestCase, allForks.BeaconState>(
+    `${ACTIVE_PRESET}/${fork}/sanity/blocks`,
+    join(SPEC_TEST_LOCATION, testPath),
+    (testcase) => {
+      const stateTB = testcase.pre as TreeBacked<allForks.BeaconState>;
+      let wrappedState = allForks.createCachedBeaconState(getConfig(fork), stateTB);
+      const verify = testcase.meta !== undefined && testcase.meta.blsSetting === BigInt(1);
+      for (let i = 0; i < Number(testcase.meta.blocksCount); i++) {
+        const signedBlock = testcase[`blocks_${i}`] as merge.SignedBeaconBlock;
+        wrappedState = allForks.stateTransition(
+          wrappedState,
+          ssz[fork].SignedBeaconBlock.createTreeBackedFromStruct(signedBlock),
+          {
+            verifyStateRoot: verify,
+            verifyProposer: verify,
+            verifySignatures: verify,
+          }
+        );
+      }
+      return wrappedState;
+    },
+    {
+      inputTypes: inputTypeSszTreeBacked,
+      sszTypes: {
+        pre: ssz[fork].BeaconState,
+        post: ssz[fork].BeaconState,
+        ...generateBlocksSZZTypeMapping(fork, 99),
+      },
+      shouldError: (testCase) => testCase.post === undefined,
+      timeout: 10000,
+      getExpected: (testCase) => testCase.post,
+      expectFunc: (testCase, expected, actual) => {
+        expectEqualBeaconState(fork, expected, actual);
+      },
+    }
+  );
+}
+
+type BlocksSZZTypeMapping = Record<string, typeof ssz[ForkName]["SignedBeaconBlock"]>;
+
+export function generateBlocksSZZTypeMapping(fork: ForkName, n: number): BlocksSZZTypeMapping {
+  const blocksMapping: BlocksSZZTypeMapping = {};
+  for (let i = 0; i < n; i++) {
+    blocksMapping[`blocks_${i}`] = ssz[fork].SignedBeaconBlock;
+  }
+  return blocksMapping;
+}
+
+interface IBlockSanityTestCase extends IBaseSpecTest {
+  [k: string]: allForks.SignedBeaconBlock | unknown | null | undefined;
+  meta: {
+    blocksCount: Uint64;
+    blsSetting: BigInt;
+  };
+  pre: allForks.BeaconState;
+  post: allForks.BeaconState;
+}
+
+interface IProcessSlotsTestCase extends IBaseSpecTest {
+  pre: allForks.BeaconState;
+  post?: allForks.BeaconState;
+  slots: Uint64;
+}
---
+++ packages/lodestar/test/spec/allForks/ssz_static.ts
@@ -0,0 +1,113 @@
+import fs from "fs";
+import path from "path";
+import {describeDirectorySpecTest, InputType, safeType} from "@chainsafe/lodestar-spec-test-util";
+import {Bytes32, ssz} from "@chainsafe/lodestar-types";
+import {expect} from "chai";
+import {CompositeType, ContainerType, Type} from "@chainsafe/ssz";
+import {IBaseSSZStaticTestCase} from "../ssz/type";
+import {SPEC_TEST_LOCATION} from "../specTestVersioning";
+import {ACTIVE_PRESET, ForkName, PresetName} from "@chainsafe/lodestar-params";
+
+/* eslint-disable @typescript-eslint/naming-convention, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, no-console */
+
+// eslint-disable-next-line
+type Types = Record<string, Type<any>>;
+
+const extraTypes = {
+  Eth1Block: new ContainerType({
+    fields: {
+      timestamp: ssz.Number64,
+      depositRoot: ssz.Root,
+      depositCount: ssz.Number64,
+    },
+  }),
+};
+
+export function sszStatic(fork: ForkName): void {
+  const rootDir = path.join(SPEC_TEST_LOCATION, `tests/${ACTIVE_PRESET}/${fork}/ssz_static`);
+  for (const typeName of fs.readdirSync(rootDir)) {
+    const type =
+      ((ssz[fork] as Types)[typeName] as Type<any> | undefined) ||
+      ((ssz.altair as Types)[typeName] as Type<any> | undefined) ||
+      ((ssz.phase0 as Types)[typeName] as Type<any> | undefined) ||
+      ((extraTypes as Types)[typeName] as Type<any> | undefined);
+    if (!type) {
+      throw Error(`No type for ${typeName}`);
+    }
+
+    const sszType = safeType(type) as CompositeType<any>;
+    testStatic(typeName, sszType, fork, ACTIVE_PRESET);
+  }
+}
+
+interface IResult {
+  root: Bytes32;
+  serialized: Uint8Array;
+}
+
+function testStatic(typeName: string, sszType: CompositeType<any>, forkName: ForkName, preset: PresetName): void {
+  const typeDir = path.join(SPEC_TEST_LOCATION, `tests/${preset}/${forkName}/ssz_static/${typeName}`);
+
+  for (const caseName of fs.readdirSync(typeDir)) {
+    describeDirectorySpecTest<IBaseSSZStaticTestCase<any>, IResult>(
+      `${preset}/${forkName}/ssz_static/${typeName}/${caseName}`,
+      path.join(typeDir, caseName),
+      (testcase) => {
+        //debugger;
+        const serialized = sszType.serialize(testcase.serialized);
+        const root = sszType.hashTreeRoot(testcase.serialized);
+        return {
+          serialized,
+          root,
+        };
+      },
+      {
+        inputTypes: {
+          roots: InputType.YAML,
+          serialized: InputType.SSZ_SNAPPY,
+        },
+        sszTypes: {
+          serialized: sszType,
+        },
+        getExpected: (testCase) => {
+          return {
+            root: Buffer.from(testCase.roots.root.replace("0x", ""), "hex"),
+            serialized: testCase.serialized_raw,
+          };
+        },
+        expectFunc: (testCase, expected, actual) => {
+          if (true && (!expected.serialized.equals(actual.serialized) || !expected.root.equals(actual.root))) {
+            console.log("testCase", testCase);
+            console.log("serialize expected", expected.serialized.toString("hex"));
+            console.log("serialize actual  ", Buffer.from(actual.serialized).toString("hex"));
+            console.log("hashTreeRoot expected", expected.root.toString("hex"));
+            console.log("hashTreeRoot actual  ", Buffer.from(actual.root).toString("hex"));
+            /*
+            const bbroot = Type.fields[2][1]
+            console.log("s bbroot hash", bbroot.hashTreeRoot(structural.beaconBlockRoot))
+            console.log("t bbroot hash", bbroot.hashTreeRoot(tree.beaconBlockRoot))
+               */
+            // debugger;
+          }
+          const structural = sszType.deserialize(testCase.serialized_raw);
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          const tree = sszType.createTreeBackedFromBytes(testCase.serialized_raw);
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          const treeFromStructural = sszType.createTreeBackedFromStruct(structural);
+          expect(tree.serialize(), "tree serialization != structural serialization").to.deep.equal(
+            sszType.serialize(structural)
+          );
+          expect(sszType.equals(tree, treeFromStructural), "tree != treeFromStructural");
+          expect(expected.serialized.equals(sszType.serialize(structural)));
+          expect(expected.root.equals(sszType.hashTreeRoot(structural)));
+          expect(expected.serialized.equals(actual.serialized), "incorrect serialize").to.be.true;
+          expect(expected.root.equals(actual.root), "incorrect hashTreeRoot").to.be.true;
+        },
+        //shouldSkip: (a, b, i) => i !== 0,
+        //shouldSkip: (a, b, i) => b !== 'case_10',
+      }
+    );
+  }
+}
