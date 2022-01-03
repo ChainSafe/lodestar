7f1dc3c26310bf5fb868286d6482bb723a6e221e
dapplion <35266934+dapplion@users.noreply.github.com>
2022-01-03 11:15:31 +0100

    Benchmark overhead of Uint8Array

--- packages/lodestar/test/memory/bytesHex.ts
+++ packages/lodestar/test/memory/bytesHex.ts
@@ -4,10 +4,10 @@ import {testRunnerMemory} from "./testRunnerMemory";
 
 // Results in Linux Dec 2021
 //
-// Bytes32 toHexString()                      - 901.1 bytes / instance
-// Bytes32 Buffer.toString(hex)               - 85.4 bytes / instance
-// Bytes32 Buffer.toString(hex) Buffer.from() - 84.8 bytes / instance
-// Bytes32 Buffer.toString(hex) + 0x          - 121.7 bytes / instance
+// Bytes32 toHexString()                        - 902.8 bytes / instance
+// Bytes32 Buffer.toString(hex)                 - 86.9 bytes / instance
+// Bytes32 Buffer.toString(hex) from Uint8Array - 87.6 bytes / instance
+// Bytes32 Buffer.toString(hex) + 0x            - 121.7 bytes / instance
 
 testRunnerMemoryBpi([
   {
@@ -19,8 +19,8 @@ testRunnerMemoryBpi([
     getInstance: () => crypto.randomBytes(32).toString("hex"),
   },
   {
-    id: "Bytes32 Buffer.toString(hex) Buffer.from()",
-    getInstance: () => Buffer.from(crypto.randomBytes(32)).toString("hex"),
+    id: "Bytes32 Buffer.toString(hex) from Uint8Array",
+    getInstance: () => Buffer.from(randomBytesUint8Array(32)).toString("hex"),
   },
   {
     id: "Bytes32 Buffer.toString(hex) + 0x",
@@ -43,4 +43,13 @@ function testRunnerMemoryBpi(testCases: {getInstance: (bytes: number) => unknown
     // eslint-disable-next-line no-console
     console.log(`${id.padEnd(longestId)} - ${bpi.toFixed(1)} bytes / instance`);
   }
+}
+
+function randomBytesUint8Array(bytes: number): Uint8Array {
+  const buf = crypto.randomBytes(bytes);
+  const uArr = new Uint8Array(bytes);
+  for (let i = 0; i < bytes; i++) {
+    uArr[i] = buf[i];
+  }
+  return uArr;
 }
--- packages/lodestar/test/perf/misc/bytesHex.test.ts
+++ packages/lodestar/test/perf/misc/bytesHex.test.ts
@@ -5,12 +5,14 @@ import {toHexString} from "@chainsafe/ssz";
 // Results in Linux Dec 2021
 //
 // misc / bytes32 to hex
-// ✓ bytes32 toHexString                                                  1184834 ops/s    844.0000 ns/op        -     346342 runs  0.404 s
-// ✓ bytes32 Buffer.toString(hex)                                         1669449 ops/s    599.0000 ns/op        -     775931 runs  0.707 s
-// ✓ bytes32 Buffer.toString(hex) + 0x                                    1683502 ops/s    594.0000 ns/op        -     670067 runs  0.606 s
+// ✓ bytes32 toHexString                                                  1248439 ops/s    801.0000 ns/op        -     731181 runs  0.808 s
+// ✓ bytes32 Buffer.toString(hex)                                         1610306 ops/s    621.0000 ns/op        -     871116 runs  0.808 s
+// ✓ bytes32 Buffer.toString(hex) from Uint8Array                         1321004 ops/s    757.0000 ns/op        -     567231 runs  0.606 s
+// ✓ bytes32 Buffer.toString(hex) + 0x                                    1647446 ops/s    607.0000 ns/op        -     446039 runs  0.404 s
 
 describe("misc / bytes32 to hex", () => {
   const bytes32 = crypto.randomBytes(32);
+  const uint8Arr = randomBytesUint8Array(32);
 
   itBench("bytes32 toHexString", () => {
     toHexString(bytes32);
@@ -20,7 +22,20 @@ describe("misc / bytes32 to hex", () => {
     bytes32.toString("hex");
   });
 
+  itBench("bytes32 Buffer.toString(hex) from Uint8Array", () => {
+    Buffer.from(uint8Arr).toString("hex");
+  });
+
   itBench("bytes32 Buffer.toString(hex) + 0x", () => {
     "0x" + bytes32.toString("hex");
   });
 });
+
+function randomBytesUint8Array(bytes: number): Uint8Array {
+  const buf = crypto.randomBytes(bytes);
+  const uArr = new Uint8Array(bytes);
+  for (let i = 0; i < bytes; i++) {
+    uArr[i] = buf[i];
+  }
+  return uArr;
+}
