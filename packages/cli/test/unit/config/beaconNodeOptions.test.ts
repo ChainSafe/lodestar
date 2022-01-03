56ba0e049cb0fa6a36c62197f8415960050e1861
Enrique Ortiz <hi@enriqueortiz.dev>
2022-01-03 10:30:42 -0400

    feat: add tests

--- packages/cli/test/unit/config/beaconNodeOptions.test.ts
+++ packages/cli/test/unit/config/beaconNodeOptions.test.ts
@@ -1,8 +1,13 @@
-import {expect} from "chai";
 import {defaultOptions, IBeaconNodeOptions} from "@chainsafe/lodestar";
-import {BeaconNodeOptions, mergeBeaconNodeOptions} from "../../../src/config";
-import {bootEnrs as pyrmontBootEnrs} from "../../../src/networks/pyrmont";
 import {RecursivePartial} from "@chainsafe/lodestar-utils";
+import {expect} from "chai";
+import fs from "fs";
+import path from "path";
+import {getBeaconPaths} from "../../../src/cmds/beacon/paths";
+import {BeaconNodeOptions, mergeBeaconNodeOptions} from "../../../src/config";
+import {enrsToNetworkConfig, getInjectableBootEnrs} from "../../../src/networks";
+import {bootEnrs as pyrmontBootEnrs} from "../../../src/networks/pyrmont";
+import {testFilesDir} from "../../utils";
 
 describe("config / beaconNodeOptions", () => {
   it("Should return pyrmont options", () => {
@@ -27,6 +32,49 @@ describe("config / beaconNodeOptions", () => {
 
     const optionsPartial = beaconNodeOptions.get();
     expect(optionsPartial).to.deep.equal(editedPartialOptions);
+  });
+
+  it("Should return options with injected custom bootnodes", async () => {
+    const expectedBootEnr = "enr:-KG4QOWkRj";
+    const rootDir = testFilesDir;
+    const bootnodesFile = path.join(testFilesDir, "bootnodesFile.txt");
+    fs.writeFileSync(bootnodesFile, expectedBootEnr);
+    const beaconPaths = getBeaconPaths({rootDir});
+    beaconPaths.bootnodesFile = bootnodesFile;
+
+    const injectableBootEnrs = await getInjectableBootEnrs(beaconPaths.bootnodesFile);
+
+    const beaconNodeOptions = new BeaconNodeOptions({
+      network: "pyrmont",
+      beaconNodeOptionsCli: injectableBootEnrs,
+    });
+
+    // Asserts only part of the data structure to avoid unnecesary duplicate code
+    const optionsPartial = beaconNodeOptions.get();
+    expect(optionsPartial?.network?.discv5?.bootEnrs).to.deep.equal([expectedBootEnr]);
+  });
+
+  it("Should return inline, CLI-provided boot ENR even if bootnodes file is provided", async () => {
+    const bootnodesFileContent = "enr:-KG4QOWkRj";
+    const expectedBootEnr = "enr:-W4gMj";
+
+    const rootDir = testFilesDir;
+    const bootnodesFile = path.join(testFilesDir, "bootnodesFile.txt");
+    fs.writeFileSync(bootnodesFile, bootnodesFileContent);
+    const beaconPaths = getBeaconPaths({rootDir});
+    beaconPaths.bootnodesFile = bootnodesFile;
+
+    const injectableBootEnrs = await getInjectableBootEnrs(beaconPaths.bootnodesFile);
+    const beaconNodeOptionsCli = mergeBeaconNodeOptions(injectableBootEnrs, enrsToNetworkConfig([expectedBootEnr]));
+
+    const beaconNodeOptions = new BeaconNodeOptions({
+      network: "pyrmont",
+      beaconNodeOptionsCli,
+    });
+
+    // Asserts only part of the data structure to avoid unnecesary duplicate code
+    const optionsPartial = beaconNodeOptions.get();
+    expect(optionsPartial?.network?.discv5?.bootEnrs).to.deep.equal([expectedBootEnr]);
   });
 
   it("Should return default options", () => {
