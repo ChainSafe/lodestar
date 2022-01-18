93fe3a5d744e8f29ef4cfdc9207fc3f87e974e8c
Dadepo Aderemi <dadepo@gmail.com>
2022-01-18 07:11:33 -0400

    fix linting error by using camelcase variable names

--- packages/cli/test/unit/validator/keys.test.ts
+++ packages/cli/test/unit/validator/keys.test.ts
@@ -9,16 +9,16 @@ describe("validator / keys / resolveKeystorePaths", () => {
   });
 
   it("should filter out deposite data files", function () {
-    const keystore_filename = "keystore-m_12381_3600_0_0_0-1642090404.json";
-    const deposite_data = "deposit_data-1642090404.json";
+    const keystoreFilename = "keystore-m_12381_3600_0_0_0-1642090404.json";
+    const depositData = "deposit_data-1642090404.json";
 
     sinon.stub(fs, "readdirSync").callsFake(() => {
-      return ([keystore_filename, deposite_data] as unknown) as Dirent[];
+      return ([keystoreFilename, depositData] as unknown) as Dirent[];
     });
 
     const result = resolveKeystorePaths("dir");
     expect(result.length).to.equal(1);
-    expect(result[0]).to.equal(`dir/${keystore_filename}`);
+    expect(result[0]).to.equal(`dir/${keystoreFilename}`);
   });
 
   afterEach(() => {
