217893a97ac6d2c318e19f2bea1b9a5ed5bd65ca
Dadepo Aderemi <dadepo@gmail.com>
2022-01-30 22:30:34 +0100

    Added first test for KeymanagerApi#listKeys

--- packages/keymanager-server/test/unit/index.test.ts
+++ packages/keymanager-server/test/unit/index.test.ts
@@ -1,7 +1,47 @@
-import {expect} from "chai";
+import {assert} from "chai";
+import sinon from "sinon";
+import {KeymanagerApi} from "../../src";
+import {ValidatorStore} from "@chainsafe/lodestar-validator";
+import {Root} from "@chainsafe/lodestar-types";
+import {SecretKeyInfo} from "../../src/server";
+import Sinon from "sinon";
+import {SlashingProtection} from "@chainsafe/lodestar-validator/src";
 
-describe("forks", () => {
-  it("TODO [DA] should be implemented", () => {
-    expect(true).to.equal(true);
+describe("keymanager", () => {
+  let validatorStoreSub: Sinon.SinonStubbedInstance<ValidatorStore>;
+  let slashingProtectionStub: Sinon.SinonStubbedInstance<SlashingProtection>;
+  let genesisValidatorRootStub: Sinon.SinonStubbedInstance<Uint8Array | Root>;
+
+  beforeEach(() => {
+    validatorStoreSub = sinon.createStubInstance(ValidatorStore);
+    slashingProtectionStub = sinon.createStubInstance(SlashingProtection);
+    genesisValidatorRootStub = sinon.createStubInstance(Uint8Array);
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it("should list keys", async () => {
+    const importKeystoresPath: string[] = [];
+    const secretKeysInfo: SecretKeyInfo[] = [];
+    const testPubKey = "0xfff";
+    validatorStoreSub.votingPubkeys.returns([testPubKey]);
+
+    const km = new KeymanagerApi(
+      (validatorStoreSub as unknown) as ValidatorStore,
+      slashingProtectionStub,
+      genesisValidatorRootStub,
+      importKeystoresPath,
+      secretKeysInfo
+    );
+
+    const keys = await km.listKeys();
+
+    assert.equal(validatorStoreSub.votingPubkeys.called, true);
+    assert.equal(keys.data.length, 1);
+    assert.equal(keys.data[0].validatingPubkey, testPubKey);
+    assert.equal(keys.data[0].derivationPath, "");
+    assert.equal(keys.data[0].readonly, false);
   });
 });
