bc1fea68ce027cb357822812dbccf4ccbaf00e4a
Dadepo Aderemi <dadepo@gmail.com>
2022-02-04 17:50:10 +0100

    Stubbing out the keystore. All tests should now pass on CI

--- packages/keymanager-server/test/unit/impl.test.ts
+++ packages/keymanager-server/test/unit/impl.test.ts
@@ -3,18 +3,20 @@ import sinon from "sinon";
 import {KeymanagerApi} from "../../src";
 import {Interchange, ValidatorStore} from "@chainsafe/lodestar-validator";
 import {Root} from "@chainsafe/lodestar-types";
-import {SecretKeyInfo} from "../../src/server";
+import {SecretKeyInfo} from "../../src";
 import Sinon from "sinon";
 import {SlashingProtection} from "@chainsafe/lodestar-validator/src";
 import fs from "fs";
 import {PublicKey, SecretKey} from "@chainsafe/bls";
 import {IInterchangeV5} from "@chainsafe/lodestar-validator/src/slashingProtection/interchange/formats/v5";
+import {Keystore} from "@chainsafe/bls-keystore";
 import lockfile from "lockfile";
 
 describe("keymanager", () => {
   let validatorStoreSub: Sinon.SinonStubbedInstance<ValidatorStore>;
   let slashingProtectionStub: Sinon.SinonStubbedInstance<SlashingProtection>;
   let genesisValidatorRootStub: Sinon.SinonStubbedInstance<Uint8Array | Root>;
+  let keystore: Sinon.SinonStubbedInstance<Keystore>;
   const interchange: Interchange = ("abc" as unknown) as Interchange;
   const PASSWORD = "passfortesting";
   // eslint-disable-next-line
@@ -24,6 +26,7 @@ describe("keymanager", () => {
     validatorStoreSub = sinon.createStubInstance(ValidatorStore);
     slashingProtectionStub = sinon.createStubInstance(SlashingProtection);
     genesisValidatorRootStub = sinon.createStubInstance(Uint8Array);
+    keystore = sinon.createStubInstance(Keystore);
   });
 
   afterEach(() => {
@@ -78,6 +81,9 @@ describe("keymanager", () => {
     it("should add a new key with KeystoresPath given", async () => {
       slashingProtectionStub.importInterchange.withArgs(interchange, genesisValidatorRootStub).resolves();
       validatorStoreSub.hasVotingPubkey.withArgs(sinon.match.any).returns(false);
+
+      subKeyStore(keystore);
+
       const fsStub = sinon
         .stub(fs.promises, "writeFile")
         .withArgs(sinon.match(/key_imported/), keyStoreStr, {encoding: "utf8"})
@@ -108,6 +114,8 @@ describe("keymanager", () => {
     it("should add a new key with no KeystoresPath given", async () => {
       slashingProtectionStub.importInterchange.withArgs(interchange, genesisValidatorRootStub).resolves();
       validatorStoreSub.hasVotingPubkey.withArgs(sinon.match.any).returns(false);
+      subKeyStore(keystore);
+
       const fsStub = sinon
         .stub(fs.promises, "writeFile")
         .withArgs(sinon.match(/key_imported/), keyStoreStr, {encoding: "utf8"})
@@ -261,6 +269,14 @@ describe("keymanager", () => {
     });
   });
 });
+
+function subKeyStore(keystore: Sinon.SinonStubbedInstance<Keystore>): void {
+  sinon.stub(Keystore, "parse").callsFake(() => {
+    return keystore;
+  });
+
+  keystore.decrypt.withArgs(sinon.match.any).resolves(Buffer.alloc(32, 1));
+}
 
 function makeinterchangeStubFromPubkey(pubkeys: string[]): IInterchangeV5 {
   const interchangeStub = <IInterchangeV5>{};
