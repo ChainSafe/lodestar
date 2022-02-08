dc9e3cddf78fce5ea0882615ba88302465fe1975
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2022-02-08 16:11:56 +0530

    Review -  Cache effectiveBalanceIncrements as Uint8Array (#3730)
    * Add comment on getEffectiveBalanceIncrementsWithLen
    
    * Call beforeEpochTransition

--- packages/beacon-state-transition/src/allForks/util/effectiveBalanceIncrements.ts
+++ packages/beacon-state-transition/src/allForks/util/effectiveBalanceIncrements.ts
@@ -8,3 +8,16 @@ export type EffectiveBalanceIncrements = Uint8Array;
 export function getEffectiveBalanceIncrementsZeroed(len: number): EffectiveBalanceIncrements {
   return new Uint8Array(len);
 }
+
+/**
+ * effectiveBalanceIncrements length will always be equal or greater than validatorCount. The
+ * getEffectiveBalanceIncrementsByteLen() modulo is used to reduce the frequency at which its Uint8Array is recreated.
+ * if effectiveBalanceIncrements has length greater than validatorCount it's not a problem since those values would
+ * never be accessed.
+ */
+export function getEffectiveBalanceIncrementsWithLen(validatorCount: number): EffectiveBalanceIncrements {
+  // TODO: Research what's the best number to minimize both memory cost and copy costs
+  const byteLen = 1024 * Math.ceil(validatorCount / 1024);
+
+  return new Uint8Array(byteLen);
+}
