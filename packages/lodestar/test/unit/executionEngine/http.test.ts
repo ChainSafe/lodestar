d02fb1b8cf93fe62456a10487160cf75225e2fb2
harkamal <gajinder@g11.in>
2022-02-09 01:59:18 +0530

    Fix test's response for fcU call

--- packages/lodestar/test/unit/executionEngine/http.test.ts
+++ packages/lodestar/test/unit/executionEngine/http.test.ts
@@ -130,7 +130,11 @@ describe("ExecutionEngine / http", () => {
         },
       ],
     };
-    returnValue = {jsonrpc: "2.0", id: 67, result: {status: "SUCCESS", payloadId: "0x"}};
+    returnValue = {
+      jsonrpc: "2.0",
+      id: 67,
+      result: {payloadStatus: {status: "VALID", latestValidHash: null, validationError: null}, payloadId: "0x"},
+    };
 
     await executionEngine.notifyForkchoiceUpdate(request.params[0].headBlockHash, request.params[0].finalizedBlockHash);
 
