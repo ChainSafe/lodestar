7818d0689db029d581e104e8dd82c99bc882aad9
harkamal <gajinder@g11.in>
2022-02-10 13:55:20 +0530

    Cleanup discontinued pyrmont network

--- packages/cli/src/cmds/beacon/index.ts
+++ packages/cli/src/cmds/beacon/index.ts
@@ -8,8 +8,8 @@ export const beacon: ICliCommand<IBeaconArgs, IGlobalArgs> = {
   describe: "Run a beacon chain node",
   examples: [
     {
-      command: "beacon --network pyrmont",
-      description: "Run a beacon chain node and connect to the pyrmont testnet",
+      command: "beacon --network prater",
+      description: "Run a beacon chain node and connect to the prater testnet",
     },
   ],
   options: beaconOptions as ICliCommandOptions<IBeaconArgs>,
--- packages/config/src/networks.ts
+++ packages/config/src/networks.ts
@@ -1,15 +1,13 @@
 import {IChainConfig} from "./chainConfig";
 import {mainnetChainConfig} from "./chainConfig/networks/mainnet";
-import {pyrmontChainConfig} from "./chainConfig/networks/pyrmont";
 import {praterChainConfig} from "./chainConfig/networks/prater";
 import {kintsugiChainConfig} from "./chainConfig/networks/kintsugi";
 
-export {mainnetChainConfig, pyrmontChainConfig, praterChainConfig, kintsugiChainConfig};
+export {mainnetChainConfig, praterChainConfig, kintsugiChainConfig};
 
-export type NetworkName = "mainnet" | "pyrmont" | "prater" | "kintsugi";
+export type NetworkName = "mainnet" | "prater" | "kintsugi";
 export const networksChainConfig: Record<NetworkName, IChainConfig> = {
   mainnet: mainnetChainConfig,
-  pyrmont: pyrmontChainConfig,
   prater: praterChainConfig,
   kintsugi: kintsugiChainConfig,
 };
