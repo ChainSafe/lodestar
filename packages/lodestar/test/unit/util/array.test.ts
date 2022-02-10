b6cda882a54f81941a1a925c084512e685007574
Tuyen Nguyen <vutuyen2636@gmail.com>
2022-02-10 10:04:05 +0700

    Implement LinkedList

--- packages/lodestar/test/unit/util/array.test.ts
+++ packages/lodestar/test/unit/util/array.test.ts
@@ -1,6 +1,6 @@
 import {expect} from "chai";
 
-import {findLastIndex} from "../../../src/util/array";
+import {findLastIndex, LinkedList} from "../../../src/util/array";
 
 describe("findLastIndex", () => {
   it("should return the last index that matches a predicate", () => {
@@ -12,5 +12,64 @@ describe("findLastIndex", () => {
   it("should return -1 if there are no matches", () => {
     expect(findLastIndex([1, 3, 5], (n) => n % 2 == 0)).to.eql(-1);
     expect(findLastIndex([1, 2, 3, 4, 5], () => false)).to.eql(-1);
+  });
+});
+
+describe("LinkedList", () => {
+  let list: LinkedList<number>;
+
+  beforeEach(() => {
+    list = new LinkedList<number>();
+  });
+
+  it("pop", () => {
+    expect(list.pop() === null);
+    expect(list.length).to.be.equal(0);
+    let count = 100;
+    for (let i = 0; i < count; i++) list.push(i + 1);
+
+    while (count > 0) {
+      expect(list.length).to.be.equal(count);
+      expect(list.pop()).to.be.equal(count);
+      count--;
+    }
+
+    expect(list.pop() === null);
+    expect(list.length).to.be.equal(0);
+  });
+
+  it("shift", () => {
+    expect(list.shift() === null);
+    expect(list.length).to.be.equal(0);
+    const count = 100;
+    for (let i = 0; i < count; i++) list.push(i);
+
+    for (let i = 0; i < count; i++) {
+      expect(list.length).to.be.equal(count - i);
+      expect(list.shift()).to.be.equal(i);
+    }
+
+    expect(list.shift() === null);
+    expect(list.length).to.be.equal(0);
+  });
+
+  it("toArray", () => {
+    expect(list.toArray()).to.be.deep.equal([]);
+
+    const count = 100;
+    for (let i = 0; i < count; i++) list.push(i);
+
+    expect(list.length).to.be.equal(count);
+    expect(list.toArray()).to.be.deep.equal(Array.from({length: count}, (_, i) => i));
+  });
+
+  it("prune", () => {
+    const count = 100;
+    for (let i = 0; i < count; i++) list.push(i);
+
+    list.prune();
+
+    expect(list.toArray()).to.be.deep.equal([]);
+    expect(list.length).to.be.equal(0);
   });
 });
