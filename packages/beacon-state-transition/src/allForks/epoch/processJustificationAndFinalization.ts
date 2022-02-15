da8f3587ea1c758fb0407936835da701695757d2
dapplion <35266934+dapplion@users.noreply.github.com>
2022-02-15 14:11:05 +0530

    Use bool array in processJustificationAndFinalization

--- packages/beacon-state-transition/src/allForks/epoch/processJustificationAndFinalization.ts
+++ packages/beacon-state-transition/src/allForks/epoch/processJustificationAndFinalization.ts
@@ -1,4 +1,5 @@
 import {GENESIS_EPOCH} from "@chainsafe/lodestar-params";
+import {BitArray} from "@chainsafe/ssz";
 import {ssz} from "@chainsafe/lodestar-types";
 import {getBlockRoot} from "../../util";
 import {CachedBeaconStateAllForks, EpochProcess} from "../../types";
@@ -26,45 +27,48 @@ export function processJustificationAndFinalization(
 
   // Process justifications
   state.previousJustifiedCheckpoint = state.currentJustifiedCheckpoint;
-  const bits = state.justificationBits;
-  for (let i = bits.bitLen - 1; i >= 1; i--) {
-    bits.set(i, bits.get(i - 1));
+  const bits = state.justificationBits.toBoolArray();
+
+  // Rotate bits
+  for (let i = bits.length - 1; i >= 1; i--) {
+    bits[i] = bits[i - 1];
   }
-  bits.set(0, false);
+  bits[0] = false;
 
   if (epochProcess.prevEpochUnslashedStake.targetStakeByIncrement * 3 >= epochProcess.totalActiveStakeByIncrement * 2) {
     state.currentJustifiedCheckpoint = ssz.phase0.Checkpoint.toViewDU({
       epoch: previousEpoch,
       root: getBlockRoot(state, previousEpoch),
     });
-    bits.set(1, true);
+    bits[1] = true;
   }
   if (epochProcess.currEpochUnslashedTargetStakeByIncrement * 3 >= epochProcess.totalActiveStakeByIncrement * 2) {
     state.currentJustifiedCheckpoint = ssz.phase0.Checkpoint.toViewDU({
       epoch: currentEpoch,
       root: getBlockRoot(state, currentEpoch),
     });
-    bits.set(0, true);
+    bits[1] = true;
   }
-  state.justificationBits = bits;
+
+  state.justificationBits = ssz.phase0.JustificationBits.toViewDU(BitArray.fromBoolArray(bits));
 
   // TODO: Consider rendering bits as array of boolean for faster repeated access here
 
   // Process finalizations
   // The 2nd/3rd/4th most recent epochs are all justified, the 2nd using the 4th as source
-  if (bits.get(1) && bits.get(2) && bits.get(3) && oldPreviousJustifiedCheckpoint.epoch + 3 === currentEpoch) {
+  if (bits[1] && bits[2] && bits[3] && oldPreviousJustifiedCheckpoint.epoch + 3 === currentEpoch) {
     state.finalizedCheckpoint = oldPreviousJustifiedCheckpoint;
   }
   // The 2nd/3rd most recent epochs are both justified, the 2nd using the 3rd as source
-  if (bits.get(1) && bits.get(2) && oldPreviousJustifiedCheckpoint.epoch + 2 === currentEpoch) {
+  if (bits[1] && bits[2] && oldPreviousJustifiedCheckpoint.epoch + 2 === currentEpoch) {
     state.finalizedCheckpoint = oldPreviousJustifiedCheckpoint;
   }
   // The 1st/2nd/3rd most recent epochs are all justified, the 1st using the 3rd as source
-  if (bits.get(0) && bits.get(1) && bits.get(2) && oldCurrentJustifiedCheckpoint.epoch + 2 === currentEpoch) {
+  if (bits[0] && bits[1] && bits[2] && oldCurrentJustifiedCheckpoint.epoch + 2 === currentEpoch) {
     state.finalizedCheckpoint = oldCurrentJustifiedCheckpoint;
   }
   // The 1st/2nd most recent epochs are both justified, the 1st using the 2nd as source
-  if (bits.get(0) && bits.get(1) && oldCurrentJustifiedCheckpoint.epoch + 1 === currentEpoch) {
+  if (bits[0] && bits[1] && oldCurrentJustifiedCheckpoint.epoch + 1 === currentEpoch) {
     state.finalizedCheckpoint = oldCurrentJustifiedCheckpoint;
   }
 }
