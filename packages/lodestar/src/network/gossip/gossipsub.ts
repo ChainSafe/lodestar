485b7354ebe0304ff6de50cc1b67c894e8edc63e
dapplion <35266934+dapplion@users.noreply.github.com>
2022-02-28 23:06:51 +0530

    Add detailed score metrics

--- packages/lodestar/src/network/gossip/gossipsub.ts
+++ packages/lodestar/src/network/gossip/gossipsub.ts
@@ -43,6 +43,7 @@ import {
 } from "./scoringParameters";
 import {Eth2Context} from "../../chain";
 import {IPeerRpcScoreStore} from "../peers";
+import {computeAllPeersScoreWeights} from "./scoreMetrics";
 
 export interface IGossipsubModules {
   config: IBeaconConfig;
@@ -441,7 +442,6 @@ export class Eth2Gossipsub extends Gossipsub {
     let peerCountScoreGossip = 0;
     let peerCountScoreMesh = 0;
     const {graylistThreshold, publishThreshold, gossipThreshold} = gossipScoreThresholds;
-    const {scoreByThreshold, score: scoreMetric} = metrics.gossipPeer;
     const gossipScores = [];
 
     for (const peerIdStr of this.peers.keys()) {
@@ -453,12 +453,36 @@ export class Eth2Gossipsub extends Gossipsub {
       gossipScores.push(score);
     }
 
+    // Access once for all calls below
+    const {scoreByThreshold, scoreWeights} = metrics.gossipPeer;
     scoreByThreshold.set({threshold: "graylist"}, peerCountScoreGraylist);
     scoreByThreshold.set({threshold: "publish"}, peerCountScorePublish);
     scoreByThreshold.set({threshold: "gossip"}, peerCountScoreGossip);
     scoreByThreshold.set({threshold: "mesh"}, peerCountScoreMesh);
 
-    scoreMetric.set(gossipScores);
+    // Breakdown on each score weight
+    const sw = computeAllPeersScoreWeights(
+      this.peers.keys(),
+      this.score.peerStats,
+      this.score.params,
+      this.score.peerIPs,
+      this.gossipTopicCache
+    );
+
+    for (const [topic, wsTopic] of sw.byTopic) {
+      scoreWeights.set({topic, p: "p1"}, wsTopic.p1w);
+      scoreWeights.set({topic, p: "p2"}, wsTopic.p2w);
+      scoreWeights.set({topic, p: "p3"}, wsTopic.p3w);
+      scoreWeights.set({topic, p: "p3b"}, wsTopic.p3bw);
+      scoreWeights.set({topic, p: "p4"}, wsTopic.p4w);
+    }
+
+    scoreWeights.set({p: "p5"}, sw.p5w);
+    scoreWeights.set({p: "p6"}, sw.p6w);
+    scoreWeights.set({p: "p7"}, sw.p7w);
+
+    // Register full score too
+    metrics.gossipPeer.score.set(sw.score);
   }
 }
 
