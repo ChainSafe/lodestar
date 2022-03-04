211ab062df599666dec36ce7ea2793d1416598e4
harkamal <gajinder@g11.in>
2022-03-04 16:48:28 +0530

    configuring nethermind with jwtAuth

--- kiln/nethermind/post-merge.sh
+++ kiln/nethermind/post-merge.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 
 cd $EL_BINARY_DIR
-dotnet run -c Release -- --config themerge_kiln_testvectors --Merge.TerminalTotalDifficulty $TTD
+dotnet run -c Release -- --config themerge_kiln_testvectors --Merge.TerminalTotalDifficulty $TTD --JsonRpc.JwtSecretFile $JWT_SECRET_HEX
