652426eea2d906e239c981768f7393a4746efc6c
harkamal <gajinder@g11.in>
2022-03-04 17:13:12 +0530

    fixes for providong jwt file

--- kiln/nethermind/post-merge.sh
+++ kiln/nethermind/post-merge.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -x
 
+. $scriptDir/common-setup.sh
+
 cd $EL_BINARY_DIR
-dotnet run -c Release -- --config themerge_kiln_testvectors --Merge.TerminalTotalDifficulty $TTD --JsonRpc.JwtSecretFile $JWT_SECRET_HEX
+dotnet run -c Release -- --config themerge_kiln_testvectors --Merge.TerminalTotalDifficulty $TTD --JsonRpc.JwtSecretFile $DATA_DIR/jwtsecret
