f597feab2f6bcb21089add6d679c6c158c5ba2f5
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2022-03-09 12:22:40 +0530

    Use in-memory peer store for ephemeral data (#3838)
    * Use in-memory peer store for ephemeral data
    
    * Bound PeerRpcScoreStore data structures
    
    * Update test type
    
    * Fix e2e test in PeerManager

--- packages/lodestar/test/e2e/network/peers/peerManager.test.ts
+++ packages/lodestar/test/e2e/network/peers/peerManager.test.ts
@@ -4,7 +4,7 @@ import sinon from "sinon";
 import {expect} from "chai";
 import {config} from "@chainsafe/lodestar-config/default";
 import {IReqResp, ReqRespMethod} from "../../../../src/network/reqresp";
-import {PeerRpcScoreStore, PeerManager, Libp2pPeerMetadataStore} from "../../../../src/network/peers";
+import {PeerRpcScoreStore, PeerManager} from "../../../../src/network/peers";
 import {NetworkEvent, NetworkEventBus} from "../../../../src/network";
 import {createNode, getAttnets, getSyncnets} from "../../../utils/network";
 import {MockBeaconChain} from "../../../utils/mocks/chain/chain";
@@ -57,8 +57,7 @@ describe("network / peers / PeerManager", function () {
     });
 
     const reqResp = new ReqRespFake();
-    const peerMetadata = new Libp2pPeerMetadataStore(libp2p.peerStore.metadataBook);
-    const peerRpcScores = new PeerRpcScoreStore(peerMetadata);
+    const peerRpcScores = new PeerRpcScoreStore();
     const networkEventBus = new NetworkEventBus();
     /* eslint-disable @typescript-eslint/no-empty-function */
     const mockSubnetsService: IAttnetsService = {
@@ -79,7 +78,6 @@ describe("network / peers / PeerManager", function () {
         metrics: null,
         chain,
         config: beaconConfig,
-        peerMetadata,
         peerRpcScores,
         networkEventBus,
         attnetsService: mockSubnetsService,
@@ -94,7 +92,7 @@ describe("network / peers / PeerManager", function () {
     );
     await peerManager.start();
 
-    return {chain, libp2p, reqResp, peerMetadata, peerManager, networkEventBus};
+    return {chain, libp2p, reqResp, peerManager, networkEventBus};
   }
 
   // Create a real event emitter with stubbed methods
@@ -107,11 +105,17 @@ describe("network / peers / PeerManager", function () {
     ping = sinon.stub();
     beaconBlocksByRange = sinon.stub();
     beaconBlocksByRoot = sinon.stub();
-    pruneRateLimiterData = sinon.stub();
+    pruneOnPeerDisconnect = sinon.stub();
   }
 
   it("Should request metadata on receivedPing of unknown peer", async () => {
-    const {reqResp, networkEventBus} = await mockModules();
+    const {reqResp, networkEventBus, peerManager} = await mockModules();
+
+    // Simulate connection so that PeerManager persists the metadata response
+    peerManager["onLibp2pPeerConnect"]({
+      stat: {direction: "inbound", status: "open"},
+      remotePeer: peerId1,
+    } as Connection);
 
     const seqNumber = BigInt(2);
     const metadata: phase0.Metadata = {seqNumber, attnets: []};
@@ -157,7 +161,7 @@ describe("network / peers / PeerManager", function () {
   });
 
   it("On peerConnect handshake flow", async function () {
-    const {chain, libp2p, reqResp, peerMetadata, networkEventBus} = await mockModules();
+    const {chain, libp2p, reqResp, peerManager, networkEventBus} = await mockModules();
 
     // Simualate a peer connection, get() should return truthy
     libp2p.connectionManager.get = sinon.stub().returns({});
@@ -190,6 +194,9 @@ describe("network / peers / PeerManager", function () {
     expect(reqResp.status.callCount).to.equal(1, "reqResp.status must be called");
     expect(reqResp.metadata.callCount).to.equal(1, "reqResp.metadata must be called");
 
-    expect(peerMetadata.metadata.get(peerId1)).to.deep.equal(remoteMetadata, "Wrong stored metadata");
+    expect(peerManager["connectedPeers"].get(peerId1.toB58String())?.metadata).to.deep.equal(
+      remoteMetadata,
+      "Wrong stored metadata"
+    );
   });
 });
--- packages/lodestar/test/utils/peer.ts
+++ packages/lodestar/test/utils/peer.ts
@@ -1,6 +1,4 @@
 import PeerId from "peer-id";
-import sinon from "sinon";
-import {PeerStoreBucket, IPeerMetadataStore} from "../../src/network/peers/metastore";
 
 /**
  * Returns a valid PeerId with opts `bits: 256, keyType: "secp256k1"`
@@ -9,24 +7,4 @@ import {PeerStoreBucket, IPeerMetadataStore} from "../../src/network/peers/metas
 export function getValidPeerId(): PeerId {
   const id = Buffer.from("002508021221039481269fe831799b1a0f1d521c1395b4831514859e4559c44d155eae46f03819", "hex");
   return new PeerId(id);
-}
-
-function getStubbedMetadataStoreItem<T>(): sinon.SinonStubbedInstance<PeerStoreBucket<T>> {
-  return {
-    get: sinon.stub(),
-    set: sinon.stub(),
-  };
-}
-
-export type StubbedIPeerMetadataStore = {
-  [K in keyof IPeerMetadataStore]: sinon.SinonStubbedInstance<IPeerMetadataStore[K]>;
-};
-
-export function getStubbedMetadataStore(): StubbedIPeerMetadataStore {
-  return {
-    encoding: getStubbedMetadataStoreItem(),
-    metadata: getStubbedMetadataStoreItem(),
-    rpcScore: getStubbedMetadataStoreItem(),
-    rpcScoreLastUpdate: getStubbedMetadataStoreItem(),
-  };
 }
