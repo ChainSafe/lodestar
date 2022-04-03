1c44d3b73f81278e2717d44d70cb77aa55de29f2
Dadepo Aderemi <dadepo@gmail.com>
2022-04-03 18:13:14 +0200

    Added test for when doppelganger is on but no duplicate live keys

--- packages/lodestar/test/e2e/doppelganger/doppelganger.test.ts
+++ packages/lodestar/test/e2e/doppelganger/doppelganger.test.ts
@@ -96,4 +96,74 @@ describe("doppelganger / doppelganger test", function () {
       "validator with doppelganger protection should be stopped after first epoch"
     );
   });
+  it("should not shut down validator if key is different", async function () {
+    this.timeout("10 min");
+
+    const enableDoppelganger = true;
+    const doppelgangerEpochsToCheck = 2;
+    const testLoggerOpts: TestLoggerOpts = {logLevel: LogLevel.info};
+    const loggerNodeA = testLogger("Node-A", testLoggerOpts);
+    const loggerNodeB = testLogger("Node-B", testLoggerOpts);
+
+    const bn = await getDevBeaconNode({
+      params: beaconParams,
+      options: {sync: {isSingleNode: true}},
+      validatorCount,
+      logger: loggerNodeA,
+    });
+    afterEachCallbacks.push(() => bn.close());
+
+    const {validators: validatorsWithDoppelganger} = await getAndInitDevValidators({
+      node: bn,
+      validatorsPerClient: validatorCount,
+      validatorClientCount: 1,
+      startIndex: 0,
+      useRestApi: false,
+      testLoggerOpts,
+      enableDoppelganger,
+      doppelgangerEpochsToCheck,
+    });
+    afterEachCallbacks.push(() => Promise.all(validatorsWithDoppelganger.map((v) => v.stop())));
+
+    const bn2 = await getDevBeaconNode({
+      params: beaconParams,
+      options: {api: {rest: {enabled: false}}},
+      validatorCount,
+      genesisTime: bn.chain.getHeadState().genesisTime,
+      logger: loggerNodeB,
+    });
+    afterEachCallbacks.push(() => bn2.close());
+
+    const {validators} = await getAndInitDevValidators({
+      node: bn2,
+      validatorsPerClient: validatorCount,
+      validatorClientCount: 1,
+      startIndex: 1,
+      useRestApi: false,
+      testLoggerOpts,
+    });
+    afterEachCallbacks.push(() => Promise.all(validators.map((v) => v.stop())));
+
+    await connect(bn2.network as Network, bn.network.peerId, bn.network.localMultiaddrs);
+
+    await Promise.all([...validatorsWithDoppelganger, ...validators].map((validator) => validator.start()));
+
+    expect(validators[0].getStatus()).to.be.equals(
+      "running",
+      "validator without doppelganger protection should be running"
+    );
+    expect(validatorsWithDoppelganger[0].getStatus()).to.be.equals(
+      "running",
+      "validator with doppelganger protection should be running before first epoch"
+    );
+    await waitForEvent<phase0.Checkpoint>(bn2.chain.emitter, ChainEvent.clockEpoch, 240000);
+    expect(validators[0].getStatus()).to.be.equals(
+      "running",
+      "validator without doppelganger protection should still be running after first epoch"
+    );
+    expect(validatorsWithDoppelganger[0].getStatus()).to.be.equals(
+      "running",
+      "validator with doppelganger protection should still be active after first epoch"
+    );
+  });
 });
