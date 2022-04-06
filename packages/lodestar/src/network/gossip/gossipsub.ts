4d766d1ad6569dc3f4aa628c57c695f1abc7828e
tuyennhv <vutuyen2636@gmail.com>
2022-04-06 14:38:29 +0700

    Handle unknown metrics in gossipsub onScrapeMetrics (#3889)
    * Handle unknown metrics in gossipsub onScrapeMetrics
    
    * Change version to 0.34.4
    
    Co-authored-by: Lion - dapplion <35266934+dapplion@users.noreply.github.com>

--- packages/lodestar/src/network/gossip/gossipsub.ts
+++ packages/lodestar/src/network/gossip/gossipsub.ts
@@ -424,13 +424,17 @@ export class Eth2Gossipsub extends Gossipsub {
         // Ignore topics with 0 peers. May prevent overriding after a fork
         if (peers.size === 0) continue;
 
-        const topic = this.gossipTopicCache.getTopic(topicString);
-        if (topic.type === GossipType.beacon_attestation) {
-          peersByBeaconAttSubnetByFork.set(topic.fork, topic.subnet, peers.size);
-        } else if (topic.type === GossipType.sync_committee) {
-          peersByBeaconSyncSubnetByFork.set(topic.fork, topic.subnet, peers.size);
-        } else {
-          peersByTypeByFork.set(topic.fork, topic.type, peers.size);
+        // there are some new topics in the network so `getKnownTopic()` returns undefined
+        // for example in prater: /eth2/82f4a72b/optimistic_light_client_update_v0/ssz_snappy
+        const topic = this.gossipTopicCache.getKnownTopic(topicString);
+        if (topic !== undefined) {
+          if (topic.type === GossipType.beacon_attestation) {
+            peersByBeaconAttSubnetByFork.set(topic.fork, topic.subnet, peers.size);
+          } else if (topic.type === GossipType.sync_committee) {
+            peersByBeaconSyncSubnetByFork.set(topic.fork, topic.subnet, peers.size);
+          } else {
+            peersByTypeByFork.set(topic.fork, topic.type, peers.size);
+          }
         }
       }
 
