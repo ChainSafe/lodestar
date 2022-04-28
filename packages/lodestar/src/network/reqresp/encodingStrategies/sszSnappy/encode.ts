66f1c3f80a6daba000e20267bddd9b6ed9ff3cc3
harkamal <gajinder@g11.in>
2022-04-28 16:47:56 +0530

    lint

--- packages/lodestar/src/network/reqresp/encodingStrategies/sszSnappy/encode.ts
+++ packages/lodestar/src/network/reqresp/encodingStrategies/sszSnappy/encode.ts
@@ -30,11 +30,11 @@ export async function* writeSszSnappyPayload<T extends RequestOrOutgoingResponse
  */
 function encodeSszSnappy(bytes: Buffer): AsyncGenerator<Buffer> {
   /**
-   * Use sync version (default) for compress as it is almost 2x faster than async 
+   * Use sync version (default) for compress as it is almost 2x faster than async
    * one and most payloads are "1 chunk" and 100kb payloads (which would mostly be
    * big bellatrix blocks with transactions) are just 2 chunks
-   * 
-   * To use async version (for e.g. on big payloads) instantiate the stream with 
+   *
+   * To use async version (for e.g. on big payloads) instantiate the stream with
    * `createCompressStream({asyncCompress: true})`
    */
   const stream = createCompressStream();
