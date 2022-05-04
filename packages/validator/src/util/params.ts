62fe0dc53b99365ba6d1af7824cc356473e49d0f
Dadepo Aderemi <dadepo@gmail.com>
2022-05-04 17:13:26 +0200

    Normalise values when comparing validator's beacon local config with remote beacon config

--- packages/validator/src/util/params.ts
+++ packages/validator/src/util/params.ts
@@ -5,16 +5,17 @@ export class NotEqualParamsError extends Error {}
 /**
  * Assert localConfig values match externalSpecJson. externalSpecJson may contain more values than localConfig.
  */
-export function assertEqualParams(localConfig: IChainConfig, externalSpecJson: Record<string, string>): void {
-  const params1Json = chainConfigToJson(localConfig) as Record<string, unknown>;
-  const params2Json = externalSpecJson;
+export function assertEqualParams(localConfig: IChainConfig, remoteConfigJson: Record<string, string>): void {
+  const localConfigJson = chainConfigToJson(localConfig) as Record<string, unknown>;
 
   const errors: string[] = [];
 
   // Ensure only that the localConfig values match the remote spec
-  for (const key of Object.keys(params1Json)) {
-    if (params1Json[key] !== params2Json[key])
-      errors.push(`${key} different value: ${params1Json[key]} != ${params2Json[key]}`);
+  for (const key of Object.keys(localConfigJson)) {
+    if (
+      (localConfigJson[key] as string)?.toLocaleLowerCase() !== (remoteConfigJson[key] as string)?.toLocaleLowerCase()
+    )
+      errors.push(`${key} different value: ${localConfigJson[key]} != ${remoteConfigJson[key]}`);
   }
 
   if (errors.length > 0) {
