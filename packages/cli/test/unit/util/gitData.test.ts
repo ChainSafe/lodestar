d5427132f1e3a2b90f0f9c228b708c094bde9e8c
dapplion <35266934+dapplion@users.noreply.github.com>
2022-05-09 18:11:57 +0200

    Simplify gitData and version guessing
    Don't print double slash in version string
    
    Dont add git-data.json to NPM releases
    
    Write git-data.json only in from source docker build
    
    Remove numCommits
    
    Test git-data.json generation from within the test
    
    Move comment
    
    Revert "Dont add git-data.json to NPM releases"
    
    This reverts commit 5fe2d388825f3e3a834058478071e8364b0d761c.
    
    Simplify gitData and version guessing
    
    Run cmd

--- packages/cli/test/unit/util/gitData.test.ts
+++ packages/cli/test/unit/util/gitData.test.ts
@@ -1,15 +1,28 @@
 import {expect} from "chai";
+import child_process from "node:child_process";
 import fs from "node:fs";
 import path from "node:path";
 import findUp from "find-up";
 import {gitDataPath, readGitDataFile} from "../../../src/util/gitData/gitDataPath";
+import {getGitData} from "../../../src/util";
+
+const WRITE_GIT_DATA_CMD = "npm run write-git-data";
 
 describe("util / gitData", () => {
+  before(() => {
+    const pkgJsonPath = findUp.sync("package.json", {cwd: __dirname});
+    if (!pkgJsonPath) {
+      throw Error("No package.json found");
+    }
+
+    const pkgJsonDir = path.resolve(path.dirname(pkgJsonPath));
+    child_process.execSync(WRITE_GIT_DATA_CMD, {cwd: pkgJsonDir});
+  });
+
   it("gitData file must exist", () => {
     const gitData = readGitDataFile();
 
-    if (!gitData.branch) throw Error("No gitData.branch");
-    if (!gitData.commit) throw Error("No gitData.commit");
+    expect(gitData).to.deep.equal(getGitData(), "Wrong git-data.json contents");
   });
 
   it("gitData path must be included in the package.json", () => {
