25c21919e15dfaa520e6176186f2515d3a46cfea
dapplion <35266934+dapplion@users.noreply.github.com>
2022-05-09 17:06:29 +0200

    Update bucket values

--- packages/lodestar/src/metrics/metrics/lodestar.ts
+++ packages/lodestar/src/metrics/metrics/lodestar.ts
@@ -517,17 +517,17 @@ export function createLodestarMetrics(
       elapsedTimeTillBecomeHead: register.histogram({
         name: "lodestar_gossip_block_elapsed_time_till_become_head",
         help: "Time elappsed between block slot time and the time block becomes head",
-        buckets: [0.1, 1, 10],
+        buckets: [0.5, 1, 2, 4, 6, 12],
       }),
       elapsedTimeTillReceived: register.histogram({
         name: "lodestar_gossip_block_elappsed_time_till_received",
         help: "Time elappsed between block slot time and the time block received via gossip",
-        buckets: [0.1, 1, 10],
+        buckets: [0.5, 1, 2, 4, 6, 12],
       }),
       elapsedTimeTillProcessed: register.histogram({
         name: "lodestar_gossip_block_elappsed_time_till_processed",
         help: "Time elappsed between block slot time and the time block processed",
-        buckets: [0.1, 1, 10],
+        buckets: [0.5, 1, 2, 4, 6, 12],
       }),
     },
 
