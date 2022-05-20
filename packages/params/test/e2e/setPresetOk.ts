d641ac8ef8b602df480d30a1902646485afdbdd2
Cayman <caymannava@gmail.com>
2022-05-20 08:01:36 -0500

    Use src instead of lib

--- packages/params/test/e2e/setPresetOk.ts
+++ packages/params/test/e2e/setPresetOk.ts
@@ -2,13 +2,13 @@
 // It demostrates how to properly change the Lodestar preset safely
 
 // 1. Import from @chainsafe/lodestar-params/setPreset only
-import {setActivePreset, PresetName} from "../../lib/setPreset.js";
+import {setActivePreset, PresetName} from "../../src/setPreset.js";
 setActivePreset(PresetName.minimal);
 
 // 2. Import from any other @chainsafe/lodestar-params paths
 import {expect} from "chai";
 
-const {SLOTS_PER_EPOCH} = await import("../../lib/index.js");
+const {SLOTS_PER_EPOCH} = await import("../../src/index.js");
 
 expect(SLOTS_PER_EPOCH).to.equal(8, "SLOTS_PER_EPOCH should have minimal preset value");
 expect(process.env.LODESTAR_PRESET).to.equal(undefined, "LODESTAR_PRESET ENV must not be set");
