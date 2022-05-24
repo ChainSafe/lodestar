7b9cd7f45ff285735c85957f37b770f91a7be279
g11tech <76567250+g11tech@users.noreply.github.com>
2022-05-24 14:07:32 +0530

    Add ropsten network (#4051)

---
+++ packages/cli/src/networks/ropsten.ts
@@ -0,0 +1,13 @@
+import {ropstenChainConfig} from "@chainsafe/lodestar-config/networks";
+
+export const chainConfig = ropstenChainConfig;
+
+/* eslint-disable max-len */
+
+export const depositContractDeployBlock = 12269949;
+export const genesisFileUrl =
+  "https://raw.githubusercontent.com/eth-clients/merge-testnets/main/ropsten-beacon-chain/genesis.ssz";
+export const bootnodesFileUrl =
+  "https://raw.githubusercontent.com/eth-clients/merge-testnets/main/ropsten-beacon-chain/bootstrap_nodes.txt";
+
+export const bootEnrs = [];
--- packages/config/src/networks.ts
+++ packages/config/src/networks.ts
@@ -2,12 +2,14 @@ import {IChainConfig} from "./chainConfig";
 import {mainnetChainConfig} from "./chainConfig/networks/mainnet";
 import {praterChainConfig} from "./chainConfig/networks/prater";
 import {kilnChainConfig} from "./chainConfig/networks/kiln";
+import {ropstenChainConfig} from "./chainConfig/networks/ropsten";
 
-export {mainnetChainConfig, praterChainConfig, kilnChainConfig};
+export {mainnetChainConfig, praterChainConfig, kilnChainConfig, ropstenChainConfig};
 
-export type NetworkName = "mainnet" | "prater" | "kiln";
+export type NetworkName = "mainnet" | "prater" | "kiln" | "ropsten";
 export const networksChainConfig: Record<NetworkName, IChainConfig> = {
   mainnet: mainnetChainConfig,
   prater: praterChainConfig,
   kiln: kilnChainConfig,
+  ropsten: ropstenChainConfig,
 };
