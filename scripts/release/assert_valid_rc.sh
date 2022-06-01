06c852e89c18ef3cc1422a464ba423280a759658
Cayman <caymannava@gmail.com>
2022-06-01 04:22:19 -0400

    Add bash assert_valid_rc script (#4096)

---
+++ scripts/release/assert_valid_rc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Asserts whether a tag is a valid release candidate or not
+# This script is meant to be run by a github workflow
+# The output of this script is to either set github variables or not
+
+set -e
+
+COMMIT=$TAG
+echo "Tag: $TAG"
+
+# assert it matches proper format vX.X.X-rc.X
+RC_REGEX="^v(([0-9]+\.[0-9]+\.[0-9]+)-rc\.[0-9])+$"
+
+if [[ $TAG =~ $RC_REGEX ]]; then
+  VERSION=${BASH_REMATCH[1]}
+  RC_BRANCH="rc/v${BASH_REMATCH[2]}"
+else
+  exit
+fi
+
+# assert it exists in branch
+HEAD_COMMIT=$(git rev-parse refs/heads/$RC_BRANCH)
+git merge-base --is-ancestor $COMMIT $HEAD_COMMIT
+
+# success
+echo "::set-output name=is_rc::true"
+echo "::set-output name=version::$VERSION"
