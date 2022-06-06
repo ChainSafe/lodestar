518c89a52fa0e0344366a0be529dd214b9f55869
Dadepo Aderemi <dadepo@gmail.com>
2022-06-06 18:47:44 +0200

    recognize the file format imported by keymanager

--- packages/cli/test/unit/validator/keys.test.ts
+++ packages/cli/test/unit/validator/keys.test.ts
@@ -14,14 +14,16 @@ describe("validator / keys / resolveKeystorePaths", () => {
     const depositData = "deposit_data-1642090404.json";
     const passwordTxt = "password.txt";
     const withoutExtensions = "withoutExtensions";
+    const importedKeystore =
+      "key_imported_0x97b1b00d3c1888b5715c2c88bf1df7b0ad715388079de211bdc153697b69b868c671af3b2d86c5cdfbade48d03888ab4.json";
 
     sinon.stub(fs, "readdirSync").callsFake(() => {
-      return ([keystoreFilename, depositData, passwordTxt, withoutExtensions] as unknown) as Dirent[];
+      return ([keystoreFilename, depositData, passwordTxt, withoutExtensions, importedKeystore] as unknown) as Dirent[];
     });
 
     const result = resolveKeystorePaths("dir");
-    expect(result.length).to.equal(1);
-    expect(result[0]).to.equal(`dir/${keystoreFilename}`);
+    expect(result.length).to.equal(2);
+    expect(result).to.deep.equal([`dir/${keystoreFilename}`, `dir/${importedKeystore}`]);
   });
 
   it("should read key voting files with accepted file names", function () {
