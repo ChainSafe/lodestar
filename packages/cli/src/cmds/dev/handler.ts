22ce5658ff10f7af3e7e5a66323f57cee4a169e9
dapplion <35266934+dapplion@users.noreply.github.com>
2022-06-07 21:05:45 +0200

    Remove unused variable

--- packages/cli/src/cmds/dev/handler.ts
+++ packages/cli/src/cmds/dev/handler.ts
@@ -68,7 +68,7 @@ export async function devHandler(args: IDevArgs & IGlobalArgs): Promise<void> {
   logger.info("Lodestar", {network: args.network, ...getVersionData()});
   if (ACTIVE_PRESET === PresetName.minimal) logger.info("ACTIVE_PRESET == minimal preset");
 
-  const db = new BeaconDb({config, controller: new LevelDbController(options.db, {logger, metrics})});
+  const db = new BeaconDb({config, controller: new LevelDbController(options.db, {logger})});
   await db.start();
 
   let anchorState;
