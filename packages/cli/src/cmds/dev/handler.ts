4bbe4a37be6ec1c8fab5660c88688d463b738f14
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2022-06-09 12:38:34 +0200

    Add new CLI tool: flare (#4101)
    * Add bcli package
    
    * Use consistent format for dev index range
    
    * Dedup code with deriveSecretKeys
    
    * Fix argument parsing
    
    * cap batchSize by MAX_VALIDATORS_PER_COMMITTEE
    
    * Fix check-build script
    
    * Add unit test
    
    * Rename to flare

--- packages/cli/src/cmds/dev/handler.ts
+++ packages/cli/src/cmds/dev/handler.ts
@@ -7,7 +7,6 @@ import {GENESIS_SLOT} from "@chainsafe/lodestar-params";
 import {BeaconNode, BeaconDb, initStateFromAnchorState, createNodeJsLibp2p, nodeUtils} from "@chainsafe/lodestar";
 import {SlashingProtection, Validator, SignerType} from "@chainsafe/lodestar-validator";
 import {LevelDbController} from "@chainsafe/lodestar-db";
-import type {SecretKey} from "@chainsafe/bls/types";
 import {interopSecretKey} from "@chainsafe/lodestar-beacon-state-transition";
 import {createIBeaconConfig} from "@chainsafe/lodestar-config";
 import {ACTIVE_PRESET, PresetName} from "@chainsafe/lodestar-params";
@@ -15,7 +14,7 @@ import {onGracefulShutdown} from "../../util/process.js";
 import {createEnr, createPeerId, overwriteEnrWithCliArgs} from "../../config/index.js";
 import {IGlobalArgs, parseEnrArgs} from "../../options/index.js";
 import {initializeOptionsAndConfig} from "../init/handler.js";
-import {mkdir, getCliLogger} from "../../util/index.js";
+import {mkdir, getCliLogger, parseRange} from "../../util/index.js";
 import {getBeaconPaths} from "../beacon/paths.js";
 import {getValidatorPaths} from "../validator/paths.js";
 import {getVersionData} from "../../util/version.js";
@@ -112,22 +111,8 @@ export async function devHandler(args: IDevArgs & IGlobalArgs): Promise<void> {
   }, logger.info.bind(logger));
 
   if (args.startValidators) {
-    const secretKeys: SecretKey[] = [];
-    const [fromIndex, toIndex] = args.startValidators.split(":").map((s) => parseInt(s));
-    const valCount = anchorState.validators.length;
-    const maxIndex = fromIndex + valCount - 1;
-
-    if (fromIndex > toIndex) {
-      throw Error(`Invalid startValidators arg '${args.startValidators}' - fromIndex > toIndex`);
-    }
-
-    if (toIndex > maxIndex) {
-      throw Error(`Invalid startValidators arg '${args.startValidators}' - state has ${valCount} validators`);
-    }
-
-    for (let i = fromIndex; i <= toIndex; i++) {
-      secretKeys.push(interopSecretKey(i));
-    }
+    const indexes = parseRange(args.startValidators);
+    const secretKeys = indexes.map((i) => interopSecretKey(i));
 
     const dbPath = path.join(validatorsDbDir, "validators");
     fs.mkdirSync(dbPath, {recursive: true});
