aa6ec0c9aa55a6d7e038149c2b9600e9fe57df91
harkamal <gajinder@g11.in>
2022-06-15 18:08:53 +0530

    small comment

--- packages/api/src/utils/client/client.ts
+++ packages/api/src/utils/client/client.ts
@@ -76,6 +76,9 @@ export function generateGenericJsonClient<
         // eslint-disable-next-line @typescript-eslint/no-unsafe-return
         return returnType.fromJson(res) as ReturnType<Api[keyof Api]>;
       } else {
+        // We need to avoid parsing the response as the servers might just
+        // response status 200 and close the request instead of writing an
+        // empty json response
         await fetchFn.request(fetchOptsSerializer(...args));
       }
     };
