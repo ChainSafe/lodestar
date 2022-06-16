16e867746cbd3571ebdfbf393181c8f21953b24d
Dadepo Aderemi <dadepo@gmail.com>
2022-06-16 12:50:11 +0200

    switching to use the test case pattern

--- packages/lodestar/test/spec/beacon-api/beacon_spec.ts
+++ packages/lodestar/test/spec/beacon-api/beacon_spec.ts
@@ -5,30 +5,20 @@ import fs from "node:fs";
 import chai from "chai";
 
 const expect = chai.expect;
-// import chaiORV from "chai-openapi-response-validator";
-import {createIBeaconConfig, IChainConfig} from "@chainsafe/lodestar-config";
-import {chainConfig as chainConfigDef} from "@chainsafe/lodestar-config/default";
-import {getClient} from "@chainsafe/lodestar-api";
-import {toHexString} from "@chainsafe/ssz";
-import {ssz} from "@chainsafe/lodestar-types";
 import {HttpClient} from "@chainsafe/lodestar-api";
+import {IChainConfig} from "@chainsafe/lodestar-config";
 import {LogLevel, testLogger, TestLoggerOpts} from "../../utils/logger.js";
 import {getDevBeaconNode} from "../../utils/node/beacon.js";
 import {isValidResponse} from "./util/api_response_validator.js";
 
-/* eslint-disable @typescript-eslint/naming-convention */
-const SECONDS_PER_SLOT = 2;
-const ALTAIR_FORK_EPOCH = 0;
 const restPort = 9596;
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-const chainConfig: IChainConfig = {...chainConfigDef, SECONDS_PER_SLOT, ALTAIR_FORK_EPOCH};
-const genesisValidatorsRoot = Buffer.alloc(32, 0xaa);
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-call
-const config = createIBeaconConfig(chainConfig, genesisValidatorsRoot);
+
 const testLoggerOpts: TestLoggerOpts = {logLevel: LogLevel.info};
 const loggerNodeA = testLogger("Node-A", testLoggerOpts);
 
+// eslint-disable-next-line @typescript-eslint/naming-convention
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const SPEC_FILE: Record<string, unknown> = (JSON.parse(
   fs.readFileSync(path.join(__dirname, "beacon-node-oapi-v2-1-0.json"), {
     encoding: "utf8",
@@ -36,6 +26,7 @@ const SPEC_FILE: Record<string, unknown> = (JSON.parse(
 ) as unknown) as Record<string, unknown>;
 
 const testParams: Pick<IChainConfig, "SECONDS_PER_SLOT"> = {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
   SECONDS_PER_SLOT: 2,
 };
 
@@ -53,7 +44,6 @@ const bn = await getDevBeaconNode({
 });
 
 // TODOS
-// Consider the style of expressing test cases in json and loop to test
 // See how to avoid eslint-disable
 
 const testCases = [
