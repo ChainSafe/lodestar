1568865dc9901a3dc1b147942c4603b48d60e8c5
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2022-06-16 17:51:42 +0200

    Add Sepolia config (#4164)

---
+++ packages/cli/src/networks/sepolia.ts
@@ -0,0 +1,12 @@
+export {sepoliaChainConfig as chainConfig} from "@chainsafe/lodestar-config/networks";
+
+/* eslint-disable max-len */
+
+export const depositContractDeployBlock = 1273020;
+export const genesisFileUrl = "https://raw.githubusercontent.com/eth-clients/merge-testnets/main/sepolia/genesis.ssz";
+export const bootnodesFileUrl =
+  "https://raw.githubusercontent.com/eth-clients/merge-testnets/main/sepolia/bootstrap_nodes.txt";
+
+export const bootEnrs = [
+  "enr:-Iq4QMCTfIMXnow27baRUb35Q8iiFHSIDBJh6hQM5Axohhf4b6Kr_cOCu0htQ5WvVqKvFgY28893DHAg8gnBAXsAVqmGAX53x8JggmlkgnY0gmlwhLKAlv6Jc2VjcDI1NmsxoQK6S-Cii_KmfFdUJL2TANL3ksaKUnNXvTCv1tLwXs0QgIN1ZHCCIyk",
+];
