293992793a5e98c73bdeb3132def6d51e2fe1d67
Tuyen Nguyen <vutuyen2636@gmail.com>
2022-06-17 17:50:05 +0700

    chore: add comments to benchmark test

--- packages/lodestar/test/perf/network/gossip/encoding.test.ts
+++ packages/lodestar/test/perf/network/gossip/encoding.test.ts
@@ -15,6 +15,7 @@ describe("snappy comparison", function () {
     "0xe4000000fed53d00000000000b00000000000000c1c7bea37b1e74310094293a3a11263525f7a5d8d2d67621f5e0825e5a37f1a0aeee010000000000b1f0033ddced8fe5636e060df211561236125fb7bdeb67eacad82f57f2438a7cafee010000000000b712789b41cdd9d60e1901e2b6b62e9bd314f539a1c9370b92c9de3c5b5a8a5c8e0f7c0a7b4e5df53220e21a594014f6f1482fa3fddf4f4145a98d048c19ef42be34fce247a528d34c5881bc2b88a6520a5833dd5cd29577f2ab915bafe7acb17824db255776d71e49ad2fd65d46fc4b296cdb629a9511ed69ab2c336c0d550f01";
   const uncompressedAttData = Buffer.from(fromHexString(attestationHex));
 
+  // double check why running each of the number below got different result?
   for (const attTimes of [1, 100, 500]) {
     const uncompressedData = Buffer.concat(Array.from({length: attTimes}, () => uncompressedAttData));
     console.log("@@@ uncompressed data length", uncompressedData.length);
