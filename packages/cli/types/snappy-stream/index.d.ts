9b1bb4ce7e9c38c55aa093921c1c332131a7e44c
dapplion <35266934+dapplion@users.noreply.github.com>
2022-06-28 21:43:32 +0200

    Remove local types

--- packages/cli/types/snappy-stream/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@chainsafe/snappy-stream" {
-  import {Transform} from "node:stream";
-
-  export function createUncompressStream(opts?: {asBuffer?: boolean}): Transform;
-  export function createCompressStream(opts?:{asyncCompress?: boolean}): Transform;
-}
