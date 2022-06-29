1253a9e8102cf36cdb33c4f497eef21321ed70db
Cayman <caymannava@gmail.com>
2022-06-29 14:27:53 -0500

    Revert eslint changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 13,
+    ecmaVersion: 10,
     project: "./tsconfig.json",
     sourceType: "module",
   },
