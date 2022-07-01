12b43bfc15f8f374428c2885e62f12785251155b
Dadepo Aderemi <dadepo@gmail.com>
2022-07-01 11:03:14 +0200

    Expose util modules from the light-client package (#4228)

---
+++ packages/light-client/src/utils/index.ts
@@ -0,0 +1,7 @@
+export * from "./chunkify.js";
+export * from "./clock.js";
+export * from "./domain.js";
+export * from "./logger.js";
+export * from "./update.js";
+export * from "./utils.js";
+export * from "./verifyMerkleBranch.js";
