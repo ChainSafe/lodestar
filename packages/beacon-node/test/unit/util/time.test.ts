97bc0a1ff055106e7b6e2a6dbea11bc946375d89
dapplion <35266934+dapplion@users.noreply.github.com>
2022-08-01 14:16:29 +0200

    Update prettyTimeDiffSec test

--- packages/beacon-node/test/unit/util/time.test.ts
+++ packages/beacon-node/test/unit/util/time.test.ts
@@ -1,21 +1,21 @@
 import {expect} from "chai";
-import {prettyTimeDiff} from "../../../src/util/time.js";
+import {prettyTimeDiffSec} from "../../../src/util/time.js";
 
-describe("util / time / prettyTimeDiff", () => {
-  const testCases: {diffMs: number; res: string}[] = [
-    {diffMs: 1500, res: "1.5 seconds"},
-    {diffMs: 15000, res: "15 seconds"},
-    {diffMs: 100000, res: "1.7 minutes"},
-    {diffMs: 1000000, res: "17 minutes"},
-    {diffMs: 10000000, res: "2.8 hours"},
-    {diffMs: 50000000, res: "14 hours"},
-    {diffMs: 100000000, res: "1.2 days"},
-    {diffMs: 500000000, res: "5.8 days"},
+describe("util / time / prettyTimeDiffSec", () => {
+  const testCases: {diffSec: number; res: string}[] = [
+    {diffSec: 1.5, res: "1.5 seconds"},
+    {diffSec: 15, res: "15 seconds"},
+    {diffSec: 100, res: "1.7 minutes"},
+    {diffSec: 1000, res: "17 minutes"},
+    {diffSec: 10000, res: "2.8 hours"},
+    {diffSec: 50000, res: "14 hours"},
+    {diffSec: 100000, res: "1.2 days"},
+    {diffSec: 500000, res: "5.8 days"},
   ];
 
-  for (const {diffMs, res} of testCases) {
-    it(`pretty ${diffMs}`, () => {
-      expect(prettyTimeDiff(diffMs)).to.equal(res);
+  for (const {diffSec, res} of testCases) {
+    it(`pretty ${diffSec}`, () => {
+      expect(prettyTimeDiffSec(diffSec)).to.equal(res);
     });
   }
 });
