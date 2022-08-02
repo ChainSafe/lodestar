e0204cfee0e0d34c3651dcef319ff0acee1dadd3
dapplion <35266934+dapplion@users.noreply.github.com>
2022-08-02 14:18:04 +0200

    better render

--- packages/flare/src/printEth1Data.ts
+++ packages/flare/src/printEth1Data.ts
@@ -2,7 +2,6 @@ import {ssz} from "@lodestar/types";
 import {getClient} from "@lodestar/api";
 import {config} from "@lodestar/config/default";
 import {toHex} from "@lodestar/utils";
-import {EPOCHS_PER_ETH1_VOTING_PERIOD, SLOTS_PER_EPOCH} from "@lodestar/params";
 
 /* eslint-disable no-console */
 
@@ -14,13 +13,14 @@ const state = ssz.bellatrix.BeaconState.deserialize(stateBytes);
 const votesByJson = new Map<string, number>();
 
 // For gnosis 1024 = 64 * 16
-const minQuorum = EPOCHS_PER_ETH1_VOTING_PERIOD * SLOTS_PER_EPOCH;
+const minQuorum = (64 * 16) / 2;
+const slotsEllapsed = state.slot % (64 * 16);
 
 for (const [i, eth1DataVote] of state.eth1DataVotes.entries()) {
   const jsonStr = JSON.stringify(ssz.phase0.Eth1DataVotes.elementType.toJson(eth1DataVote));
   votesByJson.set(jsonStr, 1 + (votesByJson.get(jsonStr) ?? 0));
 
-  console.log(i, toHex(eth1DataVote.blockHash));
+  console.log(`${i} / ${slotsEllapsed}`, toHex(eth1DataVote.blockHash));
 }
 
 for (const [jsonStr, voteCount] of votesByJson.entries()) {
