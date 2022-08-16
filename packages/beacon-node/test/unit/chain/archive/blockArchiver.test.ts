48369e76e97ce05b12905faf91fec4c2b65b50ac
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2022-08-16 13:56:42 +0200

    Revert "Move archive logic to BeaconChain class (#4379)"
    This reverts commit ee5c0066dc9d7d78a61b27483a1f20bfc290c637.

--- packages/beacon-node/test/unit/chain/archive/blockArchiver.test.ts
+++ packages/beacon-node/test/unit/chain/archive/blockArchiver.test.ts
@@ -1,6 +1,7 @@
 import {expect} from "chai";
 import sinon, {SinonStubbedInstance} from "sinon";
 import {ssz} from "@lodestar/types";
+import {ForkChoice} from "@lodestar/fork-choice";
 import {fromHexString, toHexString} from "@chainsafe/ssz";
 import {ZERO_HASH_HEX} from "../../../../src/constants/index.js";
 import {generateProtoBlock, generateEmptySignedBlock} from "../../../utils/block.js";
@@ -13,10 +14,12 @@ describe("block archiver task", function () {
   const logger = testLogger();
 
   let dbStub: StubbedBeaconDb;
+  let forkChoiceStub: SinonStubbedInstance<ForkChoice>;
   let lightclientServer: SinonStubbedInstance<LightClientServer> & LightClientServer;
 
   beforeEach(function () {
     dbStub = new StubbedBeaconDb();
+    forkChoiceStub = sinon.createStubInstance(ForkChoice);
     lightclientServer = sinon.createStubInstance(LightClientServer) as SinonStubbedInstance<LightClientServer> &
       LightClientServer;
   });
@@ -28,18 +31,14 @@ describe("block archiver task", function () {
     const blocks = Array.from({length: 5}, (_, i) =>
       generateProtoBlock({slot: i + 1, blockRoot: toHexString(Buffer.alloc(32, i + 1))})
     );
-    const finalizedCanonicalBlocks = [blocks[4], blocks[3], blocks[1], blocks[0]];
-    const finalizedNonCanonicalBlocks = [blocks[2]];
-    await archiveBlocks(
-      dbStub,
-      lightclientServer,
-      logger,
-      {epoch: 5, rootHex: ZERO_HASH_HEX},
-      {finalizedCanonicalBlocks, finalizedNonCanonicalBlocks}
-    );
+    const canonicalBlocks = [blocks[4], blocks[3], blocks[1], blocks[0]];
+    const nonCanonicalBlocks = [blocks[2]];
+    forkChoiceStub.getAllAncestorBlocks.returns(canonicalBlocks);
+    forkChoiceStub.getAllNonAncestorBlocks.returns(nonCanonicalBlocks);
+    await archiveBlocks(dbStub, forkChoiceStub, lightclientServer, logger, {epoch: 5, rootHex: ZERO_HASH_HEX});
 
     expect(dbStub.blockArchive.batchPutBinary.getCall(0).args[0]).to.deep.equal(
-      finalizedCanonicalBlocks.map((summary) => ({
+      canonicalBlocks.map((summary) => ({
         key: summary.slot,
         value: blockBuffer,
         slot: summary.slot,
