929825bcb0c2da7e34465560764588e13fe883de
dapplion <35266934+dapplion@users.noreply.github.com>
2022-08-18 15:47:25 +0200

    Remove rmDir bad implementation

--- packages/beacon-node/src/util/file.ts
+++ packages/beacon-node/src/util/file.ts
@@ -1,5 +1,4 @@
 import fs from "node:fs";
-import path from "node:path";
 import {promisify} from "node:util";
 
 /** Ensure a directory exists */
@@ -23,23 +22,4 @@ export async function writeIfNotExist(filepath: string, bytes: Uint8Array): Prom
     await promisify(fs.writeFile)(filepath, bytes);
     return true;
   }
-}
-
-export function rmDir(dir: string): void {
-  const list = fs.readdirSync(dir);
-  for (let i = 0; i < list.length; i++) {
-    const filename = path.join(dir, list[i]);
-    const stat = fs.statSync(filename);
-
-    if (filename == "." || filename == "..") {
-      // pass these files
-    } else if (stat.isDirectory()) {
-      // rmdir recursively
-      rmDir(filename);
-    } else {
-      // rm fiilename
-      fs.unlinkSync(filename);
-    }
-  }
-  fs.rmdirSync(dir);
 }
