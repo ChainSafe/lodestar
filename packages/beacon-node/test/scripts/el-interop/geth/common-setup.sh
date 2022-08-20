2a6929bafc306c7b7a793ed3628b5cb68e9f027c
g11tech <gajinder@g11.in>
2022-08-20 13:02:32 +0530

    Re-enable full merge interop tests (#4458)

---
+++ packages/beacon-node/test/scripts/el-interop/geth/common-setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+
+echo $TTD
+echo $DATA_DIR
+echo $EL_BINARY_DIR
+echo $JWT_SECRET_HEX
+
+echo $scriptDir
+echo $currentDir
+
+
+env TTD=$TTD envsubst < $scriptDir/genesisPre.tmpl > $DATA_DIR/genesis.json
+echo "45a915e4d060149eb4365960e6a7a45f334393093061116b197e3240065ff2d8" > $DATA_DIR/sk.json
+echo "12345678" > $DATA_DIR/password.txt
+pubKey="0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b"
+
+# echo a hex encoded 256 bit secret into a file
+echo $JWT_SECRET_HEX> $DATA_DIR/jwtsecret
+
+$EL_BINARY_DIR/geth --datadir $DATA_DIR init $DATA_DIR/genesis.json
+$EL_BINARY_DIR/geth --datadir $DATA_DIR account import $DATA_DIR/sk.json --password $DATA_DIR/password.txt
