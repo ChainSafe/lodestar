e5b1021b0c2b327c9f28cb4d87c11db7e1d7eee3
Nazar Hussain <nazarhussain@gmail.com>
2022-09-05 18:46:16 +0200

    Update code as per feedback

--- packages/cli/src/cmds/beacon/handler.ts
+++ packages/cli/src/cmds/beacon/handler.ts
@@ -21,18 +21,12 @@ import {initBeaconState} from "./initBeaconState.js";
  * Runs a beacon node.
  */
 export async function beaconHandler(args: IBeaconArgs & IGlobalArgs): Promise<void> {
-  const {config, options: nodeOptions, beaconPaths, network, version, commit, peerId} = await beaconHandlerInit(args);
+  const {config, options, beaconPaths, network, version, commit, peerId} = await beaconHandlerInit(args);
 
   // initialize directories
   mkdir(beaconPaths.dataDir);
   mkdir(beaconPaths.beaconDir);
   mkdir(beaconPaths.dbDir);
-
-  const options =
-    args.dev &&
-    (process.env.NODE_ENV === "dev" || process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test")
-      ? ({...nodeOptions, executionEngine: {mode: "mock"}} as IBeaconNodeOptions)
-      : nodeOptions;
 
   const abortController = new AbortController();
   const logger = getCliLogger(args, {defaultLogFile: "beacon.log"}, config);
