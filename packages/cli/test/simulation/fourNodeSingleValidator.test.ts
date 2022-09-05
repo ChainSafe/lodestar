e6dbc049086168bb69dd464d86703b7f5e28bef0
Nazar Hussain <nazarhussain@gmail.com>
2022-09-05 16:32:01 +0200

    Add a simulation test for four nodes

--- packages/cli/test/simulation/fourNodeSingleValidator.test.ts
+++ packages/cli/test/simulation/fourNodeSingleValidator.test.ts
@@ -1,44 +1,31 @@
+import {missedBlocksAssertions, nodeAssertions, participationAssertions} from "../utils/simulation/assertions.js";
 import {SimulationEnvironment} from "../utils/simulation/index.js";
-import {
-  feeRecipientsAssertions,
-  finalityAssertions,
-  forkAssertions,
-  missedBlocksAssertions,
-  nodeAssertions,
-  operationsAssertions,
-  participationAssertions,
-  peersAssertions,
-  slashingAssertions,
-} from "../utils/simulation/assertions.js";
 
-describe("Run single node, single validator, 128 interop validators (no eth1)", function () {
+describe("Run four nodes, single validator per node, 32 interop validators (no eth1)", function () {
   this.timeout("5m");
 
   const env = new SimulationEnvironment({
     beaconNodes: 4,
     validatorClients: 1,
-    validatorsPerClient: 128,
-    altairEpoch: Infinity,
-    bellatrixEpoch: Infinity,
+    validatorsPerClient: 32,
+    // We need to use the attesting participation so have to switch to altair
+    altairEpoch: 1,
+    // Use a larger value instead of Infinity
+    // https://github.com/ChainSafe/lodestar/issues/4505
+    bellatrixEpoch: 10 ** 12,
   });
-
-  env.connectNodesToFirstNode();
 
   before(async function () {
     await env.start();
+    await env.connectAllNodes();
+    await env.clock.waitForEndOfEpoch(2);
   });
 
   after(async () => {
     await env.stop();
   });
 
-  feeRecipientsAssertions(env);
-  finalityAssertions(env);
-  forkAssertions(env);
   nodeAssertions(env);
-  operationsAssertions(env);
-  peersAssertions(env);
-  slashingAssertions(env);
-  participationAssertions(env);
   missedBlocksAssertions(env);
+  participationAssertions(env);
 });
