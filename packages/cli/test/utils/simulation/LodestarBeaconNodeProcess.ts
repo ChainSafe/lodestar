ced70819f05fb7618a3cad7a123b70f053abe524
Nazar Hussain <nazarhussain@gmail.com>
2022-09-08 21:18:17 +0200

    Fix the issues after rebase

--- packages/cli/test/utils/simulation/LodestarBeaconNodeProcess.ts
+++ packages/cli/test/utils/simulation/LodestarBeaconNodeProcess.ts
@@ -65,7 +65,7 @@ export const LodestarBeaconNodeProcess: BeaconNodeConstructor = class LodestarBe
       "params.GENESIS_DELAY": String(this.params.genesisSlotsDelay),
       "params.ALTAIR_FORK_EPOCH": String(this.params.altairEpoch),
       "params.BELLATRIX_FORK_EPOCH": String(this.params.bellatrixEpoch),
-      logFormatId: this.id,
+      logPrefix: this.id,
       logFormatGenesisTime: `${this.params.genesisTime}`,
     } as unknown) as IBeaconArgs & IGlobalArgs;
 
