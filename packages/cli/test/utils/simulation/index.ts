3f3ab2ca6d438ac33c8d5dfba542848578443bb4
Nazar Hussain <nazarhussain@gmail.com>
2022-09-08 23:46:24 +0200

    Update the export

--- packages/cli/test/utils/simulation/index.ts
+++ packages/cli/test/utils/simulation/index.ts
@@ -1,23 +1,5 @@
-import {EventEmitter} from "events";
-import {TimeoutError} from "@lodestar/utils";
-
 export * from "./LodestarBeaconNodeProcess.js";
+export * from "./LodestarValidatorProcess.js";
 export * from "./SimulationEnvironment.js";
 export * from "./utils.js";
-
-export function waitForEvent<T>(
-  emitter: EventEmitter,
-  event: string,
-  timeout = 3000,
-  condition: (e: T) => boolean = () => true
-): Promise<T> {
-  return new Promise((resolve, reject) => {
-    const timer = setTimeout(() => reject(new TimeoutError(`event ${event} not received`)), timeout);
-    emitter.on(event, (e) => {
-      if (condition(e)) {
-        clearTimeout(timer);
-        resolve(e);
-      }
-    });
-  });
-}
+export * from "./types.js";
