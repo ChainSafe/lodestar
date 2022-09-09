65b38eeb2c523a32c396c6d636ea07e855241b92
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2022-09-09 15:52:00 +0200

    FIx archive finalized states db entry key typo (#4508)
    * FIx archive finalized states db entry key typo
    
    * Update computeStateSlotsToDelete test
    
    * Expose compactRange
    
    * Call compactRange at end of gcFinalizedStates
    
    * Revert gcFinalizedStates

--- packages/beacon-node/test/unit/chain/archive/stateArchiver.test.ts
+++ packages/beacon-node/test/unit/chain/archive/stateArchiver.test.ts
@@ -1,42 +1,46 @@
 import {expect} from "chai";
-import {computeEpochsToDelete} from "../../../../src/chain/archiver/archiveStates.js";
+import {computeStartSlotAtEpoch} from "@lodestar/state-transition";
+import {computeStateSlotsToDelete} from "../../../../src/chain/archiver/archiveStates.js";
 
 describe("state archiver task", () => {
-  describe("computeEpochsToDelete", () => {
+  describe("computeStateSlotsToDelete", () => {
     const testCases: {
       id: string;
       storedEpochs: number[];
-      persistEveryEpochs?: number;
-      toDelete: number[];
+      persistEveryEpochs: number;
+      epochsToDelete: number[];
     }[] = [
       {
         id: "Empty",
         storedEpochs: [],
-        toDelete: [],
+        persistEveryEpochs: 8,
+        epochsToDelete: [],
       },
       {
         id: "Equally spaced, delete x%8 != 0",
         storedEpochs: [0, 2, 4, 6, 8, 10, 12, 14, 16],
         persistEveryEpochs: 8,
-        toDelete: [2, 4, 6, 10, 12, 14],
+        epochsToDelete: [2, 4, 6, 10, 12, 14],
       },
       {
         id: "Equally spaced with offset",
         storedEpochs: [0, 3, 5, 7, 9, 11, 13, 15, 17],
         persistEveryEpochs: 8,
-        toDelete: [3, 5, 7, 11, 13, 15],
+        epochsToDelete: [3, 5, 7, 11, 13, 15],
       },
       {
         id: "Edge case with offset that causes a very large gap between epochs",
         storedEpochs: [7, 8, 23, 24],
         persistEveryEpochs: 8,
-        toDelete: [],
+        epochsToDelete: [],
       },
     ];
 
-    for (const {id, storedEpochs, persistEveryEpochs, toDelete} of testCases) {
+    for (const {id, storedEpochs, persistEveryEpochs, epochsToDelete} of testCases) {
       it(id, () => {
-        expect(computeEpochsToDelete(storedEpochs, persistEveryEpochs ?? 1024)).to.deep.equal(toDelete);
+        const storedStateSlots = storedEpochs.map((epoch) => computeStartSlotAtEpoch(epoch));
+        const stateSlotsToDelete = epochsToDelete.map((epoch) => computeStartSlotAtEpoch(epoch));
+        expect(computeStateSlotsToDelete(storedStateSlots, persistEveryEpochs)).to.deep.equal(stateSlotsToDelete);
       });
     }
   });
