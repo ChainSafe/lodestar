10bfe061cb418b4edcf012ee83721396696aed97
harkamal <gajinder@g11.in>
2022-09-11 01:58:53 +0530

    Fix getBlocksByNumber batch concurrency

--- packages/beacon-node/src/eth1/provider/eth1Provider.ts
+++ packages/beacon-node/src/eth1/provider/eth1Provider.ts
@@ -13,6 +13,11 @@ import {EthJsonRpcBlockRaw} from "../interface.js";
 import {JsonRpcHttpClient, JsonRpcHttpClientMetrics, ReqOpts} from "./jsonRpcHttpClient.js";
 import {isJsonRpcTruncatedError, quantityToNum, numToQuantity, dataToBytes} from "./utils.js";
 
+/**
+ * Number of parallel eth1 calls should be allowed to be made at any given time Range
+ * fetch requests can result into 100s of calls initiated to EL
+ */
+const ETH1_CONCURRENCY = 10;
 /* eslint-disable @typescript-eslint/naming-convention */
 
 /**
@@ -112,20 +117,23 @@ export class Eth1Provider implements IEth1Provider {
   async getBlocksByNumber(fromBlock: number, toBlock: number): Promise<EthJsonRpcBlockRaw[]> {
     const method = "eth_getBlockByNumber";
     const blocksArr = await retry(
-      (attempt) => {
+      async (attempt) => {
         // Large batch requests can return with code 200 but truncated, with broken JSON
         // This retry will split a given block range into smaller ranges exponentially
         // The underlying http client should handle network errors and retry
-        const chunkCount = 2 ** (attempt - 1);
+        const chunkCount = Math.max(2 ** (attempt - 1), Math.floor((toBlock - fromBlock) / ETH1_CONCURRENCY));
         const blockRanges = chunkifyInclusiveRange(fromBlock, toBlock, chunkCount);
-        return Promise.all(
-          blockRanges.map(([from, to]) =>
-            this.rpc.fetchBatch<IEthJsonRpcReturnTypes[typeof method]>(
-              linspace(from, to).map((blockNumber) => ({method, params: [numToQuantity(blockNumber), false]})),
-              getBlocksByNumberOpts
-            )
-          )
-        );
+        const blockChunks = [];
+        // Throwing all these in Promise.All will result into massive outgoing call of the same cardinality
+        // as the original range being fetched here.
+        for (const [from, to] of blockRanges) {
+          const chunk = await this.rpc.fetchBatch<IEthJsonRpcReturnTypes[typeof method]>(
+            linspace(from, to).map((blockNumber) => ({method, params: [numToQuantity(blockNumber), false]})),
+            getBlocksByNumberOpts
+          );
+          blockChunks.push(chunk);
+        }
+        return blockChunks;
       },
       {
         retries: 3,
