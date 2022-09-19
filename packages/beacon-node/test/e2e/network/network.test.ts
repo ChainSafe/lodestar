3a8ff2359443c7fb5ad88164090d103f933c7308
Dadepo Aderemi <dadepo@gmail.com>
2022-09-19 10:45:03 +0200

    Logging topics for debugging in CI

--- packages/beacon-node/test/e2e/network/network.test.ts
+++ packages/beacon-node/test/e2e/network/network.test.ts
@@ -247,6 +247,8 @@ describe("network", function () {
 
     expect(netA.gossip.getTopics().length).to.equal(0);
     netA.subscribeGossipCoreTopics();
+    // eslint-disable-next-line no-console
+    console.log("xyz", netA.gossip.getTopics());
     expect(netA.gossip.getTopics().length).to.equal(5);
     netA.unsubscribeGossipCoreTopics();
     expect(netA.gossip.getTopics().length).to.equal(0);
