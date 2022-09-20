1ff3858ce0c01463bad492142d0b167cdcf62dd7
Nazar Hussain <nazarhussain@gmail.com>
2022-09-20 15:54:27 +0200

    Remove a debug logging

--- packages/cli/test/utils/simulation/SimulationTracker.ts
+++ packages/cli/test/utils/simulation/SimulationTracker.ts
@@ -109,8 +109,6 @@ export class SimulationTracker {
     const lastSeenSlot = this.lastSeenSlot.get(node.id);
     const blockAttestations = await node.api.beacon.getBlockAttestations(slot);
 
-    console.log("%%%%% onBlock", node.id);
-
     if (lastSeenSlot !== undefined && slot > lastSeenSlot) {
       this.lastSeenSlot.set(node.id, slot);
     }
