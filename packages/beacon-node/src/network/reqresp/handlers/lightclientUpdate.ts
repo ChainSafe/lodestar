3fd9b22961cc88fd1bbd0d6bb04b4f5f52bdf19d
Dadepo Aderemi <dadepo@gmail.com>
2022-09-21 14:52:40 +0200

    wrapped getting onLightclientUpdate via res/req in a try catch

--- packages/beacon-node/src/network/reqresp/handlers/lightclientUpdate.ts
+++ packages/beacon-node/src/network/reqresp/handlers/lightclientUpdate.ts
@@ -1,9 +1,15 @@
 import {altair} from "@lodestar/types";
 import {IBeaconChain} from "../../../chain/index.js";
+import {ResponseError} from "../response/index.js";
+import {RespStatus} from "../../../constants/index.js";
 
 export async function* onLightclientUpdate(
   requestBody: altair.LightClientUpdateByRange,
   chain: IBeaconChain
 ): AsyncIterable<altair.LightClientUpdate[]> {
-  yield await chain.lightClientServer.getUpdates(requestBody.startPeriod, requestBody.count);
+  try {
+    yield await chain.lightClientServer.getUpdates(requestBody.startPeriod, requestBody.count);
+  } catch (e) {
+    throw new ResponseError(RespStatus.RESOURCE_UNAVAILABLE, (e as Error).message);
+  }
 }
