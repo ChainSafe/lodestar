8b759326a180db5b68affd5f0a0641a9ebe331fa
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2022-10-19 03:33:26 -0500

    Update lodestar agent detection (#4664)

--- packages/beacon-node/src/network/peers/client.ts
+++ packages/beacon-node/src/network/peers/client.ts
@@ -10,18 +10,11 @@ export enum ClientKind {
 export function clientFromAgentVersion(agentVersion: string): ClientKind {
   const slashIndex = agentVersion.indexOf("/");
   const agent = slashIndex >= 0 ? agentVersion.slice(0, slashIndex) : agentVersion;
-  switch (agent.toLowerCase()) {
-    case "lighthouse":
-      return ClientKind.Lighthouse;
-    case "teku":
-      return ClientKind.Teku;
-    case "prysm":
-      return ClientKind.Prysm;
-    case "nimbus":
-      return ClientKind.Nimbus;
-    case "js-libp2p":
-      return ClientKind.Lodestar;
-    default:
-      return ClientKind.Unknown;
-  }
+  const agentLC = agent.toLowerCase();
+  if (agentLC === "lighthouse") return ClientKind.Lighthouse;
+  if (agentLC === "teku") return ClientKind.Teku;
+  if (agentLC === "prysm") return ClientKind.Prysm;
+  if (agentLC === "nimbus") return ClientKind.Nimbus;
+  if (agentLC === "lodestar" || agentLC === "js-libp2p") return ClientKind.Lodestar;
+  return ClientKind.Unknown;
 }
