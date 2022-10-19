50fe56f4eaa4064fb98259868d32d1a7bd937dc0
Cayman <caymannava@gmail.com>
2022-10-19 15:31:06 -0500

    Remove light client latest cached slot

--- packages/beacon-node/src/chain/lightClient/index.ts
+++ packages/beacon-node/src/chain/lightClient/index.ts
@@ -159,9 +159,6 @@ export class LightClientServer {
   private readonly knownSyncCommittee = new MapDef<SyncPeriod, Set<DependantRootHex>>(() => new Set());
   private storedCurrentSyncCommittee = false;
 
-  latestForwardedFinalitySlot: Slot = 0;
-  latestForwardedOptimisticSlot: Slot = 0;
-
   /**
    * Keep in memory since this data is very transient, not useful after a few slots
    */
@@ -500,7 +497,6 @@ export class LightClientServer {
     // TODO: Once SyncAggregate are constructed from P2P too, count bits to decide "best"
     if (!this.latestHeadUpdate || attestedData.attestedHeader.slot > this.latestHeadUpdate.attestedHeader.slot) {
       this.latestHeadUpdate = headerUpdate;
-      this.latestForwardedOptimisticSlot = this.latestHeadUpdate.attestedHeader.slot;
       this.metrics?.lightclientServer.onSyncAggregate.inc({event: "update_latest_head_update"});
     }
 
@@ -520,7 +516,6 @@ export class LightClientServer {
           finalityBranch: attestedData.finalityBranch,
           signatureSlot,
         };
-        this.latestForwardedFinalitySlot = this.finalized.attestedHeader.slot;
         this.emitter.emit(ChainEvent.lightClientFinalityUpdate, this.finalized);
         this.metrics?.lightclientServer.onSyncAggregate.inc({event: "update_latest_finalized_update"});
       }
