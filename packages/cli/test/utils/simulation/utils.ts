f675174a5f5cfbde5b983b5615be48dc9cb6108a
Nazar Hussain <nazarhussain@gmail.com>
2022-10-24 22:54:01 +0200

    Update seconds per slot for the simulation tests

--- packages/cli/test/utils/simulation/utils.ts
+++ packages/cli/test/utils/simulation/utils.ts
@@ -15,7 +15,7 @@ export const logFilesDir = "test-logs";
 
 export const defaultSimulationParams: SimulationOptionalParams = {
   validatorsPerClient: 32,
-  secondsPerSlot: 2,
+  secondsPerSlot: 4,
   // delay a bit so regular sync sees it's up to date and sync is completed from the beginning
   // allow time for bls worker threads to warm up
   genesisSlotsDelay: 30,
