53f40962108615ba53e17c59a2ad688f70cd7c14
dapplion <35266934+dapplion@users.noreply.github.com>
2022-10-28 11:34:34 -0500

    Clarify FINALITY_UPDATE_RECEIVED_TOO_EARLY math

--- packages/beacon-node/src/chain/validation/lightClientFinalityUpdate.ts
+++ packages/beacon-node/src/chain/validation/lightClientFinalityUpdate.ts
@@ -25,9 +25,10 @@ export function validateLightClientFinalityUpdate(
   // [IGNORE] The finality_update is received after the block at signature_slot was given enough time to propagate
   // through the network -- i.e. validate that one-third of finality_update.signature_slot has transpired
   // (SECONDS_PER_SLOT / INTERVALS_PER_SLOT seconds after the start of the slot, with a MAXIMUM_GOSSIP_CLOCK_DISPARITY allowance)
-  const currentWallTime = Date.now() + MAXIMUM_GOSSIP_CLOCK_DISPARITY;
-  const timeAtSignatureSlot = computeTimeAtSlot(config, gossipedFinalityUpdate.signatureSlot, chain.genesisTime) * 1000;
-  if (currentWallTime < timeAtSignatureSlot + (1 / 3) * (config.SECONDS_PER_SLOT * 1000)) {
+  const signatureSlot13TimestampMs =
+    computeTimeAtSlot(config, gossipedFinalityUpdate.signatureSlot + 1 / 3, chain.genesisTime) * 1000;
+  const earliestAllowedTimestampMs = signatureSlot13TimestampMs + MAXIMUM_GOSSIP_CLOCK_DISPARITY;
+  if (Date.now() < earliestAllowedTimestampMs) {
     throw new LightClientError(GossipAction.IGNORE, {
       code: LightClientErrorCode.FINALITY_UPDATE_RECEIVED_TOO_EARLY,
     });
