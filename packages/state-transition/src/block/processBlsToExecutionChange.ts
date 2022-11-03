fbdb2f8eecd821361d8a625238fe130964915713
gajinder <develop@g11tech.io>
2022-11-03 16:34:12 +0530

    fix the credential change

--- packages/state-transition/src/block/processBlsToExecutionChange.ts
+++ packages/state-transition/src/block/processBlsToExecutionChange.ts
@@ -14,27 +14,30 @@ export function processBlsToExecutionChange(
     throw Error(`Invalid validatorIndex expected<${state.validators.length} actual=${addressChange.validatorIndex}`);
   }
   const validator = state.validators.get(addressChange.validatorIndex);
-  const credentialPrefix = validator.withdrawalCredentials.slice(0, WITHDRAWAL_PREFIX_BYTES);
+  // We need to work on the slice otherwise ssz view seems to be getting messed up
+  // once we modify this using set for putting new credentials
+  const withdrawalCredentials = validator.withdrawalCredentials.slice();
+  const credentialPrefix = withdrawalCredentials.slice(0, WITHDRAWAL_PREFIX_BYTES);
   if (!byteArrayEquals(credentialPrefix, BLS_WITHDRAWAL_PREFIX)) {
     throw Error(`Invalid withdrawalCredentials prefix expected=${BLS_WITHDRAWAL_PREFIX} actual=${credentialPrefix}`);
   }
   if (
     !byteArrayEquals(
-      validator.withdrawalCredentials.slice(WITHDRAWAL_PREFIX_BYTES, 32),
+      withdrawalCredentials.slice(WITHDRAWAL_PREFIX_BYTES, 32),
       digest(addressChange.fromBlsPubkey).slice(WITHDRAWAL_PREFIX_BYTES, 32)
     )
   ) {
     throw Error(
       `Invalid withdrawalCredentials expected=${toHexString(
-        validator.withdrawalCredentials.slice(WITHDRAWAL_PREFIX_BYTES, 32)
+        withdrawalCredentials.slice(WITHDRAWAL_PREFIX_BYTES, 32)
       )} actual=${toHexString(
         ssz.BLSPubkey.hashTreeRoot(addressChange.fromBlsPubkey).slice(WITHDRAWAL_PREFIX_BYTES, 32)
       )}`
     );
   }
-  // TODO: add verify signature sets
-  // TODO clean this up
-  validator.withdrawalCredentials.set(ETH1_ADDRESS_WITHDRAWAL_PREFIX);
-  validator.withdrawalCredentials.set([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], WITHDRAWAL_PREFIX_BYTES);
-  validator.withdrawalCredentials.set(addressChange.toExecutionAddress, 12);
+  withdrawalCredentials.set(ETH1_ADDRESS_WITHDRAWAL_PREFIX);
+  withdrawalCredentials.set([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], WITHDRAWAL_PREFIX_BYTES);
+  withdrawalCredentials.set(addressChange.toExecutionAddress, 12);
+  // Set the new credentials back
+  validator.withdrawalCredentials = withdrawalCredentials;
 }
