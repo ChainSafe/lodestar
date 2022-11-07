c78f7f972d3f309cdad2f99b27472bfb7281785c
gajinder <develop@g11tech.io>
2022-11-07 18:05:13 +0530

    lint

--- packages/beacon-node/src/node/utils/interop/state.ts
+++ packages/beacon-node/src/node/utils/interop/state.ts
@@ -1,4 +1,4 @@
-import {Bytes32, phase0, ssz, TimeSeconds, capella} from "@lodestar/types";
+import {Bytes32, phase0, ssz, TimeSeconds} from "@lodestar/types";
 import {IChainForkConfig} from "@lodestar/config";
 import {BeaconStateAllForks, initializeBeaconStateFromEth1} from "@lodestar/state-transition";
 import {createEmptyEpochContextImmutableData} from "@lodestar/state-transition";
