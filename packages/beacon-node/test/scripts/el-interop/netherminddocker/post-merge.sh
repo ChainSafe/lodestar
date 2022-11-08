1e0ecc95369f23456ebf80c0b28374147942ae21
gajinder <develop@g11tech.io>
2022-11-08 23:25:15 +0530

    add nethermind image run to the withdrawals ci run

--- packages/beacon-node/test/scripts/el-interop/netherminddocker/post-merge.sh
+++ packages/beacon-node/test/scripts/el-interop/netherminddocker/post-merge.sh
@@ -5,4 +5,12 @@ currentDir=$(pwd)
 
 . $scriptDir/common-setup.sh
 
-docker run --rm --network host --name custom-execution -v $currentDir/$DATA_DIR:/data $EL_BINARY_DIR --datadir /data/nethermind --config themerge_kiln_testvectors --Merge.TerminalTotalDifficulty $TTD --JsonRpc.JwtSecretFile /data/jwtsecret --JsonRpc.Enabled true --JsonRpc.Host 0.0.0.0 --JsonRpc.AdditionalRpcUrls "http://localhost:$ETH_PORT|http|net;eth;subscribe;engine;web3;client|no-auth,http://localhost:$ENGINE_PORT|http|eth;engine" --Sync.SnapSync false
+if [ "$TEMPLATE_FILE" == "genesisPostWithdraw.tmpl" ]
+then
+  configVector="withdrawals_test"
+else
+  configVector="themerge_kiln_testvectors"
+fi;
+
+
+docker run --rm --network host --name custom-execution -v $currentDir/$DATA_DIR:/data $EL_BINARY_DIR --datadir /data/nethermind --config $configVector --Merge.TerminalTotalDifficulty $TTD --JsonRpc.JwtSecretFile /data/jwtsecret --JsonRpc.Enabled true --JsonRpc.Host 0.0.0.0 --JsonRpc.AdditionalRpcUrls "http://localhost:$ETH_PORT|http|net;eth;subscribe;engine;web3;client|no-auth,http://localhost:$ENGINE_PORT|http|eth;engine" --Sync.SnapSync false
