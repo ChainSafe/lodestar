e7eba90c25f37ad822ad3a4c29585ef5fc5658f0
dapplion <35266934+dapplion@users.noreply.github.com>
2022-11-17 19:28:26 +0100

    Fix assertLinearChainSegment

--- packages/beacon-node/src/chain/blocks/utils/chainSegment.ts
+++ packages/beacon-node/src/chain/blocks/utils/chainSegment.ts
@@ -7,23 +7,22 @@ import {BlockImport} from "../types.js";
  * Assert this chain segment of blocks is linear with slot numbers and hashes
  */
 export function assertLinearChainSegment(config: IChainForkConfig, blocks: BlockImport[]): void {
-  for (const [i, {block}] of blocks.entries()) {
+  for (let i = 0; i < blocks.length - 1; i++) {
+    const block = blocks[i].block;
     const child = blocks[i + 1].block;
-    if (child !== undefined) {
-      // If this block has a child in this chain segment, ensure that its parent root matches
-      // the root of this block.
-      if (
-        !ssz.Root.equals(
-          config.getForkTypes(block.message.slot).BeaconBlock.hashTreeRoot(block.message),
-          child.message.parentRoot
-        )
-      ) {
-        throw new BlockError(block, {code: BlockErrorCode.NON_LINEAR_PARENT_ROOTS});
-      }
-      // Ensure that the slots are strictly increasing throughout the chain segment.
-      if (child.message.slot <= block.message.slot) {
-        throw new BlockError(block, {code: BlockErrorCode.NON_LINEAR_SLOTS});
-      }
+    // If this block has a child in this chain segment, ensure that its parent root matches
+    // the root of this block.
+    if (
+      !ssz.Root.equals(
+        config.getForkTypes(block.message.slot).BeaconBlock.hashTreeRoot(block.message),
+        child.message.parentRoot
+      )
+    ) {
+      throw new BlockError(block, {code: BlockErrorCode.NON_LINEAR_PARENT_ROOTS});
+    }
+    // Ensure that the slots are strictly increasing throughout the chain segment.
+    if (child.message.slot <= block.message.slot) {
+      throw new BlockError(block, {code: BlockErrorCode.NON_LINEAR_SLOTS});
     }
   }
 }
