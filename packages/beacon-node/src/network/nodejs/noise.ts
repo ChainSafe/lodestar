2c77f6a56156d4bbf66e78ef8205b8cef67ce523
Tuyen Nguyen <vutuyen2636@gmail.com>
2022-11-22 08:54:38 +0700

    Use @chainsafe/as-chacha20poly1305 0.1.0

--- packages/beacon-node/src/network/nodejs/noise.ts
+++ packages/beacon-node/src/network/nodejs/noise.ts
@@ -1,23 +1,22 @@
-import {newInstance, ChaCha20Poly1305} from "@chainsafe/as-chacha20poly1305-2";
 import type {ConnectionEncrypter} from "@libp2p/interface-connection-encrypter";
+import {newInstance, ChaCha20Poly1305} from "@chainsafe/as-chacha20poly1305";
 import {ICryptoInterface, noise, stablelib} from "@chainsafe/libp2p-noise";
 import {digest} from "@chainsafe/as-sha256";
 
-type bytes = Uint8Array;
-type bytes32 = Uint8Array;
+type Bytes = Uint8Array;
+type Bytes32 = Uint8Array;
 
 const ctx = newInstance();
 const asImpl = new ChaCha20Poly1305(ctx);
 
 // same to stablelib but we use as-chacha20poly1305 and as-sha256
-// TODO: find a way to reuse the following code
 const lodestarCrypto: ICryptoInterface = {
   ...stablelib,
   hashSHA256(data: Uint8Array): Uint8Array {
     return digest(data);
   },
 
-  chaCha20Poly1305Encrypt(plaintext: Uint8Array, nonce: Uint8Array, ad: Uint8Array, k: bytes32): bytes {
+  chaCha20Poly1305Encrypt(plaintext: Uint8Array, nonce: Uint8Array, ad: Uint8Array, k: Bytes32): Bytes {
     return asImpl.seal(k, nonce, plaintext, ad);
   },
 
@@ -25,9 +24,9 @@ const lodestarCrypto: ICryptoInterface = {
     ciphertext: Uint8Array,
     nonce: Uint8Array,
     ad: Uint8Array,
-    k: bytes32,
+    k: Bytes32,
     dst?: Uint8Array
-  ): bytes | null {
+  ): Bytes | null {
     return asImpl.open(k, nonce, ciphertext, ad, dst);
   },
 };
