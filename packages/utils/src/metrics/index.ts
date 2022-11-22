d482b2f5a0b6bbdfd30ea6c988f583858253f531
Dadepo Aderemi <dadepo@gmail.com>
2022-11-22 15:04:34 +0400

    track some files moved to util

---
+++ packages/utils/src/metrics/index.ts
@@ -0,0 +1,12 @@
+import {Gauge, Histogram} from "prom-client";
+
+export type IGauge<T extends string = string> = Pick<Gauge<T>, "inc" | "dec" | "set"> & {
+  addCollect: (collectFn: () => void) => void;
+};
+
+export type IHistogram<T extends string = string> = Pick<Histogram<T>, "observe" | "startTimer">;
+
+export type IAvgMinMax = {
+  addGetValuesFn(getValuesFn: () => number[]): void;
+  set(values: number[]): void;
+};
