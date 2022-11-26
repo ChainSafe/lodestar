405cfa406212138bf96ba95b4aa5d4dcfb46324d
dapplion <35266934+dapplion@users.noreply.github.com>
2022-11-26 15:20:05 +0700

    Remove unnecessary diff

--- packages/config/src/chainConfig/networks/mainnet.ts
+++ packages/config/src/chainConfig/networks/mainnet.ts
@@ -19,24 +19,4 @@ export const mainnetChainConfig: IChainConfig = {
   GENESIS_DELAY: 604800,
   // MUST NOT use `GENESIS_FORK_VERSION` here so for `minimal` networks the preset value of 0x00000001 take prevalence
   // GENESIS_FORK_VERSION: "0x00000000",
-
-  // Transition
-  // ---------------------------------------------------------------
-  // Estimated: Sept 15, 2022
-  TERMINAL_TOTAL_DIFFICULTY: BigInt("58750000000000000000000"),
-  TERMINAL_BLOCK_HASH: b("0x0000000000000000000000000000000000000000000000000000000000000000"),
-  TERMINAL_BLOCK_HASH_ACTIVATION_EPOCH: Infinity,
-
-  // Forking
-  // ---------------------------------------------------------------
-  // Altair
-  ALTAIR_FORK_VERSION: b("0x01000000"),
-  ALTAIR_FORK_EPOCH: 74240, // Oct 27, 2021, 10:56:23am UTC
-  // Bellatrix
-  BELLATRIX_FORK_VERSION: b("0x02000000"),
-  BELLATRIX_FORK_EPOCH: 144896, // Sept 6, 2022, 11:34:47am UTC
-
-  // Capella
-  CAPELLA_FORK_VERSION: b("0x03000000"),
-  CAPELLA_FORK_EPOCH: Infinity,
 };
