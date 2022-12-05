130e85e9bd5798ac3681e96e3ee3e27396d566ce
dapplion <35266934+dapplion@users.noreply.github.com>
2022-12-05 17:01:05 +0700

    Add c-kzg library and current trusted setup

--- packages/beacon-node/src/node/nodejs.ts
+++ packages/beacon-node/src/node/nodejs.ts
@@ -18,6 +18,7 @@ import {createMetrics, IMetrics, HttpMetricsServer} from "../metrics/index.js";
 import {getApi, BeaconRestApiServer} from "../api/index.js";
 import {initializeExecutionEngine, initializeExecutionBuilder} from "../execution/index.js";
 import {initializeEth1ForBlockProduction} from "../eth1/index.js";
+import {loadEthereumTrustedSetup} from "../util/kzg.js";
 import {createLibp2pMetrics} from "../metrics/metrics/libp2p.js";
 import {IBeaconNodeOptions} from "./options.js";
 import {runNodeNotifier} from "./notifier.js";
@@ -140,6 +141,11 @@ export class BeaconNode {
     // Since it is perfectly fine to have listeners > 10
     setMaxListeners(Infinity, controller.signal);
     const signal = controller.signal;
+
+    // TODO EIP-4844, where is the best place to do this?
+    if (config.EIP4844_FORK_EPOCH < Infinity) {
+      loadEthereumTrustedSetup();
+    }
 
     // start db if not already started
     await db.start();
