30246e5d8258b90f5ad8850c56f6e26af3cf400a
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2022-12-07 16:45:03 +0700

    EIP-4844: Implement beaconBlocksMaybeBlobsByRoot (#4869)
    * Implement beaconBlocksMaybeBlobsByRoot
    
    * Fix empty array typo

--- packages/beacon-node/src/network/reqresp/interface.ts
+++ packages/beacon-node/src/network/reqresp/interface.ts
@@ -14,6 +14,10 @@ export interface IReqRespBeaconNode {
   ): Promise<allForks.SignedBeaconBlock[]>;
   beaconBlocksByRoot(peerId: PeerId, request: phase0.BeaconBlocksByRootRequest): Promise<allForks.SignedBeaconBlock[]>;
   blobsSidecarsByRange(peerId: PeerId, request: eip4844.BlobsSidecarsByRangeRequest): Promise<eip4844.BlobsSidecar[]>;
+  beaconBlockAndBlobsSidecarByRoot(
+    peerId: PeerId,
+    request: eip4844.BeaconBlockAndBlobsSidecarByRootRequest
+  ): Promise<eip4844.SignedBeaconBlockAndBlobsSidecar[]>;
   pruneOnPeerDisconnect(peerId: PeerId): void;
   lightClientBootstrap(peerId: PeerId, request: Uint8Array): Promise<altair.LightClientBootstrap>;
   lightClientOptimisticUpdate(peerId: PeerId): Promise<altair.LightClientOptimisticUpdate>;
