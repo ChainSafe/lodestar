07e447388392bd77c0fdb33d0ac9a588531aa8bf
dapplion <35266934+dapplion@users.noreply.github.com>
2022-12-08 14:58:14 +0700

    Align new configs

--- packages/params/src/interface/capella.ts
+++ packages/params/src/interface/capella.ts
@@ -1,9 +1,5 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 export type CapellaPreset = {
-  // TODO CAPELLA: Remove these two when new spec tests are released
-  MAX_PARTIAL_WITHDRAWALS_PER_EPOCH: number;
-  WITHDRAWAL_QUEUE_LIMIT: number;
-
   MAX_BLS_TO_EXECUTION_CHANGES: number;
   MAX_WITHDRAWALS_PER_PAYLOAD: number;
 };
