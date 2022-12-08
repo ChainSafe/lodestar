4d8268e22ec77a730dd6a592fc0c4549de5901b6
Nazar Hussain <nazarhussain@gmail.com>
2022-12-08 16:58:31 +0100

    Fix syntax error

--- packages/cli/test/simulation/run.sh
+++ packages/cli/test/simulation/run.sh
@@ -2,7 +2,7 @@ export LODESTAR_PRESET=minimal
 for f in ./test/simulation/*.test.ts; 
 do 
   echo "Running $f";
-  if ! ts-node --esm ${f} then
+  if ! ts-node --esm ${f}; then
     exit 1
   fi
 done;
