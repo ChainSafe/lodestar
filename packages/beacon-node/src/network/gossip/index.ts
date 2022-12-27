41324a3d29418b14857ef2162ef4b728c811a881
gajinder <develop@g11tech.io>
2022-12-27 16:11:03 +0530

    Abstract and reuse getCoreTopicsAtFork for generating metrics labels

--- packages/beacon-node/src/network/gossip/index.ts
+++ packages/beacon-node/src/network/gossip/index.ts
@@ -1,3 +1,4 @@
 export {Eth2Gossipsub} from "./gossipsub.js";
 export {getGossipHandlers} from "./handlers/index.js";
+export {getCoreTopicsAtFork} from "./topic.js";
 export * from "./interface.js";
