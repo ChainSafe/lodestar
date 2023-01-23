adb54fd643504edc263497a2bd0fc2f1684c688e
harkamal <gajinder@g11.in>
2023-01-24 00:19:55 +0530

    fix lint

--- packages/beacon-node/test/utils/mocks/chain/chain.ts
+++ packages/beacon-node/test/utils/mocks/chain/chain.ts
@@ -1,7 +1,20 @@
 import sinon from "sinon";
 
 import {CompositeTypeAny, toHexString, TreeView} from "@chainsafe/ssz";
-import {phase0, allForks, UintNum64, Root, Slot, ssz, Uint16, UintBn64, RootHex, eip4844, Wei} from "@lodestar/types";
+import {
+  phase0,
+  allForks,
+  UintNum64,
+  Root,
+  Slot,
+  ssz,
+  Uint16,
+  UintBn64,
+  RootHex,
+  eip4844,
+  Wei,
+  capella,
+} from "@lodestar/types";
 import {IBeaconConfig} from "@lodestar/config";
 import {BeaconStateAllForks, CachedBeaconStateAllForks} from "@lodestar/state-transition";
 import {CheckpointWithHex, IForkChoice, ProtoBlock, ExecutionStatus, AncestorStatus} from "@lodestar/fork-choice";
