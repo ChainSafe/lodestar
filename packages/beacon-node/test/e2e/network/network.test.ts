1cba15653536a0047ba95a17e99e5b9bfbce920d
gajinder <develop@g11tech.io>
2023-02-02 18:38:22 +0530

    remove diff

--- packages/beacon-node/test/e2e/network/network.test.ts
+++ packages/beacon-node/test/e2e/network/network.test.ts
@@ -231,7 +231,7 @@ describe("network", function () {
 
     expect(netA.gossip.getTopics().length).to.equal(0);
     netA.subscribeGossipCoreTopics();
-    expect(netA.gossip.getTopics().length).to.greaterThan(0);
+    expect(netA.gossip.getTopics().length).to.equal(13);
     netA.unsubscribeGossipCoreTopics();
     expect(netA.gossip.getTopics().length).to.equal(0);
     netA.close();
