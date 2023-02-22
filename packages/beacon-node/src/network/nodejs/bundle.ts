03b7f617fd78e1f7793853c4456dcf274ddaf683
Tuyen Nguyen <vutuyen2636@gmail.com>
2023-02-22 17:56:07 +0700

    Revert "Add yamux as a stream muxer"
    This reverts commit 76bde8adae0118e36846a4e08ac58883e42f457a.

--- packages/beacon-node/src/network/nodejs/bundle.ts
+++ packages/beacon-node/src/network/nodejs/bundle.ts
@@ -1,4 +1,3 @@
-import {yamux} from "@chainsafe/libp2p-yamux";
 import {createLibp2p} from "libp2p";
 import {tcp} from "@libp2p/tcp";
 import {mplex} from "@libp2p/mplex";
@@ -59,7 +58,7 @@ export async function createNodejsLibp2p(options: Libp2pOptions): Promise<Libp2p
         },
       }),
     ],
-    streamMuxers: [yamux(), mplex({maxInboundStreams: 256})],
+    streamMuxers: [mplex({maxInboundStreams: 256})],
     peerDiscovery,
     metrics: options.metrics
       ? prometheusMetrics({
