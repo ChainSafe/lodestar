b3395a46f4d28577b05aa568c6830ee6bc4aec24
harkamal <gajinder@g11.in>
2023-03-03 18:26:39 +0530

    comment

--- packages/utils/test/unit/promiserace.test.ts
+++ packages/utils/test/unit/promiserace.test.ts
@@ -14,6 +14,10 @@ describe("assert", () => {
       setTimeout(() => reject(Error(value)), delay);
     });
 
+  // Second item in testcase row i.e. array of numbers represent delay at which promise to be resolved
+  // or rejected (-ve number)
+  // Third item in testcase row is the expected value or error message in string
+  // Last item is expected events
   const testcases: [string, number[], (string | Error)[], RaceEvent[]][] = [
     ["all resolve pre-cutoff", [100, 200], ["100", "200"], [RaceEvent.precutoff]],
     ["all resolve/reject pre-cutoff", [100, -200], ["100", "-200"], [RaceEvent.precutoff]],
