632d99a81bc7d7d67066d7dcffc1bfae2ea4c89e
Nazar Hussain <nazarhussain@gmail.com>
2023-03-07 00:38:34 +0100

    Add multiple web3 providers

--- packages/prover/src/utils/assertion.ts
+++ packages/prover/src/utils/assertion.ts
@@ -1,5 +1,5 @@
 import {Lightclient} from "@lodestar/light-client";
-import {SendAsyncProvider, SendProvider, Web3Provider} from "../interfaces.js";
+import {EIP1193Provider, RequestProvider, SendAsyncProvider, SendProvider, Web3Provider} from "../interfaces.js";
 
 export function assertLightClient(client?: Lightclient): asserts client is Lightclient {
   if (!client) {
@@ -11,6 +11,22 @@ export function isSendProvider(provider: Web3Provider): provider is SendProvider
   return "send" in provider && typeof provider.send === "function" && provider.send.length > 1;
 }
 
+export function isRequestProvider(provider: Web3Provider): provider is RequestProvider {
+  return "request" in provider && typeof provider.request === "function" && provider.request.length > 1;
+}
+
 export function isSendAsyncProvider(provider: Web3Provider): provider is SendAsyncProvider {
-  return "sendAsync" in provider && typeof provider.sendAsync === "function";
+  return (
+    "sendAsync" in provider &&
+    typeof provider.sendAsync === "function" &&
+    provider.sendAsync.constructor.name === "AsyncFunction"
+  );
+}
+
+export function isEIP1193Provider(provider: EIP1193Provider): provider is EIP1193Provider {
+  return (
+    "request" in provider &&
+    typeof provider.request === "function" &&
+    provider.request.constructor.name === "AsyncFunction"
+  );
 }
--- packages/prover/src/web3_provider.ts
+++ packages/prover/src/web3_provider.ts
@@ -1,8 +1,15 @@
 import {NetworkName} from "@lodestar/config/networks";
-import {LightNode, SendProvider, Web3Provider} from "./interfaces.js";
+import {
+  EIP1193Provider,
+  LightNode,
+  RequestProvider,
+  SendAsyncProvider,
+  SendProvider,
+  Web3Provider,
+} from "./interfaces.js";
 import {ProofProvider} from "./proof_provider/proof_provider.js";
 import {ELRequestPayload, ELResponse} from "./types.js";
-import {isSendProvider} from "./utils/assertion.js";
+import {isEIP1193Provider, isRequestProvider, isSendAsyncProvider, isSendProvider} from "./utils/assertion.js";
 import {processVerifiedELRequest} from "./utils/execution.js";
 
 type ProvableProviderInitOpts = {network?: NetworkName; checkpoint?: string} & (
@@ -33,6 +40,18 @@ export function makeProvableProvider<T extends Web3Provider>(
     return Object.assign(handleSendProvider(provider, rootProvider) as T, {rootProvider});
   }
 
+  if (isRequestProvider(provider)) {
+    return Object.assign(handleRequestProvider(provider, rootProvider) as T, {rootProvider});
+  }
+
+  if (isSendAsyncProvider(provider)) {
+    return Object.assign(handleSendAsyncProvider(provider, rootProvider) as T, {rootProvider});
+  }
+
+  if (isEIP1193Provider(provider)) {
+    return Object.assign(handleEIP1193Provider(provider, rootProvider) as T, {rootProvider});
+  }
+
   return Object.assign(provider, {rootProvider});
 }
 
@@ -56,4 +75,48 @@ function handleSendProvider(provider: SendProvider, rootProvider: ProofProvider)
   }
 
   return Object.assign(provider, {send: newSend});
+}
+
+function handleRequestProvider(provider: RequestProvider, rootProvider: ProofProvider): RequestProvider {
+  const request = provider.request.bind(provider);
+  const handler = (payload: ELRequestPayload): Promise<ELResponse | undefined> =>
+    new Promise((resolve, reject) => {
+      request(payload, (err, response) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(response);
+        }
+      });
+    });
+
+  function newRequest(payload: ELRequestPayload, callback: (err?: Error | null, response?: ELResponse) => void): void {
+    processVerifiedELRequest({payload, handler, rootProvider})
+      .then((response) => callback(undefined, response))
+      .catch((err) => callback(err, undefined));
+  }
+
+  return Object.assign(provider, {request: newRequest});
+}
+
+function handleSendAsyncProvider(provider: SendAsyncProvider, rootProvider: ProofProvider): SendAsyncProvider {
+  const sendAsync = provider.sendAsync.bind(provider);
+  const handler = (payload: ELRequestPayload): Promise<ELResponse | undefined> => sendAsync(payload);
+
+  async function newSendAsync(payload: ELRequestPayload): Promise<ELResponse | undefined> {
+    return processVerifiedELRequest({payload, handler, rootProvider});
+  }
+
+  return Object.assign(provider, {sendAsync: newSendAsync});
+}
+
+function handleEIP1193Provider(provider: EIP1193Provider, rootProvider: ProofProvider): EIP1193Provider {
+  const request = provider.request.bind(provider);
+  const handler = (payload: ELRequestPayload): Promise<ELResponse | undefined> => request(payload);
+
+  async function newRequest(payload: ELRequestPayload): Promise<ELResponse | undefined> {
+    return processVerifiedELRequest({payload, handler, rootProvider});
+  }
+
+  return Object.assign(provider, {request: newRequest});
 }
