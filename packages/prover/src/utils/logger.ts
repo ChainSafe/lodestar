74e1b1ce1f3be75cfb1d15eba8085096c191c393
Nazar Hussain <nazarhussain@gmail.com>
2023-03-13 13:25:22 +0100

    Fix lint errors

--- packages/prover/src/utils/logger.ts
+++ packages/prover/src/utils/logger.ts
@@ -35,8 +35,9 @@ class BrowserConsole extends Transport {
     const mappedMethod = this.methods[method as BrowserLogLevels];
 
     if (val <= this.levels[this.level as BrowserLogLevels]) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, no-console, @typescript-eslint/ban-ts-comment
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-expect-error
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, no-console
       console[mappedMethod](message);
     }
   }
