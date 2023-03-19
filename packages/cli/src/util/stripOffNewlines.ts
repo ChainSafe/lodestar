a91d50228f075c62e565b05e04bfe8e52fc4abc7
Nico Flaig <nflaig@protonmail.com>
2023-03-20 00:34:31 +0100

    Reuse `stripOffNewlines` to trim keystore password from file (#5283)
    * Fix stripOffNewlines to not remove pipe character(s) at the end
    
    * Add unit tests to verify stripOffNewlines works as expected
    
    * Reuse stripOffNewlines to trim keystore password from file

--- packages/cli/src/util/stripOffNewlines.ts
+++ packages/cli/src/util/stripOffNewlines.ts
@@ -2,5 +2,5 @@
  * Remove trailing new lines '\n' or '\r' if any
  */
 export function stripOffNewlines(s: string): string {
-  return s.replace(/[\n|\r]+$/g, "");
+  return s.replace(/[\n\r]+$/g, "");
 }
