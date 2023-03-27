610e79b6d49165d2a69cfde562536b0a2f660b76
tuyennhv <tuyen@chainsafe.io>
2023-03-27 17:52:47 +0700

    Revert "Pull gossip queues for better throughput (#5195)" (#5305)
    This reverts commit f03f911f9373c138103c14ce3a6c25691a48b77d.

--- packages/beacon-node/src/network/gossip/validation/queue.ts
+++ packages/beacon-node/src/network/gossip/validation/queue.ts
@@ -1,88 +1,32 @@
 import {mapValues} from "@lodestar/utils";
-import {LinkedList} from "../../util/array.js";
-import {GossipType} from "../gossip/interface.js";
-
-enum QueueType {
-  FIFO = "FIFO",
-  LIFO = "LIFO",
-}
+import {Metrics} from "../../../metrics/index.js";
+import {JobItemQueue, JobQueueOpts, QueueType} from "../../../util/queue/index.js";
+import {GossipJobQueues, GossipType, GossipValidatorFn, ResolvedType, ValidatorFnsByType} from "../interface.js";
 
 /**
  * Numbers from https://github.com/sigp/lighthouse/blob/b34a79dc0b02e04441ba01fd0f304d1e203d877d/beacon_node/network/src/beacon_processor/mod.rs#L69
  */
 const gossipQueueOpts: {
-  [K in GossipType]: GossipQueueOpts;
+  [K in GossipType]: Pick<JobQueueOpts, "maxLength" | "type" | "maxConcurrency" | "noYieldIfOneItem">;
 } = {
   // validation gossip block asap
-  [GossipType.beacon_block]: {maxLength: 1024, type: QueueType.FIFO},
+  [GossipType.beacon_block]: {maxLength: 1024, type: QueueType.FIFO, noYieldIfOneItem: true},
   // TODO DENEB: What's a good queue max given that now blocks are much bigger?
-  [GossipType.beacon_block_and_blobs_sidecar]: {maxLength: 32, type: QueueType.FIFO},
+  [GossipType.beacon_block_and_blobs_sidecar]: {maxLength: 32, type: QueueType.FIFO, noYieldIfOneItem: true},
   // lighthoue has aggregate_queue 4096 and unknown_block_aggregate_queue 1024, we use single queue
-  [GossipType.beacon_aggregate_and_proof]: {maxLength: 5120, type: QueueType.LIFO},
+  [GossipType.beacon_aggregate_and_proof]: {maxLength: 5120, type: QueueType.LIFO, maxConcurrency: 16},
   // lighthouse has attestation_queue 16384 and unknown_block_attestation_queue 8192, we use single queue
-  [GossipType.beacon_attestation]: {maxLength: 24576, type: QueueType.LIFO},
+  [GossipType.beacon_attestation]: {maxLength: 24576, type: QueueType.LIFO, maxConcurrency: 64},
   [GossipType.voluntary_exit]: {maxLength: 4096, type: QueueType.FIFO},
   [GossipType.proposer_slashing]: {maxLength: 4096, type: QueueType.FIFO},
   [GossipType.attester_slashing]: {maxLength: 4096, type: QueueType.FIFO},
-  [GossipType.sync_committee_contribution_and_proof]: {maxLength: 4096, type: QueueType.LIFO},
-  [GossipType.sync_committee]: {maxLength: 4096, type: QueueType.LIFO},
+  [GossipType.sync_committee_contribution_and_proof]: {maxLength: 4096, type: QueueType.LIFO, maxConcurrency: 16},
+  [GossipType.sync_committee]: {maxLength: 4096, type: QueueType.LIFO, maxConcurrency: 64},
   [GossipType.light_client_finality_update]: {maxLength: 1024, type: QueueType.FIFO},
   [GossipType.light_client_optimistic_update]: {maxLength: 1024, type: QueueType.FIFO},
   // lighthouse has bls changes queue set to their max 16384 to handle large spike at capella
   [GossipType.bls_to_execution_change]: {maxLength: 16384, type: QueueType.FIFO},
 };
-
-type GossipQueueOpts = {
-  type: QueueType;
-  maxLength: number;
-};
-
-export class GossipQueue<T> {
-  private readonly list = new LinkedList<T>();
-
-  constructor(private readonly opts: GossipQueueOpts) {}
-
-  get length(): number {
-    return this.list.length;
-  }
-
-  clear(): void {
-    this.list.clear();
-  }
-
-  add(item: T): T | null {
-    let droppedItem: T | null = null;
-
-    if (this.list.length + 1 > this.opts.maxLength) {
-      // LIFO -> keep latest job, drop oldest, FIFO -> drop latest job
-      switch (this.opts.type) {
-        case QueueType.LIFO:
-          droppedItem = this.list.shift();
-          break;
-        case QueueType.FIFO:
-          return item;
-      }
-    }
-
-    this.list.push(item);
-
-    return droppedItem;
-  }
-
-  next(): T | null {
-    // LIFO -> pop() remove last item, FIFO -> shift() remove first item
-    switch (this.opts.type) {
-      case QueueType.LIFO:
-        return this.list.pop();
-      case QueueType.FIFO:
-        return this.list.shift();
-    }
-  }
-
-  getAll(): T[] {
-    return this.list.toArray();
-  }
-}
 
 /**
  * Wraps a GossipValidatorFn with a queue, to limit the processing of gossip objects by type.
@@ -100,8 +44,25 @@ export class GossipQueue<T> {
  * By topic is too specific, so by type groups all similar objects in the same queue. All in the same won't allow
  * to customize different queue behaviours per object type (see `gossipQueueOpts`).
  */
-export function createGossipQueues<T>(): {[K in GossipType]: GossipQueue<T>} {
-  return mapValues(gossipQueueOpts, (opts) => {
-    return new GossipQueue<T>(opts);
+export function createValidationQueues(
+  gossipValidatorFns: ValidatorFnsByType,
+  signal: AbortSignal,
+  metrics: Metrics | null
+): GossipJobQueues {
+  return mapValues(gossipQueueOpts, (opts, type) => {
+    const gossipValidatorFn = gossipValidatorFns[type];
+    return new JobItemQueue<Parameters<GossipValidatorFn>, ResolvedType<GossipValidatorFn>>(
+      gossipValidatorFn,
+      {signal, ...opts},
+      metrics
+        ? {
+            length: metrics.gossipValidationQueueLength.child({topic: type}),
+            droppedJobs: metrics.gossipValidationQueueDroppedJobs.child({topic: type}),
+            jobTime: metrics.gossipValidationQueueJobTime.child({topic: type}),
+            jobWaitTime: metrics.gossipValidationQueueJobWaitTime.child({topic: type}),
+            concurrency: metrics.gossipValidationQueueConcurrency.child({topic: type}),
+          }
+        : undefined
+    );
   });
 }
