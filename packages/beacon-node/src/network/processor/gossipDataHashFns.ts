6fb5a5764752cf0fa0c35b467d462a41b1456ced
Tuyen Nguyen <vutuyen2636@gmail.com>
2023-04-02 08:19:32 +0700

    Fix getGossipDataHashFns()

--- packages/beacon-node/src/network/processor/gossipDataHashFns.ts
+++ packages/beacon-node/src/network/processor/gossipDataHashFns.ts
@@ -1,9 +1,12 @@
-import {getAttDataHashFromSignedAggregateAndProofSerialized} from "../../util/sszBytes.js";
+import {
+  getAttDataHashFromAttestationSerialized,
+  getAttDataHashFromSignedAggregateAndProofSerialized,
+} from "../../util/sszBytes.js";
 import {GossipDataHashFns, GossipType} from "../gossip/interface.js";
 
 export function getGossipDataHashFns(): GossipDataHashFns {
   return {
-    [GossipType.beacon_attestation]: getAttDataHashFromSignedAggregateAndProofSerialized,
+    [GossipType.beacon_attestation]: getAttDataHashFromAttestationSerialized,
     [GossipType.beacon_aggregate_and_proof]: getAttDataHashFromSignedAggregateAndProofSerialized,
   };
 }
