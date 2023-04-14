0096e304ac3333a291d28ff2f40b19deb948ff88
Cayman <caymannava@gmail.com>
2023-04-14 15:30:27 -0400

    Transfer secret ArrayBuffer back to the main thread

--- packages/cli/src/cmds/validator/keymanager/decryptKeystoreDefinitions/worker.ts
+++ packages/cli/src/cmds/validator/keymanager/decryptKeystoreDefinitions/worker.ts
@@ -1,5 +1,6 @@
 import fs from "node:fs";
 import {expose} from "@chainsafe/threads/worker";
+import {Transfer, TransferDescriptor} from "@chainsafe/threads";
 import {Keystore} from "@chainsafe/bls-keystore";
 import {LocalKeystoreDefinition} from "../interface.js";
 import {DecryptKeystoreWorkerAPI} from "./types.js";
@@ -12,11 +13,14 @@ import {DecryptKeystoreWorkerAPI} from "./types.js";
 export async function decryptKeystoreDefinition({
   keystorePath,
   password,
-}: LocalKeystoreDefinition): Promise<Uint8Array> {
+}: LocalKeystoreDefinition): Promise<TransferDescriptor<Uint8Array>> {
   const keystore = Keystore.parse(fs.readFileSync(keystorePath, "utf8"));
 
   // Memory-hogging function
-  return keystore.decrypt(password);
+  const secret = await keystore.decrypt(password);
+  // Transfer the underlying ArrayBuffer back to the main thread: https://threads.js.org/usage-advanced#transferable-objects
+  // This small performance gain may help in cases where this is run for many keystores
+  return Transfer(secret, [secret.buffer]);
 }
 
-expose({decryptKeystoreDefinition} as DecryptKeystoreWorkerAPI);
+expose({decryptKeystoreDefinition} as unknown as DecryptKeystoreWorkerAPI);
