632c996b0d150b8ed5ced3dbc601e6131d7b8902
Tuyen Nguyen <vutuyen2636@gmail.com>
2023-04-27 16:48:56 +0700

    fix: e2e network mdns timeout issue

--- packages/beacon-node/test/e2e/network/mdns.test.ts
+++ packages/beacon-node/test/e2e/network/mdns.test.ts
@@ -36,7 +36,6 @@ describe("mdns", function () {
 
   let controller: AbortController;
   beforeEach(() => (controller = new AbortController()));
-  afterEach(() => controller.abort());
 
   async function getOpts(peerId: PeerId): Promise<NetworkOptions> {
     const bindAddrUdp = `/ip4/0.0.0.0/udp/${port++}`;
@@ -50,12 +49,7 @@ describe("mdns", function () {
       bootMultiaddrs: [],
       localMultiaddrs: [],
       discv5FirstQueryDelayMs: 0,
-      discv5: {
-        enr,
-        bindAddr: bindAddrUdp,
-        bootEnrs: [],
-        enabled: true,
-      },
+      discv5: null,
     };
   }
 
