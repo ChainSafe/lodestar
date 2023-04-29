90ed27247254b4cd309fdce43c02da3866c613b2
Nico Flaig <nflaig@protonmail.com>
2023-04-29 14:14:27 +0200

    Filter by min-max key when loading pubkeys from db

--- packages/validator/src/slashingProtection/attestation/attestationByTargetRepository.ts
+++ packages/validator/src/slashingProtection/attestation/attestationByTargetRepository.ts
@@ -27,6 +27,9 @@ export class AttestationByTargetRepository {
   private readonly bucketId: string;
   private readonly dbReqOpts: DbReqOpts;
 
+  private readonly minKey: Uint8Array;
+  private readonly maxKey: Uint8Array;
+
   constructor(opts: DatabaseApiOptions) {
     this.db = opts.controller;
     this.type = new ContainerType({
@@ -36,6 +39,8 @@ export class AttestationByTargetRepository {
     }); // casing doesn't matter
     this.bucketId = getBucketNameByValue(this.bucket);
     this.dbReqOpts = {bucketId: this.bucketId};
+    this.minKey = encodeKey(this.bucket, Buffer.alloc(0));
+    this.maxKey = encodeKey(this.bucket + 1, Buffer.alloc(0));
   }
 
   async getAll(pubkey: BLSPubkey, limit?: number): Promise<SlashingProtectionAttestation[]> {
@@ -64,7 +69,7 @@ export class AttestationByTargetRepository {
   }
 
   async listPubkeys(): Promise<BLSPubkey[]> {
-    const keys = await this.db.keys(this.dbReqOpts);
+    const keys = await this.db.keys({...this.dbReqOpts, gte: this.minKey, lt: this.maxKey});
     return uniqueVectorArr(keys.map((key) => this.decodeKey(key).pubkey));
   }
 
