c38e951680c62857482daeac48fd00cc47b09a24
Nico Flaig <nflaig@protonmail.com>
2023-04-29 14:07:39 +0200

    feat(validator): add bucket labels to db metrics

--- packages/validator/src/repositories/metaDataRepository.ts
+++ packages/validator/src/repositories/metaDataRepository.ts
@@ -1,4 +1,4 @@
-import {Bucket, encodeKey, DatabaseApiOptions} from "@lodestar/db";
+import {Bucket, encodeKey, DatabaseApiOptions, DbReqOpts, getBucketNameByValue} from "@lodestar/db";
 import {Root, UintNum64} from "@lodestar/types";
 import {ssz} from "@lodestar/types";
 import {LodestarValidatorDatabaseController} from "../types.js";
@@ -13,25 +13,30 @@ export class MetaDataRepository {
   protected db: LodestarValidatorDatabaseController;
   protected bucket = Bucket.validator_metaData;
 
+  private readonly bucketId: string;
+  private readonly dbReqOpts: DbReqOpts;
+
   constructor(opts: DatabaseApiOptions) {
     this.db = opts.controller;
+    this.bucketId = getBucketNameByValue(this.bucket);
+    this.dbReqOpts = {bucketId: this.bucketId};
   }
 
   async getGenesisValidatorsRoot(): Promise<Root | null> {
-    return this.db.get(this.encodeKey(GENESIS_VALIDATORS_ROOT));
+    return this.db.get(this.encodeKey(GENESIS_VALIDATORS_ROOT), this.dbReqOpts);
   }
 
   async setGenesisValidatorsRoot(genesisValidatorsRoot: Root): Promise<void> {
-    await this.db.put(this.encodeKey(GENESIS_VALIDATORS_ROOT), Buffer.from(genesisValidatorsRoot));
+    await this.db.put(this.encodeKey(GENESIS_VALIDATORS_ROOT), Buffer.from(genesisValidatorsRoot), this.dbReqOpts);
   }
 
   async getGenesisTime(): Promise<UintNum64 | null> {
-    const bytes = await this.db.get(this.encodeKey(GENESIS_TIME));
+    const bytes = await this.db.get(this.encodeKey(GENESIS_TIME), this.dbReqOpts);
     return bytes ? ssz.UintNum64.deserialize(bytes) : null;
   }
 
   async setGenesisTime(genesisTime: UintNum64): Promise<void> {
-    await this.db.put(this.encodeKey(GENESIS_TIME), Buffer.from(ssz.UintNum64.serialize(genesisTime)));
+    await this.db.put(this.encodeKey(GENESIS_TIME), Buffer.from(ssz.UintNum64.serialize(genesisTime)), this.dbReqOpts);
   }
 
   private encodeKey(key: Uint8Array): Uint8Array {
