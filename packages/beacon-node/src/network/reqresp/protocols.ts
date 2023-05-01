56a6e8079955aa1711dd8e469137b8c7a5b52cad
dapplion <35266934+dapplion@users.noreply.github.com>
2023-05-01 12:02:31 +0900

    Use Version enum

--- packages/beacon-node/src/network/reqresp/protocols.ts
+++ packages/beacon-node/src/network/reqresp/protocols.ts
@@ -13,91 +13,91 @@ import {rateLimitQuotas} from "./rateLimit.js";
 
 export const Goodbye = toProtocol({
   method: ReqRespMethod.Goodbye,
-  version: 1,
+  version: Version.V1,
   contextBytesType: ContextBytesType.Empty,
 });
 
 export const Metadata = toProtocol({
   method: ReqRespMethod.Metadata,
-  version: 1,
+  version: Version.V1,
   contextBytesType: ContextBytesType.Empty,
 });
 
 export const MetadataV2 = toProtocol({
   method: ReqRespMethod.Metadata,
-  version: 2,
+  version: Version.V2,
   contextBytesType: ContextBytesType.Empty,
 });
 
 export const Ping = toProtocol({
   method: ReqRespMethod.Ping,
-  version: 1,
+  version: Version.V1,
   contextBytesType: ContextBytesType.Empty,
 });
 
 export const Status = toProtocol({
   method: ReqRespMethod.Status,
-  version: 1,
+  version: Version.V1,
   contextBytesType: ContextBytesType.Empty,
 });
 
 export const BeaconBlockAndBlobsSidecarByRoot = toProtocol({
   method: ReqRespMethod.BeaconBlockAndBlobsSidecarByRoot,
-  version: 1,
+  version: Version.V1,
   contextBytesType: ContextBytesType.ForkDigest,
 });
 
 export const BeaconBlocksByRange = toProtocol({
   method: ReqRespMethod.BeaconBlocksByRange,
-  version: 1,
+  version: Version.V1,
   contextBytesType: ContextBytesType.Empty,
 });
 
 export const BeaconBlocksByRangeV2 = toProtocol({
   method: ReqRespMethod.BeaconBlocksByRange,
-  version: 2,
+  version: Version.V2,
   contextBytesType: ContextBytesType.ForkDigest,
 });
 
 export const BeaconBlocksByRoot = toProtocol({
   method: ReqRespMethod.BeaconBlocksByRoot,
-  version: 1,
+  version: Version.V1,
   contextBytesType: ContextBytesType.Empty,
 });
 
 export const BeaconBlocksByRootV2 = toProtocol({
   method: ReqRespMethod.BeaconBlocksByRoot,
-  version: 2,
+  version: Version.V2,
   contextBytesType: ContextBytesType.ForkDigest,
 });
 
 export const BlobsSidecarsByRange = toProtocol({
   method: ReqRespMethod.BlobsSidecarsByRange,
-  version: 1,
+  version: Version.V1,
   contextBytesType: ContextBytesType.ForkDigest,
 });
 
 export const LightClientBootstrap = toProtocol({
   method: ReqRespMethod.LightClientBootstrap,
-  version: 1,
+  version: Version.V1,
   contextBytesType: ContextBytesType.ForkDigest,
 });
 
 export const LightClientFinalityUpdate = toProtocol({
   method: ReqRespMethod.LightClientFinalityUpdate,
-  version: 1,
+  version: Version.V1,
   contextBytesType: ContextBytesType.ForkDigest,
 });
 
 export const LightClientOptimisticUpdate = toProtocol({
   method: ReqRespMethod.LightClientOptimisticUpdate,
-  version: 1,
+  version: Version.V1,
   contextBytesType: ContextBytesType.ForkDigest,
 });
 
 export const LightClientUpdatesByRange = toProtocol({
   method: ReqRespMethod.LightClientUpdatesByRange,
-  version: 1,
+  version: Version.V1,
   contextBytesType: ContextBytesType.ForkDigest,
 });
 
