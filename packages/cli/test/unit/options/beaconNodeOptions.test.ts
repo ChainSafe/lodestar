dda86bed5008c0b4589255af9a5125535aef77bf
Cayman <caymannava@gmail.com>
2023-05-05 13:32:41 -0400

    chore: fix unit test

--- packages/cli/test/unit/options/beaconNodeOptions.test.ts
+++ packages/cli/test/unit/options/beaconNodeOptions.test.ts
@@ -160,6 +160,7 @@ describe("options / beaconNodeOptions", () => {
       },
       network: {
         discv5: {
+          config: {},
           bindAddr: "/ip4/127.0.0.1/udp/9002",
           bootEnrs: ["enr:-somedata"],
         },
