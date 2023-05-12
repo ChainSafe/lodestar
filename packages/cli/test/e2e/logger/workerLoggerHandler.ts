bead54d081b8b32996e6eb041f25e7e7075dfd3d
dapplion <35266934+dapplion@users.noreply.github.com>
2023-05-12 22:47:27 +0900

    Add tests for logging in workers

---
+++ packages/cli/test/e2e/logger/workerLoggerHandler.ts
@@ -0,0 +1,32 @@
+import worker_threads from "node:worker_threads";
+import {spawn, Worker} from "@chainsafe/threads";
+
+export type LoggerWorker = {
+  log(data: string): void;
+  close(): Promise<void>;
+};
+
+type WorkerData = {logFilepath: string};
+
+export async function getLoggerWorker(opts: WorkerData): Promise<LoggerWorker> {
+  const workerThreadjs = new Worker("./workerLogger.js", {workerData: opts});
+  const worker = workerThreadjs as unknown as worker_threads.Worker;
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  await spawn<any>(workerThreadjs, {
+    // A Lodestar Node may do very expensive task at start blocking the event loop and causing
+    // the initialization to timeout. The number below is big enough to almost disable the timeout
+    timeout: 5 * 60 * 1000,
+    // TODO: types are broken on spawn, which claims that `NetworkWorkerApi` does not satifies its contrains
+  });
+
+  return {
+    log(data) {
+      worker.postMessage(data);
+    },
+
+    async close() {
+      await workerThreadjs.terminate();
+    },
+  };
+}
