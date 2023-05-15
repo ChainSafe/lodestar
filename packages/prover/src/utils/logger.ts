f5be1ddc2ddc506a30471391daea3e52bb43edc1
Cayman <caymannava@gmail.com>
2023-05-15 16:43:00 -0400

    Fix browser tests

--- packages/prover/src/utils/logger.ts
+++ packages/prover/src/utils/logger.ts
@@ -1,5 +1,4 @@
 import {Logger} from "@lodestar/utils";
-import {getNodeLogger} from "@lodestar/logger/node";
 import {getBrowserLogger} from "@lodestar/logger/browser";
 import {getEmptyLogger} from "@lodestar/logger/empty";
 import {LogOptions} from "../interfaces.js";
@@ -7,14 +6,7 @@ import {LogOptions} from "../interfaces.js";
 export function getLogger(opts: LogOptions): Logger {
   if (opts.logger) return opts.logger;
 
-  // Code is running in the node environment
-  if (opts.logLevel && process !== undefined) {
-    // TODO for @nazarhussain: Any issue with pulling this code into the Web3Provider path?
-    // Can we just use the console.log logger for all code paths?
-    return getNodeLogger({level: opts.logLevel});
-  }
-
-  if (opts.logLevel && process === undefined) {
+  if (opts.logLevel) {
     return getBrowserLogger({level: opts.logLevel});
   }
 
