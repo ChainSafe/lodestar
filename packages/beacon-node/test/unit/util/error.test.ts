8704aea3fb017edd377898633245afc8362b4c54
Cayman <caymannava@gmail.com>
2023-05-17 11:13:12 -0400

    Fix unit test

--- packages/beacon-node/test/unit/util/error.test.ts
+++ packages/beacon-node/test/unit/util/error.test.ts
@@ -2,8 +2,6 @@ import v8 from "node:v8";
 import {expect} from "chai";
 import {RequestError, RequestErrorCode, RespStatus, ResponseError} from "@lodestar/reqresp";
 import {fromThreadBoundaryError, toThreadBoundaryError} from "../../../src/util/error.js";
-import {AttestationError, AttestationErrorCode} from "../../../src/chain/errors/attestationError.js";
-import {GossipAction} from "../../../src/chain/errors/index.js";
 
 function structuredClone<T>(value: T): T {
   return v8.deserialize(v8.serialize(value)) as T;
@@ -40,19 +38,5 @@ describe("ThreadBoundaryError", () => {
       expect.fail("clonedResponseError should be instance of ResponseError");
     }
     expect((clonedResponseError as ResponseError).toObject()).to.be.deep.equal(responseError.toObject());
-  });
-
-  it("should not able to clone AttestationError through thread boundary", () => {
-    const attestationError = new AttestationError(GossipAction.IGNORE, {code: AttestationErrorCode.BAD_TARGET_EPOCH});
-    const threadBoundaryError = toThreadBoundaryError(attestationError);
-    const clonedError = structuredClone(threadBoundaryError);
-    expect(clonedError.error).to.be.not.null;
-    if (!clonedError.error) {
-      // should not happen
-      expect.fail("clonedError.error should not be null");
-    }
-    expect(clonedError.object).to.be.null;
-    const clonedAttestationError = fromThreadBoundaryError(clonedError);
-    expect(clonedAttestationError instanceof AttestationError).to.be.false;
   });
 });
