c8ab8e7e66c298b1fc21756b52683f190b57857e
matthewkeil <me@matthewkeil.com>
2023-05-17 00:17:32 -0400

    feat(beacon-node): make async versions of verifySignatureSetsMaybeBatch

--- packages/beacon-node/src/chain/bls/maybeBatch.ts
+++ packages/beacon-node/src/chain/bls/maybeBatch.ts
@@ -1,5 +1,6 @@
 import {CoordType, PublicKey} from "@chainsafe/bls/types";
 import bls from "@chainsafe/bls";
+import blstTs from "@chainsafe/blst";
 
 const MIN_SET_COUNT_TO_BATCH = 2;
 
@@ -9,11 +10,17 @@ export type SignatureSetDeserialized = {
   signature: Uint8Array;
 };
 
+export type NapiSignatureSetDeserialized = {
+  publicKey: blstTs.PublicKey;
+  message: Uint8Array;
+  signature: Uint8Array;
+};
+
 /**
  * Verify signatures sets with batch verification or regular core verify depending on the set count.
  * Abstracted in a separate file to be consumed by the threaded pool and the main thread implementation.
  */
-export function verifySignatureSetsMaybeBatch(sets: SignatureSetDeserialized[]): boolean {
+export function verifySignatureSetsMaybeBatchSync(sets: SignatureSetDeserialized[]): boolean {
   if (sets.length >= MIN_SET_COUNT_TO_BATCH) {
     return bls.Signature.verifyMultipleSignatures(
       sets.map((s) => ({
@@ -36,4 +43,33 @@ export function verifySignatureSetsMaybeBatch(sets: SignatureSetDeserialized[]):
     const sig = bls.Signature.fromBytes(set.signature, CoordType.affine, true);
     return sig.verify(set.publicKey, set.message);
   });
+}
+
+/**
+ * Verify signatures sets with batch verification or regular core verify depending on the set count.
+ * Abstracted in a separate file to be consumed by the threaded pool and the main thread implementation.
+ */
+export async function verifySignatureSetsMaybeBatch(sets: NapiSignatureSetDeserialized[]): Promise<boolean> {
+  if (sets.length >= MIN_SET_COUNT_TO_BATCH) {
+    return blstTs.verifyMultipleAggregateSignatures(
+      sets.map((s) => ({
+        msg: s.message,
+        publicKey: s.publicKey,
+        signature: blstTs.Signature.deserialize(s.signature, blstTs.CoordType.affine),
+      }))
+    );
+  }
+
+  // .every on an empty array returns true
+  if (sets.length === 0) {
+    throw Error("Empty signature set");
+  }
+
+  const verifyPromises: Promise<boolean>[] = [];
+  for (const set of sets) {
+    const sig = blstTs.Signature.deserialize(set.signature, blstTs.CoordType.affine);
+    verifyPromises.push(sig.sigValidate().then(() => blstTs.verify(set.message, set.publicKey, sig)));
+  }
+
+  return (await Promise.all(verifyPromises)).every((v) => v);
 }
