78eb27664d7060117db9cda9827d1528b68b19be
matthewkeil <me@matthewkeil.com>
2023-05-18 05:41:27 -0400

    test(perf): update bls to new bindings

--- packages/beacon-node/test/perf/bls/bls.test.ts
+++ packages/beacon-node/test/perf/bls/bls.test.ts
@@ -1,11 +1,11 @@
 import {itBench} from "@dapplion/benchmark";
-import bls from "@chainsafe/bls";
-import type {PublicKey, SecretKey, Signature} from "@chainsafe/bls/types";
+import bls from "blst-ts-test";
+// import type {PublicKey, SecretKey, Signature} from "@chainsafe/bls/types";
 import {linspace} from "../../../src/util/numpy.js";
 
 describe("BLS ops", function () {
-  type Keypair = {publicKey: PublicKey; secretKey: SecretKey};
-  type BlsSet = {publicKey: PublicKey; message: Uint8Array; signature: Signature};
+  type Keypair = {publicKey: bls.PublicKey; secretKey: bls.SecretKey};
+  type BlsSet = {publicKey: bls.PublicKey; msg: Uint8Array; signature: bls.Signature};
 
   // Create and cache (on demand) crypto data to benchmark
   const sets = new Map<number, BlsSet>();
@@ -14,7 +14,7 @@ describe("BLS ops", function () {
   function getKeypair(i: number): Keypair {
     let keypair = keypairs.get(i);
     if (!keypair) {
-      const secretKey = bls.SecretKey.fromBytes(Buffer.alloc(32, i + 1));
+      const secretKey = bls.SecretKey.deserialize(Buffer.alloc(32, i + 1));
       const publicKey = secretKey.toPublicKey();
       keypair = {secretKey, publicKey};
       keypairs.set(i, keypair);
@@ -22,20 +22,20 @@ describe("BLS ops", function () {
     return keypair;
   }
 
-  function getSet(i: number): BlsSet {
+  async function getSet(i: number): Promise<BlsSet> {
     let set = sets.get(i);
     if (!set) {
       const {secretKey, publicKey} = getKeypair(i);
       const message = Buffer.alloc(32, i + 1);
-      set = {publicKey, message: message, signature: secretKey.sign(message)};
+      set = {publicKey, msg: message, signature: await secretKey.sign(message)};
       sets.set(i, set);
     }
     return set;
   }
 
   // Note: getSet() caches the value, does not re-compute every time
-  itBench({id: `BLS verify - ${bls.implementation}`, beforeEach: () => getSet(0)}, (set) => {
-    const isValid = set.signature.verify(set.publicKey, set.message);
+  itBench({id: "BLS verify - Napi", beforeEach: () => getSet(0)}, async (set) => {
+    const isValid = await bls.verify(set.msg, set.publicKey, set.signature);
     if (!isValid) throw Error("Invalid");
   });
 
@@ -43,10 +43,10 @@ describe("BLS ops", function () {
   // We may want to bundle up to 32 sets in a single batch.
   for (const count of [3, 8, 32]) {
     itBench({
-      id: `BLS verifyMultipleSignatures ${count} - ${bls.implementation}`,
-      beforeEach: () => linspace(0, count - 1).map((i) => getSet(i)),
-      fn: (sets) => {
-        const isValid = bls.Signature.verifyMultipleSignatures(sets);
+      id: `BLS verifyMultipleSignatures ${count} - Napi`,
+      beforeEach: async () => Promise.all(linspace(0, count - 1).map((i) => getSet(i))),
+      fn: async (sets) => {
+        const isValid = await bls.verifyMultipleAggregateSignatures(sets);
         if (!isValid) throw Error("Invalid");
       },
     });
@@ -55,10 +55,10 @@ describe("BLS ops", function () {
   // Attestations in Mainnet contain 128 max on average
   for (const count of [32, 128]) {
     itBench({
-      id: `BLS aggregatePubkeys ${count} - ${bls.implementation}`,
-      beforeEach: () => linspace(0, count - 1).map((i) => getKeypair(i).publicKey),
+      id: `BLS aggregatePubkeys ${count} - Napi`,
+      beforeEach: async () => Promise.all(linspace(0, count - 1).map((i) => getKeypair(i).publicKey)),
       fn: (pubkeys) => {
-        bls.PublicKey.aggregate(pubkeys);
+        return void bls.aggregatePublicKeys(pubkeys);
       },
     });
   }
