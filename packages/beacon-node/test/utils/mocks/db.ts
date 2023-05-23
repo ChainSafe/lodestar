f728b2c07d34d5637c37b361032446ca1111d091
harkamal <gajinder@g11.in>
2023-05-23 13:10:27 +0530

    fix test

--- packages/beacon-node/test/utils/mocks/db.ts
+++ packages/beacon-node/test/utils/mocks/db.ts
@@ -14,8 +14,13 @@ import {
   SyncCommitteeRepository,
   SyncCommitteeWitnessRepository,
   BackfilledRanges,
+
   BlobsSidecarRepository,
   BlobsSidecarArchiveRepository,
+  // TODO DENEB: cleanup following blob repos post full migration
+  BlobsSidecarRepository,
+  BlobsSidecarArchiveRepository,
+
   BLSToExecutionChangeRepository,
 } from "../../../src/db/repositories/index.js";
 import {PreGenesisState, PreGenesisStateLastProcessedBlock} from "../../../src/db/single/index.js";
