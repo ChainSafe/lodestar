f5e1f2c878fd545f8a5b1cf8e99d6d48f02adf40
dapplion <35266934+dapplion@users.noreply.github.com>
2023-05-29 11:24:15 +0300

    Assert in CI yarn prints no warnings

---
+++ scripts/assert_no_yarn_warnings.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# run yarn install --check-files, capturing stderr
+OUTPUT=$(yarn install --check-files 2>&1)
+
+echo $OUTPUT
+
+# grep the output for 'warning'
+if echo "$OUTPUT" | grep -qi 'warning'; then
+  echo "There were warnings in yarn install --check-files"
+else
+  echo "No warnings in yarn install --check-files"
+fi
