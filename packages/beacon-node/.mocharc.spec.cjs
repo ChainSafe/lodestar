be957a065ca575f136b5d1dd9c8915d509fbb4e0
Nazar Hussain <nazarhussain@gmail.com>
2023-05-30 20:36:28 +0200

    Fix preset path for the mcoha

--- packages/beacon-node/.mocharc.spec.cjs
+++ packages/beacon-node/.mocharc.spec.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   colors: true,
-  require: ["./test/setupPreset.ts", "./test/setup.ts"],
+  require: ["./test/setup_preset.ts", "./test/setup.ts"],
   "node-option": ["loader=ts-node/esm"],
   timeout: 60_000,
   // Do not run tests through workers, it's not proven to be faster than with `jobs: 2`
