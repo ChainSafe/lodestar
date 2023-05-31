3fe9afd1d6cfe558a6b16a8c2d722f8e99e330a9
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2023-05-31 19:31:42 +0300

    test: remove chain mocks (#5582)
    * Remove chain mocks
    
    * Use actual BeaconChain class in network tests
    
    * fix type errors
    
    ---------
    
    Co-authored-by: Cayman <caymannava@gmail.com>

--- packages/beacon-node/src/network/core/networkCore.ts
+++ packages/beacon-node/src/network/core/networkCore.ts
@@ -255,15 +255,18 @@ export class NetworkCore implements INetworkCore {
 
     // Must goodbye and disconnect before stopping libp2p
     await this.peerManager.goodbyeAndDisconnectAllPeers();
+    this.logger.debug("network sent goodbye to all peers");
     await this.peerManager.stop();
+    this.logger.debug("network peerManager closed");
     await this.gossip.stop();
-
+    this.logger.debug("network gossip closed");
     await this.reqResp.stop();
     await this.reqResp.unregisterAllProtocols();
-
+    this.logger.debug("network reqResp closed");
     this.attnetsService.stop();
     this.syncnetsService.stop();
     await this.libp2p.stop();
+    this.logger.debug("network lib2p closed");
 
     this.closed = true;
   }
