c1afade20a8247cf2145115f23b7e6cf3ba5a99c
dapplion <35266934+dapplion@users.noreply.github.com>
2023-06-01 10:52:26 +0300

    Add exit 1

--- scripts/assert_no_yarn_warnings.sh
+++ scripts/assert_no_yarn_warnings.sh
@@ -8,6 +8,7 @@ echo $OUTPUT
 # grep the output for 'warning'
 if echo "$OUTPUT" | grep -qi 'warning'; then
   echo "There were warnings in yarn install --check-files"
+  exit 1
 else
   echo "No warnings in yarn install --check-files"
 fi
