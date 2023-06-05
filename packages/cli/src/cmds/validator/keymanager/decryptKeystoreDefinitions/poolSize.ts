0258a33b8d7dcb057f2b0a71a76a8bbbd9a0899f
Nico Flaig <nflaig@protonmail.com>
2023-06-05 10:33:26 +0200

    fix: adjust decryption worker pool size based on keystore count

--- packages/cli/src/cmds/validator/keymanager/decryptKeystoreDefinitions/poolSize.ts
+++ packages/cli/src/cmds/validator/keymanager/decryptKeystoreDefinitions/poolSize.ts
@@ -1,17 +1,17 @@
-let defaultPoolSize: number;
+let maxPoolSize: number;
 
 try {
   if (typeof navigator !== "undefined") {
-    defaultPoolSize = navigator.hardwareConcurrency ?? 4;
+    maxPoolSize = navigator.hardwareConcurrency ?? 4;
   } else {
     // TODO change this line to use os.availableParallelism() once we upgrade to node v20
-    defaultPoolSize = (await import("node:os")).cpus().length;
+    maxPoolSize = (await import("node:os")).cpus().length;
   }
 } catch (e) {
-  defaultPoolSize = 8;
+  maxPoolSize = 8;
 }
 
 /**
- * Cross-platform aprox number of logical cores
+ * Cross-platform approx number of logical cores
  */
-export {defaultPoolSize};
+export {maxPoolSize};
