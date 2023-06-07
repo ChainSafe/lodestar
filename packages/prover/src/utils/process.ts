8cd7bd2a4351b9bdf89ee50b2383e9551e333d69
Nazar Hussain <nazarhussain@gmail.com>
2023-06-07 17:33:07 +0200

    feat(prover): add whitelist unverified methods support to prover (#5614)
    * Add support for batch request
    
    * Fix lint errors
    
    * Fix lint errors
    
    * Add e2e tests for batch request
    
    * Fix e2e tests
    
    * Add feature to limit the unverified white list methods
    
    * Update readme
    
    * Update packages/prover/src/cli/cmds/start/options.ts
    
    Co-authored-by: Matthew Keil <me@matthewkeil.com>
    
    * Make all cli options to camelCase
    
    * Update packages/prover/README.md
    
    Co-authored-by: Nico Flaig <nflaig@protonmail.com>
    
    * Update packages/prover/src/interfaces.ts
    
    Co-authored-by: Nico Flaig <nflaig@protonmail.com>
    
    * Fix args params style
    
    ---------
    
    Co-authored-by: Matthew Keil <me@matthewkeil.com>
    Co-authored-by: Nico Flaig <nflaig@protonmail.com>

--- packages/prover/src/utils/process.ts
+++ packages/prover/src/utils/process.ts
@@ -10,11 +10,11 @@ import {eth_getCode} from "../verified_requests/eth_getCode.js";
 import {eth_call} from "../verified_requests/eth_call.js";
 import {eth_estimateGas} from "../verified_requests/eth_estimateGas.js";
 import {ELRpc} from "./execution.js";
-import {isBatchRequest, isRequest} from "./json_rpc.js";
+import {getResponseForRequest, isBatchRequest, isRequest} from "./json_rpc.js";
 import {isNullish} from "./validation.js";
 
 /* eslint-disable @typescript-eslint/naming-convention, @typescript-eslint/no-explicit-any */
-export const supportedELRequests: Record<string, ELVerifiedRequestHandler<any, any>> = {
+export const verifiableMethodHandlers: Record<string, ELVerifiedRequestHandler<any, any>> = {
   eth_getBalance: eth_getBalance,
   eth_getTransactionCount: eth_getTransactionCount,
   eth_getBlockByHash: eth_getBlockByHash,
@@ -24,22 +24,42 @@ export const supportedELRequests: Record<string, ELVerifiedRequestHandler<any, a
   eth_estimateGas: eth_estimateGas,
 };
 
-export function splitRequestsInChunks(payload: JsonRpcRequestOrBatch): {
+export const verifiableMethods = Object.keys(verifiableMethodHandlers);
+export const alwaysAllowedMethods = ["eth_subscribe", "eth_unsubscribe", "eth_getProof"];
+
+export function splitRequestsInChunks(
+  payload: JsonRpcRequestOrBatch,
+  unverifiedWhitelist?: string[]
+): {
   verifiable: JsonRpcBatchRequest;
   nonVerifiable: JsonRpcBatchRequest;
+  blocked: JsonRpcBatchRequest;
 } {
   const verifiable: JsonRpcBatchRequest = [];
   const nonVerifiable: JsonRpcBatchRequest = [];
+  const blocked: JsonRpcBatchRequest = [];
 
   for (const pay of isBatchRequest(payload) ? payload : [payload]) {
-    if (isRequest(pay) && !isNullish(supportedELRequests[pay.method])) {
+    if (isRequest(pay) && verifiableMethods.includes(pay.method)) {
       verifiable.push(pay);
-    } else {
-      nonVerifiable.push(pay);
+      continue;
     }
+
+    // If unverifiedWhitelist is not set that implies all methods are allowed
+    if ((isRequest(pay) && isNullish(unverifiedWhitelist)) || unverifiedWhitelist?.includes(pay.method)) {
+      nonVerifiable.push(pay);
+      continue;
+    }
+
+    if (alwaysAllowedMethods.includes(pay.method)) {
+      nonVerifiable.push(pay);
+      continue;
+    }
+
+    blocked.push(pay);
   }
 
-  return {verifiable, nonVerifiable};
+  return {verifiable, nonVerifiable, blocked};
 }
 
 export async function processAndVerifyRequest({
@@ -54,16 +74,18 @@ export async function processAndVerifyRequest({
   logger: Logger;
 }): Promise<JsonRpcResponseOrBatch | undefined> {
   await proofProvider.waitToBeReady();
-  const {verifiable, nonVerifiable} = splitRequestsInChunks(payload);
+
+  const {verifiable, nonVerifiable, blocked} = splitRequestsInChunks(payload, proofProvider.opts.unverifiedWhitelist);
   const verifiedResponses: JsonRpcBatchResponse = [];
   const nonVerifiedResponses: JsonRpcBatchResponse = [];
+  const blockedResponses: JsonRpcBatchResponse = [];
 
   for (const request of verifiable) {
     logger.debug("Processing verifiable request", {
       method: request.method,
       params: JSON.stringify(request.params),
     });
-    const verifiableRequestHandler = supportedELRequests[request.method];
+    const verifiableRequestHandler = verifiableMethodHandlers[request.method];
     const response = await verifiableRequestHandler({payload: request, rpc, proofProvider, logger});
     verifiedResponses.push(response);
   }
@@ -74,7 +96,13 @@ export async function processAndVerifyRequest({
     nonVerifiedResponses.push(...response);
   }
 
-  const responses = [...verifiedResponses, ...nonVerifiedResponses];
+  for (const request of blocked) {
+    blockedResponses.push(
+      getResponseForRequest(request, undefined, {message: `Method "${request.method}" not allowed.`})
+    );
+  }
+
+  const responses = [...verifiedResponses, ...nonVerifiedResponses, ...blockedResponses];
 
   if (responses.length === 1) {
     return responses[0];
