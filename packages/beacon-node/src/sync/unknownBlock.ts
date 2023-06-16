7eec2e53cd39762ddbb7337d6a205b55ceaa9903
tuyennhv <tuyen@chainsafe.io>
2023-06-16 21:55:49 +0700

    fix: handle only downloaded blocks in unknown block sync (#5656)
    * fix: handle downloaded blocks only
    
    * chore: add unit test
    
    * chore: fix comment

--- packages/beacon-node/src/sync/unknownBlock.ts
+++ packages/beacon-node/src/sync/unknownBlock.ts
@@ -15,7 +15,7 @@ import {BlockError, BlockErrorCode} from "../chain/errors/index.js";
 import {beaconBlocksMaybeBlobsByRoot} from "../network/reqresp/beaconBlocksMaybeBlobsByRoot.js";
 import {wrapError} from "../util/wrapError.js";
 import {PendingBlock, PendingBlockStatus, PendingBlockType} from "./interface.js";
-import {getDescendantBlocks, getAllDescendantBlocks, getUnknownBlocks} from "./utils/pendingBlocksTree.js";
+import {getDescendantBlocks, getAllDescendantBlocks, getUnknownAndAncestorBlocks} from "./utils/pendingBlocksTree.js";
 import {SyncOptions} from "./options.js";
 
 const MAX_ATTEMPTS_PER_BLOCK = 5;
@@ -29,6 +29,7 @@ export class UnknownBlockSync {
   private readonly pendingBlocks = new Map<RootHex, PendingBlock>();
   private readonly knownBadBlocks = new Set<RootHex>();
   private readonly proposerBoostSecWindow: number;
+  private readonly maxPendingBlocks;
 
   constructor(
     private readonly config: ChainForkConfig,
@@ -46,6 +47,7 @@ export class UnknownBlockSync {
     } else {
       this.logger.debug("UnknownBlockSync disabled.");
     }
+    this.maxPendingBlocks = opts?.maxPendingBlocks ?? MAX_PENDING_BLOCKS;
 
     this.proposerBoostSecWindow = this.config.SECONDS_PER_SLOT / INTERVALS_PER_SLOT;
 
@@ -147,7 +149,7 @@ export class UnknownBlockSync {
     }
 
     // Limit pending blocks to prevent DOS attacks that cause OOM
-    const prunedItemCount = pruneSetToMax(this.pendingBlocks, MAX_PENDING_BLOCKS);
+    const prunedItemCount = pruneSetToMax(this.pendingBlocks, this.maxPendingBlocks);
     if (prunedItemCount > 0) {
       this.logger.warn(`Pruned ${prunedItemCount} pending blocks from UnknownBlockSync`);
     }
@@ -169,13 +171,32 @@ export class UnknownBlockSync {
       return;
     }
 
-    const unknownBlocks = getUnknownBlocks(this.pendingBlocks);
-    if (unknownBlocks.length === 0) {
-      this.logger.debug("No unknown block to download", {pendingBlocks: this.pendingBlocks.size});
+    const {unknowns, ancestors} = getUnknownAndAncestorBlocks(this.pendingBlocks);
+    // it's rare when there is no unknown block
+    // see https://github.com/ChainSafe/lodestar/issues/5649#issuecomment-1594213550
+    if (unknowns.length === 0) {
+      let processedBlocks = 0;
+
+      for (const block of ancestors) {
+        // when this happens, it's likely the block and parent block are processed by head sync
+        if (this.chain.forkChoice.hasBlockHex(block.parentBlockRootHex)) {
+          processedBlocks++;
+          this.processBlock(block).catch((e) => {
+            this.logger.debug("Unexpected error - process old downloaded block", {}, e);
+          });
+        }
+      }
+
+      this.logger.verbose("No unknown block, process ancestor downloaded blocks", {
+        pendingBlocks: this.pendingBlocks.size,
+        ancestorBlocks: ancestors.length,
+        processedBlocks,
+      });
       return;
     }
 
-    for (const block of unknownBlocks) {
+    // most of the time there is exactly 1 unknown block
+    for (const block of unknowns) {
       this.downloadBlock(block, connectedPeers).catch((e) => {
         this.logger.debug("Unexpected error - downloadBlock", {root: block.blockRootHex}, e);
       });
@@ -222,7 +243,7 @@ export class UnknownBlockSync {
       if (parentInForkchoice) {
         // Bingo! Process block. Add to pending blocks anyway for recycle the cache that prevents duplicate processing
         this.processBlock(block).catch((e) => {
-          this.logger.debug("Unexpected error - processBlock", {}, e);
+          this.logger.debug("Unexpected error - process newly downloaded block", {}, e);
         });
       } else if (blockSlot <= finalizedSlot) {
         // the common ancestor of the downloading chain and canonical chain should be at least the finalized slot and
@@ -314,7 +335,7 @@ export class UnknownBlockSync {
       // Send child blocks to the processor
       for (const descendantBlock of getDescendantBlocks(pendingBlock.blockRootHex, this.pendingBlocks)) {
         this.processBlock(descendantBlock).catch((e) => {
-          this.logger.debug("Unexpected error - processBlock", {}, e);
+          this.logger.debug("Unexpected error - process descendant block", {}, e);
         });
       }
     } else {
