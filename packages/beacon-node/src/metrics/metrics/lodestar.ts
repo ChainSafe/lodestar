09cacb0807fb2a1b18b57a4c1643b32ca3740057
matthewkeil <me@matthewkeil.com>
2023-06-18 00:36:31 -0700

    fix(beacon-node): rename added libuv metrics

--- packages/beacon-node/src/metrics/metrics/lodestar.ts
+++ packages/beacon-node/src/metrics/metrics/lodestar.ts
@@ -370,53 +370,53 @@ export function createLodestarMetrics(
       },
       libuvThreadPool: {
         durationInThreadPool: register.histogram({
-          name: "lodestar_bls_thread_pool_libuv_time_seconds",
+          name: "lodestar_bls_libuv_time_seconds",
           help: "Time to verify signatures in libuv with thread pool mode",
           // buckets: [0],
         }),
         jobWaitTime: register.histogram({
-          name: "lodestar_bls_thread_pool_queue_job_wait_time_seconds",
+          name: "lodestar_bls_libuv_queue_job_wait_time_seconds",
           help: "Time from job added to the queue to starting the job in seconds",
           buckets: [0.01, 0.02, 0.5, 0.1, 0.3, 1],
         }),
         totalJobsGroupsStarted: register.gauge({
-          name: "lodestar_bls_thread_pool_job_groups_started_total",
+          name: "lodestar_bls_libuv_job_groups_started_total",
           help: "Count of total jobs groups started in bls thread pool, job groups include +1 jobs",
         }),
         totalJobsStarted: register.gauge({
-          name: "lodestar_bls_thread_pool_jobs_started_total",
+          name: "lodestar_bls_libuv_jobs_started_total",
           help: "Count of total jobs started in bls thread pool, jobs include +1 signature sets",
         }),
         totalSigSetsStarted: register.gauge({
-          name: "lodestar_bls_thread_pool_sig_sets_started_total",
+          name: "lodestar_bls_libuv_sig_sets_started_total",
           help: "Count of total signature sets started in bls thread pool, sig sets include 1 pk, msg, sig",
         }),
         timePerSigSet: register.histogram({
-          name: "lodestar_bls_worker_thread_time_per_sigset_seconds",
+          name: "lodestar_bls_libuv_time_per_sigset_seconds",
           help: "Time to verify each sigset with worker thread mode",
           // Time per sig ~0.9ms on good machines
           buckets: [0.5e-3, 0.75e-3, 1e-3, 1.5e-3, 2e-3, 5e-3],
         }),
         jobsWorkTime: register.gauge({
-          name: "lodestar_bls_thread_pool_time_seconds_sum",
+          name: "lodestar_bls_libuv_time_seconds_sum",
           help: "Total time spent verifying signature sets measured on the worker",
         }),
         successJobsSignatureSetsCount: register.gauge({
-          name: "lodestar_bls_thread_pool_success_jobs_signature_sets_count",
+          name: "lodestar_bls_libuv_success_jobs_signature_sets_count",
           help: "Count of total verified signature sets",
         }),
         errorJobsSignatureSetsCount: register.gauge({
-          name: "lodestar_bls_thread_pool_error_jobs_signature_sets_count",
+          name: "lodestar_bls_libuv_error_jobs_signature_sets_count",
           help: "Count of total error-ed signature sets",
         }),
         // Re-verifying a batch means doing double work. This number must be very low or it can be a waste of CPU resources
         batchRetries: register.gauge({
-          name: "lodestar_bls_thread_pool_batch_retries_total",
+          name: "lodestar_bls_libuv_batch_retries_total",
           help: "Count of total batches that failed and had to be verified again.",
         }),
         // To count how many sigs are being validated with the optimization of batching them
         batchSigsSuccess: register.gauge({
-          name: "lodestar_bls_thread_pool_batch_sigs_success_total",
+          name: "lodestar_bls_libuv_batch_sigs_success_total",
           help: "Count of total batches that failed and had to be verified again.",
         }),
       },
