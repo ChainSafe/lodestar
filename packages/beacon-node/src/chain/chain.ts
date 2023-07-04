b69bb31f14b5dbfb372cfd1acb951d71aad1e048
matthewkeil <me@matthewkeil.com>
2023-07-03 23:25:02 -0700

    feat(beacon-node): add execution engine to hot block db

--- packages/beacon-node/src/chain/chain.ts
+++ packages/beacon-node/src/chain/chain.ts
@@ -186,6 +186,7 @@ export class BeaconChain implements IBeaconChain {
     this.genesisValidatorsRoot = anchorState.genesisValidatorsRoot;
     this.eth1 = eth1;
     this.executionEngine = executionEngine;
+    this.db.block.setExecutionEngine(executionEngine);
     this.db.blockArchive.setExecutionEngine(executionEngine);
     this.executionBuilder = executionBuilder;
     // From https://github.com/ethereum/execution-apis/blob/main/src/engine/specification.md#specification-3
--- packages/beacon-node/src/db/repositories/block.ts
+++ packages/beacon-node/src/db/repositories/block.ts
@@ -3,17 +3,25 @@ import {Db, Repository} from "@lodestar/db";
 import {allForks, ssz} from "@lodestar/types";
 import {getSignedBlockTypeFromBytes} from "../../util/multifork.js";
 import {Bucket, getBucketNameByValue} from "../buckets.js";
+import {IExecutionEngine} from "../../execution/index.js";
+import {isSignedBlindedBeaconBlock} from "./blockBlindingAndUnblinding.js";
 
 /**
  * Blocks by root
  *
  * Used to store unfinalized blocks
  */
-export class BlockRepository extends Repository<Uint8Array, allForks.SignedBeaconBlock> {
+export class BlockRepository extends Repository<Uint8Array, allForks.FullOrBlindedSignedBeaconBlock> {
+  executionEngine?: IExecutionEngine;
+
   constructor(config: ChainForkConfig, db: Db) {
     const bucket = Bucket.allForks_block;
     const type = ssz.phase0.SignedBeaconBlock; // Pick some type but won't be used
     super(config, db, bucket, type, getBucketNameByValue(bucket));
+  }
+
+  setExecutionEngine(engine: IExecutionEngine): void {
+    this.executionEngine = engine;
   }
 
   /**
@@ -28,6 +36,6 @@ export class BlockRepository extends Repository<Uint8Array, allForks.SignedBeaco
   }
 
   decodeValue(data: Buffer): allForks.SignedBeaconBlock {
-    return getSignedBlockTypeFromBytes(this.config, data, false).deserialize(data);
+    return getSignedBlockTypeFromBytes(this.config, data, isSignedBlindedBeaconBlock(data)).deserialize(data);
   }
 }
