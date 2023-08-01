6b6c8f53854002bd20f97c7023d1930521b222bc
Nico Flaig <nflaig@protonmail.com>
2023-08-01 17:48:51 +0200

    Pin genesis and bootnode file URLs by commit hash

--- packages/cli/src/networks/goerli.ts
+++ packages/cli/src/networks/goerli.ts
@@ -2,8 +2,8 @@ export {goerliChainConfig as chainConfig} from "@lodestar/config/networks";
 
 export const depositContractDeployBlock = 4367322;
 export const genesisFileUrl =
-  "https://raw.githubusercontent.com/eth2-clients/eth2-testnets/master/shared/prater/genesis.ssz";
+  "https://raw.githubusercontent.com/eth-clients/eth2-networks/f94c4d9499e55bec0fd21d31203350080633c60a/shared/prater/genesis.ssz";
 export const bootnodesFileUrl =
-  "https://raw.githubusercontent.com/eth2-clients/eth2-testnets/master/shared/prater/bootstrap_nodes.txt";
+  "https://raw.githubusercontent.com/eth-clients/eth2-networks/f94c4d9499e55bec0fd21d31203350080633c60a/shared/prater/bootstrap_nodes.txt";
 
 export const bootEnrs = [];
--- packages/cli/src/networks/sepolia.ts
+++ packages/cli/src/networks/sepolia.ts
@@ -1,9 +1,10 @@
 export {sepoliaChainConfig as chainConfig} from "@lodestar/config/networks";
 
 export const depositContractDeployBlock = 1273020;
-export const genesisFileUrl = "https://raw.githubusercontent.com/eth-clients/merge-testnets/main/sepolia/genesis.ssz";
+export const genesisFileUrl =
+  "https://raw.githubusercontent.com/eth-clients/merge-testnets/baebfaba3c0dcb8b6e4d5113e52fd6981253a152/sepolia/genesis.ssz";
 export const bootnodesFileUrl =
-  "https://raw.githubusercontent.com/eth-clients/merge-testnets/main/sepolia/bootstrap_nodes.txt";
+  "https://raw.githubusercontent.com/eth-clients/merge-testnets/baebfaba3c0dcb8b6e4d5113e52fd6981253a152/sepolia/bootstrap_nodes.txt";
 
 export const bootEnrs = [
   "enr:-Iq4QMCTfIMXnow27baRUb35Q8iiFHSIDBJh6hQM5Axohhf4b6Kr_cOCu0htQ5WvVqKvFgY28893DHAg8gnBAXsAVqmGAX53x8JggmlkgnY0gmlwhLKAlv6Jc2VjcDI1NmsxoQK6S-Cii_KmfFdUJL2TANL3ksaKUnNXvTCv1tLwXs0QgIN1ZHCCIyk",
