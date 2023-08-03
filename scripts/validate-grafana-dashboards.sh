63c671565b94f41dfa83f43c28310d471ab100bc
Nico Flaig <nflaig@protonmail.com>
2023-08-03 10:55:20 +0200

    ci: fail job to lint grafana dashboards on error

--- scripts/validate-grafana-dashboards.sh
+++ scripts/validate-grafana-dashboards.sh
@@ -3,6 +3,11 @@
 # This script will mutate the dashboards if anything needs linting
 node scripts/lint-grafana-dashboards.mjs ./dashboards
 
+if [[ $? -ne 0 ]]; then
+  echo 'linting dashboards failed'
+  exit 1
+fi
+
 if [[ $(git diff --stat ./dashboards) != '' ]]; then
   git --no-pager diff
   echo 'dashboards need fixing'
