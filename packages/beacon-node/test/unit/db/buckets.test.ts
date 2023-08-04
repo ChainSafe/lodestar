20ecdcdee808f97ac19164751cf70d34e1f3c95d
dapplion <35266934+dapplion@users.noreply.github.com>
2023-08-04 17:47:12 +0200

    Ensure bn Buckets are sorted and unique

---
+++ packages/beacon-node/test/unit/db/buckets.test.ts
@@ -0,0 +1,20 @@
+import {Bucket} from "../../../src/db/buckets.js";
+
+describe("db buckets", () => {
+  it("sorted and unique", () => {
+    let prevBucket = -1;
+
+    for (const key of Object.keys(Bucket)) {
+      if (isNaN(parseInt(key))) {
+        const bucket = (Bucket as unknown as Record<string, number>)[key];
+        if (bucket < prevBucket) {
+          throw Error(`Bucket ${key} not sorted`);
+        }
+        if (bucket === prevBucket) {
+          throw Error(`Bucket ${key}: ${bucket} duplicated`);
+        }
+        prevBucket = bucket;
+      }
+    }
+  });
+});
