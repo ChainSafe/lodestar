c4a7ef81ab903159ac3564ca4e158f3755b76cfb
Nico Flaig <nflaig@protonmail.com>
2023-09-10 09:24:44 +0200

    Run e2e tests in a single step

--- packages/cli/test/e2e/voluntaryExitFromApi.test.ts
+++ packages/cli/test/e2e/voluntaryExitFromApi.test.ts
@@ -1,37 +1,31 @@
 import path from "node:path";
 import {expect} from "chai";
 import {rimraf} from "rimraf";
-import {ApiError, getClient, routes} from "@lodestar/api";
-import {Api as KeymanagerApi, getClient as getKeymanagerClient} from "@lodestar/api/keymanager";
+import {ApiError, getClient} from "@lodestar/api";
+import {getClient as getKeymanagerClient} from "@lodestar/api/keymanager";
 import {config} from "@lodestar/config/default";
 import {interopSecretKey} from "@lodestar/state-transition";
-import {gracefullyStopChildProcess, spawnCliCommand} from "@lodestar/test-utils";
-import {phase0} from "@lodestar/types";
+import {spawnCliCommand} from "@lodestar/test-utils";
+import {getMochaContext} from "@lodestar/test-utils/mocha";
 import {retry} from "@lodestar/utils";
 import {testFilesDir} from "../utils.js";
 
 describe("voluntary exit from api", function () {
+  const testContext = getMochaContext(this);
   this.timeout("60s");
 
   const dataDir = path.join(testFilesDir, "voluntary-exit-api-test");
 
-  before("clean dataDir", () => {
+  before("Clean dataDir", () => {
     rimraf.sync(dataDir);
   });
 
-  let beaconClient: routes.beacon.Api;
-  let keymanagerClient: KeymanagerApi;
-
-  // To cleanup the event stream connection
-  const httpClientController = new AbortController();
-
-  let devProc: Awaited<ReturnType<typeof spawnCliCommand>>;
-
-  before("start dev node with keymanager", async () => {
+  it("Perform a voluntary exit", async () => {
+    // Start dev node with keymanager
     const keymanagerPort = 38012;
     const beaconPort = 39012;
 
-    devProc = await spawnCliCommand(
+    const devProc = await spawnCliCommand(
       "packages/cli/bin/lodestar.js",
       [
         // ⏎
@@ -52,7 +46,7 @@ describe("voluntary exit from api", function () {
         // Disable bearer token auth to simplify testing
         "--keymanager.authEnabled=false",
       ],
-      {pipeStdioToParent: false, logPrefix: "dev"}
+      {pipeStdioToParent: false, logPrefix: "dev", testContext}
     );
 
     // Exit early if process exits
@@ -62,12 +56,14 @@ describe("voluntary exit from api", function () {
       }
     });
 
-    beaconClient = getClient(
+    // To cleanup the event stream connection
+    const httpClientController = new AbortController();
+
+    const beaconClient = getClient(
       {baseUrl: `http://127.0.0.1:${beaconPort}`, getAbortSignal: () => httpClientController.signal},
       {config}
     ).beacon;
-
-    keymanagerClient = getKeymanagerClient(
+    const keymanagerClient = getKeymanagerClient(
       {baseUrl: `http://127.0.0.1:${keymanagerPort}`, getAbortSignal: () => httpClientController.signal},
       {config}
     );
@@ -81,37 +77,25 @@ describe("voluntary exit from api", function () {
       },
       {retryDelay: 1000, retries: 20}
     );
-  });
 
-  after(async () => {
-    // Disconnect the event stream for the client
-    httpClientController.abort();
-    devProc.removeAllListeners("exit");
-    await gracefullyStopChildProcess(devProc, 3000);
-  });
+    // 1. create signed voluntary exit message from keymanager
+    const exitEpoch = 0;
+    const indexToExit = 0;
+    const pubkeyToExit = interopSecretKey(indexToExit).toPublicKey().toHex();
 
-  const exitEpoch = 0;
-  const indexToExit = 0;
-  const pubkeyToExit = interopSecretKey(indexToExit).toPublicKey().toHex();
-
-  let signedVoluntaryExit: phase0.SignedVoluntaryExit;
-
-  it("1. create signed voluntary exit message from keymanager", async () => {
     const res = await keymanagerClient.signVoluntaryExit(pubkeyToExit, exitEpoch);
     ApiError.assert(res);
-    signedVoluntaryExit = res.response.data;
+    const signedVoluntaryExit = res.response.data;
 
     expect(signedVoluntaryExit.message.epoch).to.equal(exitEpoch);
     expect(signedVoluntaryExit.message.validatorIndex).to.equal(indexToExit);
     // Signature will be verified when submitting to beacon node
     expect(signedVoluntaryExit.signature).to.not.be.undefined;
-  });
 
-  it("2. submit signed voluntary exit message to beacon node", async () => {
+    // 2. submit signed voluntary exit message to beacon node
     ApiError.assert(await beaconClient.submitPoolVoluntaryExit(signedVoluntaryExit));
-  });
 
-  it("3. confirm validator status is 'active_exiting'", async () => {
+    // 3. confirm validator status is 'active_exiting'
     await retry(
       async () => {
         const res = await beaconClient.getStateValidator("head", pubkeyToExit);
@@ -125,5 +109,8 @@ describe("voluntary exit from api", function () {
       },
       {retryDelay: 1000, retries: 20}
     );
+
+    // Disconnect the event stream for the client
+    httpClientController.abort();
   });
 });
