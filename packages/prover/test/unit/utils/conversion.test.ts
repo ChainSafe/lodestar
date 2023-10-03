9bc5ec827a591a02c925b98f3ec9ed897af83ac5
Roman Dvorkin <rdvorkin@fireblocks.com>
2023-10-03 14:26:23 +0300

    Fix strings in test

--- packages/prover/test/unit/utils/conversion.test.ts
+++ packages/prover/test/unit/utils/conversion.test.ts
@@ -1,7 +1,7 @@
 import {expect} from "chai";
 import {chunkIntoN} from "../../../src/utils/conversion.js";
 
-describe("uitls/execution", () => {
+describe("utils/conversion", () => {
   describe("chunkIntoN", () => {
     it("should return true if splitting into chunks correctly", async () => {
       expect(chunkIntoN([1, 2, 3, 4, 5, 6], 2)).to.be.deep.eq([
