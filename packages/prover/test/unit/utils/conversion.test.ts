49534ff9fcd9228c526a2ad2fae340bc2c0dd3ef
Roman Dvorkin <rdvorkin@fireblocks.com>
2023-10-03 14:23:22 +0300

    FIX: fixed chunkIntoN to chunk correctly
    chunkIntoN was chunking into N chunks, however it is used to chunk eth_getCode and eth_getProof responses into chunks of 2, so the desired action should be to chunk into chunks of length N
    https://github.com/ChainSafe/lodestar/blob/unstable/packages/prover/src/utils/evm.ts#L105
    
    Because the current tests work on chunking array of length 4 into 2 the previous behavior was working and passing tests. When there are 6 requests the current behavior no longer works

---
+++ packages/prover/test/unit/utils/conversion.test.ts
@@ -0,0 +1,14 @@
+import {expect} from "chai";
+import {chunkIntoN} from "../../../src/utils/conversion.js";
+
+describe("uitls/execution", () => {
+  describe("chunkIntoN", () => {
+    it("should return true if splitting into chunks correctly", async () => {
+      expect(chunkIntoN([1, 2, 3, 4, 5, 6], 2)).to.be.deep.eq([
+        [1, 2],
+        [3, 4],
+        [5, 6],
+      ]);
+    });
+  });
+});
