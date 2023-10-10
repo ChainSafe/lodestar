347a6330ef47db630b3ef1d0e5218244f88b862a
Nazar Hussain <nazarhussain@gmail.com>
2023-10-10 10:59:34 +0200

    Add code coverage support for vitest

--- packages/beacon-node/vitest.config.ts
+++ packages/beacon-node/vitest.config.ts
@@ -4,5 +4,22 @@ export default defineConfig({
   test: {
     globalSetup: ["./test/globalSetup.ts"],
     reporters: ["default", "hanging-process"],
+    coverage: {
+      clean: true,
+      provider: "v8",
+      reporter: [["lcovonly", {projectRoot: "./src", file: "lcov.info"}], ["json", {file: "coverage.json"}], ["text"]],
+      reportsDirectory: "./coverage",
+      exclude: [
+        "**/*.d.ts",
+        "**/*.js",
+        "**/lib/**",
+        "**/coverage/**",
+        "**/scripts/**",
+        "**/test/**",
+        "**/types/**",
+        "**/bin/**",
+        "**/node_modules/**",
+      ],
+    },
   },
 });
