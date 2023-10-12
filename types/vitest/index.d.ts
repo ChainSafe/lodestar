d08b6e8401e159cd7cc7ceae391b8e6e29633d34
Nazar Hussain <nazarhussain@gmail.com>
2023-10-12 17:25:39 +0200

    Add custom matcher support in the vitest

---
+++ types/vitest/index.d.ts
@@ -0,0 +1,35 @@
+// eslint-disable-next-line import/no-extraneous-dependencies, @typescript-eslint/no-unused-vars
+import * as vitest from "vitest";
+
+interface CustomMatchers<R = unknown> {
+  toBeValidEpochCommittee(opts: {committeeCount: number; validatorsPerCommittee: number; slotsPerEpoch: number}): R;
+  /**
+   * @deprecated
+   * We highly recommend to not use this matcher instead use detail test case
+   * where you don't need message to explain assertion
+   *
+   * @example
+   * ```ts
+   * it("should work as expected", () => {
+   *   const a = 1;
+   *   const b = 2;
+   *   expect(a).toBeWithMessage(b, "a must be equal to b");
+   * });
+   * ```
+   * can be written as:
+   * ```ts
+   * it("a should always same as b", () => {
+   *   const a = 1;
+   *   const b = 2;
+   *   expect(a).toBe(b);
+   * });
+   * ```
+   * */
+  toBeWithMessage(expected: unknown, message: string): R;
+}
+
+declare module "vitest" {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  interface Assertion<T = any> extends CustomMatchers<T> {}
+  interface AsymmetricMatchersContaining extends CustomMatchers {}
+}
