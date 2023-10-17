adac370996278c8cbfaead6a15ca83f541fe76b7
naviechan <adrninistrator1@protonmail.com>
2023-10-17 19:01:13 +0800

    Update comment and semantic

--- packages/state-transition/src/util/deposit.ts
+++ packages/state-transition/src/util/deposit.ts
@@ -5,13 +5,15 @@ import {CachedBeaconStateAllForks, CachedBeaconStateEIP6110} from "../types.js";
 export function getEth1DepositCount(state: CachedBeaconStateAllForks): UintNum64 {
   if (state.config.getForkSeq(state.slot) >= ForkSeq.eip6110) {
     const eip6110State = state as CachedBeaconStateEIP6110;
+    // eth1DataIndexLimit = min(UintNum64, UintBn64) can be safely casted as UintNum64 
+    // since the result lies within upper and lower bound of UintNum64
     const eth1DataIndexLimit =
-      state.eth1Data.depositCount < eip6110State.depositReceiptsStartIndex
+      (state.eth1Data.depositCount < eip6110State.depositReceiptsStartIndex
         ? state.eth1Data.depositCount
-        : eip6110State.depositReceiptsStartIndex;
+        : eip6110State.depositReceiptsStartIndex) as UintNum64; 
 
     if (state.eth1DepositIndex < eth1DataIndexLimit) {
-      return Math.min(MAX_DEPOSITS, Number(eth1DataIndexLimit) - state.eth1DepositIndex); // eth1DataIndexLimit can be safely casted as number
+      return Math.min(MAX_DEPOSITS, eth1DataIndexLimit - state.eth1DepositIndex); 
     } else {
       return 0;
     }
