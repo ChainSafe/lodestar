cc5ffe401c23250d4d1b6c789b6c21128b0ab96b
naviechan <adrninistrator1@protonmail.com>
2023-10-24 20:32:11 +0800

    Update private description and lint

--- packages/beacon-node/src/eth1/provider/jwt.ts
+++ packages/beacon-node/src/eth1/provider/jwt.ts
@@ -4,7 +4,7 @@ import jwt from "jwt-simple";
 
 const {encode, decode} = jwt;
 
-/** 
+/**
  *  jwt token has iat which is issued at unix timestamp, an optional exp for expiry,
  *  an optional id as unique identifier, and an optional clv for client type/version
  */
