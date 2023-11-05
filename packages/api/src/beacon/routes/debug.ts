2d80b97ded17b72711e84505df0f217eeddb872b
Cayman <caymannava@gmail.com>
2023-11-05 02:21:44 -0500

    Add debug route definitions

--- packages/api/src/beacon/routes/debug.ts
+++ packages/api/src/beacon/routes/debug.ts
@@ -1,30 +1,27 @@
-import {ContainerType, ValueOf} from "@chainsafe/ssz";
-import {ForkName} from "@lodestar/params";
-import {allForks, Slot, RootHex, ssz, StringType} from "@lodestar/types";
+/* eslint-disable @typescript-eslint/naming-convention */
+import {ContainerType, Type, ValueOf} from "@chainsafe/ssz";
+import {allForks, ssz, StringType, phase0} from "@lodestar/types";
 import {
   ArrayOf,
-  ReturnTypes,
-  RoutesData,
-  Schema,
+  EmptyArgs,
+  EmptyGetRequestCodec,
+  EmptyMeta,
+  EmptyMetaCodec,
+  EmptyRequest,
+  ExecutionOptimisticAndVersionCodec,
+  ExecutionOptimisticAndVersionMeta,
+  ExecutionOptimisticCodec,
+  ExecutionOptimisticMeta,
   WithVersion,
-  TypeJson,
-  reqEmpty,
-  ReqSerializers,
-  ReqEmpty,
-  ReqSerializer,
-  ContainerDataExecutionOptimistic,
-  WithExecutionOptimistic,
-  ContainerData,
-} from "../../utils/index.js";
-import {HttpStatusCode} from "../../utils/client/httpStatusCode.js";
-import {parseAcceptHeader, writeAcceptHeader} from "../../utils/acceptHeader.js";
-import {ApiClientResponse, ResponseFormat} from "../../interfaces.js";
-import {ExecutionOptimistic, StateId} from "./beacon/state.js";
+} from "../../utils/codecs.js";
+import {Endpoint, RouteDefinitions} from "../../utils/types.js";
+import {Schema} from "../../utils/schema.js";
+import {StateId} from "./beacon/state.js";
 
 // See /packages/api/src/routes/index.ts for reasoning and instructions to add new routes
 
 const stringType = new StringType();
-const protoNodeSszType = new ContainerType(
+const ProtoNodeResponseType = new ContainerType(
   {
     executionPayloadBlockHash: stringType,
     executionPayloadNumber: ssz.UintNum64,
@@ -49,51 +46,65 @@ const protoNodeSszType = new ContainerType(
   },
   {jsonCase: "eth2"}
 );
+const SlotRootType = new ContainerType(
+  {
+    slot: ssz.Slot,
+    root: stringType,
+  },
+  {jsonCase: "eth2"}
+);
+const SlotRootExecutionOptimisticType = new ContainerType(
+  {
+    slot: ssz.Slot,
+    root: stringType,
+    executionOptimistic: ssz.Boolean,
+  },
+  {jsonCase: "eth2"}
+);
 
-type ProtoNodeApiType = ValueOf<typeof protoNodeSszType>;
+const ProtoNodeResponseListType = ArrayOf(ProtoNodeResponseType);
+const SlotRootListType = ArrayOf(SlotRootType);
+const SlotRootExecutionOptimisticListType = ArrayOf(SlotRootExecutionOptimisticType);
 
-export type Api = {
+type ProtoNodeResponseList = ValueOf<typeof ProtoNodeResponseListType>;
+type SlotRootList = ValueOf<typeof SlotRootListType>;
+type SlotRootExecutionOptimisticList = ValueOf<typeof SlotRootExecutionOptimisticListType>;
+
+export type Endpoints = {
   /**
    * Retrieves all possible chain heads (leaves of fork choice tree).
    */
-  getDebugChainHeads(): Promise<ApiClientResponse<{[HttpStatusCode.OK]: {data: {slot: Slot; root: RootHex}[]}}>>;
+  getDebugChainHeads: Endpoint<
+    //
+    "GET",
+    EmptyArgs,
+    EmptyRequest,
+    SlotRootList,
+    EmptyMeta
+  >;
 
   /**
    * Retrieves all possible chain heads (leaves of fork choice tree).
    */
-  getDebugChainHeadsV2(): Promise<
-    ApiClientResponse<{
-      [HttpStatusCode.OK]: {data: {slot: Slot; root: RootHex; executionOptimistic: ExecutionOptimistic}[]};
-    }>
+  getDebugChainHeadsV2: Endpoint<
+    //
+    "GET",
+    EmptyArgs,
+    EmptyRequest,
+    SlotRootExecutionOptimisticList,
+    EmptyMeta
   >;
 
   /**
    * Dump all ProtoArray's nodes to debug
    */
-  getProtoArrayNodes(): Promise<ApiClientResponse<{[HttpStatusCode.OK]: {data: ProtoNodeApiType[]}}>>;
-
-  /**
-   * Get full BeaconState object
-   * Returns full BeaconState object for given stateId.
-   * Depending on `Accept` header it can be returned either as json or as bytes serialized by SSZ
-   *
-   * @param stateId State identifier.
-   * Can be one of: "head" (canonical head in node's view), "genesis", "finalized", "justified", \<slot\>, \<hex encoded stateRoot with 0x prefix\>.
-   */
-  getState(
-    stateId: StateId,
-    format?: "json"
-  ): Promise<
-    ApiClientResponse<{[HttpStatusCode.OK]: {data: allForks.BeaconState; executionOptimistic: ExecutionOptimistic}}>
-  >;
-  getState(stateId: StateId, format: "ssz"): Promise<ApiClientResponse<{[HttpStatusCode.OK]: Uint8Array}>>;
-  getState(
-    stateId: StateId,
-    format?: ResponseFormat
-  ): Promise<
-    ApiClientResponse<{
-      [HttpStatusCode.OK]: Uint8Array | {data: allForks.BeaconState; executionOptimistic: ExecutionOptimistic};
-    }>
+  getProtoArrayNodes: Endpoint<
+    //
+    "GET",
+    EmptyArgs,
+    EmptyRequest,
+    ProtoNodeResponseList,
+    EmptyMeta
   >;
 
   /**
@@ -101,93 +112,92 @@ export type Api = {
    * Returns full BeaconState object for given stateId.
    * Depending on `Accept` header it can be returned either as json or as bytes serialized by SSZ
    *
-   * @param stateId State identifier.
+   * param stateId State identifier.
    * Can be one of: "head" (canonical head in node's view), "genesis", "finalized", "justified", \<slot\>, \<hex encoded stateRoot with 0x prefix\>.
    */
-  getStateV2(
-    stateId: StateId,
-    format?: "json"
-  ): Promise<
-    ApiClientResponse<{
-      [HttpStatusCode.OK]: {data: allForks.BeaconState; executionOptimistic: ExecutionOptimistic; version: ForkName};
-    }>
+  getState: Endpoint<
+    //
+    "GET",
+    {stateId: StateId},
+    {params: {state_id: string}},
+    phase0.BeaconState,
+    ExecutionOptimisticMeta
   >;
-  getStateV2(stateId: StateId, format: "ssz"): Promise<ApiClientResponse<{[HttpStatusCode.OK]: Uint8Array}>>;
-  getStateV2(
-    stateId: StateId,
-    format?: ResponseFormat
-  ): Promise<
-    ApiClientResponse<{
-      [HttpStatusCode.OK]:
-        | Uint8Array
-        | {data: allForks.BeaconState; executionOptimistic: ExecutionOptimistic; version: ForkName};
-    }>
+
+  /**
+   * Get full BeaconState object
+   * Returns full BeaconState object for given stateId.
+   * Depending on `Accept` header it can be returned either as json or as bytes serialized by SSZ
+   *
+   * param stateId State identifier.
+   * Can be one of: "head" (canonical head in node's view), "genesis", "finalized", "justified", \<slot\>, \<hex encoded stateRoot with 0x prefix\>.
+   */
+  getStateV2: Endpoint<
+    //
+    "GET",
+    {stateId: StateId},
+    {params: {state_id: string}},
+    allForks.BeaconState,
+    ExecutionOptimisticAndVersionMeta
   >;
 };
 
-export const routesData: RoutesData<Api> = {
-  getDebugChainHeads: {url: "/eth/v1/debug/beacon/heads", method: "GET"},
-  getDebugChainHeadsV2: {url: "/eth/v2/debug/beacon/heads", method: "GET"},
-  getProtoArrayNodes: {url: "/eth/v0/debug/forkchoice", method: "GET"},
-  getState: {url: "/eth/v1/debug/beacon/states/{state_id}", method: "GET"},
-  getStateV2: {url: "/eth/v2/debug/beacon/states/{state_id}", method: "GET"},
-};
-
-/* eslint-disable @typescript-eslint/naming-convention */
-
-export type ReqTypes = {
-  getDebugChainHeads: ReqEmpty;
-  getDebugChainHeadsV2: ReqEmpty;
-  getProtoArrayNodes: ReqEmpty;
-  getState: {params: {state_id: string}; headers: {accept?: string}};
-  getStateV2: {params: {state_id: string}; headers: {accept?: string}};
-};
-
-export function getReqSerializers(): ReqSerializers<Api, ReqTypes> {
-  const getState: ReqSerializer<Api["getState"], ReqTypes["getState"]> = {
-    writeReq: (state_id, format) => ({
-      params: {state_id: String(state_id)},
-      headers: {accept: writeAcceptHeader(format)},
-    }),
-    parseReq: ({params, headers}) => [params.state_id, parseAcceptHeader(headers.accept)],
-    schema: {params: {state_id: Schema.StringRequired}},
-  };
-
-  return {
-    getDebugChainHeads: reqEmpty,
-    getDebugChainHeadsV2: reqEmpty,
-    getProtoArrayNodes: reqEmpty,
-    getState: getState,
-    getStateV2: getState,
-  };
-}
-
-export function getReturnTypes(): ReturnTypes<Api> {
-  const SlotRoot = new ContainerType(
-    {
-      slot: ssz.Slot,
-      root: stringType,
+export const definitions: RouteDefinitions<Endpoints> = {
+  getDebugChainHeads: {
+    url: "/eth/v1/debug/beacon/heads",
+    method: "GET",
+    req: EmptyGetRequestCodec,
+    resp: {
+      data: SlotRootListType,
+      meta: EmptyMetaCodec,
     },
-    {jsonCase: "eth2"}
-  );
-
-  const SlotRootExecutionOptimistic = new ContainerType(
-    {
-      slot: ssz.Slot,
-      root: stringType,
-      executionOptimistic: ssz.Boolean,
+  },
+  getDebugChainHeadsV2: {
+    url: "/eth/v2/debug/beacon/heads",
+    method: "GET",
+    req: EmptyGetRequestCodec,
+    resp: {
+      data: SlotRootExecutionOptimisticListType,
+      meta: EmptyMetaCodec,
     },
-    {jsonCase: "eth2"}
-  );
-
-  return {
-    getDebugChainHeads: ContainerData(ArrayOf(SlotRoot)),
-    getDebugChainHeadsV2: ContainerData(ArrayOf(SlotRootExecutionOptimistic)),
-    getProtoArrayNodes: ContainerData(ArrayOf(protoNodeSszType)),
-    getState: ContainerDataExecutionOptimistic(ssz.phase0.BeaconState),
-    getStateV2: WithExecutionOptimistic(
-      // Teku returns fork as UPPERCASE
-      WithVersion((fork: ForkName) => ssz[fork.toLowerCase() as ForkName].BeaconState as TypeJson<allForks.BeaconState>)
-    ),
-  };
-}
+  },
+  getProtoArrayNodes: {
+    url: "/eth/v0/debug/forkchoice",
+    method: "GET",
+    req: EmptyGetRequestCodec,
+    resp: {
+      data: ProtoNodeResponseListType,
+      meta: EmptyMetaCodec,
+    },
+  },
+  getState: {
+    url: "/eth/v1/debug/beacon/states/{state_id}",
+    method: "GET",
+    req: {
+      writeReq: ({stateId}) => ({params: {state_id: String(stateId)}}),
+      parseReq: ({params}) => ({stateId: params.state_id}),
+      schema: {
+        params: {state_id: Schema.StringRequired},
+      },
+    },
+    resp: {
+      data: ssz.phase0.BeaconState,
+      meta: ExecutionOptimisticCodec,
+    },
+  },
+  getStateV2: {
+    url: "/eth/v2/debug/beacon/states/{state_id}",
+    method: "GET",
+    req: {
+      writeReq: ({stateId}) => ({params: {state_id: String(stateId)}}),
+      parseReq: ({params}) => ({stateId: params.state_id}),
+      schema: {
+        params: {state_id: Schema.StringRequired},
+      },
+    },
+    resp: {
+      data: WithVersion((fork) => ssz[fork].BeaconState as Type<allForks.BeaconState>),
+      meta: ExecutionOptimisticAndVersionCodec,
+    },
+  },
+};
