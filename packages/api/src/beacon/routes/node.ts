1b556ff3502f2a4b2495a3e2bde5813ebdaad222
Cayman <caymannava@gmail.com>
2023-11-20 11:45:46 -0500

    Add node route definitions

--- packages/api/src/beacon/routes/node.ts
+++ packages/api/src/beacon/routes/node.ts
@@ -1,21 +1,48 @@
-import {ContainerType} from "@chainsafe/ssz";
-import {allForks, ssz, StringType} from "@lodestar/types";
+/* eslint-disable @typescript-eslint/naming-convention */
+import {ByteListType, ContainerType, OptionalType} from "@chainsafe/ssz";
+import {allForks, altair, ssz} from "@lodestar/types";
+import {AnyGetEndpoint, Endpoint, ResponseCodec, RouteDefinitions, Schema} from "../../utils/index.js";
 import {
   ArrayOf,
-  reqEmpty,
-  jsonType,
-  ReturnTypes,
-  RoutesData,
-  Schema,
-  ReqSerializers,
-  ReqEmpty,
-  ContainerData,
-} from "../../utils/index.js";
-import {HttpStatusCode} from "../../utils/client/httpStatusCode.js";
-import {ApiClientResponse} from "../../interfaces.js";
+  EmptyArgs,
+  EmptyGetRequestCodec,
+  EmptyMeta,
+  EmptyMetaCodec,
+  EmptyRequest,
+  EmptyResponseCodec,
+  EmptyResponseData,
+} from "../../utils/codecs.js";
+import {HttpStatusCode} from "../../utils/httpStatusCode.js";
+import {WireFormat} from "../../utils/headers.js";
 
 // See /packages/api/src/routes/index.ts for reasoning and instructions to add new routes
 
+export const byteListType = new ByteListType(100);
+export const NetworkIdentityType = new ContainerType(
+  {
+    peerId: byteListType,
+    enr: byteListType,
+    p2pAddresses: ArrayOf(byteListType),
+    discoveryAddresses: ArrayOf(byteListType),
+    metadata: new ContainerType({
+      ...ssz.phase0.Metadata.fields,
+      syncnets: new OptionalType(ssz.altair.Metadata.fields.syncnets),
+    }),
+  },
+  {jsonCase: "eth2"}
+);
+
+export const PeerCountType = new ContainerType(
+  {
+    disconnected: ssz.UintNum64,
+    connecting: ssz.UintNum64,
+    connected: ssz.UintNum64,
+    disconnecting: ssz.UintNum64,
+  },
+  {jsonCase: "eth2"}
+);
+
+// Differs from the ssz type because each property is string-encoded
 export type NetworkIdentity = {
   /** Cryptographic hash of a peer’s public key. [Read more](https://docs.libp2p.io/concepts/peer-id/) */
   peerId: string;
@@ -32,11 +59,10 @@ export type PeerDirection = "inbound" | "outbound";
 
 export type NodePeer = {
   peerId: string;
-  enr: string;
+  enr?: string;
   lastSeenP2pAddress: string;
   state: PeerState;
-  // the spec does not specify direction for a disconnected peer, lodestar uses null in that case
-  direction: PeerDirection | null;
+  direction: PeerDirection;
 };
 
 export type PeerCount = {
@@ -77,148 +103,257 @@ export type NodeHealthOptions = {
 /**
  * Read information about the beacon node.
  */
-export type Api = {
+export type Endpoints = {
   /**
    * Get node network identity
    * Retrieves data about the node's network presence
    */
-  getNetworkIdentity: () => Promise<ApiClientResponse<{[HttpStatusCode.OK]: {data: NetworkIdentity}}>>;
+  getNetworkIdentity: Endpoint<
+    //
+    "GET",
+    EmptyArgs,
+    EmptyRequest,
+    NetworkIdentity,
+    EmptyMeta
+  >;
+
   /**
    * Get node network peers
    * Retrieves data about the node's network peers. By default this returns all peers. Multiple query params are combined using AND conditions
-   * @param state
-   * @param direction
    */
-  getPeers(
-    filters?: FilterGetPeers
-  ): Promise<ApiClientResponse<{[HttpStatusCode.OK]: {data: NodePeer[]; meta: {count: number}}}>>;
+  getPeers: Endpoint<
+    "GET",
+    FilterGetPeers,
+    {query: {state?: PeerState[]; direction?: PeerDirection[]}},
+    NodePeer[],
+    {count: number}
+  >;
+
   /**
    * Get peer
    * Retrieves data about the given peer
-   * @param peerId
    */
-  getPeer(
-    peerId: string
-  ): Promise<
-    ApiClientResponse<{[HttpStatusCode.OK]: {data: NodePeer}}, HttpStatusCode.BAD_REQUEST | HttpStatusCode.NOT_FOUND>
+  getPeer: Endpoint<
+    //
+    "GET",
+    {peerId: string},
+    {params: {peer_id: string}},
+    NodePeer,
+    EmptyMeta
   >;
 
   /**
    * Get peer count
    * Retrieves number of known peers.
    */
-  getPeerCount(): Promise<ApiClientResponse<{[HttpStatusCode.OK]: {data: PeerCount}}>>;
+  getPeerCount: Endpoint<
+    //
+    "GET",
+    EmptyArgs,
+    EmptyRequest,
+    PeerCount,
+    EmptyMeta
+  >;
 
   /**
    * Get version string of the running beacon node.
    * Requests that the beacon node identify information about its implementation in a format similar to a [HTTP User-Agent](https://tools.ietf.org/html/rfc7231#section-5.5.3) field.
    */
-  getNodeVersion(): Promise<ApiClientResponse<{[HttpStatusCode.OK]: {data: {version: string}}}>>;
+  getNodeVersion: Endpoint<
+    //
+    "GET",
+    EmptyArgs,
+    EmptyRequest,
+    {version: string},
+    EmptyMeta
+  >;
 
   /**
    * Get node syncing status
    * Requests the beacon node to describe if it's currently syncing or not, and if it is, what block it is up to.
    */
-  getSyncingStatus(): Promise<ApiClientResponse<{[HttpStatusCode.OK]: {data: SyncingStatus}}>>;
+  getSyncingStatus: Endpoint<
+    //
+    "GET",
+    EmptyArgs,
+    EmptyRequest,
+    SyncingStatus,
+    EmptyMeta
+  >;
 
   /**
    * Get health check
    * Returns node health status in http status codes. Useful for load balancers.
    */
-  getHealth(
-    options?: NodeHealthOptions
-  ): Promise<
-    ApiClientResponse<
-      {[HttpStatusCode.OK]: void; [HttpStatusCode.PARTIAL_CONTENT]: void},
-      HttpStatusCode.BAD_REQUEST | HttpStatusCode.SERVICE_UNAVAILABLE
-    >
+  getHealth: Endpoint<
+    //
+    "GET",
+    NodeHealthOptions,
+    {query: {syncing_status?: number}},
+    EmptyResponseData,
+    EmptyMeta
   >;
 };
 
-export const routesData: RoutesData<Api> = {
-  getNetworkIdentity: {url: "/eth/v1/node/identity", method: "GET"},
-  getPeers: {url: "/eth/v1/node/peers", method: "GET"},
-  getPeer: {url: "/eth/v1/node/peers/{peer_id}", method: "GET"},
-  getPeerCount: {url: "/eth/v1/node/peer_count", method: "GET"},
-  getNodeVersion: {url: "/eth/v1/node/version", method: "GET"},
-  getSyncingStatus: {url: "/eth/v1/node/syncing", method: "GET"},
-  getHealth: {url: "/eth/v1/node/health", method: "GET"},
-};
-
-/* eslint-disable @typescript-eslint/naming-convention */
-
-export type ReqTypes = {
-  getNetworkIdentity: ReqEmpty;
-  getPeers: {query: {state?: PeerState[]; direction?: PeerDirection[]}};
-  getPeer: {params: {peer_id: string}};
-  getPeerCount: ReqEmpty;
-  getNodeVersion: ReqEmpty;
-  getSyncingStatus: ReqEmpty;
-  getHealth: {query: {syncing_status?: number}};
-};
-
-export function getReqSerializers(): ReqSerializers<Api, ReqTypes> {
-  return {
-    getNetworkIdentity: reqEmpty,
-
-    getPeers: {
-      writeReq: (filters) => ({query: filters || {}}),
-      parseReq: ({query}) => [query],
+export const definitions: RouteDefinitions<Endpoints> = {
+  getNetworkIdentity: {
+    url: "/eth/v1/node/identity",
+    method: "GET",
+    req: EmptyGetRequestCodec,
+    resp: {
+      onlySupport: WireFormat.json,
+      data: {
+        toJson: (data) => ({
+          peerId: data.peerId,
+          enr: data.enr,
+          p2pAddresses: data.p2pAddresses,
+          discoveryAddresses: data.discoveryAddresses,
+          metadata: ssz.altair.Metadata.toJson(data.metadata as altair.Metadata),
+        }),
+        // TODO validation
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        fromJson: (data: any) => ({
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+          peerId: data.peerId,
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+          enr: data.enr,
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+          p2pAddresses: data.p2pAddresses,
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+          discoveryAddresses: data.discoveryAddresses,
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+          metadata: ssz.altair.Metadata.fromJson(data.metadata),
+        }),
+        serialize: () => {
+          throw new Error("Not implemented");
+        },
+        deserialize: () => {
+          throw new Error("Not implemented");
+        },
+      },
+      meta: EmptyMetaCodec,
+    },
+  },
+  getPeers: {
+    url: "/eth/v1/node/peers",
+    method: "GET",
+    req: {
+      writeReq: ({state, direction}) => ({query: {state, direction}}),
+      parseReq: ({query}) => ({state: query.state, direction: query.direction}),
       schema: {query: {state: Schema.StringArray, direction: Schema.StringArray}},
     },
-    getPeer: {
-      writeReq: (peer_id) => ({params: {peer_id}}),
-      parseReq: ({params}) => [params.peer_id],
+    resp: {
+      onlySupport: WireFormat.json,
+      data: {
+        toJson: (data) => data,
+        fromJson: (data) => data as NodePeer[],
+        serialize: () => {
+          throw new Error("Not implemented");
+        },
+        deserialize: () => {
+          throw new Error("Not implemented");
+        },
+      },
+      meta: {
+        toJson: (d) => d,
+        fromJson: (d) => d as {count: number},
+        toHeadersObject: () => {
+          throw new Error("Not implemented");
+        },
+        fromHeaders: () => {
+          throw new Error("Not implemented");
+        },
+      },
+    },
+  },
+  getPeer: {
+    url: "/eth/v1/node/peers/{peer_id}",
+    method: "GET",
+    req: {
+      writeReq: ({peerId}) => ({params: {peer_id: peerId}}),
+      parseReq: ({params}) => ({peerId: params.peer_id}),
       schema: {params: {peer_id: Schema.StringRequired}},
     },
-
-    getPeerCount: reqEmpty,
-    getNodeVersion: reqEmpty,
-    getSyncingStatus: reqEmpty,
-    getHealth: {
-      writeReq: (options) => ({
-        query: options?.syncingStatus !== undefined ? {syncing_status: options.syncingStatus} : {},
-      }),
-      parseReq: ({query}) => [{syncingStatus: query.syncing_status}],
+    resp: {
+      onlySupport: WireFormat.json,
+      data: {
+        toJson: (data) => data,
+        fromJson: (data) => data as NodePeer,
+        serialize: () => {
+          throw new Error("Not implemented");
+        },
+        deserialize: () => {
+          throw new Error("Not implemented");
+        },
+      },
+      meta: EmptyMetaCodec,
+    },
+  },
+  getPeerCount: {
+    url: "/eth/v1/node/peer_count",
+    method: "GET",
+    req: EmptyGetRequestCodec,
+    resp: {
+      onlySupport: WireFormat.json,
+      data: {
+        toJson: (data) => data,
+        fromJson: (data) => data as PeerCount,
+        serialize: () => {
+          throw new Error("Not implemented");
+        },
+        deserialize: () => {
+          throw new Error("Not implemented");
+        },
+      },
+      meta: EmptyMetaCodec,
+    },
+  },
+  getNodeVersion: {
+    url: "/eth/v1/node/version",
+    method: "GET",
+    req: EmptyGetRequestCodec,
+    resp: {
+      onlySupport: WireFormat.json,
+      data: {
+        toJson: (data) => data,
+        fromJson: (data) => data as {version: string},
+        serialize: () => {
+          throw new Error("Not implemented");
+        },
+        deserialize: () => {
+          throw new Error("Not implemented");
+        },
+      },
+      meta: EmptyMetaCodec,
+    },
+  },
+  getSyncingStatus: {
+    url: "/eth/v1/node/syncing",
+    method: "GET",
+    req: EmptyGetRequestCodec,
+    resp: {
+      onlySupport: WireFormat.json,
+      data: {
+        toJson: (data) => data,
+        fromJson: (data) => data as SyncingStatus,
+        serialize: () => {
+          throw new Error("Not implemented");
+        },
+        deserialize: () => {
+          throw new Error("Not implemented");
+        },
+      },
+      meta: EmptyMetaCodec,
+    },
+  },
+  getHealth: {
+    url: "/eth/v1/node/health",
+    method: "GET",
+    req: {
+      writeReq: ({syncingStatus}) => ({query: {syncing_status: syncingStatus}}),
+      parseReq: ({query}) => ({syncingStatus: query.syncing_status}),
       schema: {query: {syncing_status: Schema.Uint}},
     },
-  };
-}
-
-export function getReturnTypes(): ReturnTypes<Api> {
-  const stringType = new StringType();
-  const NetworkIdentity = new ContainerType(
-    {
-      peerId: stringType,
-      enr: stringType,
-      p2pAddresses: ArrayOf(stringType),
-      discoveryAddresses: ArrayOf(stringType),
-      metadata: ssz.altair.Metadata,
-    },
-    {jsonCase: "eth2"}
-  );
-
-  const PeerCount = new ContainerType(
-    {
-      disconnected: ssz.UintNum64,
-      connecting: ssz.UintNum64,
-      connected: ssz.UintNum64,
-      disconnecting: ssz.UintNum64,
-    },
-    {jsonCase: "eth2"}
-  );
-
-  return {
-    //
-    // TODO: Consider just converting the JSON case without custom types
-    //
-    getNetworkIdentity: ContainerData(NetworkIdentity),
-    // All these types don't contain any BigInt nor Buffer instances.
-    // Use jsonType() to translate the casing in a generic way.
-    getPeers: jsonType("snake"),
-    getPeer: jsonType("snake"),
-    getPeerCount: ContainerData(PeerCount),
-    getNodeVersion: jsonType("snake"),
-    getSyncingStatus: jsonType("snake"),
-  };
-}
+    resp: EmptyResponseCodec as ResponseCodec<AnyGetEndpoint>,
+  },
+};
