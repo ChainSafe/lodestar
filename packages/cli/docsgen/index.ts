6d958dabb7f8f6dabbecab1295694ac9343a5afa
Matthew Keil <me@matthewkeil.com>
2023-11-28 02:58:03 -0500

    fix: bug in build:docs if folder not present

--- packages/cli/docsgen/index.ts
+++ packages/cli/docsgen/index.ts
@@ -20,5 +20,6 @@ const DOCS_PAGES_FOLDER = path.join(dirname, "..", "..", "..", "docs", "pages");
 for (const cmd of cmds) {
   const docstring = renderCommandPage(cmd, globalOptions, LODESTAR_COMMAND);
   const folder = path.join(DOCS_PAGES_FOLDER, cmd.docsFolder ?? "");
+  if (!fs.existsSync(folder)) fs.mkdirSync(folder, {recursive: true});
   fs.writeFileSync(path.join(folder, `${cmd.command}-cli.md`), docstring);
 }
