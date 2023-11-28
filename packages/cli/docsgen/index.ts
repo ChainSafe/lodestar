1953bd05b63bb70988eb70a14aca6ffc05353ba2
Matthew Keil <me@matthewkeil.com>
2023-11-28 01:57:42 -0500

    feat: remove old comments

--- packages/cli/docsgen/index.ts
+++ packages/cli/docsgen/index.ts
@@ -8,9 +8,9 @@ import {renderCommandPage} from "./markdown.js";
 // Outputs a markdown format ready to be consumed by mkdocs
 //
 // Usage:
-// ts-node docsgen docs/cli.md
+// ts-node packages/cli/docsgen
 //
-// After generation the resulting .md should be mv to the path expected
+// After generation the resulting .md files, they are written to the path expected
 // by the mkdocs index and other existing paths in the documentation
 
 const dirname = path.dirname(new URL(import.meta.url).pathname);
@@ -20,31 +20,5 @@ const DOCS_PAGES_FOLDER = path.join(dirname, "..", "..", "..", "docs", "pages");
 for (const cmd of cmds) {
   const docstring = renderCommandPage(cmd, globalOptions, LODESTAR_COMMAND);
   const folder = path.join(DOCS_PAGES_FOLDER, cmd.docsFolder ?? "");
-  // fs.mkdirSync(folder, {recursive: true});
   fs.writeFileSync(path.join(folder, `${cmd.command}-cli.md`), docstring);
 }
-
-// /**
-//  * Render a Yargs options dictionary to a markdown table
-//  */
-// function getOptionsTable(
-//   options: Record<string, CliOptionDefinition>,
-//   {showHidden}: {showHidden?: boolean} = {}
-// ): string {
-//   const visibleOptions = Object.entries(options).filter(([, opt]) => showHidden || !opt.hidden);
-
-//   if (visibleOptions.length === 0) {
-//     return "";
-//   }
-
-//   /* eslint-disable @typescript-eslint/naming-convention */
-//   return toMarkdownTable(
-//     visibleOptions.map(([key, opt]) => ({
-//       Option: `\`--${key}\``,
-//       Type: opt.type ?? "",
-//       Description: opt.description ?? "",
-//       Default: String(opt.defaultDescription || opt.default || ""),
-//     })),
-//     ["Option", "Type", "Description", "Default"]
-//   );
-// }
