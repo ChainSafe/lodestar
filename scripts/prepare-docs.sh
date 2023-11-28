909a84ae07ec3f40d045c1ca283a155019c0de3d
Matthew Keil <me@matthewkeil.com>
2023-11-28 09:43:16 -0500

    feat: check README ts examples and cop

--- scripts/prepare-docs.sh
+++ scripts/prepare-docs.sh
@@ -5,8 +5,12 @@ DOCS_DIR=docs
 # exit when any command fails
 set -e
 
-# Copy contributing doc
+# Copy contributing docs
 cp CONTRIBUTING.md $DOCS_DIR/pages/contribution/getting-started.md
+
+# Copy package README.md to docs
+cp -r packages/light-client/README.md $DOCS_DIR/pages/lightclient-prover/lightclient.md
+cp -r packages/prover/README.md $DOCS_DIR/pages/lightclient-prover/prover.md
 
 # Copy visual assets
 rm -rf $DOCS_DIR/pages/assets
