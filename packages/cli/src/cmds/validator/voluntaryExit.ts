21caba1a31d414bfcde5a30b81270c060aef83e3
Luca Winter <70237279+eth2353@users.noreply.github.com>
2023-11-30 13:07:09 +0100

    Update example command
    Co-authored-by: Nico Flaig <nflaig@protonmail.com>

--- packages/cli/src/cmds/validator/voluntaryExit.ts
+++ packages/cli/src/cmds/validator/voluntaryExit.ts
@@ -39,7 +39,7 @@ If no `pubkeys` are provided, it will exit all validators that have been importe
     },
     {
       command:
-        "validator voluntary-exit --network goerli --externalSigner.url=http://signer:9000 --externalSigner.pubkeys 0xF00",
+        "validator voluntary-exit --network goerli --externalSigner.url http://signer:9000 --externalSigner.fetch --pubkeys 0xF00",
       description:
         "Perform a voluntary exit for the validator who has a public key 0xF00 and its secret key is on a remote signer",
     },
