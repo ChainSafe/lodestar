549f859a92d9750b2c06bac50b9ae32109eb1a40
Nico Flaig <nflaig@protonmail.com>
2023-12-02 11:05:38 +0100

    fix: set consensus version header in state and block response

--- packages/api/src/beacon/server/beacon.ts
+++ packages/api/src/beacon/server/beacon.ts
@@ -1,4 +1,5 @@
 import {ChainForkConfig} from "@lodestar/config";
+import {ssz} from "@lodestar/types";
 import {Api, ReqTypes, routesData, getReturnTypes, getReqSerializers} from "../routes/beacon/index.js";
 import {ServerRoutes, getGenericJsonServer} from "../../utils/server/index.js";
 import {ServerApi} from "../../interfaces.js";
@@ -30,15 +31,35 @@ export function getRoutes(config: ChainForkConfig, api: ServerApi<Api>): ServerR
     },
     getBlockV2: {
       ...serverRoutes.getBlockV2,
-      handler: async (req) => {
+      handler: async (req, res) => {
         const response = await api.getBlockV2(...reqSerializers.getBlockV2.parseReq(req));
         if (response instanceof Uint8Array) {
+          const slot = extractSlotFromBlockBytes(response);
+          const version = config.getForkName(slot);
+          void res.header("Eth-Consensus-Version", version);
           // Fastify 3.x.x will automatically add header `Content-Type: application/octet-stream` if Buffer
           return Buffer.from(response);
         } else {
+          void res.header("Eth-Consensus-Version", response.version);
           return returnTypes.getBlockV2.toJson(response);
         }
       },
     },
   };
+}
+
+function extractSlotFromBlockBytes(block: Uint8Array): number {
+  const {signature} = ssz.phase0.SignedBeaconBlock.fields;
+  /**
+   * class SignedBeaconBlock(Container):
+   *   message: BeaconBlock                 [offset - 4 bytes]
+   *   signature: BLSSignature              [fixed - 96 bytes]
+   *
+   * class BeaconBlock(Container):
+   *   slot: Slot                           [fixed - 8 bytes]
+   *   ...
+   */
+  const offset = 4 + signature.lengthBytes;
+  const bytes = block.subarray(offset, offset + ssz.Slot.byteLength);
+  return ssz.Slot.deserialize(bytes);
 }
