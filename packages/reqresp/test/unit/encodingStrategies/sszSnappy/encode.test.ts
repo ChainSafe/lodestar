6e6b061b197254fab1451e878ea1696ea7d0f82b
Nazar Hussain <nazarhussain@gmail.com>
2023-12-14 12:11:49 +0100

    Fix the types

--- packages/reqresp/test/unit/encodingStrategies/sszSnappy/encode.test.ts
+++ packages/reqresp/test/unit/encodingStrategies/sszSnappy/encode.test.ts
@@ -8,7 +8,7 @@ import {expectEqualByteChunks} from "../../../utils/index.js";
 
 describe("encodingStrategies / sszSnappy / encode", () => {
   it.each(encodingStrategiesTestCases)("$id", async ({binaryPayload, chunks}) => {
-    const encodedChunks = await pipe(writeSszSnappyPayload(binaryPayload.data), all);
+    const encodedChunks = await pipe(writeSszSnappyPayload(Buffer.from(binaryPayload.data)), all);
     expectEqualByteChunks(
       encodedChunks as Uint8Array[],
       chunks.map((c) => c.subarray())
@@ -19,7 +19,7 @@ describe("encodingStrategies / sszSnappy / encode", () => {
     it.each(encodingStrategiesMainnetTestCases)("$id", async ({payload, streamedBody}) => {
       const bodySize = payload.data.length;
 
-      const encodedChunks = await pipe(writeSszSnappyPayload(payload.data), all);
+      const encodedChunks = await pipe(writeSszSnappyPayload(Buffer.from(payload.data)), all);
       const encodedStream = Buffer.concat(encodedChunks as Uint8Array[]);
       const expectedStreamed = Buffer.concat([Buffer.from(varintEncode(bodySize)), streamedBody]);
       expect(encodedStream).toEqual(expectedStreamed);
