7e85f1000c83384be60a88d39c353a9a9a5218a4
tuyennhv <tuyen@chainsafe.io>
2023-12-19 21:05:09 +0700

    fix: track jobItemWorkReq function time (#6203)
    * fix: track jobItemWorkReq function time
    
    * docs: add panel for BLS jobItemWorkReq metrics

--- packages/beacon-node/src/metrics/metrics/lodestar.ts
+++ packages/beacon-node/src/metrics/metrics/lodestar.ts
@@ -460,9 +460,15 @@ export function createLodestarMetrics(
         name: "lodestar_bls_thread_pool_batchable_sig_sets_total",
         help: "Count of total batchable signature sets",
       }),
-      signatureDeserializationMainThreadDuration: register.gauge({
+      signatureDeserializationMainThreadDuration: register.histogram({
         name: "lodestar_bls_thread_pool_signature_deserialization_main_thread_time_seconds",
         help: "Total time spent deserializing signatures on main thread",
+        buckets: [0.001, 0.005, 0.01, 0.1],
+      }),
+      pubkeysAggregationMainThreadDuration: register.histogram({
+        name: "lodestar_bls_thread_pool_pubkeys_aggregation_main_thread_time_seconds",
+        help: "Total time spent aggregating pubkeys on main thread",
+        buckets: [0.001, 0.005, 0.01, 0.1],
       }),
     },
 
