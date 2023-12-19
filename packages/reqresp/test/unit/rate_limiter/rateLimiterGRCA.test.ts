a164aa8131376e07f946e7c0ccf3381198308067
Nazar Hussain <nazarhussain@gmail.com>
2023-12-19 15:01:41 +0100

    test: migrate reqresp unit tests to vitest (#6191)
    * Migrate reqresp unit tests to vitest
    
    * Fix the build step
    
    * Fix the types
    
    * Fix the source changes
    
    * Remove the  usage in src

--- packages/reqresp/test/unit/rate_limiter/rateLimiterGRCA.test.ts
+++ packages/reqresp/test/unit/rate_limiter/rateLimiterGRCA.test.ts
@@ -1,59 +1,58 @@
-import {expect} from "chai";
-import sinon from "sinon";
+import {describe, it, expect, vi, beforeEach, afterEach} from "vitest";
 import {RateLimiterGRCA} from "../../../src/rate_limiter/rateLimiterGRCA.js";
 
 describe("rateLimiterGRCA", () => {
   let rateLimiter: RateLimiterGRCA<null>;
   const limit = 500;
   const limitTimeMs = 60 * 1000; // 1 min
-  const sandbox = sinon.createSandbox();
 
   beforeEach(() => {
-    sandbox.useFakeTimers();
+    vi.useFakeTimers();
     rateLimiter = RateLimiterGRCA.fromQuota({quotaTimeMs: limitTimeMs, quota: limit});
   });
 
   afterEach(() => {
-    sandbox.restore();
+    vi.restoreAllMocks();
+    vi.useRealTimers();
   });
 
   describe("allows()", () => {
     it("should throw error if requested for a zero value", () => {
-      expect(() => rateLimiter.allows(null, 0)).to.throw("Token value should always be positive. Given: 0");
+      expect(() => rateLimiter.allows(null, 0)).toThrow("Token value should always be positive. Given: 0");
     });
 
     it("should throw error if requested for a negative value", () => {
-      expect(() => rateLimiter.allows(null, -1)).to.throw("Token value should always be positive. Given: -1");
+      expect(() => rateLimiter.allows(null, -1)).toThrow("Token value should always be positive. Given: -1");
     });
 
     it("should return valid number of requests within request window", () => {
-      expect(rateLimiter.allows(null, 10)).to.be.true;
-      expect(rateLimiter.allows(null, 50)).to.be.true;
+      expect(rateLimiter.allows(null, 10)).toBe(true);
+      expect(rateLimiter.allows(null, 50)).toBe(true);
     });
 
     it("should return valid number of requests within request window for maximum requests", () => {
-      expect(rateLimiter.allows(null, limit)).to.be.true;
+      expect(rateLimiter.allows(null, limit)).toBe(true);
     });
 
     it("should return zero within request window for higher number of requests", () => {
-      expect(rateLimiter.allows(null, limit + 1)).to.be.false;
+      expect(rateLimiter.allows(null, limit + 1)).toBe(false);
     });
 
     it("should return zero once the tracker limit reached", () => {
       rateLimiter.allows(null, limit);
-      expect(rateLimiter.allows(null, 10)).to.be.false;
+      expect(rateLimiter.allows(null, 10)).toBe(false);
     });
 
     it("should return over limit values before limit reached", () => {
       rateLimiter.allows(null, limit - 10);
-      expect(rateLimiter.allows(null, 15)).to.be.false;
+      expect(rateLimiter.allows(null, 15)).toBe(false);
     });
 
     it("should reset the rate after the time limit", () => {
       rateLimiter.allows(null, limit);
-      expect(rateLimiter.allows(null, 10)).to.be.false;
-      sandbox.clock.tick(limitTimeMs);
-      expect(rateLimiter.allows(null, 10)).to.be.true;
+      expect(rateLimiter.allows(null, 10)).toBe(false);
+      vi.advanceTimersByTime(limitTimeMs);
+      expect(rateLimiter.allows(null, 10)).toBe(true);
     });
   });
 
