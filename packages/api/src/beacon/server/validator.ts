621f4f6b9f235dfad154795f47aed18806ba282b
NC <adrninistrator1@protonmail.com>
2023-12-22 14:59:39 +0800

    Add `Eth-Consensus-Version` to produceBlockV3

--- packages/api/src/beacon/server/validator.ts
+++ packages/api/src/beacon/server/validator.ts
@@ -4,6 +4,24 @@ import {ServerRoutes, getGenericJsonServer} from "../../utils/server/index.js";
 import {ServerApi} from "../../interfaces.js";
 
 export function getRoutes(config: ChainForkConfig, api: ServerApi<Api>): ServerRoutes<Api, ReqTypes> {
-  // All routes return JSON, use a server auto-generator
-  return getGenericJsonServer<ServerApi<Api>, ReqTypes>({routesData, getReturnTypes, getReqSerializers}, config, api);
+  const reqSerializers = getReqSerializers();
+  const returnTypes = getReturnTypes();
+
+  // Most of routes return JSON, use a server auto-generator
+  const serverRoutes = getGenericJsonServer<ServerApi<Api>, ReqTypes>(
+    {routesData, getReturnTypes, getReqSerializers},
+    config,
+    api
+  );
+  return {
+    ...serverRoutes,
+    produceBlockV3: {
+      ...serverRoutes.produceBlockV3,
+      handler: async (req, res) => {
+        const response = await api.produceBlockV3(...reqSerializers.produceBlockV3.parseReq(req));
+        res.header("Eth-Consensus-Version", response.version);
+        return returnTypes.produceBlockV3.toJson(response);
+      }
+    },
+  };
 }
