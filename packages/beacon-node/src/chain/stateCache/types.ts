073e2a7d7d48bc8fc174402f8fe92bab7eb5a87c
Cayman <caymannava@gmail.com>
2024-01-03 10:55:22 -0500

    chore: tweak comments

--- packages/beacon-node/src/chain/stateCache/types.ts
+++ packages/beacon-node/src/chain/stateCache/types.ts
@@ -5,7 +5,19 @@ import {routes} from "@lodestar/api";
 export type CheckpointHex = {epoch: Epoch; rootHex: RootHex};
 
 /**
+ * Lodestar currently keeps two state caches around.
+ *
+ * 1. BlockStateCache is keyed by state root, and intended to keep extremely recent states around (eg: post states from the latest blocks)
+ *    These states are most likely to be useful for state transition of new blocks.
+ *
+ * 2. CheckpointStateCache is keyed by checkpoint, and intended to keep states which have just undergone an epoch transition.
+ *    These states are useful for gossip verification and for avoiding an epoch transition during state transition of first-in-epoch blocks
+ */
+
+/**
  * Store up to n recent block states.
+ *
+ * The cache key is state root
  */
 export interface BlockStateCache {
   get(rootHex: RootHex): CachedBeaconStateAllForks | null;
@@ -19,20 +31,25 @@ export interface BlockStateCache {
 }
 
 /**
- * Store checkpoint states to preserve epoch transition, this helps lodestar run exactly 1 epoch transition per epoch
+ * Store checkpoint states to preserve epoch transition, this helps lodestar run exactly 1 epoch transition per epoch in normal network conditions.
+ *
  * There are 2 types of checkpoint states:
  *
- * - Previous Root Checkpoint State where root is from previous epoch, this is added when we prepare for next slot,
+ * - Previous Root Checkpoint State: where root is from previous epoch, this is added when we prepare for next slot,
  * or to validate gossip block
+ * ```
  *        epoch:       (n-2)   (n-1)     n     (n+1)
  *               |-------|-------|-------|-------|
  *        root     ---------------------^
+ * ```
  *
  * - Current Root Checkpoint State: this is added when we process block slot 0 of epoch n, note that this block could
  * be skipped so we don't always have this checkpoint state
+ * ```
  *        epoch:       (n-2)   (n-1)     n     (n+1)
  *               |-------|-------|-------|-------|
  *        root      ---------------------^
+ * ```
  */
 export interface CheckpointStateCache {
   init?: () => Promise<void>;
