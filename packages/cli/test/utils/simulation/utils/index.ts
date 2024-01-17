912ec0931c137f7df4086d4b9331f8839e8b3199
Nazar Hussain <nazarhussain@gmail.com>
2024-01-17 19:48:57 +0100

    test: stabilize sim tests  (#6312)
    * Fix sim test genesis delay
    
    * Update the defaults for the all sim tests
    
    * Increase the limit for slotImportTolerance for unknown block sync
    
    * Update the additional slots for TTD
    
    * Adjust the ttd time
    
    * Adjust the ttd
    
    * Adjust the ttd
    
    * Update the ttd calculation
    
    * Add timestamp to report
    
    * Update the slot to check the merge transition
    
    * Update ttd calculations
    
    * Increase the genesis delay
    
    * Update the shangai ttd calculation
    
    * Increase the slot tolerance
    
    * Disable broadcast validation for unknown block tests
    
    * Fix lint errors

--- packages/cli/test/utils/simulation/utils/index.ts
+++ packages/cli/test/utils/simulation/utils/index.ts
@@ -1,7 +1,14 @@
 /* eslint-disable no-console */
 import {activePreset} from "@lodestar/params";
 import {Epoch} from "@lodestar/types";
-import {ETH_TTD_INCREMENT} from "../constants.js";
+import {ChainConfig, ChainForkConfig, createChainForkConfig} from "@lodestar/config";
+import {
+  CLIQUE_SEALING_PERIOD,
+  ETH_TTD_INCREMENT,
+  SIM_ENV_CHAIN_ID,
+  SIM_ENV_NETWORK_ID,
+  SIM_TESTS_SECONDS_PER_SLOT,
+} from "../constants.js";
 import {SimulationEnvironment} from "../SimulationEnvironment.js";
 
 export const logFilesDir = "test-logs";
@@ -9,6 +16,55 @@ export const logFilesDir = "test-logs";
 export const avg = (arr: number[]): number => {
   return arr.length === 0 ? 0 : arr.reduce((p, c) => p + c, 0) / arr.length;
 };
+
+export function defineSimTestConfig(
+  opts: Partial<ChainConfig> & {
+    cliqueSealingPeriod?: number;
+    additionalSlotsForTTD?: number;
+    runTillEpoch: number;
+  }
+): {
+  estimatedTimeoutMs: number;
+  forkConfig: ChainForkConfig;
+} {
+  const genesisDelaySeconds =
+    (process.env.GENESIS_DELAY_SLOTS ? parseInt(process.env.GENESIS_DELAY_SLOTS) : 40) * SIM_TESTS_SECONDS_PER_SLOT;
+
+  const estimatedTimeoutMs =
+    getEstimatedTimeInSecForRun({
+      genesisDelaySeconds,
+      secondsPerSlot: SIM_TESTS_SECONDS_PER_SLOT,
+      runTill: opts.runTillEpoch,
+      // After adding Nethermind its took longer to complete
+      graceExtraTimeFraction: 0.3,
+    }) * 1000;
+
+  const ttd = getEstimatedTTD({
+    bellatrixForkEpoch: opts.BELLATRIX_FORK_EPOCH ?? Infinity,
+    secondsPerSlot: opts.SECONDS_PER_SLOT ?? SIM_TESTS_SECONDS_PER_SLOT,
+    cliqueSealingPeriod: opts.cliqueSealingPeriod ?? CLIQUE_SEALING_PERIOD,
+    // To make sure bellatrix already started
+    additionalSlots: opts.additionalSlotsForTTD ?? 2,
+  });
+
+  /* eslint-disable @typescript-eslint/naming-convention */
+  const forkConfig = createChainForkConfig({
+    ...opts,
+    GENESIS_DELAY: genesisDelaySeconds,
+    SECONDS_PER_SLOT: opts.SECONDS_PER_SLOT ?? SIM_TESTS_SECONDS_PER_SLOT,
+    TERMINAL_TOTAL_DIFFICULTY: ttd,
+    DEPOSIT_CHAIN_ID: SIM_ENV_CHAIN_ID,
+    DEPOSIT_NETWORK_ID: SIM_ENV_NETWORK_ID,
+    SECONDS_PER_ETH1_BLOCK: opts.cliqueSealingPeriod ?? CLIQUE_SEALING_PERIOD,
+    ETH1_FOLLOW_DISTANCE: 1,
+  });
+  /* eslint-enable @typescript-eslint/naming-convention */
+
+  return {
+    estimatedTimeoutMs,
+    forkConfig,
+  };
+}
 
 export const getEstimatedTimeInSecForRun = ({
   genesisDelaySeconds,
@@ -27,43 +83,36 @@ export const getEstimatedTimeInSecForRun = ({
 };
 
 export const getEstimatedTTD = ({
-  genesisDelaySeconds,
   cliqueSealingPeriod,
   secondsPerSlot,
   additionalSlots,
   bellatrixForkEpoch,
 }: {
-  genesisDelaySeconds: number;
   cliqueSealingPeriod: number;
   additionalSlots: number;
   secondsPerSlot: number;
   bellatrixForkEpoch: number;
 }): bigint => {
-  // Need to investigate why TTD always remain 1 epoch ahead, for now just subtract 1 epoch
   const secondsTillBellatrix =
-    genesisDelaySeconds +
-    (bellatrixForkEpoch - 1) * activePreset.SLOTS_PER_EPOCH * secondsPerSlot +
-    additionalSlots * secondsPerSlot;
+    bellatrixForkEpoch * activePreset.SLOTS_PER_EPOCH * secondsPerSlot + additionalSlots * secondsPerSlot;
 
   return BigInt(Math.ceil(secondsTillBellatrix / cliqueSealingPeriod) * ETH_TTD_INCREMENT);
 };
 
 export const getEstimatedShanghaiTime = ({
-  genesisDelaySeconds,
-  eth1GenesisTime,
+  genesisTime,
   secondsPerSlot,
   capellaForkEpoch,
   additionalSlots,
 }: {
-  genesisDelaySeconds: number;
-  eth1GenesisTime: number;
+  genesisTime: number;
   secondsPerSlot: number;
   capellaForkEpoch: number;
   additionalSlots: number;
 }): number => {
   const secondsTillCapella = capellaForkEpoch * activePreset.SLOTS_PER_EPOCH * secondsPerSlot;
 
-  return eth1GenesisTime + genesisDelaySeconds + secondsTillCapella + additionalSlots * secondsPerSlot;
+  return genesisTime + secondsTillCapella + additionalSlots * secondsPerSlot;
 };
 
 export const squeezeString = (val: string, length: number, sep = "..."): string => {
