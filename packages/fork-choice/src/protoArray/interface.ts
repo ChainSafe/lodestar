6981e0f787c710279fd4d88f57ba05d94e3d9c0e
g11tech <develop@g11tech.io>
2024-01-27 22:37:49 +0530

    fix: ignore forkchoice invalidations if latestValidHash not found (#6361)
    * fix: ignore forkchoice invalidations if latestValidHash not found
    
    * rename for better understanding
    
    * update the lvh search start index
    
    * apply feedback

--- packages/fork-choice/src/protoArray/interface.ts
+++ packages/fork-choice/src/protoArray/interface.ts
@@ -29,7 +29,7 @@ export type LVHValidResponse = {
 export type LVHInvalidResponse = {
   executionStatus: ExecutionStatus.Invalid;
   latestValidExecHash: RootHex | null;
-  invalidateFromBlockHash: RootHex;
+  invalidateFromParentBlockRoot: RootHex;
 };
 export type LVHExecResponse = LVHValidResponse | LVHInvalidResponse;
 
