9ae9cc8f494c709cde400111cd0a317f4b56fed1
Nico Flaig <nflaig@protonmail.com>
2024-02-03 18:12:07 +0100

    Update api index exports

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,11 +1,11 @@
 // Re-exporting beacon only for backwards compatibility
 export * from "./beacon/index.js";
-export * from "./interfaces.js";
-export {HttpStatusCode} from "./utils/client/httpStatusCode.js";
-export type {HttpErrorCodes, HttpSuccessCodes} from "./utils/client/httpStatusCode.js";
-export {HttpClient, HttpError, ApiError, FetchError, isFetchError, fetch} from "./utils/client/index.js";
+export {HttpStatusCode} from "./utils/httpStatusCode.js";
+export type {HttpErrorCodes, HttpSuccessCodes} from "./utils/httpStatusCode.js";
+export {HttpClient, FetchError, isFetchError, fetch} from "./utils/client/index.js";
 export type {IHttpClient, HttpClientOptions, HttpClientModules, Metrics} from "./utils/client/index.js";
-export * from "./utils/routes.js";
+export {ApiError} from "./utils/error.js";
+// export * from "./utils/routes.js"; TODO: do we need those?
 
 // NOTE: Don't export server here so it's not bundled to all consumers
 
--- packages/api/src/utils/client/index.ts
+++ packages/api/src/utils/client/index.ts
@@ -1,5 +1,6 @@
 export * from "./fetch.js";
 export * from "./httpClient.js";
 export * from "./method.js";
+export * from "./metrics.js";
 export * from "./request.js";
 export * from "./response.js";
