7b7c3d84ffeb22985994c49619e2d725a3f0e263
Nazar Hussain <nazarhussain@gmail.com>
2024-02-07 10:58:38 +0100

    Variables using .env file

---
+++ .github/actions/dotenv/index.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const core = require("@actions/core");
+const dotEnv = require("dotenv");
+const envFile = "test.env";
+
+if (!fs.existsSync(envFile)) {
+  core.setFailed("File .env not found");
+}
+
+const result = dotEnv.config({path: envFile});
+if (result.error) {
+  core.setFailed(result.error.message);
+} else {
+  core.setOutput("env", result.parsed);
+  core.info("Env file loaded");
+  core.info("Populating env variables}");
+
+  for (const key in result.parsed) {
+    const value = result.parsed[key];
+    core.setOutput(key, value);
+
+    if (exportVariables) {
+      core.exportVariable(key, value);
+    }
+  }
+}
