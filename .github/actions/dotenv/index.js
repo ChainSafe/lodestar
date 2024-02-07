960857f106a1b034490485163d0cf5a012403d89
Nazar Hussain <nazarhussain@gmail.com>
2024-02-07 11:24:30 +0100

    Update the workflow tasks

--- .github/actions/dotenv/index.js
+++ .github/actions/dotenv/index.js
@@ -13,11 +13,17 @@ if (result.error) {
 } else {
   core.setOutput("env", result.parsed);
   core.info("Env file loaded");
-  core.info("Populating env variables}");
+  core.info("Populating env variables...");
 
   for (const key in result.parsed) {
     const value = result.parsed[key];
-    core.setOutput(key, value);
+    core.info(`${key}=${value}`);
+
+    // Export variable
     core.exportVariable(key, value);
+
+    // Set to output so it can be used in as the input for the next job/step
+    core.setOutput(key, value);
+    
   }
 }
