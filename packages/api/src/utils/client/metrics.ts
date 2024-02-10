5c251851beb8434f2478c0e2c0fc7f9ab7983a73
Nico Flaig <nflaig@protonmail.com>
2024-02-10 05:16:38 +0100

    feat: add new panels to validator client dashboard (#6415)
    * Add url as label request to fallbacks and score metrics
    
    * Add url as label to request errors
    
    * Add connected beacon nodes panel
    
    * Add requests to fallback nodes panel
    
    * Add REST API request errors panel

--- packages/api/src/utils/client/metrics.ts
+++ packages/api/src/utils/client/metrics.ts
@@ -3,7 +3,7 @@ import {Gauge, GaugeExtra, Histogram} from "@lodestar/utils";
 export type Metrics = {
   requestTime: Histogram<{routeId: string}>;
   streamTime: Histogram<{routeId: string}>;
-  requestErrors: Gauge<{routeId: string}>;
-  requestToFallbacks: Gauge<{routeId: string}>;
-  urlsScore: GaugeExtra<{urlIndex: number}>;
+  requestErrors: Gauge<{routeId: string; baseUrl: string}>;
+  requestToFallbacks: Gauge<{routeId: string; baseUrl: string}>;
+  urlsScore: GaugeExtra<{urlIndex: number; baseUrl: string}>;
 };
