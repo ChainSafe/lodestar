da0bcc8f1933349320be546aeeee3970de04b610
Nazar Hussain <nazarhussain@gmail.com>
2024-02-12 14:36:33 +0100

    test: support .env.test to reuse env variables across the tests  (#6408)
    * Variables using .env file
    
    * Add env variable support to normal tests
    
    * Fix the order of variables
    
    * Update the order of jobs
    
    * Export the varible using actions toolkit
    
    * Update the workflow tasks
    
    * Fix the sim workflow
    
    * Add .env.test file support to tests
    
    * Fix the lint
    
    * Update the task description
    
    * Move lodestar preset to env file
    
    * Fix the directory path
    
    * newline
    
    * Update debugging spec tests section
    
    * Update the env variable for preset
    
    * Fix the path for setup files
    
    * Update code as per feedback
    
    * Fix the e2e variables
    
    * Update doc
    
    * Fix the bash script
    
    * Fix sim geth runne
    
    * Update the env file
    
    * Fix e2e tests
    
    * Update the script tasks
    
    * Update the script tasks
    
    * Add minimal for e2e tests
    
    * Add minimal for e2e tests
    
    * Update comments in preset tests
    
    * Downgrade nethermind version
    
    * Load env file in e2e env
    
    * Add the issue link in env variable
    
    * Update bash script for failsafe current dir
    
    * Fix the mistaken genesis extension for el nodes
    
    * Add missing capella wait for one test
    
    * Downgrade the geth version
    
    * Update .env.test
    
    ---------
    
    Co-authored-by: Nico Flaig <nflaig@protonmail.com>

--- packages/params/test/e2e/overridePreset.test.ts
+++ packages/params/test/e2e/overridePreset.test.ts
@@ -21,8 +21,8 @@ describe("Override preset", function () {
   vi.setConfig({testTimeout: 30_000});
 
   it("Should correctly override preset", async () => {
-    // These commands can not run with minimal preset
-    if (process.env.LODESTAR_PRESET === "minimal") delete process.env.LODESTAR_PRESET;
+    // `LODESTAR_PRESET` must not be set to properly test preset override
+    if (process.env.LODESTAR_PRESET) delete process.env.LODESTAR_PRESET;
 
     await exec(`node --loader ts-node/esm ${path.join(__dirname, scriptNames.ok)}`);
   });
