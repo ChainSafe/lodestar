8759569237668cbd4dcd419c29d9a2f472dbbe59
Nazar Hussain <nazarhussain@gmail.com>
2024-02-12 17:06:35 +0100

    Fix the build issue

--- packages/beacon-node/src/eth1/provider/jwt.ts
+++ packages/beacon-node/src/eth1/provider/jwt.ts
@@ -1,6 +1,9 @@
 import type {TAlgorithm} from "jwt-simple";
 // TODO: fix jwt-simple types
-import {encode, decode} from "jwt-simple";
+import jwt from "jwt-simple";
+
+// eslint-disable-next-line import/no-named-as-default-member
+const {encode, decode} = jwt;
 
 /**
  * jwt token has iat which is issued at unix timestamp, an optional exp for expiry,
