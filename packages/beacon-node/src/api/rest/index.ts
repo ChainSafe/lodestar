95ab7a0b6b8194616bf08c410873b141ea2d6ccc
harkamal <gajinder@g11.in>
2024-02-24 16:16:26 +0530

    update limit

--- packages/beacon-node/src/api/rest/index.ts
+++ packages/beacon-node/src/api/rest/index.ts
@@ -21,7 +21,7 @@ export const beaconRestApiServerOpts: BeaconRestApiServerOpts = {
   port: 9596,
   cors: "*",
   // beacon -> validator API is trusted, and for large amounts of keys the payload is multi-MB
-  bodyLimit: 100 * 1024 * 1024, // 100MB for block + blobs
+  bodyLimit: 20 * 1024 * 1024, // 20MB for big block + blobs
 };
 
 export type BeaconRestApiServerModules = RestApiServerModules & {
