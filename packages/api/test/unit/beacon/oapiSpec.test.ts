3d202076dc976f1857b5f5b5fde6c21c8b577933
Nico Flaig <nflaig@protonmail.com>
2024-02-27 14:33:19 +0100

    Update beacon api spec to v2.5.0

--- packages/api/test/unit/beacon/oapiSpec.test.ts
+++ packages/api/test/unit/beacon/oapiSpec.test.ts
@@ -23,8 +23,7 @@ import {testData as validatorTestData} from "./testData/validator.js";
 // eslint-disable-next-line @typescript-eslint/naming-convention
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-// TODO: update once release is out
-const version = "v2.4.2";
+const version = "v2.5.0";
 const openApiFile: OpenApiFile = {
   url: `https://github.com/ethereum/beacon-APIs/releases/download/${version}/beacon-node-oapi.json`,
   filepath: path.join(__dirname, "../../../oapi-schemas/beacon-node-oapi.json"),
