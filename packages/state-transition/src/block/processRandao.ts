1eba34780f25dded93b888e755e8fb959c7dd558
Julien Eluard <julien.eluard@gmail.com>
2024-02-27 11:01:41 +0100

    chore: remove buffer-xor usage

--- packages/state-transition/src/block/processRandao.ts
+++ packages/state-transition/src/block/processRandao.ts
@@ -1,10 +1,18 @@
-import xor from "buffer-xor";
 import {digest} from "@chainsafe/as-sha256";
 import {allForks} from "@lodestar/types";
 import {EPOCHS_PER_HISTORICAL_VECTOR} from "@lodestar/params";
 import {getRandaoMix} from "../util/index.js";
 import {verifyRandaoSignature} from "../signatureSets/index.js";
 import {CachedBeaconStateAllForks} from "../types.js";
+
+function xor(array1: Uint8Array, array2: Uint8Array): Uint8Array {
+  const length = Math.max(array1.length, array2.length);
+  const xoredArray = new Uint8Array(length);
+  for (let i = 0; i < xoredArray.length; i++) {
+    xoredArray[i] = array1[i] ^ array2[i];
+  }
+  return xoredArray;
+}
 
 /**
  * Commit a randao reveal to generate pseudorandomness seeds
@@ -28,6 +36,6 @@ export function processRandao(
   }
 
   // mix in RANDAO reveal
-  const randaoMix = xor(Buffer.from(getRandaoMix(state, epoch)), Buffer.from(digest(randaoReveal)));
+  const randaoMix = xor(getRandaoMix(state, epoch), digest(randaoReveal));
   state.randaoMixes.set(epoch % EPOCHS_PER_HISTORICAL_VECTOR, randaoMix);
 }
