53622cd9714d6028120844544ad29d9c676f67e3
Cayman <caymannava@gmail.com>
2024-02-29 16:50:20 -0500

    chore: tweak perf output

--- packages/beacon-node/test/perf/network/gossip/snappy.test.ts
+++ packages/beacon-node/test/perf/network/gossip/snappy.test.ts
@@ -4,49 +4,58 @@ import * as snappy from "snappy";
 import {itBench} from "@dapplion/benchmark";
 
 describe("network / gossip / snappy", () => {
-  for (const msgLen of [100, 200, 300, 400, 500, 1000, 10000, 100000]) {
-    const uncompressed = randomBytes(msgLen);
-    const compressed = snappyjs.compress(uncompressed);
-    const RUNS_FACTOR = 1000;
+  const msgLens = [100, 200, 300, 400, 500, 1000, 10000, 100000];
+  describe("compress", () => {
+    for (const msgLen of msgLens) {
+      const uncompressed = randomBytes(msgLen);
+      const RUNS_FACTOR = 1000;
 
-    itBench({
-      id: `snappyjs compress ${msgLen} bytes`,
-      runsFactor: RUNS_FACTOR,
-      fn: () => {
-        for (let i = 0; i < RUNS_FACTOR; i++) {
-          snappyjs.compress(uncompressed);
-        }
-      },
-    });
+      itBench({
+        id: `${msgLen} bytes - compress - snappyjs`,
+        runsFactor: RUNS_FACTOR,
+        fn: () => {
+          for (let i = 0; i < RUNS_FACTOR; i++) {
+            snappyjs.compress(uncompressed);
+          }
+        },
+      });
 
-    itBench({
-      id: `snappyjs uncompress ${msgLen} bytes`,
-      runsFactor: RUNS_FACTOR,
-      fn: () => {
-        for (let i = 0; i < RUNS_FACTOR; i++) {
-          snappyjs.uncompress(compressed);
-        }
-      },
-    });
+      itBench({
+        id: `${msgLen} bytes - compress - snappy`,
+        runsFactor: RUNS_FACTOR,
+        fn: () => {
+          for (let i = 0; i < RUNS_FACTOR; i++) {
+            snappy.compressSync(uncompressed);
+          }
+        },
+      });
+    }
+  });
+  describe("uncompress", () => {
+    for (const msgLen of [100, 200, 300, 400, 500, 1000, 10000, 100000]) {
+      const uncompressed = randomBytes(msgLen);
+      const compressed = snappyjs.compress(uncompressed);
+      const RUNS_FACTOR = 1000;
 
-    itBench({
-      id: `snappy compress ${msgLen} bytes`,
-      runsFactor: RUNS_FACTOR,
-      fn: () => {
-        for (let i = 0; i < RUNS_FACTOR; i++) {
-          snappy.compressSync(uncompressed);
-        }
-      },
-    });
+      itBench({
+        id: `${msgLen} bytes - uncompress - snappyjs`,
+        runsFactor: RUNS_FACTOR,
+        fn: () => {
+          for (let i = 0; i < RUNS_FACTOR; i++) {
+            snappyjs.uncompress(compressed);
+          }
+        },
+      });
 
-    itBench({
-      id: `snappy uncompress ${msgLen} bytes`,
-      runsFactor: RUNS_FACTOR,
-      fn: () => {
-        for (let i = 0; i < RUNS_FACTOR; i++) {
-          snappy.uncompressSync(compressed);
-        }
-      },
-    });
-  }
+      itBench({
+        id: `${msgLen} bytes - uncompress - snappy`,
+        runsFactor: RUNS_FACTOR,
+        fn: () => {
+          for (let i = 0; i < RUNS_FACTOR; i++) {
+            snappy.uncompressSync(compressed);
+          }
+        },
+      });
+    }
+  });
 });
