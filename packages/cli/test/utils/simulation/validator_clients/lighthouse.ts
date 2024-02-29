193ad5a3e229f5f4ab3d23f8eca2d4b934036e9e
Hiroyuki Naito <hiroyukinaito.eth@gmail.com>
2024-02-29 17:43:30 +0900

    Changed Fetch error

--- packages/cli/test/utils/simulation/validator_clients/lighthouse.ts
+++ packages/cli/test/utils/simulation/validator_clients/lighthouse.ts
@@ -84,8 +84,7 @@ export const generateLighthouseValidatorNode: ValidatorNodeGenerator<ValidatorCl
           await fetch(`http://127.0.0.1:${ports.validator.keymanagerPort}/lighthouse/health`);
           return {ok: true};
         } catch (err) {
-          if (err instanceof Error && !err.message.includes('ECONNREFUSED')) {
-            return {ok: true};
+          if (err instanceof FetchError && err.code !== "ECONNREFUSED") {            return {ok: true};
           }
           return {ok: false, reason: (err as Error).message, checkId: "/lighthouse/health query"};
         }
