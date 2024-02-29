f0c380ed1a532e151a1324329729ca651dd50ff5
Nico Flaig <nflaig@protonmail.com>
2024-02-29 14:26:51 +0100

    Add comment to eslint disable

--- packages/api/src/utils/client/fetch.ts
+++ packages/api/src/utils/client/fetch.ts
@@ -5,6 +5,7 @@
  */
 async function wrappedFetch(url: string | URL, init?: RequestInit): Promise<Response> {
   try {
+    // This function wraps global fetch and we should only directly call it here
     // eslint-disable-next-line no-restricted-globals
     return await fetch(url, init);
   } catch (e) {
