f9ebd274ac5f52316b2215c7bd10633cf392f7d3
NC <adrninistrator1@protonmail.com>
2024-03-07 18:39:03 +0800

    Update packages/api/src/beacon/routes/beacon/rewards.ts
    Co-authored-by: Nico Flaig <nflaig@protonmail.com>

--- packages/api/src/beacon/routes/beacon/rewards.ts
+++ packages/api/src/beacon/routes/beacon/rewards.ts
@@ -105,7 +105,7 @@ export type Api = {
    */
   getAttestationsRewards(
     epoch: Epoch,
-    filters?: ValidatorId[]
+    validatorIds?: ValidatorId[]
   ): Promise<
     ApiClientResponse<
       {[HttpStatusCode.OK]: {data: AttestationsRewards; executionOptimistic: ExecutionOptimistic}},
