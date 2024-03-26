121edfe328cef373f27d6e800ef1c5dfa6e849e3
Tuyen Nguyen <vutuyen2636@gmail.com>
2024-03-26 10:41:27 +0700

    fix: merge unstable

--- packages/beacon-node/test/e2e/chain/stateCache/nHistoricalStates.test.ts
+++ packages/beacon-node/test/e2e/chain/stateCache/nHistoricalStates.test.ts
@@ -399,10 +399,10 @@ describe(
           )?.value
         ).toEqual(reloadCount);
 
-        const persistMetricValues = await (followupBn.metrics?.cpStateCache.statePersistDuration as Histogram).get();
+        const stateSszMetricValues = await (followupBn.metrics?.cpStateCache.stateSszDuration as Histogram).get();
         expect(
-          persistMetricValues?.values.find(
-            (value) => value.metricName === "lodestar_cp_state_cache_state_persist_seconds_count"
+          stateSszMetricValues?.values.find(
+            (value) => value.metricName === "lodestar_cp_state_cache_state_ssz_seconds_count"
           )?.value
         ).toEqual(persistCount);
 
