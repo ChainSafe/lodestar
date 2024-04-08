5d1f509c6f149e074b995dddcdb7977a96758674
Nazar Hussain <nazarhussain@gmail.com>
2024-04-08 10:36:50 +0200

    Fix merge conflict issue

--- packages/cli/test/utils/simulation/web3JsPlugins.ts
+++ packages/cli/test/utils/simulation/web3JsPlugins.ts
@@ -160,36 +160,6 @@ export class BlobsEIP4844Transaction extends FeeMarketEIP1559Transaction {
 }
 
 class Web3AdminPlugin extends Web3PluginBase {
-  pluginNamespace = "admin";
-
-  async nodeInfo(): Promise<{
-    enode: string;
-    id: string;
-    ip: string;
-    listenAddr: string;
-    name: string;
-    ports: {
-      discovery: number;
-      listener: number;
-    };
-    protocols: {
-      eth: {
-        difficulty: number;
-        genesis: string;
-        head: string;
-        network: number;
-      };
-    };
-  }> {
-    return this.requestManager.send({method: "admin_nodeInfo", params: []});
-  }
-
-  async addPeer(enode: string): Promise<boolean> {
-    return this.requestManager.send({method: "admin_addPeer", params: [enode]});
-  }
-}
-
-class Web3AdminPlugin extends Web3PluginBase {
   /**
    * The admin plugin as available via the provider object
    * like in the example below.
