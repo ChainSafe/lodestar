524261a3d4987a6c9d4c61d7b9a5868d5cdf155a
Matthew Keil <me@matthewkeil.com>
2024-04-23 10:46:35 -0400

    Update packages/cli/src/preInitialization.ts
    Co-authored-by: Nico Flaig <nflaig@protonmail.com>

--- packages/cli/src/preInitialization.ts
+++ packages/cli/src/preInitialization.ts
@@ -101,7 +101,7 @@ if (uvThreadpoolSize) {
 if (isNaN(parseInt(`${process.env.UV_THREADPOOL_SIZE}`))) {
   // eslint-disable-next-line no-console
   throw new Error(
-    `UV_THREADPOOL_SIZE=${process.env.UV_THREADPOOL_SIZE}, but must be set to a number. Using default value of ${defaultThreadpoolSize}`
+    `UV_THREADPOOL_SIZE=${process.env.UV_THREADPOOL_SIZE}, but must be set to a number`
   );
 }
 
