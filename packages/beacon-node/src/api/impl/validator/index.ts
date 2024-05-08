1d74606117c3773a90ec1983c0ec449846af443b
twoeths <tuyen@chainsafe.io>
2024-05-08 18:09:39 +0700

    fix: attestation pool for electra (#6744)
    * feat: attestationPool to group by slot by data root by committee index for electra
    
    * fix: gossip validation and assert.notNull() util
    
    * fix: remove light-client stats.html
    
    * fix: lint and check-types

--- packages/beacon-node/src/api/impl/validator/index.ts
+++ packages/beacon-node/src/api/impl/validator/index.ts
@@ -1063,23 +1063,26 @@ export function getValidatorApi({
       };
     },
 
-    async getAggregatedAttestation({attestationDataRoot, slot}) {
+    async getAggregatedAttestation({attestationDataRoot, slot, committeeIndex}) {
       notWhileSyncing();
 
       await waitForSlot(slot); // Must never request for a future slot > currentSlot
 
       const dataRootHex = toHex(attestationDataRoot);
-      const aggregate = chain.attestationPool.getAggregate(slot, dataRootHex);
+      const aggregate = chain.attestationPool.getAggregate(slot, committeeIndex, dataRootHex);
 
       if (!aggregate) {
-        throw new ApiError(404, `No aggregated attestation for slot=${slot}, dataRoot=${dataRootHex}`);
+        throw new ApiError(
+          404,
+          `No aggregated attestation for slot=${slot} committeeIndex=${committeeIndex}, dataRoot=${dataRootHex}`
+        );
       }
 
       metrics?.production.producedAggregateParticipants.observe(aggregate.aggregationBits.getTrueBitIndexes().length);
 
       return {
         data: aggregate,
-        version: config.getForkName(slot),
+        meta: {version: config.getForkName(slot)},
       };
     },
 
