0de4d17c8ec914292bbb8afa25e140b827c6cd5c
harkamal <gajinder@g11.in>
2024-05-10 00:53:06 +0530

    fx

--- packages/light-client/test/unit/webEsmBundle.browser.test.ts
+++ packages/light-client/test/unit/webEsmBundle.browser.test.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access */
 import {expect, describe, it, vi} from "vitest";
 import {Lightclient, LightclientEvent, utils, transport} from "../../src/index.js";
 
