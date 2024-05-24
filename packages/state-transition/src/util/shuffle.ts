1c687b76075800aa021600264754d640e46162e6
Cayman <caymannava@gmail.com>
2024-05-24 09:14:10 -0400

    Update packages/state-transition/src/util/shuffle.ts
    Co-authored-by: twoeths <tuyen@chainsafe.io>

--- packages/state-transition/src/util/shuffle.ts
+++ packages/state-transition/src/util/shuffle.ts
@@ -91,7 +91,7 @@ function innerShuffleList(input: Shuffleable, seed: Bytes32, dir: boolean): void
   // check if list size fits in uint32
   assert.equal(listSize, input.length, "input length does not fit uint32");
   // check that the seed is 32 bytes
-  assert.lte(seed.length, 32, "seed length is not lte 32 bytes");
+  assert.lte(seed.length, _SHUFFLE_H_SEED_SIZE, `seed length is not lte ${_SHUFFLE_H_SEED_SIZE} bytes`);
 
   const buf = Buffer.alloc(_SHUFFLE_H_TOTAL_SIZE);
   let r = 0;
