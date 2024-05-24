a1af1ce85aafd35660dd360c33d9ae6efbe62f3f
Nico Flaig <nflaig@protonmail.com>
2024-05-24 20:24:49 +0200

    Update head event error logging

--- packages/validator/src/services/chainHeaderTracker.ts
+++ packages/validator/src/services/chainHeaderTracker.ts
@@ -38,7 +38,10 @@ export class ChainHeaderTracker {
         signal,
         onEvent: this.onHeadUpdate,
         onError: (e) => {
-          this.logger.verbose("Failed to receive head event", {}, e);
+          // Ignore noisy errors due to beacon node being offline
+          if (!e.message.includes("ECONNREFUSED")) {
+            this.logger.error("Failed to receive head event", {}, e);
+          }
         },
         onClose: () => {
           this.logger.verbose("Closed stream for head event", {});
