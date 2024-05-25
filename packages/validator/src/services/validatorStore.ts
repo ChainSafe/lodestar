d971d31d38cbf9fb3fa2cc22b87ce4ad605d2302
g11tech <develop@g11tech.io>
2024-05-25 13:45:36 +0530

    chore: types and lint fixes (#6819)

--- packages/validator/src/services/validatorStore.ts
+++ packages/validator/src/services/validatorStore.ts
@@ -29,7 +29,6 @@ import {
   BlindedBeaconBlock,
   BLSPubkey,
   BLSSignature,
-  electra,
   Epoch,
   phase0,
   Root,
@@ -806,9 +805,7 @@ export class ValidatorStore {
       );
     }
     if (isAfterElectra && data.index !== 0) {
-      throw Error(
-        `Non-zero committee index post-electra during signing: att.committeeIndex ${data.index}`
-      );
+      throw Error(`Non-zero committee index post-electra during signing: att.committeeIndex ${data.index}`);
     }
     if (this.config.getForkSeq(duty.slot) >= ForkSeq.electra && data.index !== 0) {
       throw Error(`Attestataion data index must be 0 post electra: index ${data.index}`);
