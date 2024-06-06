c9a15919e18f14ff3aa3fdd723709d0ead1f8dd0
Navie Chan <naviechan@gmail.com>
2024-06-06 17:30:55 +0200

    Fix merge

--- packages/fork-choice/src/forkChoice/interface.ts
+++ packages/fork-choice/src/forkChoice/interface.ts
@@ -54,7 +54,7 @@ export enum NotReorgedReason {
   ReorgMoreThanOneSlot = "reorgMoreThanOneSlot",
   ProposerBoostNotWornOff = "proposerBoostNotWornOff",
   HeadBlockNotWeak = "headBlockNotWeak",
-  ParentBlockIsStrong = "parentBlockIsStrong",
+  ParentBlockNotStrong = "ParentBlockNotStrong",
   NotProposingOnTime = "notProposingOnTime",
 }
 
--- packages/fork-choice/test/unit/forkChoice/getProposerHead.test.ts
+++ packages/fork-choice/test/unit/forkChoice/getProposerHead.test.ts
@@ -193,7 +193,7 @@ describe("Forkchoice / GetProposerHead", function () {
       parentBlock: {...baseParentHeadBlock, weight: 211},
       headBlock: {...baseHeadBlock},
       expectReorg: false,
-      expectedNotReorgedReason: NotReorgedReason.ParentBlockIsStrong,
+      expectedNotReorgedReason: NotReorgedReason.ParentBlockNotStrong,
     },
     {
       id: "No reorg if not proposing on time",
