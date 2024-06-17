37586596fe8b0e6b93f8b65698621b2e56a8d07f
Nico Flaig <nflaig@protonmail.com>
2024-06-17 13:38:55 +0100

    Remove trace

--- packages/validator/src/validator.ts
+++ packages/validator/src/validator.ts
@@ -23,11 +23,6 @@ import {BeaconHealth, Metrics} from "./metrics.js";
 import {MetaDataRepository} from "./repositories/metaDataRepository.js";
 import {DoppelgangerService} from "./services/doppelgangerService.js";
 
-process.on("uncaughtException", async (err) => {
-  console.trace("uncaughtException");
-  console.log(err);
-});
-
 export type ValidatorModules = {
   opts: ValidatorOptions;
   genesis: Genesis;
