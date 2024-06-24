375a760767876335b3ec7ec3d74c3f31a890d1af
Navie Chan <naviechan@gmail.com>
2024-06-24 13:10:59 +0300

    Address comment

--- packages/beacon-node/test/spec/utils/specTestIterator.ts
+++ packages/beacon-node/test/spec/utils/specTestIterator.ts
@@ -66,6 +66,7 @@ export const defaultSkipOpts: SkipOpts = {
     /^capella\/light_client\/single_merkle_proof\/BeaconBlockBody.*/,
     /^deneb\/light_client\/single_merkle_proof\/BeaconBlockBody.*/,
   ],
+  // TODO Electra: Review this test in the next spec test release
   skippedTests: [/incorrect_not_enough_consolidation_churn_available/],
   skippedRunners: ["merkle_proof", "networking"],
 };
