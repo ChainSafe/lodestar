b42eb4eec3a09d3d317e42d39a3bf8ba263cc54e
Nico Flaig <nflaig@protonmail.com>
2024-06-30 12:33:44 +0100

    Only sanitize primary node url once

--- packages/validator/src/validator.ts
+++ packages/validator/src/validator.ts
@@ -136,13 +136,15 @@ export class Validator {
 
       // Add notifier to warn user if primary node is unhealthy as there might
       // not be any errors in the logs due to fallback nodes handling the requests
-      const {urlsInits, urlsScore} = this.api.httpClient;
-      if (urlsInits.length > 1) {
+      const {httpClient} = this.api;
+      if (httpClient.urlsInits.length > 1) {
+        const primaryNodeUrl = toSafePrintableUrl(httpClient.urlsInits[0].baseUrl);
+
         this.clock?.runEveryEpoch(async () => {
           // Only emit warning if URL score is 0 to prevent false positives
           // if just a single request fails which might happen due to other reasons
-          if (urlsScore[0] === 0) {
-            this.logger?.warn("Primary beacon node is unhealthy", {url: toSafePrintableUrl(urlsInits[0].baseUrl)});
+          if (httpClient.urlsScore[0] === 0) {
+            this.logger?.warn("Primary beacon node is unhealthy", {url: primaryNodeUrl});
           }
         });
       }
