45d0ad3eb5e72645403fe02a9f5d05cf65f05bf9
matthewkeil <me@matthewkeil.com>
2024-07-12 19:16:37 -0100

    refactor: use fromHex util

--- packages/beacon-node/test/utils/getBlobCellAndProofs.ts
+++ packages/beacon-node/test/utils/getBlobCellAndProofs.ts
@@ -1,8 +1,8 @@
 import fs from "node:fs";
 import path from "node:path";
 import yaml from "js-yaml";
-
 import {Blob, Cell, KZGProof} from "c-kzg";
+import {fromHex} from "@lodestar/utils";
 
 interface BlobCellAndProofMock {
   blob: Blob;
@@ -18,13 +18,6 @@ interface BlobCellAndProofYamlFormat {
   output: [string[], string[]];
 }
 
-function bytesFromHex(hexString: string): Uint8Array {
-  if (hexString.startsWith("0x")) {
-    hexString = hexString.slice(2);
-  }
-  return Uint8Array.from(Buffer.from(hexString, "hex"));
-}
-
 export function getBlobCellAndProofs(): BlobCellAndProofMock[] {
   const mocks = [] as BlobCellAndProofMock[];
   const mocksDir = path.resolve(__dirname, "..", "fixtures", "blobsAndCells");
@@ -36,9 +29,9 @@ export function getBlobCellAndProofs(): BlobCellAndProofMock[] {
         output: [cells, proofs],
       } = yaml.load(fs.readFileSync(filepath, "utf-8")) as BlobCellAndProofYamlFormat;
       mocks.push({
-        blob: bytesFromHex(blob),
-        cells: cells.map(bytesFromHex),
-        proofs: proofs.map(bytesFromHex),
+        blob: fromHex(blob),
+        cells: cells.map(fromHex),
+        proofs: proofs.map(fromHex),
       });
     }
   }
