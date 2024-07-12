99d5c837c8560566017090d516efaab9fb1b7392
matthewkeil <me@matthewkeil.com>
2024-07-12 19:09:58 -0100

    refactor: getDataColumnSidecars

--- packages/beacon-node/test/unit/db/api/repositories/dataColumn.test.ts
+++ packages/beacon-node/test/unit/db/api/repositories/dataColumn.test.ts
@@ -13,7 +13,7 @@ import {
   CUSTODY_COLUMNS_IN_IN_WRAPPER_INDEX,
 } from "../../../../../src/db/repositories/dataColumnSidecars.js";
 import {testLogger} from "../../../../utils/logger.js";
-import {computeDataColumnSidecars} from "../../../../../src/util/blobs.js";
+import {getDataColumnSidecars} from "../../../../../src/util/blobs.js";
 import {loadEthereumTrustedSetup, initCKZG} from "../../../../../src/util/kzg.js";
 
 /* eslint-disable @typescript-eslint/naming-convention */
@@ -53,7 +53,7 @@ describe("block archive repository", function () {
     singedBlock.message.body.blobKzgCommitments.push(commitment);
     singedBlock.message.body.blobKzgCommitments.push(commitment);
     singedBlock.message.body.blobKzgCommitments.push(commitment);
-    const dataColumnSidecars = computeDataColumnSidecars(config, singedBlock, {
+    const dataColumnSidecars = getDataColumnSidecars(config, singedBlock, {
       blobs: [blob, blob, blob],
       kzgProofs: [commitment, commitment, commitment],
     });
