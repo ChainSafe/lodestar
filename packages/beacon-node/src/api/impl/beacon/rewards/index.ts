fed08fe5107a809d8862494ae6a86ebc6f2f0ee9
Cayman <caymannava@gmail.com>
2024-07-18 14:07:10 -0400

    chore: refactor block and state api utils (#6963)
    * chore: refactor block and state api utils
    
    * Update packages/beacon-node/src/api/impl/beacon/blocks/utils.ts
    
    Co-authored-by: Nico Flaig <nflaig@protonmail.com>
    
    * Update packages/beacon-node/src/api/impl/beacon/state/utils.ts
    
    Co-authored-by: Nico Flaig <nflaig@protonmail.com>
    
    * chore: address pr comments
    
    * chore: tiny cleanup
    
    * Use more informative return types
    
    * Align block and state utils
    
    * Let server handler deserialize state bytes
    
    ---------
    
    Co-authored-by: Nico Flaig <nflaig@protonmail.com>

--- packages/beacon-node/src/api/impl/beacon/rewards/index.ts
+++ packages/beacon-node/src/api/impl/beacon/rewards/index.ts
@@ -1,14 +1,14 @@
 import {routes} from "@lodestar/api";
 import {ApplicationMethods} from "@lodestar/api/server";
 import {ApiModules} from "../../types.js";
-import {resolveBlockId} from "../blocks/utils.js";
+import {getBlockResponse} from "../blocks/utils.js";
 
 export function getBeaconRewardsApi({
   chain,
 }: Pick<ApiModules, "chain">): ApplicationMethods<routes.beacon.rewards.Endpoints> {
   return {
     async getBlockRewards({blockId}) {
-      const {block, executionOptimistic, finalized} = await resolveBlockId(chain, blockId);
+      const {block, executionOptimistic, finalized} = await getBlockResponse(chain, blockId);
       const data = await chain.getBlockRewards(block.message);
       return {data, meta: {executionOptimistic, finalized}};
     },
@@ -17,7 +17,7 @@ export function getBeaconRewardsApi({
       return {data: rewards, meta: {executionOptimistic, finalized}};
     },
     async getSyncCommitteeRewards({blockId, validatorIds}) {
-      const {block, executionOptimistic, finalized} = await resolveBlockId(chain, blockId);
+      const {block, executionOptimistic, finalized} = await getBlockResponse(chain, blockId);
       const data = await chain.getSyncCommitteeRewards(block.message, validatorIds);
       return {data, meta: {executionOptimistic, finalized}};
     },
