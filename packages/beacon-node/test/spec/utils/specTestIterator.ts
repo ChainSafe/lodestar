784a9bac92c75122581dbfb342b3a59f8b316fbf
Tuyen Nguyen <vutuyen2636@gmail.com>
2024-08-02 14:12:52 +0700

    fix: skip light_client spec tests

--- packages/beacon-node/test/spec/utils/specTestIterator.ts
+++ packages/beacon-node/test/spec/utils/specTestIterator.ts
@@ -65,7 +65,7 @@ export const defaultSkipOpts: SkipOpts = {
     "capella/light_client/single_merkle_proof/BeaconBlockBody",
     "deneb/light_client/single_merkle_proof/BeaconBlockBody",
   ],
-  skippedRunners: ["merkle_proof"],
+  skippedRunners: ["merkle_proof", "light_client"],
 };
 
 /**
