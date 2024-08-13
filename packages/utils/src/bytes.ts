1fdb6e2dfebde5a0846b616f1907df8cacde0375
Cayman <caymannava@gmail.com>
2024-08-13 16:23:58 -0400

    Update packages/utils/src/bytes.ts

--- packages/utils/src/bytes.ts
+++ packages/utils/src/bytes.ts
@@ -59,6 +59,9 @@ export function toHex(buffer: Uint8Array | Parameters<typeof Buffer.from>[0]): s
 // Shared buffer to convert root to hex
 const rootBuf = Buffer.alloc(32);
 
+/**
+ * Convert a Uint8Array, length 32, to 0x-prefixed hex string
+ */
 export function toRootHex(root: Uint8Array): string {
   rootBuf.set(root);
   return `0x${rootBuf.toString("hex")}`;
