be115a669e088f4f0ba50b9baa995a46ebf72952
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-08-16 19:23:42 +0100

    chore: add more explanation on deprecation note and yarn lint:fix

--- packages/beacon-node/src/util/kzg.ts
+++ packages/beacon-node/src/util/kzg.ts
@@ -1,7 +1,7 @@
 import path from "node:path";
 import fs from "node:fs";
-import { fileURLToPath } from "node:url";
-import { fromHex, toHex } from "@lodestar/utils";
+import {fileURLToPath} from "node:url";
+import {fromHex, toHex} from "@lodestar/utils";
 
 /* eslint-disable @typescript-eslint/naming-convention */
 
@@ -22,14 +22,14 @@ export let ckzg: {
   // Deprecated: Use `computeCellsAndKzgProofs`
   computeCells(blob: Uint8Array): Uint8Array[];
   computeCellsAndKzgProofs(blob: Uint8Array): [Uint8Array[], Uint8Array[]];
-  // Deprecated
+  // Deprecated: Now being done internally in `recoverCellsAndKzgProofs`
   cellsToBlob(cells: Uint8Array[]): Uint8Array;
   // Deprecated: Use recoverCellsAndKzgProofs
   recoverAllCells(cellIds: number[], cells: Uint8Array[]): Uint8Array[];
   recoverCellsAndKzgProofs(cellIndices: number[], cells: Uint8Array[]): [Uint8Array[], Uint8Array[]];
-  // Deprecated: not used
+  // Deprecated: not used and removed from specs
   verifyCellKzgProof(commitmentBytes: Uint8Array, cellId: number, cell: Uint8Array, proofBytes: Uint8Array): boolean;
-  // This API will no longer take rowIndices in the future, in particular the rowIndices is no longer used
+  // This API will no longer take rowIndices in the future, in particular the rowIndices is no longer used.
   verifyCellKzgProofBatch(
     commitmentsBytes: Uint8Array[],
     rowIndices: number[],
