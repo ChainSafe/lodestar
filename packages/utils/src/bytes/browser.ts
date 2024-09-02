f475bb0fe03d1c56b4d7727ca12a7589d00af89e
Tuyen Nguyen <vutuyen2636@gmail.com>
2024-09-02 09:56:55 +0700

    feat: add toPubkeyHex util

--- packages/utils/src/bytes/browser.ts
+++ packages/utils/src/bytes/browser.ts
@@ -1,26 +1,22 @@
+// "0".charCodeAt(0) = 48
+const CHAR_CODE_0 = 48;
+// "x".charCodeAt(0) = 120
+const CHAR_CODE_X = 120;
+
 export function toHex(bytes: Uint8Array): string {
   const charCodes = new Array<number>(bytes.length * 2 + 2);
-  charCodes[0] = 48;
-  charCodes[1] = 120;
+  charCodes[0] = CHAR_CODE_0;
+  charCodes[1] = CHAR_CODE_X;
 
-  for (let i = 0; i < bytes.length; i++) {
-    const byte = bytes[i];
-    const first = (byte & 0xf0) >> 4;
-    const second = byte & 0x0f;
-
-    // "0".charCodeAt(0) = 48
-    // "a".charCodeAt(0) = 97 => delta = 87
-    charCodes[2 + 2 * i] = first < 10 ? first + 48 : first + 87;
-    charCodes[2 + 2 * i + 1] = second < 10 ? second + 48 : second + 87;
-  }
+  bytesIntoCharCodes(bytes, charCodes);
   return String.fromCharCode(...charCodes);
 }
 
 const rootCharCodes = new Array<number>(32 * 2 + 2);
 // "0".charCodeAt(0)
-rootCharCodes[0] = 48;
+rootCharCodes[0] = CHAR_CODE_0;
 // "x".charCodeAt(0)
-rootCharCodes[1] = 120;
+rootCharCodes[1] = CHAR_CODE_X;
 
 /**
  * Convert a Uint8Array, length 32, to 0x-prefixed hex string
@@ -30,17 +26,26 @@ export function toRootHex(root: Uint8Array): string {
     throw Error(`Expect root to be 32 bytes, got ${root.length}`);
   }
 
-  for (let i = 0; i < root.length; i++) {
-    const byte = root[i];
-    const first = (byte & 0xf0) >> 4;
-    const second = byte & 0x0f;
-
-    // "0".charCodeAt(0) = 48
-    // "a".charCodeAt(0) = 97 => delta = 87
-    rootCharCodes[2 + 2 * i] = first < 10 ? first + 48 : first + 87;
-    rootCharCodes[2 + 2 * i + 1] = second < 10 ? second + 48 : second + 87;
-  }
+  bytesIntoCharCodes(root, rootCharCodes);
   return String.fromCharCode(...rootCharCodes);
+}
+
+const pubkeyCharCodes = new Array<number>(48 * 2 + 2);
+// "0".charCodeAt(0)
+pubkeyCharCodes[0] = CHAR_CODE_0;
+// "x".charCodeAt(0)
+pubkeyCharCodes[1] = CHAR_CODE_X;
+
+/**
+ * Convert a Uint8Array, length 48, to 0x-prefixed hex string
+ */
+export function toPubkeyHex(pubkey: Uint8Array): string {
+  if (pubkey.length !== CHAR_CODE_0) {
+    throw Error(`Expect pubkey to be 48 bytes, got ${pubkey.length}`);
+  }
+
+  bytesIntoCharCodes(pubkey, pubkeyCharCodes);
+  return String.fromCharCode(...pubkeyCharCodes);
 }
 
 export function fromHex(hex: string): Uint8Array {
@@ -63,4 +68,24 @@ export function fromHex(hex: string): Uint8Array {
     bytes[i] = byte;
   }
   return bytes;
+}
+
+/**
+ * Populate charCodes from bytes. Note that charCodes index 0 and 1 ("0x") are not populated.
+ */
+function bytesIntoCharCodes(bytes: Uint8Array, charCodes: number[]): void {
+  if (bytes.length * 2 + 2 !== charCodes.length) {
+    throw Error(`Expect charCodes to be of length ${bytes.length * 2 + 2}, got ${charCodes.length}`);
+  }
+
+  for (let i = 0; i < bytes.length; i++) {
+    const byte = bytes[i];
+    const first = (byte & 0xf0) >> 4;
+    const second = byte & 0x0f;
+
+    // "0".charCodeAt(0) = 48
+    // "a".charCodeAt(0) = 97 => delta = 87
+    charCodes[2 + 2 * i] = first < 10 ? first + 48 : first + 87;
+    charCodes[2 + 2 * i + 1] = second < 10 ? second + 48 : second + 87;
+  }
 }
