7cd907df06525a7d4eb895e9c88834ced84d8b37
Nico Flaig <nflaig@protonmail.com>
2024-09-23 10:08:36 +0100

    chore: upgrade fastify to v5

--- packages/api/src/utils/server/parser.ts
+++ packages/api/src/utils/server/parser.ts
@@ -2,22 +2,10 @@ import type * as fastify from "fastify";
 import {MediaType} from "../headers.js";
 
 export function addSszContentTypeParser(server: fastify.FastifyInstance): void {
-  // Cache body schema symbol, does not change per request
-  let bodySchemaSymbol: symbol | undefined;
-
   server.addContentTypeParser(
     MediaType.ssz,
     {parseAs: "buffer"},
-    async (request: fastify.FastifyRequest, payload: Buffer) => {
-      if (bodySchemaSymbol === undefined) {
-        // Get body schema symbol to be able to access validation function
-        // https://github.com/fastify/fastify/blob/af2ccb5ff681c1d0ac22eb7314c6fa803f73c873/lib/symbols.js#L25
-        bodySchemaSymbol = Object.getOwnPropertySymbols(request.context).find((s) => s.description === "body-schema");
-      }
-      // JSON schema validation will be applied to `Buffer` object, it is required to override validation function
-      // See https://github.com/fastify/help/issues/1012, it is not possible right now to define a schema per content type
-      (request.context as unknown as Record<symbol, unknown>)[bodySchemaSymbol as symbol] = () => true;
-
+    async (_request: fastify.FastifyRequest, payload: Buffer) => {
       // We could just return the `Buffer` here which is a subclass of `Uint8Array` but downstream code does not require it
       // and it's better to convert it here to avoid unexpected behavior such as `Buffer.prototype.slice` not copying memory
       // See https://github.com/nodejs/node/issues/41588#issuecomment-1016269584
