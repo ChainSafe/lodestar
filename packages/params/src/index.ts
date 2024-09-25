bf607863e11dd96a255e1ab61792d764a6d0dd2a
Nico Flaig <nflaig@protonmail.com>
2024-09-25 21:39:48 +0100

    chore: remove signing domain for consolidations (#7092)

--- packages/params/src/index.ts
+++ packages/params/src/index.ts
@@ -146,7 +146,6 @@ export const DOMAIN_SYNC_COMMITTEE = Uint8Array.from([7, 0, 0, 0]);
 export const DOMAIN_SYNC_COMMITTEE_SELECTION_PROOF = Uint8Array.from([8, 0, 0, 0]);
 export const DOMAIN_CONTRIBUTION_AND_PROOF = Uint8Array.from([9, 0, 0, 0]);
 export const DOMAIN_BLS_TO_EXECUTION_CHANGE = Uint8Array.from([10, 0, 0, 0]);
-export const DOMAIN_CONSOLIDATION = Uint8Array.from([11, 0, 0, 0]);
 
 // Application specific domains
 
