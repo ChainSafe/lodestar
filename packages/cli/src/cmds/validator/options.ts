fd1e4f804d1d3acad72e6710d1d22ff436199b55
Nico Flaig <nflaig@protonmail.com>
2024-09-26 17:51:29 +0100

    Remove group tag from keymanager option

--- packages/cli/src/cmds/validator/options.ts
+++ packages/cli/src/cmds/validator/options.ts
@@ -146,7 +146,6 @@ export const keymanagerOptions: CliCommandOptions<KeymanagerArgs> = {
     hidden: true,
     type: "boolean",
     description: "Return stacktraces in HTTP error responses",
-    group: "api",
   },
 };
 
