20fc5ee55da1d89aa9524ff3325cb97616d6ae9b
matthewkeil <me@matthewkeil.com>
2024-10-03 19:26:14 +0700

    fix: EpochShuffling ssz type issue

--- packages/state-transition/test/unit/util/shuffling.test.ts
+++ packages/state-transition/test/unit/util/shuffling.test.ts
@@ -1,15 +1,27 @@
 import {describe, it, expect} from "vitest";
+import {ssz} from "@lodestar/types";
 import {generateState} from "../../utils/state.js";
 import {computeEpochShuffling, computeEpochShufflingAsync} from "../../../src/util/epochShuffling.js";
 import {computeEpochAtSlot} from "../../../src/index.js";
 
 describe("EpochShuffling", () => {
   it("async and sync versions should be identical", async () => {
-    const activeIndices = Uint32Array.from(Array.from({length: 1_000}, (_, i) => i));
+    const numberOfValidators = 1000;
+    const activeIndices = Uint32Array.from(Array.from({length: numberOfValidators}, (_, i) => i));
     const state = generateState();
     state.slot = 12345;
-    state.validators = activeIndices;
-
+    state.validators = ssz.phase0.Validators.toViewDU(
+      Array.from({length: numberOfValidators}, () => ({
+        activationEligibilityEpoch: 0,
+        activationEpoch: 0,
+        exitEpoch: Infinity,
+        effectiveBalance: 32,
+        pubkey: Buffer.alloc(48, 0xaa),
+        slashed: false,
+        withdrawableEpoch: Infinity,
+        withdrawalCredentials: Buffer.alloc(8, 0x01),
+      }))
+    );
     const epoch = computeEpochAtSlot(state.slot);
 
     const sync = computeEpochShuffling(state, activeIndices, epoch);
