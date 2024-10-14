2309803a18dc276ad775b4cc2c7f91f434dd54ba
Nazar Hussain <nazarhussain@gmail.com>
2024-10-14 11:48:33 +0200

    Fix few types

--- packages/beacon-node/test/spec/presets/transition.test.ts
+++ packages/beacon-node/test/spec/presets/transition.test.ts
@@ -82,7 +82,7 @@ const transition =
           expectEqualBeaconState(forkNext, expected, actual);
         },
         // Do not manually skip tests here, do it in packages/beacon-node/test/spec/presets/index.test.ts
-        shouldSkip: (_testcase, name, _index) => skipTestNames?.some((skipTestName) => name.includes(skipTestName)),
+        shouldSkip: (_testcase, name, _index) => skipTestNames?.some((skipTestName) => name.includes(skipTestName)) ?? false,
       },
     };
   };
