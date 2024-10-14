4f31ed97a557dac2d348ad7a08ca85788fa3000d
Nazar Hussain <nazarhussain@gmail.com>
2024-10-14 19:29:25 +0200

    Fix formatting

--- packages/fork-choice/test/unit/protoArray/getCommonAncestor.test.ts
+++ packages/fork-choice/test/unit/protoArray/getCommonAncestor.test.ts
@@ -79,7 +79,7 @@ describe("getCommonAncestor", () => {
     it(`${nodeA} & ${nodeB} -> ${ancestor}`, () => {
       // biome-ignore lint/style/noNonNullAssertion: <explanation>
       const ancestorNode = fc.getCommonAncestor(fc.getNode(nodeA)!, fc.getNode(nodeB)!);
-      if(ancestor) {
+      if (ancestor) {
         expect(ancestorNode?.blockRoot).toBe(ancestor);
       } else {
         expect(ancestorNode).toBeNull();
