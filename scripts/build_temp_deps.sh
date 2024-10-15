ae6b5e17f5f8febf8e740e144008aea7f704984c
matthewkeil <me@matthewkeil.com>
2024-10-15 18:03:45 +0700

    fix: update build_temp_deps.sh

--- scripts/build_temp_deps.sh
+++ scripts/build_temp_deps.sh
@@ -1,13 +1,7 @@
 SCRIPTS_DIR="$( cd "$( dirname "$0" )" && pwd )"
 BLST_TS_DIR="$SCRIPTS_DIR/../temp-deps/blst-ts"
-BLST_DIR="$BLST_TS_DIR/blst"
-
-
-cd $BLST_DIR
-chmod +x ./build.sh
-./build.sh -ggdb
 
 cd $BLST_TS_DIR
-yarn global add @napi-rs/cli
+# yarn global add @napi-rs/cli
 yarn
-yarn build:debug
+yarn build
