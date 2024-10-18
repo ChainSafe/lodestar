f752f36974898ea47b3a5d15a5e103007f7c0cbe
Tuyen Nguyen <vutuyen2636@gmail.com>
2024-10-18 10:21:03 +0700

    fix: remove BalancesTreeCache interface

--- packages/state-transition/src/cache/balancesTreeCache.ts
+++
@@ -1,5 +0,0 @@
-import {UintNumberType, ListBasicTreeViewDU} from "@chainsafe/ssz";
-
-export interface IBalancesTreeCache {
-  getUnusedBalances(): ListBasicTreeViewDU<UintNumberType> | undefined;
-}
