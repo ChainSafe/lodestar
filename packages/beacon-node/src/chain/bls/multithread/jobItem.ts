9bed959b6a12054871b2bfe284db3c53d288aa6d
matthewkeil <me@matthewkeil.com>
2024-10-29 13:57:42 +0700

    fix: metric collection value

--- packages/beacon-node/src/chain/bls/multithread/jobItem.ts
+++ packages/beacon-node/src/chain/bls/multithread/jobItem.ts
@@ -61,7 +61,7 @@ export async function jobItemWorkReq(job: JobQueueItem, metrics: Metrics | null)
         })),
       };
     case JobQueueItemType.sameMessage: {
-      metrics?.blsThreadPool.aggregateWithRandomnessSets.inc(job.sets);
+      metrics?.blsThreadPool.aggregateWithRandomnessSets.inc(job.sets.length);
       const timer = metrics?.blsThreadPool.aggregateWithRandomnessJobTime.startTimer();
       const {pk, sig} = await asyncAggregateWithRandomness(
         job.sets.map((set) => ({pk: set.publicKey, sig: set.signature}))
