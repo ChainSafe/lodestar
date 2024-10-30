7941b236f5409186481ec360821f9a358df2f884
Nico Flaig <nflaig@protonmail.com>
2024-10-30 11:19:22 +0000

    Remove unused import

--- packages/validator/src/services/validatorStore.ts
+++ packages/validator/src/services/validatorStore.ts
@@ -42,7 +42,7 @@ import {
   SignedAggregateAndProof,
 } from "@lodestar/types";
 import {routes} from "@lodestar/api";
-import {fromHex, isEmptyObject, toPubkeyHex, toRootHex} from "@lodestar/utils";
+import {fromHex, toPubkeyHex, toRootHex} from "@lodestar/utils";
 import {ISlashingProtection} from "../slashingProtection/index.js";
 import {PubkeyHex} from "../types.js";
 import {externalSignerPostSignature, SignableMessageType, SignableMessage} from "../util/externalSignerClient.js";
