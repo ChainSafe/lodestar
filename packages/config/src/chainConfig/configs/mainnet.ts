ce9ac6d6c697a4d186a3fcff74960efb49d66e6a
Nico Flaig <nflaig@protonmail.com>
2024-12-13 10:16:38 +0100

    Add comment on how MAX_REQUEST_BLOB_SIDECARS is calculated

--- packages/config/src/chainConfig/configs/mainnet.ts
+++ packages/config/src/chainConfig/configs/mainnet.ts
@@ -102,6 +102,7 @@ export const chainConfig: ChainConfig = {
   // `2**12` (= 4096 epochs, ~18 days)
   MIN_EPOCHS_FOR_BLOB_SIDECARS_REQUESTS: 4096,
   MAX_BLOBS_PER_BLOCK: 6,
+  // MAX_REQUEST_BLOCKS_DENEB * MAX_BLOBS_PER_BLOCK
   MAX_REQUEST_BLOB_SIDECARS: 768,
 
   // Electra
