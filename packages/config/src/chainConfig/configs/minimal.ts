12bbf66284d11170954c4525b5d1f2e41f0aaa5e
Nico Flaig <nflaig@protonmail.com>
2025-01-17 17:35:24 +0000

    Add remark to minimal preset values

--- packages/config/src/chainConfig/configs/minimal.ts
+++ packages/config/src/chainConfig/configs/minimal.ts
@@ -63,6 +63,7 @@ export const chainConfig: ChainConfig = {
   SHARD_COMMITTEE_PERIOD: 64,
   // [customized] process deposits more quickly, but insecure
   ETH1_FOLLOW_DISTANCE: 16,
+  // TODO FOCIL: these values don't make sense, we only have 6 seconds per slot
   ATTESTATION_DEADLINE: 4,
   PROPOSER_INCLUSION_LIST_CUT_OFF: 11,
 
