3ca2f10ac4454151f4075a687abc6b879c711293
Nazar Hussain <nazarhussain@gmail.com>
2025-01-20 18:35:07 +0100

    Revert changes to the benchmark

--- packages/state-transition/test/perf/util/loadState/findModifiedValidators.test.ts
+++ packages/state-transition/test/perf/util/loadState/findModifiedValidators.test.ts
@@ -59,17 +59,17 @@ describe("find modified validators by different ways", () => {
         expectedModifiedValidators.length === 0
           ? "no difference"
           : expectedModifiedValidators.length + " modified validators";
-
-      const modifiedState = state.clone();
-      for (const validatorIndex of expectedModifiedValidators) {
-        modifiedState.validators.get(validatorIndex).pubkey = Buffer.alloc(48, 0);
-      }
-      modifiedState.commit();
-
       bench({
         id: `${prefix} - ${testCaseName}`,
-        fn: () => {
-          const clonedState = modifiedState.clone();
+        beforeEach: ()  => {
+          const clonedState = state.clone();
+          for (const validatorIndex of expectedModifiedValidators) {
+            clonedState.validators.get(validatorIndex).pubkey = Buffer.alloc(48, 0);
+          }
+          clonedState.commit();    
+          return clonedState
+        },
+        fn: (clonedState) => {
           const validatorsBytes = Uint8Array.from(stateBytes.subarray(validatorsRange.start, validatorsRange.end));
           const validatorsBytes2 = clonedState.validators.serialize();
           const modifiedValidators: number[] = [];
