0b70c46601d068089d65c2356d97afa891cbd0b6
Cayman <caymannava@gmail.com>
2025-01-23 13:40:24 -0500

    chore: tweak serialized cache types

--- packages/beacon-node/src/util/serializedCache.ts
+++ packages/beacon-node/src/util/serializedCache.ts
@@ -4,14 +4,13 @@
  * This is a thin wrapper around WeakMap
  */
 export class SerializedCache {
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  map: WeakMap<any, Uint8Array> = new WeakMap();
+  map: WeakMap<object, Uint8Array> = new WeakMap();
 
-  get<T>(obj: T): Uint8Array | undefined {
+  get(obj: object): Uint8Array | undefined {
     return this.map.get(obj);
   }
 
-  set<T>(obj: T, serialized: Uint8Array): void {
+  set(obj: object, serialized: Uint8Array): void {
     this.map.set(obj, serialized);
   }
 
