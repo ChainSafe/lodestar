60fe2df230f477639e213198ea4cbd7a616b4230
Nazar Hussain <nazarhussain@gmail.com>
2025-01-29 21:33:50 +0100

    Fix lint error

--- packages/beacon-node/test/perf/chain/verifyImportBlocks.test.ts
+++ packages/beacon-node/test/perf/chain/verifyImportBlocks.test.ts
@@ -2,10 +2,10 @@ import {afterAll, beforeAll, bench, describe, setBenchOpts} from "@chainsafe/ben
 import {config} from "@lodestar/config/default";
 import {LevelDbController} from "@lodestar/db";
 import {SAFE_SLOTS_TO_IMPORT_OPTIMISTICALLY, SLOTS_PER_EPOCH} from "@lodestar/params";
-import {beforeValue} from "../../../../state-transition/test/utils/beforeValueBenchmark.js";
 import {sleep} from "@lodestar/utils";
 import {defaultOptions as defaultValidatorOptions} from "@lodestar/validator";
 import {rangeSyncTest} from "../../../../state-transition/test/perf/params.js";
+import {beforeValue} from "../../../../state-transition/test/utils/beforeValueBenchmark.js";
 import {getNetworkCachedBlock, getNetworkCachedState} from "../../../../state-transition/test/utils/testFileCache.js";
 import {AttestationImportOpt, BlockSource, getBlockInput} from "../../../src/chain/blocks/types.js";
 import {BeaconChain} from "../../../src/chain/index.js";
