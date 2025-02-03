5dadd14066a19a0392ff1bcee637a1891c46fb32
Nazar Hussain <nazarhussain@gmail.com>
2025-02-03 11:11:09 +0100

    Remove unused type

--- types/vcdiff-wasm/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "vcdiff-wasm" {
-  export default async function (): Promise<{
-    encoder: (s: Uint8Array, i: Uint8Array) => Uint8Array;
-    decoder: (s: Uint8Array, i: Uint8Array) => Uint8Array;
-  }>;
-}
