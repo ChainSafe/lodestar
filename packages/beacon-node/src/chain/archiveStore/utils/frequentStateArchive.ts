b5f1d159bb4658bca40e9fcde9866caea326e26a
Nazar Hussain <nazarhussain@gmail.com>
2025-02-07 17:33:12 +0100

    Move strategies/FrequencyStateArchiveStrategy to utils/frequentStateArchive

--- packages/beacon-node/src/chain/archiveStore/utils/frequentStateArchive.ts
+++ packages/beacon-node/src/chain/archiveStore/utils/frequentStateArchive.ts
@@ -34,51 +34,7 @@ export class FrequencyStateArchiveStrategy implements StateArchiveStrategy {
   async onFinalizedCheckpoint(_finalized: CheckpointWithHex, _metrics?: Metrics | null): Promise<void> {}
   async onCheckpoint(_stateRoot: RootHex, _metrics?: Metrics | null): Promise<void> {}
 
-  /**
-   * Persist states every some epochs to
-   * - Minimize disk space, storing the least states possible
-   * - Minimize the sync progress lost on unexpected crash, storing temp state every few epochs
-   *
-   * At epoch `e` there will be states peristed at intervals of `PERSIST_STATE_EVERY_EPOCHS` = 32
-   * and one at `PERSIST_TEMP_STATE_EVERY_EPOCHS` = 1024
-   * ```
-   *        |                |             |           .
-   * epoch - 1024*2    epoch - 1024    epoch - 32    epoch
-   * ```
-   */
-  async maybeArchiveState(finalized: CheckpointWithHex, metrics?: Metrics | null): Promise<void> {
-    const lastStoredSlot = await this.db.stateArchive.lastKey();
-    const lastStoredEpoch = computeEpochAtSlot(lastStoredSlot ?? 0);
-    const {archiveStateEpochFrequency} = this.opts;
-
-    if (finalized.epoch - lastStoredEpoch >= Math.min(PERSIST_TEMP_STATE_EVERY_EPOCHS, archiveStateEpochFrequency)) {
-      await this.archiveState(finalized, metrics);
-
-      // Only check the current and previous intervals
-      const minEpoch = Math.max(
-        0,
-        (Math.floor(finalized.epoch / archiveStateEpochFrequency) - 1) * archiveStateEpochFrequency
-      );
-
-      const storedStateSlots = await this.db.stateArchive.keys({
-        lt: computeStartSlotAtEpoch(finalized.epoch),
-        gte: computeStartSlotAtEpoch(minEpoch),
-      });
-
-      const statesSlotsToDelete = computeStateSlotsToDelete(storedStateSlots, archiveStateEpochFrequency);
-      if (statesSlotsToDelete.length > 0) {
-        await this.db.stateArchive.batchDelete(statesSlotsToDelete);
-      }
-
-      // More logs to investigate the rss spike issue https://github.com/ChainSafe/lodestar/issues/5591
-      this.logger.verbose("Archived state completed", {
-        finalizedEpoch: finalized.epoch,
-        minEpoch,
-        storedStateSlots: storedStateSlots.join(","),
-        statesSlotsToDelete: statesSlotsToDelete.join(","),
-      });
-    }
-  }
+ 
 
   /**
    * Archives finalized states from active bucket to archive bucket.
