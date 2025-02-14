ec00ce6885fe43c3b417de67300034467d79e846
Tuyen Nguyen <twoeths@users.noreply.github.com>
2025-02-14 09:10:06 +0700

    fix: loop in fetchUnknownBlockRoot

--- packages/beacon-node/src/sync/unknownBlock.ts
+++ packages/beacon-node/src/sync/unknownBlock.ts
@@ -548,8 +548,7 @@ export class UnknownBlockSync {
     let lastError: Error | null = null;
     let partialDownload = null;
     let fetchedPeerId = null;
-    // TODO: should it be loop through MAX_ATTEMPTS_PER_BLOCK instead?
-    for (let i = 0; i < 1; i++) {
+    for (let i = 0; i < MAX_ATTEMPTS_PER_BLOCK; i++) {
       const peer = shuffledPeers[i % shuffledPeers.length];
       if (partialDownload !== null) {
         const [prevBlockInput] = partialDownload.blocks;
