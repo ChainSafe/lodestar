7e21cdc0abf54d3046b067a67abd9031e701b1c5
Nico Flaig <nflaig@protonmail.com>
2025-02-20 20:06:21 +0000

    Simplify the filtering

--- packages/params/test/e2e/ensure-config-is-synced.test.ts
+++ packages/params/test/e2e/ensure-config-is-synced.test.ts
@@ -54,18 +54,14 @@ function assertCorrectPreset(localPreset: BeaconPreset, remotePreset: BeaconPres
 async function downloadRemoteConfig(preset: "mainnet" | "minimal", commit: string): Promise<BeaconPreset> {
   const downloadedParams = await Promise.all(
     Object.values(ForkName)
-      .map((forkName) => {
-        // TODO Fulu: check against remote presets
-        if (forkName !== ForkName.fulu) {
-          return axios({
-            url: `https://raw.githubusercontent.com/ethereum/consensus-specs/${commit}/presets/${preset}/${forkName}.yaml`,
-            timeout: 30 * 1000,
-          }).then((response) => loadConfigYaml(response.data));
-        }
-
-        return null;
-      })
-      .filter((a) => a !== null)
+      // TODO Fulu: check against remote presets
+      .filter((forkName) => forkName !== ForkName.fulu)
+      .map((forkName) =>
+        axios({
+          url: `https://raw.githubusercontent.com/ethereum/consensus-specs/${commit}/presets/${preset}/${forkName}.yaml`,
+          timeout: 30 * 1000,
+        }).then((response) => loadConfigYaml(response.data))
+      )
   );
 
   // Merge all the fetched yamls for the different forks
