08bdf4c61ac3e950f70cb600a21419c1b4a42503
matthewkeil <me@matthewkeil.com>
2025-02-21 02:21:07 +0700

    fix: check-types

--- packages/params/test/e2e/ensure-config-is-synced.test.ts
+++ packages/params/test/e2e/ensure-config-is-synced.test.ts
@@ -50,14 +50,18 @@ function assertCorrectPreset(localPreset: BeaconPreset, remotePreset: BeaconPres
 
 async function downloadRemoteConfig(preset: "mainnet" | "minimal", commit: string): Promise<BeaconPreset> {
   const downloadedParams = await Promise.all(
-    Object.values(ForkName).map((forkName) => {
-      if (forkName !== ForkName.fulu) {
-        return axios({
-          url: `https://raw.githubusercontent.com/ethereum/consensus-specs/${commit}/presets/${preset}/${forkName}.yaml`,
-          timeout: 30 * 1000,
-        }).then((response) => loadConfigYaml(response.data));
-      }
-    })
+    Object.values(ForkName)
+      .map((forkName) => {
+        if (forkName !== ForkName.fulu) {
+          return axios({
+            url: `https://raw.githubusercontent.com/ethereum/consensus-specs/${commit}/presets/${preset}/${forkName}.yaml`,
+            timeout: 30 * 1000,
+          }).then((response) => loadConfigYaml(response.data));
+        }
+
+        return null;
+      })
+      .filter((a) => a !== null)
   );
 
   // Merge all the fetched yamls for the different forks
