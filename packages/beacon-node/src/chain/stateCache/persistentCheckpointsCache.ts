cd8f64926e5269e079229b3422262f62a9b58125
Tuyen Nguyen <twoeths@users.noreply.github.com>
2025-03-02 09:54:19 +0700

    chore: more log

--- packages/beacon-node/src/chain/stateCache/persistentCheckpointsCache.ts
+++ packages/beacon-node/src/chain/stateCache/persistentCheckpointsCache.ts
@@ -524,6 +524,8 @@ export class PersistentCheckpointStateCache implements CheckpointStateCache {
     const maxEpoch = Math.max(...Array.from(this.epochIndex.keys()));
     const reloadedCpSlot = computeStartSlotAtEpoch(reloadedCp.epoch);
     let firstState: CachedBeaconStateAllForks | null = null;
+    const logCtx = {cpEpoch: reloadedCp.epoch, cpRoot: reloadedCp.rootHex};
+
     // no need to check epochs before `maxEpoch - this.maxEpochsInMemory + 1` before they are all persisted
     for (let epoch = maxEpoch - this.maxEpochsInMemory + 1; epoch <= maxEpoch; epoch++) {
       // if there's at least 1 state in memory in an epoch, just return the 1st one
@@ -550,22 +552,20 @@ export class PersistentCheckpointStateCache implements CheckpointStateCache {
               reloadedCpSlot < state.slot &&
               toRootHex(getBlockRootAtSlot(state, reloadedCpSlot)) === reloadedCp.rootHex
             ) {
+              this.logger.verbose("Reload: use block state as seed state", {stateSlot: state.slot, ...logCtx});
               return state;
             }
           } catch (e) {
             // getBlockRootAtSlot may throw error
-            this.logger.debug(
-              "Error finding seed state to reload",
-              {epoch, root: rootHex, reloadedEpoch: reloadedCp.epoch, reloadedRoot: reloadedCp.rootHex},
-              e as Error
-            );
+            this.logger.debug("Error finding seed state to reload", {epoch, root: rootHex, ...logCtx}, e as Error);
           }
         }
       }
     }
 
+    // fallback to using the default seed state from block state cache
     const seedBlockState = this.blockStateCache.getSeedState();
-    this.logger.verbose("Reload: use block state as seed state", {slot: seedBlockState.slot});
+    this.logger.verbose("Reload: use default block state as seed state", {stateSlot: seedBlockState.slot, ...logCtx});
     return seedBlockState;
   }
 
