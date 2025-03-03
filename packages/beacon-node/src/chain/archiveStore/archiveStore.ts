d41f5bd23be80c68eb2a2db972f1a50641a89ee7
Nazar Hussain <nazarhussain@gmail.com>
2025-03-03 06:31:39 +0100

    Fix import names

--- packages/beacon-node/src/chain/archiveStore/archiveStore.ts
+++ packages/beacon-node/src/chain/archiveStore/archiveStore.ts
@@ -5,7 +5,7 @@ import {Metrics} from "../../metrics/metrics.js";
 import {JobItemQueue} from "../../util/queue/index.js";
 import {ChainEvent} from "../emitter.js";
 import {IBeaconChain} from "../interface.js";
-import {archiveBlocks} from "./utils/archiveBlocks1.js";
+import {archiveBlocks} from "./utils/archiveBlocks.js";
 import {PROCESS_FINALIZED_CHECKPOINT_QUEUE_LEN} from "./constants.js";
 import {ArchiveMode, ArchiverOpts, StateArchiveStrategy} from "./interface.js";
 import {pruneHistory} from "./utils/pruneHistory.js";
