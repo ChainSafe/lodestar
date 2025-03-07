34a12f733d61506989e242e356b49eba8a7d415e
Tuyen Nguyen <twoeths@users.noreply.github.com>
2025-03-07 09:49:22 +0700

    fix: head sync - do not always start from local head

--- packages/beacon-node/src/sync/range/range.ts
+++ packages/beacon-node/src/sync/range/range.ts
@@ -113,7 +113,7 @@ export class RangeSync extends (EventEmitter as {new (): RangeSyncEmitter}) {
    */
   addPeer(peerId: PeerIdStr, localStatus: phase0.Status, peerStatus: phase0.Status): void {
     // Compute if we should do a Finalized or Head sync with this peer
-    const {syncType, startEpoch, target} = getRangeSyncTarget(localStatus, peerStatus, this.chain.forkChoice);
+    const {syncType, startEpoch, target} = getRangeSyncTarget(localStatus, peerStatus, this.chain);
     this.logger.debug("Sync peer joined", {
       peer: peerId,
       syncType,
