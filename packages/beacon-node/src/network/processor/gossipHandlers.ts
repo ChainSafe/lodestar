fb591527ee96bc94f7fda6877b8b5a3521ddfcd7
Nico Flaig <nflaig@protonmail.com>
2025-03-11 02:00:20 +0000

    fix: use correct subnet when validating gossip attestations (#7543)
    Fix a bug in gossip validation of electra attestations

--- packages/beacon-node/src/network/processor/gossipHandlers.ts
+++ packages/beacon-node/src/network/processor/gossipHandlers.ts
@@ -615,18 +615,18 @@ function getBatchHandlers(modules: ValidatorFnsModules, options: GossipHandlerOp
         return results;
       }
       // all attestations should have same attestation data as filtered by network processor
-      const {subnet, fork} = gossipHandlerParams[0].topic;
+      const {fork} = gossipHandlerParams[0].topic;
       const validationParams = gossipHandlerParams.map((param) => ({
         attestation: null,
         serializedData: param.gossipData.serializedData,
         attSlot: param.gossipData.msgSlot,
         attDataBase64: param.gossipData.indexed,
+        subnet: param.topic.subnet,
       })) as GossipAttestation[];
       const {results: validationResults, batchableBls} = await validateGossipAttestationsSameAttData(
         fork,
         chain,
-        validationParams,
-        subnet
+        validationParams
       );
       for (const [i, validationResult] of validationResults.entries()) {
         if (validationResult.err) {
@@ -647,6 +647,7 @@ function getBatchHandlers(modules: ValidatorFnsModules, options: GossipHandlerOp
         } = validationResult.result;
         metrics?.registerGossipUnaggregatedAttestation(gossipHandlerParams[i].seenTimestampSec, indexedAttestation);
 
+        const {subnet} = validationResult.result;
         try {
           // Node may be subscribe to extra subnets (long-lived random subnets). For those, validate the messages
           // but don't add to attestation pool, to save CPU and RAM
