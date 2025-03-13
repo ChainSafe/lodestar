538806e36c1f9e2013248cb695251fc40394241b
Cayman <caymannava@gmail.com>
2025-03-13 11:37:31 -0400

    chore: add holesky-revival pseudo-fork

--- packages/state-transition/src/util/validator.ts
+++ packages/state-transition/src/util/validator.ts
@@ -67,6 +67,14 @@ export function getBalanceChurnLimit(epochCtx: EpochCache): number {
 }
 
 export function getActivationExitChurnLimit(epochCtx: EpochCache): number {
+  /**
+   * This is a pseudo-fork to support the "hack" option D discussed here:
+   * https://notes.ethereum.org/@ethpandaops/post-finality-path-forward-holesky
+   */
+  const HOLESKY_REVIVAL_EPOCH = Infinity;
+  if (epochCtx.epoch >= HOLESKY_REVIVAL_EPOCH) {
+    return epochCtx.config.MAX_PER_EPOCH_ACTIVATION_EXIT_CHURN_LIMIT;
+  }
   return Math.min(epochCtx.config.MAX_PER_EPOCH_ACTIVATION_EXIT_CHURN_LIMIT, getBalanceChurnLimit(epochCtx));
 }
 
