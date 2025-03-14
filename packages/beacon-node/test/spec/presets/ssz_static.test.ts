435f2d6ed73ec9d13a5cfe8f98fd2fc2cb4deaf3
Nico Flaig <nflaig@protonmail.com>
2025-03-14 22:47:45 +0000

    Adjust skipped forks whisk --> eip7441

--- packages/beacon-node/test/spec/presets/ssz_static.test.ts
+++ packages/beacon-node/test/spec/presets/ssz_static.test.ts
@@ -41,6 +41,7 @@ const sszStatic =
     if (skippedTypes?.includes(typeName)) {
       return;
     }
+    console.log(fork);
 
     /* eslint-disable @typescript-eslint/strict-boolean-expressions */
     const sszType =
@@ -80,7 +81,7 @@ const sszStatic =
 specTestIterator(path.join(ethereumConsensusSpecsTests.outputDir, "tests", ACTIVE_PRESET), {
   ssz_static: {
     type: RunnerType.custom,
-    // starting from v1.4.0-beta.6, there is "whisk" fork in ssz_static tests but we ignore them
-    fn: sszStatic("whisk"),
+    // starting from v1.5.0-beta.3, there is "eip7441" fork in ssz_static tests but we ignore them
+    fn: sszStatic("eip7441"),
   },
 });
