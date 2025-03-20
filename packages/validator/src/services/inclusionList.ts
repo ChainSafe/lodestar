fdb9dd896cb08b5c46d6058f64ce8b049efdf797
NC <17676176+ensi321@users.noreply.github.com>
2025-03-19 21:56:22 -0700

    Fix empty IL

--- packages/validator/src/services/inclusionList.ts
+++ packages/validator/src/services/inclusionList.ts
@@ -51,7 +51,7 @@ export class InclusionListService {
     // (a) the validator has received a valid block from the expected block proposer for the assigned slot or
     // (b) one-third of the slot has transpired (SECONDS_PER_SLOT / 3 seconds after the start of slot) -- whichever comes first.
     // TODO EIP-7805: Review this timing. Spec says only mandates us to broadcast before 11s
-    await Promise.race([sleep(this.clock.msToSlot(slot + 1 / 3), signal), this.emitter.waitForBlockSlot(slot)]);
+    await sleep(this.clock.msToSlot(slot + 2 / 3), signal);
 
     // If there is more than one duty, all validators on duty will sign and publish the same IL
     const inclusionListTransactions = await this.produceInclusionList(slot);
