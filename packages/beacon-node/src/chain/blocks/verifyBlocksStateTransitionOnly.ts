be256e1bffe4109f5d17fa811359f8cc38fcc38f
Tuyen Nguyen <twoeths@users.noreply.github.com>
2025-03-25 09:11:08 +0700

    fix: remove incorrect comment

--- packages/beacon-node/src/chain/blocks/verifyBlocksStateTransitionOnly.ts
+++ packages/beacon-node/src/chain/blocks/verifyBlocksStateTransitionOnly.ts
@@ -69,7 +69,6 @@ export async function verifyBlocksStateTransitionOnly(
     const hashTreeRootTimer = metrics?.stateHashTreeRootTime.startTimer({
       source: StateHashTreeRootSource.blockTransition,
     });
-    // state root is computed inside stateTransition(), so it should take no time here
     const stateRoot = postState.batchHashTreeRoot(blockHCGroup);
     hashTreeRootTimer?.();
 
