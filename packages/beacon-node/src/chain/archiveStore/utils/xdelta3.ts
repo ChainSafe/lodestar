ef9dc75a293624bba5945fa8fd5f4de1e04fdde3
Nazar Hussain <nazarhussain@gmail.com>
2025-04-28 15:58:08 +0200

    Apply suggestions from code review
    Co-authored-by: NC <17676176+ensi321@users.noreply.github.com>

--- packages/beacon-node/src/chain/archiveStore/utils/xdelta3.ts
+++ packages/beacon-node/src/chain/archiveStore/utils/xdelta3.ts
@@ -6,7 +6,7 @@ export class XDelta3Codec implements IStateDiffCodec {
     try {
       return encodeSync(base, changed);
     } catch (err) {
-      throw new Error(`Can not compute binary diff error=${(err as Error).message}`);
+      throw new Error(`Cannot compute binary diff error=${(err as Error).message}`);
     }
   }
 
@@ -14,7 +14,7 @@ export class XDelta3Codec implements IStateDiffCodec {
     try {
       return decodeSync(base, delta);
     } catch (err) {
-      throw new Error(`Can not apply binary diff patch error=${(err as Error).message}`);
+      throw new Error(`Cannot apply binary diff patch error=${(err as Error).message}`);
     }
   }
 }
