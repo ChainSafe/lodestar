8064c0b8818abe06462c889f3af10007594798d6
Ijay Abby <inyangasi4@gmail.com>
2025-04-30 17:45:36 +0300

    Fix testcase to parseInt

--- packages/beacon-node/test/unit/execution/engine/types.test.ts
+++ packages/beacon-node/test/unit/execution/engine/types.test.ts
@@ -17,9 +17,9 @@ describe("execution / engine / types", () => {
 
       // Assert 1-byte request_type prefix is set correctly
       expect(serialized.length).toBe(3);
-      expect(Number(serialized[0].substring(0, 2))).toBe(DEPOSIT_REQUEST_TYPE);
-      expect(Number(serialized[1].substring(0, 2))).toBe(WITHDRAWAL_REQUEST_TYPE);
-      expect(Number(serialized[2].substring(0, 2))).toBe(CONSOLIDATION_REQUEST_TYPE);
+      expect(parseInt(serialized[0].substring(0, 2))).toBe(DEPOSIT_REQUEST_TYPE);
+      expect(parseInt(serialized[1].substring(0, 2))).toBe(WITHDRAWAL_REQUEST_TYPE);
+      expect(parseInt(serialized[2].substring(0, 2))).toBe(CONSOLIDATION_REQUEST_TYPE);
 
       // Assert execution requests can be deserialized
       expect(ssz.electra.DepositRequests.deserialize(fromHex(serialized[0].slice(2)))).toEqual(
@@ -44,8 +44,8 @@ describe("execution / engine / types", () => {
 
       // Assert withdrawals are omitted
       expect(serialized.length).toBe(2);
-      expect(Number(serialized[0].substring(0, 2))).toBe(DEPOSIT_REQUEST_TYPE);
-      expect(Number(serialized[1].substring(0, 2))).toBe(CONSOLIDATION_REQUEST_TYPE);
+      expect(parseInt(serialized[0].substring(0, 2))).toBe(DEPOSIT_REQUEST_TYPE);
+      expect(parseInt(serialized[1].substring(0, 2))).toBe(CONSOLIDATION_REQUEST_TYPE);
 
       // Assert execution requests can be deserialized
       expect(ssz.electra.DepositRequests.deserialize(fromHex(serialized[0].slice(2)))).toEqual(
