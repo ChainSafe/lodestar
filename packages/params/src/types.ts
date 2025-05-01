4e948f3615c8663ac71eb25a748e9d043acfa0d1
Cayman <caymannava@gmail.com>
2025-05-01 17:30:19 -0400

    chore: apply suggestions from code review
    Co-authored-by: Nico Flaig <nflaig@protonmail.com>
    Co-authored-by: Matthew Keil <me@matthewkeil.com>

--- packages/params/src/types.ts
+++ packages/params/src/types.ts
@@ -95,6 +95,7 @@ export type BeaconPreset = {
   PENDING_CONSOLIDATIONS_LIMIT: number;
   MAX_CONSOLIDATION_REQUESTS_PER_PAYLOAD: number;
   WHISTLEBLOWER_REWARD_QUOTIENT_ELECTRA: number;
+
   // FULU
   ///////////
   FIELD_ELEMENTS_PER_CELL: number;
@@ -200,6 +201,7 @@ export const beaconPresetTypes: BeaconPresetTypes = {
   PENDING_CONSOLIDATIONS_LIMIT: "number",
   MAX_CONSOLIDATION_REQUESTS_PER_PAYLOAD: "number",
   WHISTLEBLOWER_REWARD_QUOTIENT_ELECTRA: "number",
+
   // FULU
   ///////////
   FIELD_ELEMENTS_PER_CELL: "number",
