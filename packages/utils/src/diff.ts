fc73731eca065bbc845a7476e67c3035b83e0554
Nazar Hussain <nazarhussain@gmail.com>
2025-05-07 17:24:44 +0200

    Update the usage of console log check

--- packages/utils/src/diff.ts
+++ packages/utils/src/diff.ts
@@ -203,12 +203,12 @@ export function getDiffs(val1: Diffable, val2: Diffable, objectPath: string): Di
  */
 export function diff(val1: unknown, val2: unknown, outputValues = false, filename?: string): void {
   if (!isDiffable(val1)) {
-    // biome-ignore lint/suspicious/noConsoleLog: <explanation>
+    // biome-ignore lint/suspicious/noConsole: <explanation>
     console.log("val1 is not Diffable");
     return;
   }
   if (!isDiffable(val2)) {
-    // biome-ignore lint/suspicious/noConsoleLog: <explanation>
+    // biome-ignore lint/suspicious/noConsole: <explanation>
     console.log("val2 is not Diffable");
     return;
   }
@@ -228,7 +228,7 @@ export function diff(val1: unknown, val2: unknown, outputValues = false, filenam
     if (filename) {
       fs.writeFileSync(filename, output);
     } else {
-      // biome-ignore lint/suspicious/noConsoleLog: <explanation>
+      // biome-ignore lint/suspicious/noConsole: <explanation>
       console.log(output);
     }
   }
