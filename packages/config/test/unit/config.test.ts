58df57c354460f3f0112ecf033ef9c97ca52297c
NC <17676176+ensi321@users.noreply.github.com>
2025-05-20 23:56:48 -0700

    Make unit test run on fulu fork

--- packages/config/test/unit/config.test.ts
+++ packages/config/test/unit/config.test.ts
@@ -1,17 +1,33 @@
-import {describe, expect, it} from "vitest";
+import {beforeAll, describe, expect, it} from "vitest";
 import {chainConfig} from "../../src/default.js";
-import {createForkConfig} from "../../src/index.js";
+import {ChainConfig, createForkConfig} from "../../src/index.js";
 
 describe("getMaxBlobsPerBlock", () => {
+  let defaultConfig: ChainConfig;
+
+  beforeAll(() => {
+    // Force tests to run on fulu fork
+    // TODO Fulu: getMaxBlobsPerBlock's result is hardcoded for deneb and electra in fusaka devnet-0. So we need to define
+    // defaultConfig to force tests to run on fulu to get expected result. Remove this after devnet-0.
+    defaultConfig = {
+      ...chainConfig,
+      ALTAIR_FORK_EPOCH: 0,
+      BELLATRIX_FORK_EPOCH: 0,
+      CAPELLA_FORK_EPOCH: 0,
+      DENEB_FORK_EPOCH: 0,
+      ELECTRA_FORK_EPOCH: 0,
+      FULU_FORK_EPOCH: 0,
+    }
+  })
   it("should throw an error if BLOB_SCHEDULE is empty", () => {
-    const config = createForkConfig({...chainConfig, BLOB_SCHEDULE: []});
+    const config = createForkConfig({...defaultConfig, BLOB_SCHEDULE: []});
 
     expect(() => config.getMaxBlobsPerBlock(0)).toThrowError();
   });
 
   it("should return same value for passed epochs if there is only a single BLOB_SCHEDULE entry", () => {
     const config = createForkConfig({
-      ...chainConfig,
+      ...defaultConfig,
       BLOB_SCHEDULE: [{EPOCH: 0, MAX_BLOBS_PER_BLOCK: 10}],
     });
 
@@ -22,7 +38,7 @@ describe("getMaxBlobsPerBlock", () => {
 
   it("should select correct value for passed epoch based on BLOB_SCHEDULE thresholds", () => {
     const config = createForkConfig({
-      ...chainConfig,
+      ...defaultConfig,
       BLOB_SCHEDULE: [
         {EPOCH: 0, MAX_BLOBS_PER_BLOCK: 1},
         {EPOCH: 2, MAX_BLOBS_PER_BLOCK: 2},
@@ -40,7 +56,7 @@ describe("getMaxBlobsPerBlock", () => {
 
   it("should return correct values if BLOB_SCHEDULE entries are unsorted", () => {
     const config = createForkConfig({
-      ...chainConfig,
+      ...defaultConfig,
       BLOB_SCHEDULE: [
         {EPOCH: 15, MAX_BLOBS_PER_BLOCK: 1},
         {EPOCH: 5, MAX_BLOBS_PER_BLOCK: 3},
@@ -57,7 +73,7 @@ describe("getMaxBlobsPerBlock", () => {
 
   it("should return minimum value if epoch is below lowest configured BLOB_SCHEDULE epoch", () => {
     const config = createForkConfig({
-      ...chainConfig,
+      ...defaultConfig,
       BLOB_SCHEDULE: [
         {EPOCH: 5, MAX_BLOBS_PER_BLOCK: 3},
         {EPOCH: 10, MAX_BLOBS_PER_BLOCK: 5},
