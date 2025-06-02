8fe4573fc63deb9b087319dc278ad767dfc08748
NC <17676176+ensi321@users.noreply.github.com>
2025-06-02 15:33:43 -0700

    add processProposerLookahead

--- packages/state-transition/src/epoch/processProposerLookahead.ts
+++ packages/state-transition/src/epoch/processProposerLookahead.ts
@@ -1,15 +1,22 @@
-import {aggregateSerializedPublicKeys} from "@chainsafe/blst";
-import {EPOCHS_PER_SYNC_COMMITTEE_PERIOD, ForkSeq} from "@lodestar/params";
+import {ForkSeq, MIN_SEED_LOOKAHEAD, SLOTS_PER_EPOCH} from "@lodestar/params";
 import {ssz} from "@lodestar/types";
-import {CachedBeaconStateAltair} from "../types.js";
-import {getNextSyncCommitteeIndices} from "../util/seed.js";
+import {CachedBeaconStateFulu} from "../types.js";
+import {computeProposerIndices} from "../util/seed.js";
 
 /**
- * Rotate nextSyncCommittee to currentSyncCommittee if sync committee period is over.
- *
- * PERF: Once every `EPOCHS_PER_SYNC_COMMITTEE_PERIOD`, do an expensive operation to compute the next committee.
- * Calculating the next sync committee has a proportional cost to $VALIDATOR_COUNT
+ * This function updates the `proposer_lookahead` field in the beacon state
+ * by shifting out proposer indices from the earliest epoch and appending new
+ * proposer indices for the latest epoch. With `MIN_SEED_LOOKAHEAD` set to `1`, 
+ * this means that at the start of epoch `N`, the proposer lookahead for epoch 
+ * `N+1` will be computed and included in the beacon state's lookahead.
  */
-// TODO Fulu: Implement this
-export function processProposerLookahead(fork: ForkSeq, state: CachedBeaconStateAltair): void {
+export function processProposerLookahead(fork: ForkSeq, state: CachedBeaconStateFulu): void {
+  // Shift out proposers in the first epoch
+  const remainingProposerLookahead = state.proposerLookahead.getAll().slice(SLOTS_PER_EPOCH);
+
+  // Fill in the last epoch with new proposer indices
+  const lastEpochProposerLookahead = computeProposerIndices(fork, state, state.epochCtx.epoch + MIN_SEED_LOOKAHEAD + 1);
+
+  state.proposerLookahead = ssz.fulu.ProposerLookahead.toViewDU([...remainingProposerLookahead, ...lastEpochProposerLookahead]);
+
 }
