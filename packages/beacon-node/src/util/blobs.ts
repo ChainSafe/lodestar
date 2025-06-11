c7023bc31a9bc9bbf79f401c8f4f130e02f5f557
matthewkeil <me@matthewkeil.com>
2025-06-11 22:01:17 +0200

    feat: add fulu column functions for db unit tests

--- packages/beacon-node/src/util/blobs.ts
+++ packages/beacon-node/src/util/blobs.ts
@@ -1,9 +1,17 @@
 import {digest as sha256Digest} from "@chainsafe/as-sha256";
 import {Tree} from "@chainsafe/persistent-merkle-tree";
 import {ChainForkConfig} from "@lodestar/config";
-import {ForkAll, ForkName, KZG_COMMITMENT_GINDEX0, VERSIONED_HASH_VERSION_KZG} from "@lodestar/params";
+import {
+  ForkAll,
+  ForkName,
+  KZG_COMMITMENTS_GINDEX,
+  KZG_COMMITMENT_GINDEX0,
+  NUMBER_OF_COLUMNS,
+  VERSIONED_HASH_VERSION_KZG,
+} from "@lodestar/params";
 import {signedBlockToSignedHeader} from "@lodestar/state-transition";
-import {BeaconBlockBody, SSZTypesFor, SignedBeaconBlock, deneb, ssz} from "@lodestar/types";
+import {BeaconBlockBody, SSZTypesFor, SignedBeaconBlock, deneb, fulu, ssz} from "@lodestar/types";
+import {kzg} from "./rustKzg.js";
 
 type VersionHash = Uint8Array;
 
@@ -22,6 +30,14 @@ export function computeInclusionProof(
   const bodyView = (ssz[fork].BeaconBlockBody as SSZTypesFor<ForkAll, "BeaconBlockBody">).toView(body);
   const commitmentGindex = KZG_COMMITMENT_GINDEX0 + index;
   return new Tree(bodyView.node).getSingleProof(BigInt(commitmentGindex));
+}
+
+export function computeKzgCommitmentsInclusionProof(
+  fork: ForkName,
+  body: BeaconBlockBody
+): fulu.KzgCommitmentsInclusionProof {
+  const bodyView = (ssz[fork].BeaconBlockBody as SSZTypesFor<ForkAll, "BeaconBlockBody">).toView(body);
+  return new Tree(bodyView.node).getSingleProof(BigInt(KZG_COMMITMENTS_GINDEX));
 }
 
 export function computeBlobSidecars(
@@ -44,5 +60,53 @@ export function computeBlobSidecars(
       contents.kzgCommitmentInclusionProofs?.[index] ?? computeInclusionProof(fork, signedBlock.message.body, index);
 
     return {index, blob, kzgCommitment, kzgProof, signedBlockHeader, kzgCommitmentInclusionProof};
+  });
+}
+
+/**
+ * Turns a SignedBeaconBlock and an array of Blobs from a given slot into an array of
+ * DataColumnSidecars that are ready to be served by gossip and req/resp.
+ *
+ * Implementation of get_data_column_sidecars
+ * https://github.com/ethereum/consensus-specs/blob/dev/specs/_features/eip7594/das-core.md#get_data_column_sidecars
+ */
+export function computeDataColumnSidecars(
+  config: ChainForkConfig,
+  signedBlock: SignedBeaconBlock,
+  contents: fulu.Contents & {kzgCommitmentsInclusionProof?: fulu.KzgCommitmentsInclusionProof; cells?: fulu.Cell[][]}
+): fulu.DataColumnSidecars {
+  const blobKzgCommitments = (signedBlock as deneb.SignedBeaconBlock).message.body.blobKzgCommitments;
+  if (blobKzgCommitments === undefined) {
+    throw Error("Invalid block with missing blobKzgCommitments for computeBlobSidecars");
+  }
+  if (blobKzgCommitments.length === 0) {
+    return [];
+  }
+  const fork = config.getForkName(signedBlock.message.slot);
+  const signedBlockHeader = signedBlockToSignedHeader(config, signedBlock);
+  const kzgCommitmentsInclusionProof =
+    contents.kzgCommitmentsInclusionProof ?? computeKzgCommitmentsInclusionProof(fork, signedBlock.message.body);
+  const {blobs, kzgProofs} = contents;
+  const cellsAndProofs = Array.from({length: blobs.length}, (_, rowNumber) => {
+    const cells = contents.cells?.[rowNumber] ?? kzg.computeCells(blobs[rowNumber]);
+    const proofs = kzgProofs.slice(rowNumber * NUMBER_OF_COLUMNS, (rowNumber + 1) * NUMBER_OF_COLUMNS);
+    return {cells, proofs};
+  });
+
+  return Array.from({length: NUMBER_OF_COLUMNS}, (_, columnIndex) => {
+    // columnIndex'th column
+    const column = Array.from({length: blobs.length}, (_, rowNumber) => cellsAndProofs[rowNumber].cells[columnIndex]);
+    const kzgProofs = Array.from(
+      {length: blobs.length},
+      (_, rowNumber) => cellsAndProofs[rowNumber].proofs[columnIndex]
+    );
+    return {
+      index: columnIndex,
+      column,
+      kzgCommitments: blobKzgCommitments,
+      kzgProofs,
+      signedBlockHeader,
+      kzgCommitmentsInclusionProof,
+    };
   });
 }
