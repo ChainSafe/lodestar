701f8a970a3f8d70747d5ed2528eec7fb069cd26
Nico Flaig <nflaig@protonmail.com>
2025-06-12 13:02:53 +0200

    revert changes to blob sidecar event

--- packages/beacon-node/src/chain/blocks/importBlock.ts
+++ packages/beacon-node/src/chain/blocks/importBlock.ts
@@ -436,19 +436,22 @@ export async function importBlock(
       }
       if (
         blockInput.type === BlockInputType.availableData &&
-        this.emitter.listenerCount(routes.events.EventType.blobSidecar) &&
-        (blockInput.blockData.fork === ForkName.deneb || blockInput.blockData.fork === ForkName.electra)
+        this.emitter.listenerCount(routes.events.EventType.blobSidecar)
       ) {
-        const {blobs} = blockInput.blockData;
-        for (const blobSidecar of blobs) {
-          const {index, kzgCommitment} = blobSidecar;
-          this.emitter.emit(routes.events.EventType.blobSidecar, {
-            blockRoot: blockRootHex,
-            slot: blockSlot,
-            index,
-            kzgCommitment: toHexString(kzgCommitment),
-            versionedHash: toHexString(kzgCommitmentToVersionedHash(kzgCommitment)),
-          });
+        if (blockInput.blockData.fork === ForkName.deneb || blockInput.blockData.fork === ForkName.electra) {
+          const {blobs} = blockInput.blockData;
+          for (const blobSidecar of blobs) {
+            const {index, kzgCommitment} = blobSidecar;
+            this.emitter.emit(routes.events.EventType.blobSidecar, {
+              blockRoot: blockRootHex,
+              slot: blockSlot,
+              index,
+              kzgCommitment: toHexString(kzgCommitment),
+              versionedHash: toHexString(kzgCommitmentToVersionedHash(kzgCommitment)),
+            });
+          }
+        } else {
+          // TODO add event for datacolumns
         }
       }
     });
