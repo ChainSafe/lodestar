37c7603991046be706fd1255f41139900f511d5b
Nico Flaig <nflaig@protonmail.com>
2025-07-25 16:06:22 +0100

    Move commit after afterProcessEpoch

--- packages/state-transition/src/stateTransition.ts
+++ packages/state-transition/src/stateTransition.ts
@@ -246,14 +246,6 @@ function processSlotsWithTransientCache(
 
       postState.slot++;
 
-      // Running commit here is not strictly necessary. The cost of running commit twice (here + after process block)
-      // Should be negligible but gives better metrics to differentiate the cost of it for block and epoch proc.
-      {
-        const timer = metrics?.epochTransitionCommitTime.startTimer();
-        postState.commit();
-        timer?.();
-      }
-
       // Upgrade state if exactly at epoch boundary
       const stateEpoch = computeEpochAtSlot(postState.slot);
       if (stateEpoch === config.ALTAIR_FORK_EPOCH) {
@@ -278,6 +270,14 @@ function processSlotsWithTransientCache(
       {
         const timer = metrics?.epochTransitionStepTime.startTimer({step: EpochTransitionStep.afterProcessEpoch});
         postState.epochCtx.afterProcessEpoch(postState, epochTransitionCache);
+        timer?.();
+      }
+
+      // Running commit here is not strictly necessary. The cost of running commit twice (here + after process block)
+      // Should be negligible but gives better metrics to differentiate the cost of it for block and epoch proc.
+      {
+        const timer = metrics?.epochTransitionCommitTime.startTimer();
+        postState.commit();
         timer?.();
       }
 
