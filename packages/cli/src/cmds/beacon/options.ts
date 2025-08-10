55d3f830c080677bc6dd56a365a9242be3cd4cd5
Nico Flaig <nflaig@protonmail.com>
2025-08-10 11:48:31 +0100

    Tweak description

--- packages/cli/src/cmds/beacon/options.ts
+++ packages/cli/src/cmds/beacon/options.ts
@@ -127,7 +127,7 @@ export const beaconExtraOptions: CliCommandOptions<BeaconExtraArgs> = {
 
   persistNetworkIdentity: {
     description:
-      "Whether to reuse the same peer-id across restarts. Peer das requires cgc to persist relative to a given ENR. Setting to false will reset ENR and also peer das validator custody group count",
+      "Whether to reuse the same peer-id across restarts. Validator custody requires custody group count to persist relative to a given ENR. Setting to false will reset ENR and validator custody requirements on restarts.",
     default: true,
     type: "boolean",
   },
