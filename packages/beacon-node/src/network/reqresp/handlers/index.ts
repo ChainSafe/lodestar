193ab536bdcf5ef0bf7ceae8cf708d5db5aab0ff
Katya Ryazantseva <sibkatya@gmail.com>
2025-08-21 20:45:59 +0200

    fix: lint

--- packages/beacon-node/src/network/reqresp/handlers/index.ts
+++ packages/beacon-node/src/network/reqresp/handlers/index.ts
@@ -56,7 +56,7 @@ export function getReqRespHandlers({db, chain}: {db: IBeaconDb; chain: IBeaconCh
     },
     [ReqRespMethod.LightClientFinalityUpdate]: () => onLightClientFinalityUpdate(chain),
     [ReqRespMethod.LightClientOptimisticUpdate]: () => onLightClientOptimisticUpdate(chain),
-    [ReqRespMethod.InclusionListByCommitteeIndices]: (req) => {
+    [ReqRespMethod.InclusionListByCommitteeIndices]: (_req) => {
       //const _body = ssz.eip7805.InclusionListByCommitteeIndicesRequest.deserialize(req.data);
       // TODO EIP-7805: Implement this
       throw Error("InclusionListByCommitteeIndices is not implemented");
