5f3d260bb370b23a4976440f01e6ea181e2c6d5f
Nico Flaig <nflaig@protonmail.com>
2025-08-31 11:27:11 +0100

    Start sim tests from electra fork

--- packages/cli/test/sim/mixedClient.test.ts
+++ packages/cli/test/sim/mixedClient.test.ts
@@ -5,18 +5,15 @@ import {Simulation} from "../utils/crucible/simulation.js";
 import {defineSimTestConfig, logFilesDir} from "../utils/crucible/utils/index.js";
 import {connectAllNodes, waitForSlot} from "../utils/crucible/utils/network.js";
 
-const altairForkEpoch = 2;
-const bellatrixForkEpoch = 4;
-const capellaForkEpoch = 6;
-const denebForkEpoch = 8;
-const runTillEpoch = 10;
+const runTillEpoch = 4;
 const syncWaitEpoch = 2;
 
 const {estimatedTimeoutMs, forkConfig} = defineSimTestConfig({
-  ALTAIR_FORK_EPOCH: altairForkEpoch,
-  BELLATRIX_FORK_EPOCH: bellatrixForkEpoch,
-  CAPELLA_FORK_EPOCH: capellaForkEpoch,
-  DENEB_FORK_EPOCH: denebForkEpoch,
+  ALTAIR_FORK_EPOCH: 0,
+  BELLATRIX_FORK_EPOCH: 0,
+  CAPELLA_FORK_EPOCH: 0,
+  DENEB_FORK_EPOCH: 0,
+  ELECTRA_FORK_EPOCH: 0,
   runTillEpoch: runTillEpoch + syncWaitEpoch,
   initialNodes: 2,
 });
@@ -61,8 +58,8 @@ await env.start({runTimeoutMs: estimatedTimeoutMs});
 await connectAllNodes(env.nodes);
 
 // Stopping at last slot usually cause assertion to fail because of missing data as node are shutting down
-await waitForSlot("Waiting for the one additional epoch for capellaFork", {
-  slot: env.clock.getLastSlotOfEpoch(capellaForkEpoch + 1) + 2,
+await waitForSlot("Waiting for the simulation to complete", {
+  slot: env.clock.getLastSlotOfEpoch(runTillEpoch) + 2,
   env,
 });
 
