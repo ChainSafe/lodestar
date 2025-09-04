8b13bcadb4f5a9ce8de2ebac99b76bba468f508d
Tuyen Nguyen <twoeths@users.noreply.github.com>
2025-09-04 16:20:15 +0700

    chore: test DataColumnSidecar dbs

--- packages/beacon-node/test/unit/db/api/repositories/dataColumn.test.ts
+++ packages/beacon-node/test/unit/db/api/repositories/dataColumn.test.ts
@@ -1,6 +1,6 @@
 import {createChainForkConfig} from "@lodestar/config";
 import {LevelDbController} from "@lodestar/db";
-import {ssz} from "@lodestar/types";
+import {Root, fulu, ssz} from "@lodestar/types";
 import {toHex} from "@lodestar/utils";
 import {rimraf} from "rimraf";
 import {afterEach, beforeEach, describe, expect, it} from "vitest";
@@ -8,6 +8,7 @@ import {DataColumnSidecarRepository} from "../../../../../src/db/repositories/da
 import {getDataColumnSidecarsFromBlock} from "../../../../../src/util/dataColumns.js";
 import {kzg} from "../../../../../src/util/kzg.js";
 import {testLogger} from "../../../../utils/logger.js";
+import {DataColumnSidecarArchiveRepository} from "../../../../../src/db/repositories/dataColumnSidecarArchive.js";
 
 /* eslint-disable @typescript-eslint/naming-convention */
 const config = createChainForkConfig({
@@ -19,11 +20,32 @@ const config = createChainForkConfig({
 describe("block archive repository", () => {
   const testDir = "./.tmp";
   let dataColumnRepo: DataColumnSidecarRepository;
+  let dataColumnArchiveRepo: DataColumnSidecarArchiveRepository;
   let db: LevelDbController;
+  let blockRoot: Root;
+  let allDataColumnSidecars: fulu.DataColumnSidecars;
+  const blobKzgCommitmentsLen = 3;
+  const dataColumn = ssz.fulu.DataColumnSidecar.defaultValue();
+  const blockSlot = 11;
+  dataColumn.signedBlockHeader.message.slot = blockSlot;
 
   beforeEach(async () => {
     db = await LevelDbController.create({name: testDir}, {logger: testLogger()});
     dataColumnRepo = new DataColumnSidecarRepository(config, db);
+    dataColumnArchiveRepo = new DataColumnSidecarArchiveRepository(config, db);
+
+    blockRoot = ssz.phase0.BeaconBlockHeader.hashTreeRoot(dataColumn.signedBlockHeader.message);
+    const blob = ssz.deneb.Blob.defaultValue();
+    const commitment = kzg.blobToKzgCommitment(blob);
+    const signedBlock = ssz.fulu.SignedBeaconBlock.defaultValue();
+    const blobs = [blob, blob, blob];
+    const commitments = Array.from({length: blobKzgCommitmentsLen}, () => commitment);
+    signedBlock.message.body.blobKzgCommitments = commitments;
+    const cellsAndProofs = blobs.map((b) => kzg.computeCellsAndKzgProofs(b));
+    allDataColumnSidecars = getDataColumnSidecarsFromBlock(config, signedBlock, cellsAndProofs);
+    for (let j = 0; j < allDataColumnSidecars.length; j++) {
+      allDataColumnSidecars[j].index = j;
+    }
   });
 
   afterEach(async () => {
@@ -32,21 +54,6 @@ describe("block archive repository", () => {
   });
 
   it("should get data column sidecars by parent root", async () => {
-    const dataColumn = ssz.fulu.DataColumnSidecar.defaultValue();
-    const blockRoot = ssz.phase0.BeaconBlockHeader.hashTreeRoot(dataColumn.signedBlockHeader.message);
-    const blob = ssz.deneb.Blob.defaultValue();
-    const commitment = kzg.blobToKzgCommitment(blob);
-    const signedBlock = ssz.fulu.SignedBeaconBlock.defaultValue();
-    const blobs = [blob, blob, blob];
-    const commitments = [commitment, commitment, commitment];
-    signedBlock.message.body.blobKzgCommitments = commitments;
-    const cellsAndProofs = blobs.map((b) => kzg.computeCellsAndKzgProofs(b));
-
-    const allDataColumnSidecars = getDataColumnSidecarsFromBlock(config, signedBlock, cellsAndProofs);
-    for (let j = 0; j < allDataColumnSidecars.length; j++) {
-      allDataColumnSidecars[j].index = j;
-    }
-
     const dataColumnSidecars = allDataColumnSidecars.slice(0, 7);
     const dataColumnsLen = dataColumnSidecars.length;
 
@@ -58,22 +65,6 @@ describe("block archive repository", () => {
   });
 
   it("should get data column sidecars by matching bytes", async () => {
-    const dataColumn = ssz.fulu.DataColumnSidecar.defaultValue();
-    const blockRoot = ssz.phase0.BeaconBlockHeader.hashTreeRoot(dataColumn.signedBlockHeader.message);
-    const blob = ssz.deneb.Blob.defaultValue();
-    const commitment = kzg.blobToKzgCommitment(blob);
-    const signedBlock = ssz.fulu.SignedBeaconBlock.defaultValue();
-    const blobs = [blob, blob, blob];
-    const commitments = [commitment, commitment, commitment];
-    signedBlock.message.body.blobKzgCommitments = commitments;
-    const cellsAndProofs = blobs.map((b) => kzg.computeCellsAndKzgProofs(b));
-
-    const allDataColumnSidecars = getDataColumnSidecarsFromBlock(config, signedBlock, cellsAndProofs);
-    for (let j = 0; j < allDataColumnSidecars.length; j++) {
-      allDataColumnSidecars[j].index = j;
-    }
-
-    const blobKzgCommitmentsLen = 3;
     const columnsSize =
       ssz.fulu.DataColumnSidecar.minSize +
       blobKzgCommitmentsLen *
@@ -92,6 +83,30 @@ describe("block archive repository", () => {
       expect(toHex(retrievedDataColumnSidecarBytes[i].value)).toEqual(
         toHex(ssz.fulu.DataColumnSidecar.serialize(dataColumnSidecars[i]))
       );
+    }
+  });
+
+  it("should migrate DataColumnSidecars to archived db", async () => {
+    // same api to writeBlockInputToDb
+    await dataColumnRepo.putMany(blockRoot, allDataColumnSidecars);
+    // same api to migrateDataColumnSidecarsFromHotToColDb
+    const dataColumnSidecarBytes = await dataColumnRepo.valuesBinary(blockRoot);
+    await dataColumnArchiveRepo.putManyBinary(
+      blockSlot,
+      dataColumnSidecarBytes.map((p) => ({key: p.id, value: p.value}))
+    );
+    // same api to onDataColumnSidecarsByRange when serving p2p requests
+    const dataColumnSidecars = await dataColumnArchiveRepo.getManyBinary(
+      blockSlot,
+      allDataColumnSidecars.map((c) => c.index)
+    );
+    expect(dataColumnSidecars).toHaveLength(allDataColumnSidecars.length);
+    for (const [i, serialized] of dataColumnSidecars.entries()) {
+      expect(serialized).toBeDefined();
+      if (serialized == null) {
+        throw Error("Unexpected undefined dataColumnSidecar");
+      }
+      expect(toHex(serialized)).toEqual(toHex(ssz.fulu.DataColumnSidecar.serialize(allDataColumnSidecars[i])));
     }
   });
 });
