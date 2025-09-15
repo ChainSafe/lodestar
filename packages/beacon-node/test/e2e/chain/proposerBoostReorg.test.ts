758a28479ce9890a3d9b2117f61987b67cd7ad95
Tuyen Nguyen <twoeths@users.noreply.github.com>
2025-09-15 10:31:39 +0700

    chore: support fulu for proposerBoostReorg e2e test

--- packages/beacon-node/test/e2e/chain/proposerBoostReorg.test.ts
+++ packages/beacon-node/test/e2e/chain/proposerBoostReorg.test.ts
@@ -16,12 +16,27 @@ describe("proposer boost reorg", () => {
   vi.setConfig({testTimeout: 60000});
 
   const validatorCount = 8;
-  const testParams: Pick<ChainConfig, "SECONDS_PER_SLOT" | "REORG_PARENT_WEIGHT_THRESHOLD" | "PROPOSER_SCORE_BOOST"> = {
-    SECONDS_PER_SLOT: 2,
+  const ELECTRA_FORK_EPOCH = 0;
+  const FULU_FORK_EPOCH = 1;
+  const SECONDS_PER_SLOT = 2;
+  const testParams: Partial<ChainConfig> = {
+    SECONDS_PER_SLOT,
+    ALTAIR_FORK_EPOCH: ELECTRA_FORK_EPOCH,
+    BELLATRIX_FORK_EPOCH: ELECTRA_FORK_EPOCH,
+    CAPELLA_FORK_EPOCH: ELECTRA_FORK_EPOCH,
+    DENEB_FORK_EPOCH: ELECTRA_FORK_EPOCH,
+    ELECTRA_FORK_EPOCH: ELECTRA_FORK_EPOCH,
+    FULU_FORK_EPOCH: FULU_FORK_EPOCH,
     // need this to make block `reorgSlot - 1` strong enough
     REORG_PARENT_WEIGHT_THRESHOLD: 80,
     // need this to make block `reorgSlot + 1` to become the head
     PROPOSER_SCORE_BOOST: 120,
+    BLOB_SCHEDULE: [
+      {
+        EPOCH: 1,
+        MAX_BLOBS_PER_BLOCK: 3,
+      },
+    ],
   };
 
   const afterEachCallbacks: (() => Promise<unknown> | void)[] = [];
@@ -46,14 +61,14 @@ describe("proposer boost reorg", () => {
   it(`should reorg a late block at slot ${reorgSlot}`, async () => {
     // the node needs time to transpile/initialize bls worker threads
     const genesisSlotsDelay = 7;
-    const genesisTime = Math.floor(Date.now() / 1000) + genesisSlotsDelay * testParams.SECONDS_PER_SLOT;
+    const genesisTime = Math.floor(Date.now() / 1000) + genesisSlotsDelay * SECONDS_PER_SLOT;
     const testLoggerOpts: TestLoggerOpts = {
       level: LogLevel.debug,
       timestampFormat: {
         format: TimestampFormatCode.EpochSlot,
         genesisTime,
         slotsPerEpoch: SLOTS_PER_EPOCH,
-        secondsPerSlot: testParams.SECONDS_PER_SLOT,
+        secondsPerSlot: SECONDS_PER_SLOT,
       },
     };
     const logger = testLogger("BeaconNode", testLoggerOpts);
