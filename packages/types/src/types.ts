02d003bcaf7d24727eb7459d35b836112d2fbfa7
NC <17676176+ensi321@users.noreply.github.com>
2025-09-24 15:22:05 -0700

    Revert gloas types

--- packages/types/src/types.ts
+++ packages/types/src/types.ts
@@ -278,7 +278,7 @@ type TypesByFork = {
     BeaconBlockHeader: phase0.BeaconBlockHeader;
     SignedBeaconBlockHeader: phase0.SignedBeaconBlockHeader;
     BeaconBlock: gloas.BeaconBlock;
-    BeaconBlockBody: gloas.BeaconBlockBody;
+    BeaconBlockBody: electra.BeaconBlockBody;
     BeaconState: gloas.BeaconState;
     SignedBeaconBlock: gloas.SignedBeaconBlock;
     Metadata: fulu.Metadata;
@@ -289,7 +289,7 @@ type TypesByFork = {
     LightClientFinalityUpdate: electra.LightClientFinalityUpdate;
     LightClientOptimisticUpdate: electra.LightClientOptimisticUpdate;
     LightClientStore: electra.LightClientStore;
-    BlindedBeaconBlock: electra.BlindedBeaconBlock; // TODO GLOAS: blinded may no longer be used
+    BlindedBeaconBlock: electra.BlindedBeaconBlock;
     BlindedBeaconBlockBody: electra.BlindedBeaconBlockBody;
     SignedBlindedBeaconBlock: electra.SignedBlindedBeaconBlock;
     ExecutionPayload: deneb.ExecutionPayload;
