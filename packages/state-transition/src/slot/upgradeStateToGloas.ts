5106f67adfd16e36d26f7cfaf56587cf7d9e5cae
viktorking7 <140458814+viktorking7@users.noreply.github.com>
2025-10-06 10:04:39 +0200

    Update upgradeStateToGloas.ts

--- packages/state-transition/src/slot/upgradeStateToGloas.ts
+++ packages/state-transition/src/slot/upgradeStateToGloas.ts
@@ -4,7 +4,7 @@ import {CachedBeaconStateFulu, CachedBeaconStateGloas} from "../types.js";
 
 /**
  * Upgrade a state from Fulu to Gloas.
- */
+* TODO GLOAS: Implement this
 export function upgradeStateToGloas(stateFulu: CachedBeaconStateFulu): CachedBeaconStateGloas {
   const {config} = stateFulu;
 
