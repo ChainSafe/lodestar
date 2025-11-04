e97b053d93ecd0f5f47e14742cf421ebb700e561
Nazar Hussain <nazarhussain@gmail.com>
2025-11-04 18:58:00 +0100

    update the browser provider

--- configs/vitest.config.browser.ts
+++ configs/vitest.config.browser.ts
@@ -21,29 +21,21 @@ export const browserTestProject = defineProject({
       headless: true,
       ui: false,
       screenshotFailures: false,
-      // Recommended provider is `playwright` but it's causing following error on CI
-      // Error: Failed to connect to the browser session "af5be85a-7f29-4299-b680-b07f0cfc2520" within the timeout.
-      // TODO: Debug the issue in later versions of playwright and vitest
       provider: playwright(),
       connectTimeout: 90_0000,
       instances: [
-        // TODO: Add support for webkit when available
-        // Invalid types from webdriverio for capabilities
         {
           browser: "firefox",
           maxConcurrency: 1,
-          capabilities: {
-            browserVersion: "stable",
-          },
-        } as never,
-        // Invalid types from webdriverio for capabilities
+        },
         {
           browser: "chromium",
           maxConcurrency: 1,
-          capabilities: {
-            browserVersion: "stable",
-          },
-        } as never,
+        },
+        {
+          browser: "webkit",
+          maxConcurrency: 1,
+        },
       ],
     },
   },
