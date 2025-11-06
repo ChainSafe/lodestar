d91acea734247e28aafc8a5faf8ceb3f8edbc9fb
Nico Flaig <nflaig@protonmail.com>
2025-11-06 11:19:34 +0000

    Update packages/beacon-node/test/spec/utils/specTestIterator.ts

--- packages/beacon-node/test/spec/utils/specTestIterator.ts
+++ packages/beacon-node/test/spec/utils/specTestIterator.ts
@@ -59,7 +59,7 @@ const coveredTestRunners = [
 // ],
 // ```
 export const defaultSkipOpts: SkipOpts = {
-  skippedForks: ["eip7732", "eip7805"],
+  skippedForks: ["eip7805"],
   // TODO: capella
   // BeaconBlockBody proof in lightclient is the new addition in v1.3.0-rc.2-hotfix
   // Skip them for now to enable subsequently
