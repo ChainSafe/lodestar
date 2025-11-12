187d021d27f3c7cb4f154ab7643f0ef4a23e5116
Nazar Hussain <nazarhussain@gmail.com>
2025-11-12 18:08:27 -0300

    update snappyjs vs snappy usage

--- packages/reqresp/src/utils/snappyCompress.ts
+++ packages/reqresp/src/utils/snappyCompress.ts
@@ -1,4 +1,4 @@
-import {compress} from "snappyjs";
+import {compressSync} from "snappy";
 import {ChunkType, IDENTIFIER_FRAME, UNCOMPRESSED_CHUNK_SIZE, crc} from "./snappyCommon.js";
 
 // The logic in this file is largely copied (in simplified form) from https://github.com/ChainSafe/node-snappy-stream/
@@ -7,7 +7,7 @@ export async function* encodeSnappy(bytes: Buffer): AsyncGenerator<Buffer> {
 
   for (let i = 0; i < bytes.length; i += UNCOMPRESSED_CHUNK_SIZE) {
     const chunk = bytes.subarray(i, i + UNCOMPRESSED_CHUNK_SIZE);
-    const compressed = compress(chunk);
+    const compressed = compressSync(chunk);
     if (compressed.length < chunk.length) {
       const size = compressed.length + 4;
       yield Buffer.concat([Buffer.from([ChunkType.COMPRESSED, size, size >> 8, size >> 16]), crc(chunk), compressed]);
