d8bdaf5770b0aa9571feced8ca23c072137745fe
Nazar Hussain <nazarhussain@gmail.com>
2025-12-01 22:15:47 +0100

    fix the transitive dependencies by isolating all

--- packages/reqresp/src/response/index.ts
+++ packages/reqresp/src/response/index.ts
@@ -1,6 +1,5 @@
 import {PeerId, Stream} from "@libp2p/interface";
 import {pipe} from "it-pipe";
-import {Uint8ArrayList} from "uint8arraylist";
 import {Logger, TimeoutError, withTimeout} from "@lodestar/utils";
 import {requestDecode} from "../encoders/requestDecode.js";
 import {responseEncodeError, responseEncodeSuccess} from "../encoders/responseEncode.js";
