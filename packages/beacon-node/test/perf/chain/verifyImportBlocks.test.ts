e89cac6502df81caa1b1dcb809d68ddd5d25fc13
Tuyen Nguyen <twoeths@users.noreply.github.com>
2025-12-18 13:19:57 +0700

    fix: address comment in verifyImportBlocks.test.ts perf test

--- packages/beacon-node/test/perf/chain/verifyImportBlocks.test.ts
+++ packages/beacon-node/test/perf/chain/verifyImportBlocks.test.ts
@@ -1,6 +1,5 @@
 import {generateKeyPair} from "@libp2p/crypto/keys";
 import {afterAll, beforeAll, bench, describe, setBenchOpts} from "@chainsafe/benchmark";
-import {PubkeyIndexMap} from "@chainsafe/pubkey-index-map";
 import {config} from "@lodestar/config/default";
 import {LevelDbController} from "@lodestar/db/controller/level";
 import {SLOTS_PER_EPOCH} from "@lodestar/params";
@@ -92,8 +91,8 @@ describe.skip("verify+import blocks - range sync perf test", () => {
         {
           privateKey: await generateKeyPair("secp256k1"),
           config: state.config,
-          pubkey2index: new PubkeyIndexMap(),
-          index2pubkey: [],
+          pubkey2index: state.epochCtx.pubkey2index,
+          index2pubkey: state.epochCtx.index2pubkey,
           db,
           dataDir: ".",
           dbName: ".",
