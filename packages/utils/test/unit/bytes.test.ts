9933c2715af39befa6dbf630aac70f14c9bb93a1
Robby <rnfairchild11@gmail.com>
2026-01-27 13:31:25 -0500

    Revert "fix browser tests"
    This reverts commit 62c43f38ef72186f6ecc1d87ba389544a51ff77e.

--- packages/utils/test/unit/bytes.test.ts
+++ packages/utils/test/unit/bytes.test.ts
@@ -13,24 +13,24 @@ import {
 
 describe("intToBytes", () => {
   const zeroedArray = (length: number): number[] => Array.from({length}, () => 0);
-  const testCases: {input: [bigint | number, number]; output: Uint8Array}[] = [
-    {input: [255, 1], output: Uint8Array.from([255])},
-    {input: [1, 4], output: Uint8Array.from([1, 0, 0, 0])},
-    {input: [BigInt(255), 1], output: Uint8Array.from([255])},
-    {input: [65535, 2], output: Uint8Array.from([255, 255])},
-    {input: [BigInt(65535), 2], output: Uint8Array.from([255, 255])},
-    {input: [16777215, 3], output: Uint8Array.from([255, 255, 255])},
-    {input: [BigInt(16777215), 3], output: Uint8Array.from([255, 255, 255])},
-    {input: [4294967295, 4], output: Uint8Array.from([255, 255, 255, 255])},
-    {input: [BigInt(4294967295), 4], output: Uint8Array.from([255, 255, 255, 255])},
-    {input: [65535, 8], output: Uint8Array.from([255, 255, ...zeroedArray(8 - 2)])},
-    {input: [BigInt(65535), 8], output: Uint8Array.from([255, 255, ...zeroedArray(8 - 2)])},
-    {input: [65535, 32], output: Uint8Array.from([255, 255, ...zeroedArray(32 - 2)])},
-    {input: [BigInt(65535), 32], output: Uint8Array.from([255, 255, ...zeroedArray(32 - 2)])},
-    {input: [65535, 48], output: Uint8Array.from([255, 255, ...zeroedArray(48 - 2)])},
-    {input: [BigInt(65535), 48], output: Uint8Array.from([255, 255, ...zeroedArray(48 - 2)])},
-    {input: [65535, 96], output: Uint8Array.from([255, 255, ...zeroedArray(96 - 2)])},
-    {input: [BigInt(65535), 96], output: Uint8Array.from([255, 255, ...zeroedArray(96 - 2)])},
+  const testCases: {input: [bigint | number, number]; output: Buffer}[] = [
+    {input: [255, 1], output: Buffer.from([255])},
+    {input: [1, 4], output: Buffer.from([1, 0, 0, 0])},
+    {input: [BigInt(255), 1], output: Buffer.from([255])},
+    {input: [65535, 2], output: Buffer.from([255, 255])},
+    {input: [BigInt(65535), 2], output: Buffer.from([255, 255])},
+    {input: [16777215, 3], output: Buffer.from([255, 255, 255])},
+    {input: [BigInt(16777215), 3], output: Buffer.from([255, 255, 255])},
+    {input: [4294967295, 4], output: Buffer.from([255, 255, 255, 255])},
+    {input: [BigInt(4294967295), 4], output: Buffer.from([255, 255, 255, 255])},
+    {input: [65535, 8], output: Buffer.from([255, 255, ...zeroedArray(8 - 2)])},
+    {input: [BigInt(65535), 8], output: Buffer.from([255, 255, ...zeroedArray(8 - 2)])},
+    {input: [65535, 32], output: Buffer.from([255, 255, ...zeroedArray(32 - 2)])},
+    {input: [BigInt(65535), 32], output: Buffer.from([255, 255, ...zeroedArray(32 - 2)])},
+    {input: [65535, 48], output: Buffer.from([255, 255, ...zeroedArray(48 - 2)])},
+    {input: [BigInt(65535), 48], output: Buffer.from([255, 255, ...zeroedArray(48 - 2)])},
+    {input: [65535, 96], output: Buffer.from([255, 255, ...zeroedArray(96 - 2)])},
+    {input: [BigInt(65535), 96], output: Buffer.from([255, 255, ...zeroedArray(96 - 2)])},
   ];
   for (const {input, output} of testCases) {
     const type = typeof input;
