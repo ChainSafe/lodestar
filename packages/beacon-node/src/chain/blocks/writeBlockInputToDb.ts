ad3a51f77676af6a2ea25f206504c029c5cf39c0
Cayman <caymannava@gmail.com>
2026-01-30 11:38:09 -0500

    chore: more pr review

--- packages/beacon-node/src/chain/blocks/writeBlockInputToDb.ts
+++ packages/beacon-node/src/chain/blocks/writeBlockInputToDb.ts
@@ -48,7 +48,9 @@ export async function writeBlockInputToDb(this: BeaconChain, blocksInputs: IBloc
         // Supernodes may only have a subset of the data columns by the time the block begins to be imported
         // because full data availability can be assumed after NUMBER_OF_COLUMNS / 2 columns are available.
         // Here, however, all data columns must be fully available/reconstructed before persisting to the DB.
-        await blockInput.waitForComputedAllData(BLOB_AVAILABILITY_TIMEOUT);
+        await blockInput.waitForComputedAllData(BLOB_AVAILABILITY_TIMEOUT).catch(() => {
+          this.logger.debug("Failed to wait for computed all data", {slot, blockRoot: blockRootHex});
+        });
       }
 
       const {custodyColumns} = this.custodyConfig;
