52fb7392447b45a27763a6fad14f7b3b3a8ce9c9
Nico Flaig <nflaig@protonmail.com>
2026-02-21 17:18:33 +0000

    apply type safety suggestions from myself

--- packages/beacon-node/src/chain/produceBlock/produceBlockBody.ts
+++ packages/beacon-node/src/chain/produceBlock/produceBlockBody.ts
@@ -4,6 +4,7 @@ import {
   BUILDER_INDEX_SELF_BUILD,
   ForkName,
   ForkPostBellatrix,
+  ForkPostCapella,
   ForkPostDeneb,
   ForkPostFulu,
   ForkPostGloas,
@@ -568,13 +569,13 @@ export async function produceBlockBody<T extends BlockType>(
   }
 
   if (ForkSeq[fork] >= ForkSeq.gloas) {
-    const {blsToExecutionChanges, payloadAttestations} = blockBody as gloas.BeaconBlockBody;
+    const {blsToExecutionChanges, payloadAttestations} = blockBody as BeaconBlockBody<ForkPostGloas>;
     Object.assign(logMeta, {
       blsToExecutionChanges: blsToExecutionChanges.length,
       payloadAttestations: payloadAttestations.length,
     });
   } else if (ForkSeq[fork] >= ForkSeq.capella) {
-    const {blsToExecutionChanges, executionPayload} = blockBody as capella.BeaconBlockBody;
+    const {blsToExecutionChanges, executionPayload} = blockBody as BeaconBlockBody<ForkPostCapella & ForkPreGloas>;
     Object.assign(logMeta, {
       blsToExecutionChanges: blsToExecutionChanges.length,
     });
