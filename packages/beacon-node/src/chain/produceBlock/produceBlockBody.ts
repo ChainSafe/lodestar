731c3b3017d3cd9d669d9856dc1fcb83653131a9
Nico Flaig <nflaig@protonmail.com>
2026-02-21 16:29:54 +0000

    fix: log payload attestations for gloas block bodies

--- packages/beacon-node/src/chain/produceBlock/produceBlockBody.ts
+++ packages/beacon-node/src/chain/produceBlock/produceBlockBody.ts
@@ -567,7 +567,13 @@ export async function produceBlockBody<T extends BlockType>(
     });
   }
 
-  if (ForkSeq[fork] >= ForkSeq.capella) {
+  if (ForkSeq[fork] >= ForkSeq.gloas) {
+    const {blsToExecutionChanges, payloadAttestations} = blockBody as gloas.BeaconBlockBody;
+    Object.assign(logMeta, {
+      blsToExecutionChanges: blsToExecutionChanges.length,
+      payloadAttestations: payloadAttestations.length,
+    });
+  } else if (ForkSeq[fork] >= ForkSeq.capella) {
     const {blsToExecutionChanges, executionPayload} = blockBody as capella.BeaconBlockBody;
     Object.assign(logMeta, {
       blsToExecutionChanges: blsToExecutionChanges.length,
