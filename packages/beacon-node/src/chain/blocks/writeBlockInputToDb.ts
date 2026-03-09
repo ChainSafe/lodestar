b1659d9acb48b1d257e6e06e7e5e7a2f95a4e32b
Nico Flaig <nflaig@protonmail.com>
2026-03-09 14:58:48 +0000

    Restore comment

--- packages/beacon-node/src/chain/blocks/writeBlockInputToDb.ts
+++ packages/beacon-node/src/chain/blocks/writeBlockInputToDb.ts
@@ -131,6 +131,8 @@ export async function persistBlockInput(this: BeaconChain, blockInput: IBlockInp
     })
     .finally(() => {
       this.seenBlockInputCache.prune(blockInput.blockRootHex);
+      // Without forcefully clearing this cache, we would rely on WeakMap to evict memory which is not reliable.
+      // Clear here (after the DB write) so that writeBlockInputToDb can still use the cached serialized bytes.
       this.serializedCache.delete(blockInput.getSerializedCacheKeys());
       this.logger.debug("Pruned block input", {
         slot: blockInput.slot,
