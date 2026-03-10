9b9ba9cca6595aed4353aa0dbbd83e50cb8d34f5
Cayman <caymannava@gmail.com>
2026-03-10 12:55:19 -0400

    chore: hide disableTcp

--- packages/cli/src/options/beaconNodeOptions/network.ts
+++ packages/cli/src/options/beaconNodeOptions/network.ts
@@ -314,6 +314,7 @@ export const options: CliCommandOptions<NetworkArgs> = {
   },
 
   disableTcp: {
+    hidden: true,
     type: "boolean",
     description: "Disable TCP transport",
     defaultDescription: String(defaultOptions.network.disableTcp === true),
