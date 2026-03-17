76809448daee947560f977b8b487d02890fa0148
Nico Flaig <nflaig@protonmail.com>
2026-03-17 13:06:36 +0000

    remove unused imports

--- packages/state-transition/src/util/genesis.ts
+++ packages/state-transition/src/util/genesis.ts
@@ -15,16 +15,10 @@ import {processDeposit} from "../block/processDeposit.js";
 import {EpochCacheImmutableData} from "../cache/epochCache.js";
 import {createCachedBeaconState} from "../cache/stateCache.js";
 import {increaseBalance} from "../index.js";
-import {
-  BeaconStateAllForks,
-  CachedBeaconStateAllForks,
-  CachedBeaconStateElectra,
-  CachedBeaconStateGloas,
-} from "../types.js";
+import {BeaconStateAllForks, CachedBeaconStateAllForks, CachedBeaconStateElectra} from "../types.js";
 import {newFilledArray} from "./array.js";
 import {getTemporaryBlockHeader} from "./blockRoot.js";
 import {computeEpochAtSlot} from "./epoch.js";
-import {initializePayloadTimelinessCommittee} from "./gloas.js";
 import {getNextSyncCommittee} from "./syncCommittee.js";
 import {getActiveValidatorIndices, getMaxEffectiveBalance} from "./validator.js";
 
