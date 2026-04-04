87b7d800a69dd2516f91f73e7808a7dde0551c96
Nico Flaig <nflaig@protonmail.com>
2026-04-04 10:37:43 +0100

    add newlines

--- packages/beacon-node/src/network/reqresp/handlers/beaconBlocksByRoot.ts
+++ packages/beacon-node/src/network/reqresp/handlers/beaconBlocksByRoot.ts
@@ -10,6 +10,7 @@ export async function* onBeaconBlocksByRoot(
 ): AsyncIterable<ResponseOutgoing> {
   // The phase0 req/resp spec uses MIN_EPOCHS_FOR_BLOCK_REQUESTS to define the minimum range peers MUST serve.
   // Archival nodes may still serve older retained blocks to allow genesis sync.
+
   for (const blockRoot of requestBody) {
     const root = blockRoot;
     const block = await chain.getSerializedBlockByRoot(toRootHex(root));
