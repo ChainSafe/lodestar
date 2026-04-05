5b274824cd280b15bdf73a5dd5e01c6b7d85aec6
Nico Flaig <nflaig@protonmail.com>
2026-04-05 08:52:16 +0100

    Skip voluntary exit test

--- packages/beacon-node/test/spec/utils/specTestIterator.ts
+++ packages/beacon-node/test/spec/utils/specTestIterator.ts
@@ -75,7 +75,11 @@ export const defaultSkipOpts: SkipOpts = {
     /^gloas\/fork_choice\/.*$/,
     /^gloas\/ssz_static\/ForkChoiceNode.*$/,
   ],
-  skippedTests: [],
+  skippedTests: [
+    // Broken in v1.7.0-alpha.3 due to missing voluntary_exit.ssz_snappy input file
+    // Fixed by https://github.com/ethereum/consensus-specs/pull/5005 in v1.7.0-alpha.4
+    /^gloas\/operations\/voluntary_exit\/pyspec_tests\/builder_voluntary_exit__success$/,
+  ],
   skippedRunners: [],
 };
 
