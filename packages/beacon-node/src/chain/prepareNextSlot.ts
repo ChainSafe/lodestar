cc021047de48d127b3f2c131248c5a83b8ba2143
Nico Flaig <nflaig@protonmail.com>
2026-04-21 10:51:10 +0100

    use head root

--- packages/beacon-node/src/chain/prepareNextSlot.ts
+++ packages/beacon-node/src/chain/prepareNextSlot.ts
@@ -213,7 +213,7 @@ export class PrepareNextSlotScheduler {
           const data = getPayloadAttributesForSSE(fork as ForkPostBellatrix, this.chain, {
             prepareState: updatedPrepareState,
             prepareSlot,
-            parentBlockRoot: fromHex(updatedHeadRoot),
+            parentBlockRoot: fromHex(headRoot),
             parentBlockHash,
             // The likely consumers of this API are builders and will anyway ignore the
             // feeRecipient, so just pass zero hash for now till a real use case arises
