80099591e9a06628a2c5869557e2dff5a02f26d4
Cayman <caymannava@gmail.com>
2026-04-30 03:18:20 -0400

    chore: small rename

--- packages/beacon-node/src/node/nodejs.ts
+++ packages/beacon-node/src/node/nodejs.ts
@@ -221,7 +221,7 @@ export class BeaconNode {
 
     let executionEngineOpts = opts.executionEngine;
     if (opts.executionEngine.mode === "mock") {
-      const lastEth1BlockHash =
+      const latestEth1BlockHash =
         isStatePostBellatrix(anchorState) && anchorState.isExecutionStateType
           ? isStatePostGloas(anchorState)
             ? toRootHex(anchorState.latestBlockHash)
@@ -230,7 +230,7 @@ export class BeaconNode {
       executionEngineOpts = {
         ...opts.executionEngine,
         genesisBlockHash: ZERO_HASH_HEX,
-        eth1BlockHash: opts.executionEngine.eth1BlockHash ?? lastEth1BlockHash,
+        eth1BlockHash: opts.executionEngine.eth1BlockHash ?? latestEth1BlockHash,
         genesisTime: anchorState.genesisTime,
         config,
       };
