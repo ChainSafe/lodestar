1d4e5f45ec3fbc4a659b4634da43cb19726e25e3
Cayman <caymannava@gmail.com>
2026-05-12 12:02:38 -0400

    chore: remove unused flare package (#9358)
    **Motivation**
    
    - bitrot
    
    **Description**
    
    - remove `flare` package and all mentions of it
    
    ---------
    
    Co-authored-by: Nazar Hussain <nazarhussain@gmail.com>

--- packages/flare/src/cmds/index.ts
+++
@@ -1,8 +0,0 @@
-import {CliCommand} from "@lodestar/utils";
-import {selfSlashAttester} from "./selfSlashAttester.js";
-import {selfSlashProposer} from "./selfSlashProposer.js";
-
-export const cmds: Required<CliCommand<Record<never, never>, Record<never, never>>>["subcommands"] = [
-  selfSlashProposer,
-  selfSlashAttester,
-];
