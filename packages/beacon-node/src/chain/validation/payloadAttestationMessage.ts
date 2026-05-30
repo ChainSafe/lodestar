d72763f6a310c55977c4315df1defd3fb5c04fca
Nico Flaig <nflaig@protonmail.com>
2026-05-30 16:32:28 +0100

    refactor

--- packages/beacon-node/src/chain/validation/payloadAttestationMessage.ts
+++ packages/beacon-node/src/chain/validation/payloadAttestationMessage.ts
@@ -61,12 +61,11 @@ async function validatePayloadAttestationMessage(
   // [IGNORE] The message's block `data.beacon_block_root` has been seen (via
   // gossip or non-gossip sources) (a client MAY queue attestation for processing
   // once the block is retrieved. Note a client might want to request payload after).
-  const blockRootHex = toRootHex(data.beaconBlockRoot);
-  const block = chain.forkChoice.getBlockHexDefaultStatus(blockRootHex);
+  const block = chain.forkChoice.getBlockDefaultStatus(data.beaconBlockRoot);
   if (!block) {
     throw new PayloadAttestationError(GossipAction.IGNORE, {
       code: PayloadAttestationErrorCode.UNKNOWN_BLOCK_ROOT,
-      blockRoot: blockRootHex,
+      blockRoot: toRootHex(data.beaconBlockRoot),
     });
   }
 
@@ -75,7 +74,7 @@ async function validatePayloadAttestationMessage(
   if (block.slot !== data.slot) {
     throw new PayloadAttestationError(GossipAction.IGNORE, {
       code: PayloadAttestationErrorCode.INVALID_BLOCK_SLOT,
-      blockRoot: blockRootHex,
+      blockRoot: toRootHex(data.beaconBlockRoot),
       blockSlot: block.slot,
       slot: data.slot,
     });
