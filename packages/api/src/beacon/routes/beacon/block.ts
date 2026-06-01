b68fc56ae0a33894fefcd7912b1676180007c895
Nico Flaig <nflaig@protonmail.com>
2026-06-01 22:38:24 +0100

    chore: pluralize gloas execution payload endpoint paths (#9440)
    See https://github.com/ethereum/beacon-APIs/pull/613

--- packages/api/src/beacon/routes/beacon/block.ts
+++ packages/api/src/beacon/routes/beacon/block.ts
@@ -616,7 +616,7 @@ export function getDefinitions(config: ChainForkConfig): RouteDefinitions<Endpoi
       },
     },
     publishExecutionPayloadEnvelope: {
-      url: "/eth/v1/beacon/execution_payload_envelope",
+      url: "/eth/v1/beacon/execution_payload_envelopes",
       method: "POST",
       req: {
         writeReqJson: ({signedExecutionPayloadEnvelope}) => {
@@ -661,7 +661,7 @@ export function getDefinitions(config: ChainForkConfig): RouteDefinitions<Endpoi
       },
     },
     publishExecutionPayloadBid: {
-      url: "/eth/v1/beacon/execution_payload_bid",
+      url: "/eth/v1/beacon/execution_payload_bids",
       method: "POST",
       req: {
         writeReqJson: ({signedExecutionPayloadBid}) => {
@@ -705,7 +705,7 @@ export function getDefinitions(config: ChainForkConfig): RouteDefinitions<Endpoi
       },
     },
     getSignedExecutionPayloadEnvelope: {
-      url: "/eth/v1/beacon/execution_payload_envelope/{block_id}",
+      url: "/eth/v1/beacon/execution_payload_envelopes/{block_id}",
       method: "GET",
       req: blockIdOnlyReq,
       resp: {
