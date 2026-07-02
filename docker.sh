17e808734ccbc96749c9977d429e58ec9bdd3343
bing <spiralladder@fastmail.com>
2026-07-02 19:37:39 +0200

    rm docker script used for deployment

--- docker.sh
+++
@@ -1,7 +0,0 @@
-COMMIT=$(jj log -r @ --no-graph -T 'commit_id')
-echo ${COMMIT}
-
-docker buildx build . --push \
-  --tag bhbhbhbhbhbh/lodestar:${COMMIT}\
-  --platform linux/amd64 \
-  --build-arg COMMIT=${COMMIT}
