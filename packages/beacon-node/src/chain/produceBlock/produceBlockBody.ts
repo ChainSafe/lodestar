a1273ad656406a7ecfc8ddb0ad8f54124394dfde
Nico Flaig <nflaig@protonmail.com>
2026-07-07 11:52:19 +0100

    Apply suggestion from @nflaig

--- packages/beacon-node/src/chain/produceBlock/produceBlockBody.ts
+++ packages/beacon-node/src/chain/produceBlock/produceBlockBody.ts
@@ -840,7 +840,7 @@ export function getPayloadAttributesForSSE(
   } as SSEPayloadAttributes;
 
   if (!isForkPostGloas(fork)) {
-    // Removed in Gloas, ELs can get the parent block number from the parent block hash
+    // Removed in Gloas, builders can get the parent block number from the EL via the parent block hash
     (ssePayloadAttributes as bellatrix.SSEPayloadAttributes).parentBlockNumber = prepareState.payloadBlockNumber;
   }
 
