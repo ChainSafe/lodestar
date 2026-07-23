6123fff35133a9b6e607616b9c6d8e71220672cd
markolazic01 <markolazic01@gmail.com>
2026-07-24 01:41:21 +0200

    scaffold builder cli command

---
+++ packages/cli/src/cmds/builder/handler.ts
@@ -0,0 +1,4 @@
+import {GlobalArgs} from "../../options/index.js";
+import {IBuilderCliArgs} from "./options.js";
+
+export async function builderHandler(_args: IBuilderCliArgs & GlobalArgs): Promise<void> {}
---
+++ packages/cli/src/cmds/builder/options.ts
@@ -0,0 +1,4 @@
+import {CliCommandOptions} from "@lodestar/utils";
+
+export type IBuilderCliArgs = Record<never, never>;
+export const builderOptions: CliCommandOptions<IBuilderCliArgs> = {};
