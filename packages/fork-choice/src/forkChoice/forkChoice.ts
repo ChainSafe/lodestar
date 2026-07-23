82ff95b17d6432ab4779b4080c6217a56ba57970
twoeths <twoeths@users.noreply.github.com>
2026-07-23 14:20:35 +0700

    fix: use justified effective balance falling back to head state's

--- packages/fork-choice/src/forkChoice/forkChoice.ts
+++ packages/fork-choice/src/forkChoice/forkChoice.ts
@@ -1558,8 +1558,10 @@ export class ForkChoice implements IForkChoice {
       for (let index = 0; index < state.getBeaconCommitteeCountPerSlot(epoch); index++) {
         for (const validatorIndex of state.getBeaconCommittee(node.slot, index)) {
           if (equivocatingIndices.has(validatorIndex)) {
-            // cannot use this.fcStore.justified.balances because it could be zero for equivocating validators
-            headWeight += state.effectiveBalanceIncrements[validatorIndex];
+            // the spec specifies to use effective_balance of justified state but it could be zero for equivocating validators
+            // due to getEffectiveBalanceIncrementsZeroInactive()
+            headWeight +=
+              this.fcStore.justified.balances[validatorIndex] ?? state.effectiveBalanceIncrements[validatorIndex];
           }
         }
       }
