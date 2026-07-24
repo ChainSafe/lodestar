aaf8cc9363d87c6acf7516f1995d03b07b2d36f8
Nico Flaig <nflaig@protonmail.com>
2026-07-24 15:29:20 +0100

    chore: reduce MAX_BIDS_PER_BUILDER_PER_SLOT to 3

--- packages/config/src/chainConfig/configs/minimal.ts
+++ packages/config/src/chainConfig/configs/minimal.ts
@@ -188,8 +188,8 @@ export const chainConfig: ChainConfig = {
   // Gloas
   // 2**7 (= 128) payloads
   MAX_REQUEST_PAYLOADS: 128,
-  // 6 bids
-  MAX_BIDS_PER_BUILDER_PER_SLOT: 6,
+  // 3 bids
+  MAX_BIDS_PER_BUILDER_PER_SLOT: 3,
 
   // Blob Scheduling
   // ---------------------------------------------------------------
