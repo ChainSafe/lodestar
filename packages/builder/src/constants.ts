812319989aa7a46286298e0e965328297486dfe5
markolazic01 <markolazic01@gmail.com>
2026-07-25 01:39:47 +0200

    remove constants

--- packages/builder/src/constants.ts
+++
@@ -1,4 +0,0 @@
-// Builder-API constants from builder-specs gloas.
-// builder-specs/specs/gloas/builder.md#constants
-export const MAX_DATA_SIZE = 4096;
-export const MAX_EXECUTION_PAYMENT = 2n ** 64n - 1n;
