1cdc6d75565b1f71291f7206b87e88171dbd752b
markolazic01 <markolazic01@gmail.com>
2026-07-25 19:05:12 +0200

    feat: builder setup wip

--- packages/builder/src/builder.ts
+++ packages/builder/src/builder.ts
@@ -1,4 +1,50 @@
+import {Logger} from "@lodestar/utils"
+import {Metrics} from "./metrics.js";
+import {BuilderSigner} from "./services/builderSigner.js";
+import { BeaconConfig, createBeaconConfig, ChainForkConfig } from "@lodestar/config";
+import { Genesis } from "@lodestar/types/phase0";
+import { SecretKey } from "@chainsafe/blst";
+
+export type BuilderModules = {
+  opts: BuilderOptions;
+  config: BeaconConfig;
+  builderSigner: BuilderSigner;
+  logger: Logger;
+  metrics: Metrics | null;
+}
+
+export type BuilderOptions = {
+  logger: Logger;
+  config: ChainForkConfig;
+  secretKey: SecretKey;
+}
+
 /**
  * Main class for the Builder client.
  */
-export class Builder {}
+export class Builder {
+
+  readonly builderSigner: BuilderSigner;
+  private readonly config: BeaconConfig;
+  private readonly logger: Logger;
+
+  constructor({
+    opts,
+    builderSigner,
+    config,
+    logger,
+    metrics,
+  }: BuilderModules) {
+    this.builderSigner = builderSigner;
+    this.config = config;
+    this.logger = logger;
+  }
+
+  static init(opts: BuilderOptions, genesis: Genesis, metrics: Metrics | null = null): Builder {
+    const config = createBeaconConfig(opts.config, genesis.genesisValidatorsRoot);
+    const builderSigner = new BuilderSigner(config, opts.secretKey )
+
+    return new Builder({opts, builderSigner, config, logger: opts.logger, metrics});
+  }
+
+}
