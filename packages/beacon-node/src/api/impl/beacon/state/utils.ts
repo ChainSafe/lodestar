1577ef3bdb1c721cc7a18803388cf62912237421
Nico Flaig <nflaig@protonmail.com>
2026-09-04 01:39:14 +0100

    Revert "chore: remove unused head branch from resolveStateId"
    This reverts commit 34e9e591126690fb7a4c75d8193ccc86843ba074.

--- packages/beacon-node/src/api/impl/beacon/state/utils.ts
+++ packages/beacon-node/src/api/impl/beacon/state/utils.ts
@@ -25,6 +25,10 @@ export function resolveStateId(
   forkChoice: IForkChoice,
   stateId: routes.beacon.StateId
 ): RootHex | Slot | CheckpointWithHex {
+  if (stateId === "head") {
+    return forkChoice.getHead().stateRoot;
+  }
+
   if (stateId === "genesis") {
     return GENESIS_SLOT;
   }
