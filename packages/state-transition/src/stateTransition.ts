ba947b93cb48cf182f6d0713b868e2e6647823d4
markolazic01 <markolazic01@gmail.com>
2026-09-06 00:59:44 +0200

    feat: track stateTransition steps in metrics

--- packages/state-transition/src/stateTransition.ts
+++ packages/state-transition/src/stateTransition.ts
@@ -81,6 +81,15 @@ export enum StateHashTreeRootSource {
 }
 
 /**
+ * Epoch transition steps tracked in metrics
+ */
+export enum StateTransitionStep {
+  stateClone = "stateClone",
+  processSlots = "processSlots",
+  verifyProposerSignature = "verifyProposerSignature",
+}
+
+/**
  * Implementation Note: follows the optimizations in protolambda's eth2fastspec (https://github.com/protolambda/eth2fastspec)
  */
 export function stateTransition(
@@ -99,7 +108,9 @@ export function stateTransition(
   const blockSlot = block.slot;
 
   // .clone() before mutating state in state transition
+  const cloneTimer = metrics?.stateTransitionStepTime.startTimer({step: StateTransitionStep.stateClone});
   let postState = state.clone(options.dontTransferCache);
+  cloneTimer?.();
 
   if (metrics) {
     onStateCloneMetrics(postState, metrics, StateCloneSource.stateTransition);
@@ -110,11 +121,18 @@ export function stateTransition(
 
   // Process slots (including those with no blocks) since block.
   // Includes state upgrades
+  const processSlotsTimer = metrics?.stateTransitionStepTime.startTimer({step: StateTransitionStep.processSlots});
   postState = processSlotsWithTransientCache(postState, blockSlot, options, {metrics, validatorMonitor});
+  processSlotsTimer?.();
 
   // Verify proposer signature only
-  if (verifyProposer && !verifyProposerSignature(postState.config, signedBlock)) {
-    throw new Error("Invalid block signature");
+  if (verifyProposer) {
+    const timer = metrics?.stateTransitionStepTime.startTimer({step: StateTransitionStep.verifyProposerSignature});
+    const isValidProposerSignature = verifyProposerSignature(postState.config, signedBlock);
+    timer?.();
+    if (!isValidProposerSignature) {
+      throw new Error("Invalid block signature");
+    }
   }
 
   // Process block
