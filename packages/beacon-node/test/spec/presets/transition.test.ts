ef5f89d464f47210508c89e9c817ab894c09f8d1
Nico Flaig <nflaig@protonmail.com>
2026-09-23 10:25:28 +0100

    docs: retain original transition skip instructions

--- packages/beacon-node/test/spec/presets/transition.test.ts
+++ packages/beacon-node/test/spec/presets/transition.test.ts
@@ -90,7 +90,7 @@ const transition =
             expectEqualBeaconState(forkNext, expected, actual);
           }
         },
-        // Prefer adding skips in packages/beacon-node/test/spec/utils/specTestIterator.ts.
+        // Do not manually skip tests here, do it in packages/beacon-node/test/spec/utils/specTestIterator.ts
         shouldSkip: (_testcase, name, _index) =>
           skipTestNames?.some((skipTestName) => name.includes(skipTestName)) ?? false,
       },
