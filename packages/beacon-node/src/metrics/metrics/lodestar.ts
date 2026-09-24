3e439aa363e6f3179ad894d7f264221fcdde224b
twoeths <10568965+twoeths@users.noreply.github.com>
2026-09-24 15:43:07 +0700

    chore: track gloas newPayload metrics (#10113)
    **Motivation**
    
    - today we have geth issue but there is no way to see in on metrics
    
    **Description**
    
    - track newPayload() result from EL
    - also 2 standard metrics: `recvToValidation`, `validationTime`

--- packages/beacon-node/src/metrics/metrics/lodestar.ts
+++ packages/beacon-node/src/metrics/metrics/lodestar.ts
@@ -1011,6 +1011,13 @@ export function createLodestarMetrics(
         help: "Count of errors, by error type, while processing execution payload envelopes",
         labelNames: ["error"],
       }),
+      executionPayload: {
+        recvToValidation: register.histogram({
+          name: "lodestar_gossip_execution_payload_envelope_received_to_execution_payload_verification_seconds",
+          help: "Time elapsed between execution payload envelope received and execution payload verification",
+          buckets: [0.05, 0.1, 0.25, 0.5, 1, 2, 3, 4, 6, 8],
+        }),
+      },
     },
     gossipExecutionPayloadBid: {
       elapsedTimeTillReceived: register.histogram<{source: OpSource}>({
